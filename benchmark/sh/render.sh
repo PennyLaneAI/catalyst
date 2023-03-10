c6a3a8ab90f405912653610b82158ea1aea73a7a
Sergei Mironov <sergei.mironov@xanadu.ai>
2023-03-10 11:14:59 +0000

    [Benchmark] Plot trial plots

--- benchmark/sh/render.sh
+++ benchmark/sh/render.sh
@@ -13,30 +13,59 @@ Regular circuits
 ================
 
 ![](./_img/regular_compile_$TAG.svg.png)
+
+![](./_img/regular_compile_trial_$TAG.svg.png)
+
 ![](./_img/regular_runtime_$TAG.svg.png)
+
+![](./_img/regular_runtime_trial_$TAG.svg.png)
 
 Deep circuits
 =============
 
 ![](./_img/deep_compile_$TAG.svg.png)
+
+![](./_img/deep_compile_trial_$TAG.svg.png)
+
 ![](./_img/deep_runtime_$TAG.svg.png)
+
+![](./_img/deep_runtime_trial_$TAG.svg.png)
 
 Variational circuits (line plots)
 =================================
 
 ![](./_img/variational_compile_adjoint_backprop_lineplot_$TAG.svg.png)
+
+![](./_img/variational_compile_trial_adjoint_backprop_lineplot_$TAG.svg.png)
+
 ![](./_img/variational_compile_finitediff_lineplot_$TAG.svg.png)
+
+![](./_img/variational_compile_trial_finitediff_lineplot_$TAG.svg.png)
+
 ![](./_img/variational_compile_parametershift_lineplot_$TAG.svg.png)
 
+![](./_img/variational_compile_trial_parametershift_lineplot_$TAG.svg.png)
+
+
 ![](./_img/variational_runtime_adjoint_backprop_lineplot_$TAG.svg.png)
+
+![](./_img/variational_runtime_trial_adjoint_backprop_lineplot_$TAG.svg.png)
+
 ![](./_img/variational_runtime_finitediff_lineplot_$TAG.svg.png)
+
+![](./_img/variational_runtime_trial_finitediff_lineplot_$TAG.svg.png)
+
 ![](./_img/variational_runtime_parametershift_lineplot_$TAG.svg.png)
+
+![](./_img/variational_runtime_trial_parametershift_lineplot_$TAG.svg.png)
 
 Variational circuits (bar charts)
 =================================
 
 ![](./_img/variational_runtime_adjoint_backprop_$TAG.svg.png)
+
 ![](./_img/variational_runtime_finitediff_$TAG.svg.png)
+
 ![](./_img/variational_runtime_parametershift_$TAG.svg.png)
 EOF
 
