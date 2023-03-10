697029c55b1849e1b8ef8db31d1f815f7b423a67
David Ittah <dime10@users.noreply.github.com>
2023-03-10 15:36:35 -0500

    Fix linting

--- .github/workflows/set_dep_versions.py
+++ .github/workflows/set_dep_versions.py
@@ -1,7 +1,25 @@
+# Copyright 2022-2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module computes commit hashes for LLVM and MLIR-HLO based on a given JAX version.
+"""
+
 import os
-import requests
 import re
 import sys
+
+import requests
 
 jax_version = sys.argv[1]
 
@@ -25,7 +43,9 @@ url = f"https://api.github.com/search/commits?q=repo:tensorflow/mlir-hlo+{piper_
 response = requests.get(url).json()
 hlo_commit = response["items"][0]["sha"]
 
-with open(os.path.join(os.path.dirname(__file__), "../../.dep-versions"), "w") as f:
+with open(
+    os.path.join(os.path.dirname(__file__), "../../.dep-versions"), "w", encoding="UTF-8"
+) as f:
     f.write(
         f"""\
 jax={jax_version}
