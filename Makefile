PennyLaneAI/catalyst

commit d560e53a7e885793720f0def64f583bea3c0255c

Remove python bindings (#40)
* Remove python_bindings folder.

* Change type of file to look for.

* Add patch and apply them after dialects are built.

* Fix imports.

* Limit depth and use jax ir.

* Fix path.

* Mock mlir_quantum.

* Remove FromElementsOp constructor.

* Fix IfOp.

* Use ForOp constructor.

* Fix patch.

* Remove wrong substitution.

* Patch instead of replace.

* Move conditional path to init.

* Make python a conditional variable.

* Remove unnecessary variable.

* Use Makefile for wheel generation.

* Fix path.

* Use CMAKE_C{XX}_COMPILER_LAUNCHER variable.

* Use ccache by default.

* Keep MHLO as a PHONY rule and do not depend on LLVM.

* Add option for LLD.

* Use gcc and g++ for wheel.

* Revert changes.

* Just add the patches.

* Fix path.

* Add a couple of patches.

* Selective copying.

* Add patches during the wheel creating.

frontend/catalyst/__init__.py
@@ -15,12 +15,23 @@
 This package contains the Catalyst Python interface.
 """
 
-# pylint: disable=missing-module-docstring
 
 from catalyst._version import __version__
 from catalyst.compiler import compile
 from catalyst.compilation_pipelines import qjit, QJIT
 from catalyst.pennylane_extensions import for_loop, while_loop, cond, measure, grad
+from catalyst._configuration import INSTALLED
+
+if not INSTALLED:
+    import os
+
+    default_bindings_path = os.path.join(
+        os.path.dirname(__file__), "../../mlir/build/python_packages/quantum"
+    )
+    if os.path.exists(default_bindings_path):  # pragma: no cover
+        import sys
+
+        sys.path.insert(0, default_bindings_path)
 
 
 __all__ = (

frontend/catalyst/python_bindings/__init__.py
@@ -1,2 +1,0 @@
-from ._quantum_ops_gen import *
-from ._gradient_ops_gen import *

mlir/Makefile
@@ -1,5 +1,5 @@
 
-PYTHON := python3
+PYTHON?=python3
 C_COMPILER?=clang
 CXX_COMPILER?=clang++
 MK_ABSPATH := $(abspath $(lastword $(MAKEFILE_LIST)))
@@ -8,6 +8,9 @@ DIALECTS_BUILD_DIR?=$(MK_DIR)/build
 LLVM_BUILD_DIR?=$(MK_DIR)/llvm-project/build
 MHLO_BUILD_DIR?=$(MK_DIR)/mlir-hlo/build
 RT_BUILD_DIR?=$(MK_DIR)/../runtime/build
+C_COMPILER_LAUNCHER?=ccache
+CXX_COMPILER_LAUNCHER?=ccache
+ENABLE_LLD?=ON
 
 .PHONY: help
 help:
@@ -34,17 +37,18 @@ llvm:
 		-DLLVM_ENABLE_PROJECTS="mlir" \
 		-DLLVM_ENABLE_ASSERTIONS=ON \
 		-DMLIR_ENABLE_BINDINGS_PYTHON=ON \
-		-DPython3_EXECUTABLE=$$(which $(PYTHON)) \
+		-DPython3_EXECUTABLE=$(PYTHON) \
 		-DPython3_NumPy_INCLUDE_DIRS=$$($(PYTHON) -c "import numpy as np; print(np.get_include())") \
 		-DCMAKE_C_COMPILER=$(C_COMPILER) \
 		-DCMAKE_CXX_COMPILER=$(CXX_COMPILER) \
-		-DLLVM_ENABLE_LLD=ON \
-		-DLLVM_CCACHE_BUILD=ON
+		-DCMAKE_C_COMPILER_LAUNCHER=$(C_COMPILER_LAUNCHER) \
+		-DCMAKE_CXX_COMPILER_LAUNCHER=$(CXX_COMPILER_LAUNCHER) \
+		-DLLVM_ENABLE_LLD=$(ENABLE_LLD)
 
 	cmake --build $(LLVM_BUILD_DIR) --target check-mlir llvm-symbolizer
 
 .PHONY: mhlo
-mhlo: | llvm
+mhlo:
 	@echo "build MLIR-HLO"
 	cmake -G Ninja -S mlir-hlo -B $(MHLO_BUILD_DIR) \
 		-DCMAKE_BUILD_TYPE=Release \
@@ -52,10 +56,12 @@ mhlo: | llvm
 		-DMLIR_DIR=$(LLVM_BUILD_DIR)/lib/cmake/mlir \
 		-DCMAKE_C_COMPILER=$(C_COMPILER) \
 		-DCMAKE_CXX_COMPILER=$(CXX_COMPILER) \
-		-DLLVM_ENABLE_LLD=ON \
-		-DLLVM_CCACHE_BUILD=ON
+		-DCMAKE_C_COMPILER_LAUNCHER=$(C_COMPILER_LAUNCHER) \
+		-DCMAKE_CXX_COMPILER_LAUNCHER=$(CXX_COMPILER_LAUNCHER) \
+		-DLLVM_ENABLE_LLD=$(ENABLE_LLD)
 
 	cmake --build $(MHLO_BUILD_DIR) --target check-mlir-hlo
+
 
 .PHONY: dialects
 dialects:
@@ -64,17 +70,22 @@ dialects:
 		-DCMAKE_BUILD_TYPE=Release \
 		-DLLVM_ENABLE_ASSERTIONS=ON \
 		-DQUANTUM_ENABLE_BINDINGS_PYTHON=ON \
-		-DPython3_EXECUTABLE="$$(which $(PYTHON))" \
+		-DPython3_EXECUTABLE="$(PYTHON)" \
 		-DMLIR_DIR=$(LLVM_BUILD_DIR)/lib/cmake/mlir \
 		-DMHLO_BINARY_DIR=$(MHLO_BUILD_DIR)/bin \
 		-DRUNTIME_LIB_DIR=$(RT_BUILD_DIR)/lib \
 		-DMLIR_LIB_DIR=$(LLVM_BUILD_DIR)/lib \
 		-DCMAKE_C_COMPILER=$(C_COMPILER) \
 		-DCMAKE_CXX_COMPILER=$(CXX_COMPILER) \
-		-DLLVM_ENABLE_LLD=ON \
-		-DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+		-DCMAKE_C_COMPILER_LAUNCHER=$(C_COMPILER_LAUNCHER) \
+		-DCMAKE_CXX_COMPILER_LAUNCHER=$(CXX_COMPILER_LAUNCHER) \
+		-DLLVM_ENABLE_LLD=$(ENABLE_LLD)
 
 	cmake --build $(DIALECTS_BUILD_DIR) --target check-dialects quantum-lsp-server
+	patch -d $(DIALECTS_BUILD_DIR)/python_packages/quantum/mlir_quantum --follow-symlinks < patches/use-ir-from-jax.patch
+	patch -d $(DIALECTS_BUILD_DIR)/python_packages/quantum/mlir_quantum/dialects --follow-symlinks < patches/use-cext-from-jax.patch
+	patch -d $(DIALECTS_BUILD_DIR)/python_packages/quantum/mlir_quantum/dialects --follow-symlinks < patches/remove-gradient-cext-import.patch
+	patch -d $(DIALECTS_BUILD_DIR)/python_packages/quantum/mlir_quantum/dialects --follow-symlinks < patches/remove-quantum-cext-import.patch
 
 .PHONY: test
 test:

.github/workflows/check-catalyst.yaml
@@ -269,7 +269,6 @@ jobs:
       - name: Build Catalyst-Runtime
         run: |
           pushd runtime
-          CACHED_QIR=true \
           RT_BUILD_DIR=../runtime-build \
           QIR_STDLIB_INCLUDES_DIR="$(pwd)/qir-stdlib-build/include" \
           QIR_STDLIB_DIR="$(pwd)/qir-stdlib-build/" make runtime

.github/workflows/wheel-linux-x86-64.yaml
@@ -223,6 +223,10 @@ jobs:
           -DMLIR_DIR=$GITHUB_WORKSPACE/llvm-build/lib/cmake/mlir
 
         cmake --build quantum-build --target check-dialects -j$(nproc)
+        patch -d quantum-build/python_packages/quantum/mlir_quantum --follow-symlinks < mlir/patches/use-ir-from-jax.patch
+        patch -d quantum-build/python_packages/quantum/mlir_quantum/dialects --follow-symlinks < mlir/patches/use-cext-from-jax.patch
+        patch -d quantum-build/python_packages/quantum/mlir_quantum/dialects --follow-symlinks < mlir/patches/remove-gradient-cext-import.patch
+        patch -d quantum-build/python_packages/quantum/mlir_quantum/dialects --follow-symlinks < mlir/patches/remove-quantum-cext-import.patch
 
     # Build Catalyst-Runtime
     - name: Build Catalyst-Runtime
@@ -252,8 +256,12 @@ jobs:
         cp --dereference $GITHUB_WORKSPACE/llvm-build/lib/libmlir_float16_utils.so.* $GITHUB_WORKSPACE/frontend/catalyst/lib
         cp --dereference $GITHUB_WORKSPACE/llvm-build/lib/libmlir_c_runner_utils.so* $GITHUB_WORKSPACE/frontend/catalyst/lib
         # Copy mlir bindings to frontend/mlir_quantum
-        mkdir -p $GITHUB_WORKSPACE/frontend/mlir_quantum
+        mkdir -p $GITHUB_WORKSPACE/frontend/mlir_quantum/dialects
         cp -R --dereference $GITHUB_WORKSPACE/quantum-build/python_packages/quantum/mlir_quantum/runtime $GITHUB_WORKSPACE/frontend/mlir_quantum/runtime
+        cp --dereference $GITHUB_WORKSPACE/quantum-build/python_packages/quantum/mlir_quantum/ir.py $GITHUB_WORKSPACE/frontend/mlir_quantum/
+        for file in arith tensor scf gradient quantum _ods_common ; do \
+           cp --dereference $GITHUB_WORKSPACE/quantum-build/python_packages/quantum/mlir_quantum/dialects/*${file}* $GITHUB_WORKSPACE/frontend/mlir_quantum/dialects ; \
+        done
         find $GITHUB_WORKSPACE/frontend -type d -name __pycache__ -exec rm -rf {} +
 
     - name: Build wheel

doc/conf.py
@@ -79,7 +79,15 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ["mlir_quantum.runtime"]
+MOCK_MODULES = [
+    "mlir_quantum.runtime",
+    "mlir_quantum.dialects",
+    "mlir_quantum.dialects.arith",
+    "mlir_quantum.dialects.tensor",
+    "mlir_quantum.dialects.scf",
+    "mlir_quantum.dialects.quantum",
+    "mlir_quantum.dialects.gradient",
+]
 
 mock = Mock()
 for mod_name in MOCK_MODULES:

frontend/catalyst/compilation_pipelines.py
@@ -31,24 +31,14 @@ from jax.interpreters.mlir import ir
 
 import pennylane as qml
 
+from mlir_quantum.runtime import get_ranked_memref_descriptor, ranked_memref_to_numpy
+
 import catalyst.jax_tracer as tracer
 from catalyst import compiler
 from catalyst.utils.gen_mlir import append_modules
 from catalyst.utils.patching import Patcher
 from catalyst.pennylane_extensions import QFunc
-from catalyst._configuration import INSTALLED
 from catalyst.utils.tracing import TracingContext
-
-default_bindings_path = os.path.join(
-    os.path.dirname(__file__), "../../mlir/build/python_packages/quantum"
-)
-if not INSTALLED and os.path.exists(default_bindings_path):  # pragma: no cover
-    import sys
-
-    sys.path.insert(0, default_bindings_path)
-
-# pylint: disable=wrong-import-position,wrong-import-order
-from mlir_quantum.runtime import get_ranked_memref_descriptor, ranked_memref_to_numpy
 
 # Required for JAX tracer objects as PennyLane wires.
 # pylint: disable=unnecessary-lambda

frontend/catalyst/jax_primitives.py
@@ -21,24 +21,35 @@ import jax
 from jax.interpreters import mlir, xla
 from jax._src import util
 from jax._src.lib.mlir import ir
-from jaxlib.mlir.dialects._ods_common import get_op_results_or_values
 from jaxlib.mlir.dialects._func_ops_gen import CallOp
 from jaxlib.mlir.dialects._mhlo_ops_gen import ConstantOp, ConvertOp
 
-from catalyst.python_bindings._arith_ops_gen import IndexCastOp
-from catalyst.python_bindings._tensor_ops_gen import ExtractOp as TensorExtractOp, FromElementsOp
-from catalyst.python_bindings._scf_ops_gen import IfOp, ConditionOp, ForOp, WhileOp, YieldOp
-from catalyst.python_bindings import AllocOp, ExtractOp, InsertOp, DeallocOp
-from catalyst.python_bindings import CustomOp, MultiRZOp, QubitUnitaryOp, MeasureOp
-from catalyst.python_bindings import SampleOp, CountsOp, ExpvalOp, VarianceOp, ProbsOp, StateOp
-from catalyst.python_bindings import GradOp
-from catalyst.python_bindings import (
+from mlir_quantum.dialects.arith import IndexCastOp
+from mlir_quantum.dialects.tensor import ExtractOp as TensorExtractOp, FromElementsOp
+from mlir_quantum.dialects.scf import IfOp, ConditionOp, ForOp, WhileOp, YieldOp
+from mlir_quantum.dialects.gradient import GradOp
+from mlir_quantum.dialects.quantum import (
+    SampleOp,
+    CountsOp,
+    ExpvalOp,
+    VarianceOp,
+    ProbsOp,
+    StateOp,
+    CustomOp,
+    MultiRZOp,
+    QubitUnitaryOp,
+    MeasureOp,
+    AllocOp,
+    ExtractOp,
+    InsertOp,
+    DeallocOp,
     ComputationalBasisOp,
     NamedObsOp,
     HermitianOp,
     TensorOp,
     HamiltonianOp,
 )
+
 from catalyst.utils.calculate_grad_shape import calculate_grad_shape, Signature
 
 
@@ -604,8 +615,11 @@ def _qmeasure_lowering(jax_ctx: mlir.LoweringRuleContext, qubit: ir.Value):
     result_type = ir.IntegerType.get_signless(1)
     result, new_qubit = MeasureOp(result_type, qubit.type, qubit).results
 
+    result_from_elements_op = ir.RankedTensorType.get((), result.type)
+    from_elements_op = FromElementsOp.build_generic([result_from_elements_op], [result])
+
     return (
-        FromElementsOp(ir.RankedTensorType.get((), result.type), [result]).results[0],
+        from_elements_op.results[0],
         new_qubit,
     )
 
@@ -881,7 +895,9 @@ def _expval_lowering(jax_ctx: mlir.LoweringRuleContext, obs: ir.Value, shots: in
     result_type = ir.F64Type.get()
 
     mres = ExpvalOp(result_type, obs, shots=shots_attr).result
-    return FromElementsOp(ir.RankedTensorType.get((), result_type), [mres]).results
+    result_from_elements_op = ir.RankedTensorType.get((), result_type)
+    from_elements_op = FromElementsOp.build_generic([result_from_elements_op], [mres])
+    return from_elements_op.results
 
 
 #
@@ -917,7 +933,9 @@ def _var_lowering(jax_ctx: mlir.LoweringRuleContext, obs: ir.Value, shots: int):
     result_type = ir.F64Type.get()
 
     mres = VarianceOp(result_type, obs, shots=shots_attr).result
-    return FromElementsOp(ir.RankedTensorType.get((), result_type), [mres]).results
+    result_from_elements_op = ir.RankedTensorType.get((), result_type)
+    from_elements_op = FromElementsOp.build_generic([result_from_elements_op], [mres])
+    return from_elements_op.results
 
 
 #
@@ -1026,15 +1044,10 @@ def _qcond_lowering(
 
     pred_extracted = TensorExtractOp(ir.IntegerType.get_signless(1), pred, []).result
 
-    if_op_scf = IfOp(
-        IfOp.build_generic(
-            results=result_types,
-            operands=get_op_results_or_values([pred_extracted]),
-        )
-    )
+    if_op_scf = IfOp(pred_extracted, result_types, hasElse=True)
 
     # if block
-    if_block = if_op_scf.regions[0].blocks.append()
+    if_block = if_op_scf.then_block
     name_stack = util.extend_name_stack(jax_ctx.module_context.name_stack, "if")
     if_ctx = jax_ctx.module_context.replace(name_stack=xla.extend_name_stack(name_stack, "if"))
     with ir.InsertionPoint(if_block):
@@ -1051,7 +1064,7 @@ def _qcond_lowering(
         YieldOp([o[0] for o in out[0]])
 
     # else block
-    else_block = if_op_scf.regions[1].blocks.append()
+    else_block = if_op_scf.else_block
     name_stack = util.extend_name_stack(jax_ctx.module_context.name_stack, "else")
     else_ctx = jax_ctx.module_context.replace(name_stack=xla.extend_name_stack(name_stack, "else"))
     with ir.InsertionPoint(else_block):
@@ -1233,15 +1246,10 @@ def _qfor_lowering(
         loop_operands.append(p)
     loop_operands.extend(loop_args)
 
-    for_op_scf = ForOp(
-        ForOp.build_generic(
-            results=result_types,
-            operands=loop_operands,
-        )
-    )
+    for_op_scf = ForOp(loop_operands[0], loop_operands[1], loop_operands[2], iter_args=loop_args)
 
     name_stack = util.extend_name_stack(jax_ctx.module_context.name_stack, "for")
-    body_block = for_op_scf.regions[0].blocks.append(*loop_carry_types)
+    body_block = for_op_scf.body
     body_ctx = jax_ctx.module_context.replace(name_stack=xla.extend_name_stack(name_stack, "body"))
 
     with ir.InsertionPoint(body_block):
@@ -1249,9 +1257,9 @@ def _qfor_lowering(
 
         # Convert the index type iteration variable expected by MLIR to tensor<i64> expected by JAX.
         body_args[0] = IndexCastOp(loop_index_type, body_args[0]).result
-        body_args[0] = FromElementsOp(
-            ir.RankedTensorType.get((), loop_index_type), [body_args[0]]
-        ).result
+        result_from_elements_op = ir.RankedTensorType.get((), loop_index_type)
+        from_elements_op = FromElementsOp.build_generic([result_from_elements_op], [body_args[0]])
+        body_args[0] = from_elements_op.result
 
         # recursively generate the mlir for the loop body
         out, _ = mlir.jaxpr_subcomp(

commit f21c4731b1a9161970d368139a40b884b4e61d68

Fix broken test configuration after removing hard-coded bindings (#53)
* Build runtime tests in release mode

* Separate lit and pytest suite targets

* Fix import order on Catalyst init

We first need to ensure that the default build path for the mlir
bindings is available before importing the rest of the module.

The current state breaks tests run via makefiles without setting up
environment variables.

* Decouple frontend lit tests from mlir build

This fixes an issue with running the frontend lit tests via cmake which
rebuilds the mlir dialects after patches have been applied via the make
script. Failure introduced in #40.

* Update runtime/Makefile

Co-authored-by: David Ittah <dime10@users.noreply.github.com>

---------

Co-authored-by: erick-xanadu <110487834+erick-xanadu@users.noreply.github.com>

.github/workflows/check-formatting.yaml
@@ -6,7 +6,7 @@ on:
 
 jobs:
   format-python:
-    name: Python Formatting
+    name: Python Formatting & Linting
     runs-on: ubuntu-latest
 
     steps:

.github/workflows/check-jax-release.yaml
@@ -83,9 +83,13 @@ jobs:
       run: |
         make runtime
 
-    - name: Run Frontend Test suite
+    - name: Run Frontend LIT suite
       run: |
-        make test-frontend
+        make lit
+
+    - name: Run Frontend PyTest suite
+      run: |
+        make pytest
 
     - name: Run Demos
       run: |

Makefile
@@ -47,18 +47,20 @@ dialects:
 runtime:
 	$(MAKE) -C runtime all
 
-.PHONY: test test-runtime test-dialects test-frontend test-demos
-test: test-runtime test-dialects test-frontend test-demos
+.PHONY: test test-runtime test-frontend lit pytest test-demos
+test: test-runtime test-frontend test-demos
 
 test-runtime:
 	$(MAKE) -C runtime test
 
-test-dialects:
-	$(MAKE) -C mlir test
+test-frontend: lit pytest
 
-test-frontend:
-	@echo "check the Catalyst lit and Python test suites"
+lit:
+	@echo "check the Catalyst lit test suite"
 	cmake --build $(DIALECTS_BUILD_DIR) --target check-frontend
+
+pytest:
+	@echo "check the Catalyst PyTest suite"
 	$(PYTHON) pytest frontend/test/pytest --tb=native -n auto
 
 test-demos:

frontend/catalyst/__init__.py
@@ -17,9 +17,6 @@ This package contains the Catalyst Python interface.
 
 
 from catalyst._version import __version__
-from catalyst.compiler import compile
-from catalyst.compilation_pipelines import qjit, QJIT
-from catalyst.pennylane_extensions import for_loop, while_loop, cond, measure, grad
 from catalyst._configuration import INSTALLED
 
 if not INSTALLED:
@@ -32,6 +29,15 @@ if not INSTALLED:
         import sys
 
         sys.path.insert(0, default_bindings_path)
+
+from catalyst.compilation_pipelines import qjit, QJIT  # pylint: disable=wrong-import-position
+from catalyst.pennylane_extensions import (  # pylint: disable=wrong-import-position
+    for_loop,
+    while_loop,
+    cond,
+    measure,
+    grad,
+)
 
 
 __all__ = (

runtime/Makefile
@@ -13,7 +13,8 @@ BUILD_TYPE?=Release
 
 coverage: CODE_COVERAGE=ON
 coverage: BUILD_TYPE=Debug
-test: BUILD_TYPE=Debug
+test: CODE_COVERAGE=OFF
+test: BUILD_TYPE=Release
 
 .PHONY: help
 help:
@@ -99,12 +100,12 @@ endif
 .PHONY: check-tidy
 check-tidy: | qir
 	@echo "build Catalyst Runtime with RUNTIME_CLANG_TIDY=ON"
-	rm -rf 
 	cmake -G Ninja -B $(MK_DIR)/BuildTidy . \
+		-DCMAKE_BUILD_TYPE=$(BUILD_TYPE) \
 		-DCMAKE_C_COMPILER=$(C_COMPILER) \
 		-DCMAKE_CXX_COMPILER=$(CXX_COMPILER) \
 		-DQIR_STDLIB_LIB=$(QIR_STDLIB_DIR) \
 		-DQIR_STDLIB_INCLUDES=$(QIR_STDLIB_INCLUDES_DIR) \
-		-DRUNTIME_CLANG_TIDY=ON 
+		-DRUNTIME_CLANG_TIDY=ON
 
 	cmake --build $(MK_DIR)/BuildTidy --target rt_capi -j$(if $(nprocs:-=),$(nprocs),$$(nproc))

mlir/test/frontend/CMakeLists.txt
@@ -7,5 +7,4 @@ configure_lit_site_cfg(
 
 add_lit_testsuite(check-frontend "Run the frontend tests"
     .  # the frontend tests are located in the same directory as the config file
-    DEPENDS ${DIALECT_TESTS_DEPEND}
 )