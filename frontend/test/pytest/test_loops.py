19980030aaf23bd3ba340437a0fd90afd57acea7
David Ittah <dime10@users.noreply.github.com>
2023-03-21 19:56:09 -0400

    Lint loop tests

--- frontend/test/pytest/test_loops.py
+++ frontend/test/pytest/test_loops.py
@@ -11,24 +11,33 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Test suite for loop operations in Catalyst."""
 
 import pytest
-
-from catalyst import qjit, measure, while_loop, for_loop
 import numpy as np
 import pennylane as qml
 
+from catalyst import qjit, measure, while_loop, for_loop
 
-class TestWhileLoopToJaxpr:
-    def test_simple_loop(self):
-        expected = """{ lambda ; a:f64[]. let
+# pylint: disable=no-value-for-parameter
+
+
+class TestLoopToJaxpr:
+    """Collection of tests that examine the generated JAXPR of loops."""
+
+    def test_while_loop(self):
+        """Check the while loop JAXPR."""
+
+        expected = """\
+{ lambda ; a:f64[]. let
     b:i64[] c:f64[] = qwhile[
       body_jaxpr={ lambda ; d:i64[] e:f64[]. let f:i64[] = add d 1 in (f, e) }
       body_nconsts=0
       cond_jaxpr={ lambda ; g:i64[] h:f64[]. let i:bool[] = lt g 10 in (i,) }
       cond_nconsts=0
     ] 0 a
-  in (b, c) }"""
+  in (b, c) }\
+"""
 
         @qjit
         def circuit(x: float):
@@ -38,11 +47,39 @@ class TestWhileLoopToJaxpr:
 
             return loop((0, x))
 
-        assert expected == str(circuit._jaxpr)
+        assert expected == str(circuit.jaxpr)
+
+    def test_for_loop(self):
+        """Check the for loop JAXPR."""
+
+        expected = """\
+{ lambda ; a:f64[] b:i64[]. let
+    c:i64[] d:f64[] = qfor[
+      body_jaxpr={ lambda ; e:i64[] f:i64[] g:f64[]. let
+          h:i64[] = add f 1
+        in (h, g) }
+      body_nconsts=0
+    ] 0 b 1 0 0 a
+  in (c, d) }\
+"""
+
+        @qjit
+        def circuit(x: float, n: int):
+            @for_loop(0, n, 1)
+            def loop(_, v):
+                return v[0] + 1, v[1]
+
+            return loop((0, x))
+
+        assert expected == str(circuit.jaxpr)
 
 
 class TestWhileLoops:
+    """Test the Catalyst while_loop operation."""
+
     def test_alternating_loop(self):
+        """Test simple while loop."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
@@ -60,6 +97,8 @@ class TestWhileLoops:
         assert not circuit(4)
 
     def test_closure_condition_fn(self):
+        """Test while loop with captured values (closures) in the condition function."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
@@ -77,6 +116,8 @@ class TestWhileLoops:
         assert not circuit(4)
 
     def test_closure_body_fn(self):
+        """Test while loop with captured values (closures) in the body function."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
@@ -96,6 +137,8 @@ class TestWhileLoops:
         assert not circuit(4)
 
     def test_assert_joint_closure(self):
+        """Test while loop with captured values (closures) in both body and condition functions."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
@@ -115,6 +158,8 @@ class TestWhileLoops:
         assert not circuit(4)
 
     def test_assert_reference_outside_measure(self):
+        """Test while loop in conjunction with the measure op."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
@@ -134,6 +179,8 @@ class TestWhileLoops:
         assert not circuit(4)
 
     def test_multiple_loop_arguments(self):
+        """Test while loop with multiple (loop-carried) arguments."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n: int):
@@ -151,6 +198,8 @@ class TestWhileLoops:
         assert not circuit(4)
 
     def test_nested_loops(self):
+        """Test nested while loops."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n, m):
@@ -169,7 +218,11 @@ class TestWhileLoops:
 
 
 class TestForLoops:
+    """Test the Catalyst for_loop operation."""
+
     def test_required_index(self):
+        """Check for loop error message when the iteration index is missing."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
@@ -178,18 +231,19 @@ class TestForLoops:
                 pass
 
             loop_fn()
-            return
 
         # TODO: raise better error for user
         with pytest.raises(TypeError, match="takes 0 positional arguments but 1 was given"):
             circuit(5)
 
     def test_basic_loop(self):
+        """Test simple for loop."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
             @for_loop(0, n, 1)
-            def loop_fn(i):
+            def loop_fn(_):
                 qml.PauliX(0)
 
             loop_fn()
@@ -199,11 +253,13 @@ class TestForLoops:
         assert not circuit(2)
 
     def test_loop_caried_values(self):
+        """Test for loop with updating loop carried values."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(n):
             @for_loop(0, n, 1)
-            def loop_fn(i, x):
+            def loop_fn(_, x):
                 qml.RY(x, wires=0)
                 return x + np.pi / 4
 
@@ -216,6 +272,8 @@ class TestForLoops:
         assert np.allclose(circuit(4), 0.0)
 
     def test_dynamic_wires(self):
+        """Test for loops with iteration index-dependant wires."""
+
         @qjit()
         @qml.qnode(qml.device("lightning.qubit", wires=6))
         def circuit(n: int):
@@ -234,13 +292,15 @@ class TestForLoops:
         assert np.allclose(circuit(6), expected)
 
     def test_closure(self):
+        """Test for loop with captured values (closures)."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=1))
         def circuit(x):
             y = 2 * x
 
             @for_loop(0, 1, 1)
-            def loop_fn(i):
+            def loop_fn(_):
                 qml.RY(y, wires=0)
 
             loop_fn()
@@ -249,6 +309,8 @@ class TestForLoops:
         assert np.allclose(circuit(np.pi / 4), 0.0)
 
     def test_nested_loops(self):
+        """Test nested for loops."""
+
         @qjit
         @qml.qnode(qml.device("lightning.qubit", wires=4))
         def circuit(n):
@@ -277,28 +339,127 @@ class TestForLoops:
         assert np.allclose(circuit(4), np.eye(2**4)[0])
 
 
+class TestClassicalCompilation:
+    """Test that Catalyst loops can be used outside of quantum functions."""
+
+    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
+    def test_while_loop(self, x, n):
+        """Test while loop in classical function."""
+
+        @qjit
+        def mulc(x: int, n: int):
+            @while_loop(lambda v, _: v < n)
+            def loop(v, i):
+                return v + 1, i + x
+
+            _, x_times_n = loop(0, 0)
+            return x_times_n
+
+        assert mulc.mlir
+        assert mulc(x, n) == x * n
+
+    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
+    def test_while_nested_loop(self, x, n):
+        """Test nested while loops in classical function."""
+
+        @qjit
+        def mulc(x: int, n: int):
+            @while_loop(lambda i, _: i < x)
+            def loop(i, sum):
+                @while_loop(lambda j: j < n)
+                def loop2(j):
+                    return j + 1
+
+                return i + 1, sum + loop2(0)
+
+            _, x_times_n = loop(0, 0)
+            return x_times_n
+
+        assert mulc(x, n) == x * n
+
+    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
+    def test_for_loop(self, x, n):
+        """Test for loop in classical function."""
+
+        @qjit
+        def mulc(x: int, n: int):
+            @for_loop(0, n, 1)
+            def loop(_, agg):
+                return agg + x
+
+            x_times_n = loop(0)
+            return x_times_n
+
+        assert mulc.mlir
+        assert mulc(x, n) == x * n
+
+    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
+    def test_nested_for_loop(self, x, n):
+        """Test nested for loops in classical function."""
+
+        @qjit
+        def mulc(x: int, n: int):
+            @for_loop(0, x, 1)
+            def loop(_, counter):
+                @for_loop(0, n, 1)
+                def loop2(j, _):
+                    return j + 1
+
+                return counter + loop2(0)
+
+            x_times_n = loop(0)
+            return x_times_n
+
+        assert mulc.mlir
+        assert mulc(x, n) == x * n
+
+    @pytest.mark.parametrize("x,n", [(2, 4), (4, 6)])
+    def test_for_loop_2(self, x, n):
+        """Test for loop in classical function with different step size."""
+
+        @qjit
+        def mulc(x: int, n: int):
+            @for_loop(0, n, 2)
+            def loop(_, agg):
+                return agg + x
+
+            x_times_n = loop(0)
+            return x_times_n
+
+        def muli(x, n):
+            agg = 0
+            for _ in range(0, n, 2):
+                agg += x
+            return agg
+
+        assert mulc.mlir
+        assert mulc(x, n) == muli(x, n)
+
+
 class TestInterpretationControlFlow:
     """Test that the loops' executions are semantically equivalent when compiled and interpreted."""
 
-    # pylint: disable=missing-function-docstring
     def test_while_loop(self):
+        """Test simple while loop."""
+
         def muli(x: int, n: int):
             @while_loop(lambda v, _: v < n)
             def loop(v, i):
                 return v + 1, i + x
 
-            counter, x_times_n = loop(0, 0)
+            _, x_times_n = loop(0, 0)
             return x_times_n
 
         mulc = qjit(muli)
         assert mulc(1, 2) == muli(1, 2)
 
-    # pylint: disable=missing-function-docstring
     def test_for_loop(self):
+        """Test simple for loop."""
+
         def muli(x: int, n: int):
             @for_loop(0, n, 1)
-            def loop(i, agg):
-                return (agg + x,)
+            def loop(_, agg):
+                return agg + x
 
             x_times_n = loop(0)
             return x_times_n
@@ -306,8 +467,8 @@ class TestInterpretationControlFlow:
         mulc = qjit(muli)
         assert np.allclose(mulc(1, 2), muli(1, 2))
 
-    # pylint: disable=missing-function-docstring
     def test_qnode_with_while_loop(self):
+        """Test while loop inside QNode."""
         num_wires = 2
         device = qml.device("lightning.qubit", wires=num_wires)
 
@@ -324,8 +485,8 @@ class TestInterpretationControlFlow:
         compiled_circuit = qjit(interpreted_circuit)
         assert np.allclose(compiled_circuit(num_wires), interpreted_circuit(num_wires))
 
-    # pylint: disable=missing-function-docstring
     def test_qnode_with_for_loop(self):
+        """Test for loop inside QNode."""
         num_wires = 2
         device = qml.device("lightning.qubit", wires=num_wires)
 
@@ -341,91 +502,6 @@ class TestInterpretationControlFlow:
 
         compiled_circuit = qjit(interpreted_circuit)
         assert np.allclose(compiled_circuit(num_wires), interpreted_circuit(num_wires))
-
-
-class TestClassicalCompilation:
-    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
-    def test_while_loop(self, x, n):
-        @qjit
-        def mulc(x: int, n: int):
-            @while_loop(lambda v, _: v < n)
-            def loop(v, i):
-                return v + 1, i + x
-
-            counter, x_times_n = loop(0, 0)
-            return x_times_n
-
-        assert mulc.mlir
-        assert mulc(x, n) == x * n
-
-    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
-    def test_while_nested_loop(self, x, n):
-        @qjit
-        def mulc(x: int, n: int):
-            @while_loop(lambda i, _: i < x)
-            def loop(i, sum):
-                @while_loop(lambda j: j < n)
-                def loop2(j):
-                    return j + 1
-
-                return i + 1, sum + loop2(0)
-
-            counter, x_times_n = loop(0, 0)
-            return x_times_n
-
-        assert mulc(x, n) == x * n
-
-    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
-    def test_for_loop(self, x, n):
-        @qjit
-        def mulc(x: int, n: int):
-            @for_loop(0, n, 1)
-            def loop(i, agg):
-                return agg + x
-
-            x_times_n = loop(0)
-            return x_times_n
-
-        assert mulc.mlir
-        assert mulc(x, n) == x * n
-
-    @pytest.mark.parametrize("x,n", [(2, 3), (4, 5)])
-    def test_nested_for_loop(self, x, n):
-        @qjit
-        def mulc(x: int, n: int):
-            @for_loop(0, x, 1)
-            def loop(i, counter):
-                @for_loop(0, n, 1)
-                def loop2(j, carry):
-                    return j + 1
-
-                return counter + loop2(0)
-
-            x_times_n = loop(0)
-            return x_times_n
-
-        assert mulc.mlir
-        assert mulc(x, n) == x * n
-
-    @pytest.mark.parametrize("x,n", [(2, 4), (4, 6)])
-    def test_for_loop(self, x, n):
-        @qjit
-        def mulc(x: int, n: int):
-            @for_loop(0, n, 2)
-            def loop(i, agg):
-                return agg + x
-
-            x_times_n = loop(0)
-            return x_times_n
-
-        def muli(x, n):
-            agg = 0
-            for y in range(0, n, 2):
-                agg += x
-            return agg
-
-        assert mulc.mlir
-        assert mulc(x, n) == muli(x, n)
 
 
 class TestResultStructureInterpreted:
