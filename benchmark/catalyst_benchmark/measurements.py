af90ad41f8f4d815c714cbaf78bc0ec6c133bc25
Sergei Mironov <sergei.mironov@xanadu.ai>
2023-03-23 15:32:18 +0000

    [Benchmark][CodeFactor] Fix issues

--- benchmark/catalyst_benchmark/measurements.py
+++ benchmark/catalyst_benchmark/measurements.py
@@ -26,13 +26,11 @@ def catalyst_version() -> str:
     verstring = catalyst._version.__version__
     if "dev" in verstring:
         try:
-            commit = (
-                check_output(
-                    ["git", "rev-parse", "HEAD"], cwd=dirname(catalyst.__file__)
-                )
+            commit = (  # nosec: B607
+                check_output(["git", "rev-parse", "HEAD"], cwd=dirname(catalyst.__file__))
                 .decode()
                 .strip()[:7]
-            )  # nosec: B607
+            )
             verstring += f"+g{commit}"
         except Exception:
             verstring += "+g?"
