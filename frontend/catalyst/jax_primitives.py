5479e0d8a491802332a59225ac1bb7cf17bc31a4
Jacob Mai Peng <jacobmpeng@gmail.com>
2023-05-19 13:48:29 -0400

    [Frontend] Support constant negative step sizes in for loops (#129)
    * Implement lowering with option of reversed iteration transformation
    
    * Fill in logic for iterating in reverse, add tests for infinite ranges, negative steps, and 'constant-folded' expressions
    
    * Update documenting comments, autoformatting
    
    * Address pylint loop unpacking, improve variable names (lower to higher vs start to stop)
    
    * Update changelog and for_loop docstring, simplify # iterations computation
    
    * typo: step -> self
    
    * [CI] remove clang dependency from readthedocs build

--- frontend/catalyst/jax_primitives.py
+++ frontend/catalyst/jax_primitives.py
@@ -25,7 +25,9 @@ from jax._src.lib.mlir import ir
 from jax.interpreters import mlir, xla
 from jaxlib.mlir.dialects._func_ops_gen import CallOp
 from jaxlib.mlir.dialects._mhlo_ops_gen import ConstantOp, ConvertOp
-from mlir_quantum.dialects.arith import IndexCastOp
+from mlir_quantum.dialects.arith import AddIOp, CeilDivSIOp
+from mlir_quantum.dialects.arith import ConstantOp as ArithConstantOp
+from mlir_quantum.dialects.arith import IndexCastOp, MulIOp, SubIOp
 from mlir_quantum.dialects.gradient import GradOp, JVPOp, VJPOp
 from mlir_quantum.dialects.quantum import (
     AllocOp,
@@ -1331,8 +1333,13 @@ def _qwhile_lowering(
 #
 def qfor(body_jaxpr, body_nconsts, *header_and_iter_args_plus_consts):
     """Bind operands to operation."""
+    step = header_and_iter_args_plus_consts[2]
+    apply_reverse_transform = isinstance(step, int) and step < 0
     return qfor_p.bind(
-        *header_and_iter_args_plus_consts, body_jaxpr=body_jaxpr, body_nconsts=body_nconsts
+        *header_and_iter_args_plus_consts,
+        body_jaxpr=body_jaxpr,
+        body_nconsts=body_nconsts,
+        apply_reverse_transform=apply_reverse_transform,
     )
 
 
@@ -1356,6 +1363,7 @@ def _qfor_lowering(
     *iter_args_plus_consts: tuple,
     body_jaxpr: jax.core.ClosedJaxpr,
     body_nconsts: int,
+    apply_reverse_transform: bool,
 ):
     # Separate constants from iteration arguments.
     # The MLIR value provided by JAX for the iteration index is not needed
@@ -1391,6 +1399,19 @@ def _qfor_lowering(
         ).result  # tensor<i64> -> i64
         p = IndexCastOp(ir.IndexType.get(), p).result  # i64 -> index
         loop_operands.append(p)
+
+    if apply_reverse_transform:
+        zero = ArithConstantOp(ir.IndexType.get(), 0)
+        one = ArithConstantOp(ir.IndexType.get(), 1)
+        start_val, stop_val, step_val = loop_operands[0], loop_operands[1], loop_operands[2]
+
+        # Iterate from 0 to the number of iterations (ceil((stop - start) / step))
+        distance = SubIOp(stop_val, start_val)
+        num_iterations = CeilDivSIOp(distance, step_val)
+        loop_operands[0] = zero
+        loop_operands[1] = num_iterations
+        loop_operands[2] = one
+
     loop_operands.extend(loop_args)
 
     for_op_scf = ForOp(loop_operands[0], loop_operands[1], loop_operands[2], iter_args=loop_args)
@@ -1403,6 +1424,9 @@ def _qfor_lowering(
         body_args = list(body_block.arguments)
 
         # Convert the index type iteration variable expected by MLIR to tensor<i64> expected by JAX.
+        if apply_reverse_transform:
+            # iv = start + normalized_iv * step
+            body_args[0] = AddIOp(start_val, MulIOp(body_args[0], step_val))
         body_args[0] = IndexCastOp(loop_index_type, body_args[0]).result
         result_from_elements_op = ir.RankedTensorType.get((), loop_index_type)
         from_elements_op = FromElementsOp.build_generic([result_from_elements_op], [body_args[0]])
