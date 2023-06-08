f2c65ed37e350a2ecedcf70e9912c5318e23091d
Erick Eduardo Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2023-06-08 20:23:29 +0000

    Initial prototype of basic block counter.

--- mlir/lib/Quantum/Transforms/bb_counter.cpp
+++ mlir/lib/Quantum/Transforms/bb_counter.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
@@ -28,163 +29,215 @@ using namespace catalyst::quantum;
 
 namespace {
 
-bool
-hasBasicBlockCounterAttribute(func::FuncOp &op) {
-  StringAttr bbCounterAttr = StringAttr::get(op->getContext(), "catalyst.bbcounter");
-  return op->hasAttr(bbCounterAttr);
-}
-
-void
-removeBasicBlockCounterAttribute(func::FuncOp &op) {
-  StringAttr bbCounterAttr = StringAttr::get(op->getContext(), "catalyst.bbcounter");
-  op->removeAttr(bbCounterAttr);
-}
-
-std::vector<Block *>
-getBasicBlocksInPostOrder(func::FuncOp &op) {
-  // Walk is post-order, so all basic blocks encountered are in post-order.
-  // The last block in nested_basic_blocks will be the block for the whole function.
-  std::vector<Block *> blocks;
-  op.walk([&](mlir::Operation *nestedOp) {
-    for (Region &region : nestedOp->getRegions()) {
-      for (Block &nestedBlock : region.getBlocks()) {
-        blocks.push_back(&nestedBlock);
-      }
-    }
-  });
-  return blocks;
-}
-
-std::vector<DifferentiableGate>
-getDifferentiableGates(func::FuncOp &op) {
-  std::vector<DifferentiableGate> ops;
-  op.walk([&](mlir::Operation *nestedOp) {
-    if (DifferentiableGate gate = dyn_cast<DifferentiableGate>(nestedOp)) {
-      ops.push_back(gate);
-    }
-  });
-  return ops;
-}
-
-void
-printDifferentiableParam(DifferentiableGate gate, std::unordered_map<Block *, int> &blockOffsetMap)
+bool hasBasicBlockCounterAttribute(func::FuncOp &op)
 {
-  ValueRange diffParams = gate.getDiffParams();
-  for (Value diffParam : diffParams) {
-    mlir::Operation *definingOp = diffParam.getDefiningOp();
+    StringAttr bbCounterAttr = StringAttr::get(op->getContext(), "catalyst.bbcounter");
+    return op->hasAttr(bbCounterAttr);
+}
+
+void removeBasicBlockCounterAttribute(func::FuncOp &op)
+{
+    StringAttr bbCounterAttr = StringAttr::get(op->getContext(), "catalyst.bbcounter");
+    op->removeAttr(bbCounterAttr);
+}
+
+std::vector<Block *> getBasicBlocksInPostOrder(func::FuncOp &op)
+{
+    // Walk is post-order, so all basic blocks encountered are in post-order.
+    // The last block in nested_basic_blocks will be the block for the whole function.
+    std::vector<Block *> blocks;
+    op.walk([&](mlir::Operation *nestedOp) {
+        for (Region &region : nestedOp->getRegions()) {
+            for (Block &nestedBlock : region.getBlocks()) {
+                blocks.push_back(&nestedBlock);
+            }
+        }
+    });
+    return blocks;
+}
+
+std::vector<DifferentiableGate> getDifferentiableGates(func::FuncOp &op)
+{
+    std::vector<DifferentiableGate> ops;
+    op.walk([&](mlir::Operation *nestedOp) {
+        if (DifferentiableGate gate = dyn_cast<DifferentiableGate>(nestedOp)) {
+            ops.push_back(gate);
+        }
+    });
+    return ops;
+}
+
+int getDefinitionsBlockOffset(Value value, std::unordered_map<Block *, int> &blockOffsetMap)
+{
+    mlir::Operation *definingOp = value.getDefiningOp();
     Block *definingBlock = definingOp->getBlock();
-    int blockOffset = blockOffsetMap.at(definingBlock);
-  }
+    return blockOffsetMap.at(definingBlock);
 }
 
-int
-getDefinitionsBlockOffset(Value value, std::unordered_map<Block *, int> &blockOffsetMap)
+std::unordered_map<void *, int>
+getDifferentiableParamsBasicBlockOffsetMap(DifferentiableGate gate,
+                                           std::unordered_map<Block *, int> &blockOffsetMap)
 {
-  mlir::Operation *definingOp = value.getDefiningOp();
-  Block *definingBlock = definingOp->getBlock();
-  return blockOffsetMap.at(definingBlock);
+    std::unordered_map<void *, int> map;
+    ValueRange diffParams = gate.getDiffParams();
+    for (Value diffParam : diffParams) {
+        int definingBlockOffset = getDefinitionsBlockOffset(diffParam, blockOffsetMap);
+        map.insert({diffParam.getAsOpaquePointer(), definingBlockOffset});
+    }
+    return map;
 }
 
-std::unordered_map<void*, int>
-getDifferentiableParamsBasicBlockOffsetMap(DifferentiableGate gate, std::unordered_map<Block *, int> &blockOffsetMap)
+std::set<mlir::Operation *> getDifferentiableParamDefinitions(DifferentiableGate gate)
 {
-  std::unordered_map<void*, int> map;
-  ValueRange diffParams = gate.getDiffParams();
-  for (Value diffParam : diffParams) {
-    int definingBlockOffset = getDefinitionsBlockOffset(diffParam , blockOffsetMap);
-    map.insert({diffParam.getAsOpaquePointer(), definingBlockOffset});
-  }
-  return map;
+    std::set<mlir::Operation *> definitions;
+    ValueRange diffParams = gate.getDiffParams();
+    for (Value diffParam : diffParams) {
+        mlir::Operation *definingOp = diffParam.getDefiningOp();
+        definitions.insert(definingOp);
+    }
+    return definitions;
 }
 
-std::unordered_map<void*, int>
-getDifferentiableParamBasicBlockOffsetMap(std::vector<DifferentiableGate> gates, std::unordered_map<Block *, int> &blockOffsetMap)
+std::set<mlir::Operation *> getDifferentiableParamDefinitions(std::vector<DifferentiableGate> gates)
 {
-  std::unordered_map<void*, int> map;
-  for (DifferentiableGate gate : gates) {
-    std::unordered_map<void*, int> instructionMap = getDifferentiableParamsBasicBlockOffsetMap(gate, blockOffsetMap);
-    map.merge(instructionMap);
-  }
-  return map;
+    std::set<mlir::Operation *> definitions;
+    for (DifferentiableGate gate : gates) {
+        std::set<mlir::Operation *> definitions_in_gate = getDifferentiableParamDefinitions(gate);
+        definitions.merge(definitions_in_gate);
+    }
+    return definitions;
 }
 
-void
-printDifferentiableParams(std::vector<DifferentiableGate> &gates, std::unordered_map<Block *, int> &blockOffsetMap)
+std::unordered_map<void *, int>
+getDifferentiableParamBasicBlockOffsetMap(std::vector<DifferentiableGate> gates,
+                                          std::unordered_map<Block *, int> &blockOffsetMap)
 {
-  for (auto gate : gates) {
-    printDifferentiableParam(gate, blockOffsetMap);
-  }
+    std::unordered_map<void *, int> map;
+    for (DifferentiableGate gate : gates) {
+        std::unordered_map<void *, int> instructionMap =
+            getDifferentiableParamsBasicBlockOffsetMap(gate, blockOffsetMap);
+        map.merge(instructionMap);
+    }
+    return map;
 }
-
 
 struct BasicBlockCounterTransform : public OpRewritePattern<func::FuncOp> {
     using OpRewritePattern<func::FuncOp>::OpRewritePattern;
 
     LogicalResult match(func::FuncOp op) const override;
     void rewrite(func::FuncOp op, PatternRewriter &rewriter) const override;
-private:
-    Value allocateBasicBlockCounterArray(func::FuncOp &op, PatternRewriter &rewriter, size_t arraySize) const;
-    void addIncrements(PatternRewriter &rewriter, Location loc, std::vector<Block *> &, Value &) const;
+
+  private:
+    Value allocateBasicBlockCounterArray(func::FuncOp &op, PatternRewriter &rewriter,
+                                         size_t arraySize) const;
+    void addIncrements(PatternRewriter &rewriter, Location loc, std::vector<Block *> &,
+                       Value &) const;
 };
 
-Value
-BasicBlockCounterTransform::allocateBasicBlockCounterArray(func::FuncOp &op, PatternRewriter &rewriter, size_t arraySize) const {
-  Region &firstRegion = op.getRegion();
-  Block &firstBlock = firstRegion.front();
-  rewriter.setInsertionPointToStart(&firstBlock);
-  Type i64Type = IntegerType::get(rewriter.getContext(), 64);
-  Value c1 = rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getI64IntegerAttr(1));
-  Type arrayType = LLVM::LLVMArrayType::get(i64Type, arraySize);
-  return rewriter.create<LLVM::AllocaOp>(op->getLoc(), arrayType, c1);
+Value BasicBlockCounterTransform::allocateBasicBlockCounterArray(func::FuncOp &op,
+                                                                 PatternRewriter &rewriter,
+                                                                 size_t arraySize) const
+{
+    Region &firstRegion = op.getRegion();
+    Block &firstBlock = firstRegion.front();
+    rewriter.setInsertionPointToStart(&firstBlock);
+    Type i64Type = IntegerType::get(rewriter.getContext(), 64);
+    Value c1 = rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getI64IntegerAttr(1));
+    Type arrayType = LLVM::LLVMArrayType::get(i64Type, arraySize);
+    Type ptrToArrayType = LLVM::LLVMPointerType::get(arrayType);
+    return rewriter.create<LLVM::AllocaOp>(op->getLoc(), ptrToArrayType, c1);
 }
 
-void
-BasicBlockCounterTransform::addIncrements(PatternRewriter &rewriter, Location loc, std::vector<Block *> &blocks, Value &blockCounterArray) const {
-  std::vector<Value> bbCounters;
-  Block *entryBlock = blocks.back();
-  size_t blocksSize = blocks.size();
-  mlir::Operation *definitionOfArray = blockCounterArray.getDefiningOp();
-  Value c1 = rewriter.create<LLVM::ConstantOp>(loc, rewriter.getI64IntegerAttr(1));
-  for (size_t i = 0; i < blocksSize; i++) {
-    Block *block = blocks[i];
+void BasicBlockCounterTransform::addIncrements(PatternRewriter &rewriter, Location loc,
+                                               std::vector<Block *> &blocks,
+                                               Value &blockCounterArray) const
+{
+    std::vector<Value> bbCounters;
+    Block *entryBlock = blocks.back();
+    size_t blocksSize = blocks.size();
+    mlir::Operation *definitionOfArray = blockCounterArray.getDefiningOp();
+    Value c1 = rewriter.create<LLVM::ConstantOp>(loc, rewriter.getI64IntegerAttr(1));
+    for (size_t i = 0; i < blocksSize; i++) {
+        Block *block = blocks[i];
 
-    // The entry block, the next instructions have to be placed after
-    // the definition of the block counter.
-    if (block == entryBlock) {
-      rewriter.setInsertionPointAfter(definitionOfArray); 
-    } else {
-      rewriter.setInsertionPointToStart(block);
+        // The entry block, the next instructions have to be placed after
+        // the definition of the block counter.
+        if (block == entryBlock) {
+            rewriter.setInsertionPointAfter(definitionOfArray);
+        }
+        else {
+            rewriter.setInsertionPointToStart(block);
+        }
+
+        Value loadArray = rewriter.create<LLVM::LoadOp>(loc, blockCounterArray);
+        Value currentCount = rewriter.create<LLVM::ExtractValueOp>(loc, loadArray, i);
+        Value newCount = rewriter.create<LLVM::AddOp>(loc, c1, currentCount);
+        Value newArray = rewriter.create<LLVM::InsertValueOp>(loc, loadArray, newCount, i);
+        rewriter.create<LLVM::StoreOp>(loc, newArray, blockCounterArray);
     }
- 
-    ArrayRef<int64_t> basicBlockIndex{static_cast<int64_t>(i)};
-    Value currentCount = rewriter.create<LLVM::ExtractValueOp>(loc, blockCounterArray, basicBlockIndex);
-    Value newCount = rewriter.create<LLVM::AddOp>(loc, c1, currentCount);
-    rewriter.create<LLVM::InsertValueOp>(loc, blockCounterArray, newCount, basicBlockIndex);
-  }
 }
 
-LogicalResult
-BasicBlockCounterTransform::match(func::FuncOp op) const {
-  return hasBasicBlockCounterAttribute(op) ? success() : failure();
+LogicalResult BasicBlockCounterTransform::match(func::FuncOp op) const
+{
+    return hasBasicBlockCounterAttribute(op) ? success() : failure();
 }
 
-void
-BasicBlockCounterTransform::rewrite(func::FuncOp op, PatternRewriter &rewriter) const {
-  auto blocks = getBasicBlocksInPostOrder(op);
-  size_t howManyBlocks = blocks.size();
-  Value blockCounterArray = this->allocateBasicBlockCounterArray(op, rewriter, howManyBlocks);
-  this->addIncrements(rewriter, op->getLoc(), blocks, blockCounterArray);
-  std::unordered_map<Block *, int> blockNumberingMap;
-  for (size_t i = 0; i < howManyBlocks; i++)
-  {
-    blockNumberingMap.insert({blocks[i], i});
-  }
+void BasicBlockCounterTransform::rewrite(func::FuncOp op, PatternRewriter &rewriter) const
+{
+    auto blocks = getBasicBlocksInPostOrder(op);
+    size_t howManyBlocks = blocks.size();
+    Value blockCounterArray = this->allocateBasicBlockCounterArray(op, rewriter, howManyBlocks);
+    this->addIncrements(rewriter, op->getLoc(), blocks, blockCounterArray);
+    std::unordered_map<Block *, int> blockNumberingMap;
+    for (size_t i = 0; i < howManyBlocks; i++) {
+        blockNumberingMap.insert({blocks[i], i});
+    }
 
-  auto differentiableGates = getDifferentiableGates(op);
-  auto valueDefiningBlockMap = getDifferentiableParamBasicBlockOffsetMap(differentiableGates, blockNumberingMap);
+    auto differentiableGates = getDifferentiableGates(op);
+    auto definitionVector = getDifferentiableParamDefinitions(differentiableGates);
+    std::unordered_map<mlir::Operation *, int> definitionNumberMap;
+    size_t i = 0;
+    for (mlir::Operation *operation : definitionVector) {
+        definitionNumberMap.insert({operation, i++});
+    }
 
+    auto valueDefiningBlockMap =
+        getDifferentiableParamBasicBlockOffsetMap(differentiableGates, blockNumberingMap);
 
-  removeBasicBlockCounterAttribute(op);
+    for (auto gate : differentiableGates) {
+        rewriter.setInsertionPoint(gate);
+        auto diffParams = gate.getDiffParams();
+        std::vector<Value> paramIds;
+        std::vector<Value> paramRuntimeDefinitions;
+        for (auto diffParam : diffParams) {
+            // So, what I need to do is
+            // for each diffParam, I need to get the corresponding
+            // static definition
+            mlir::Operation *operation = diffParam.getDefiningOp();
+            int paramIdInt = definitionNumberMap.at(operation);
+            Value paramId = rewriter.create<arith::ConstantOp>(
+                gate->getLoc(), rewriter.getI64Type(),
+                rewriter.getIntegerAttr(rewriter.getI64Type(), paramIdInt));
+            paramIds.push_back(paramId);
+
+            int64_t offset = valueDefiningBlockMap.at(diffParam.getAsOpaquePointer());
+            ArrayRef<int64_t> basicBlockIndex{static_cast<int64_t>(offset)};
+            Value loadArray = rewriter.create<LLVM::LoadOp>(gate->getLoc(), blockCounterArray);
+            Value runtimeCount =
+                rewriter.create<LLVM::ExtractValueOp>(gate->getLoc(), loadArray, offset);
+
+            paramRuntimeDefinitions.push_back(runtimeCount);
+        }
+
+        // Now that we have paramIds and paramRuntimeDefinitions
+        // we can substitute the op.
+        CustomOp customOp = cast<CustomOp>(gate);
+        rewriter.replaceOpWithNewOp<TraceCustomOp>(
+            gate, customOp.getResultTypes(), customOp.getParams(), paramIds,
+            paramRuntimeDefinitions, customOp.getInQubits(), customOp.getGateName());
+    }
+
+    removeBasicBlockCounterAttribute(op);
 }
 
 } // namespace
