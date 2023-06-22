c4e3854032756d2a76bd51e05739ac645674ddab
Sergei Mironov <sergei.mironov@xanadu.ai>
2023-06-22 16:31:00 +0000

    adjoint: Use successor ODS field for Yields

--- mlir/lib/Quantum/IR/QuantumOps.cpp
+++ mlir/lib/Quantum/IR/QuantumOps.cpp
@@ -313,22 +313,5 @@ LogicalResult AdjointOp::verify()
         return emitOpError("quantum measurements are not allowed in the adjoint regions");
     }
 
-    bool yieldEncountered = false;
-    Block &b = this->getRegion().front();
-    for (auto i = b.begin(); i != b.end(); i++) {
-
-        if (YieldOp yield = dyn_cast<YieldOp>(*i)) {
-            yieldEncountered = true;
-        }
-        else {
-            if (yieldEncountered) {
-                return emitOpError("quantum yield must be the last operation of the adjoint block");
-            }
-        }
-    }
-    if (!yieldEncountered) {
-        return emitOpError("quantum yield must present in the adjoint block");
-    }
-
     return success();
 }
