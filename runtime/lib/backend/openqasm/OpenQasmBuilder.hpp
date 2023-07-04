471d7ab0498acb084b8b1768fc96844c354541fe
Ali Asadi <ali@xanadu.ai>
2023-07-04 13:36:06 -0400

    Add HermitianObs to the OpenQasm3/Braket device (#179)
    * Add HermitianObs
    
    * Add frontend tests
    
    * Update Makefile
    
    * Update changelog
    
    * Fix the device name in ExecutionContext
    
    * Update code docs

--- runtime/lib/backend/openqasm/OpenQasmBuilder.hpp
+++ runtime/lib/backend/openqasm/OpenQasmBuilder.hpp
@@ -16,6 +16,7 @@
 
 #include <algorithm>
 #include <array>
+#include <complex>
 #include <iomanip>
 #include <memory>
 #include <sstream>
@@ -220,6 +221,70 @@ class QasmRegister {
 };
 
 /**
+ * The OpenQasm Matrix Builder for the following matrix data-types:
+ * - `std::vector<double>`
+ * - `std::vector<std::complex<double>>`
+ *
+ * @note It doesn't store the given matrix.
+ */
+struct MatrixBuilder {
+    [[nodiscard]] static auto toOpenQasm(const std::vector<std::complex<double>> &matrix,
+                                         size_t num_cols, size_t precision = 5,
+                                         [[maybe_unused]] const std::string &version = "3.0")
+        -> std::string
+    {
+        constexpr std::complex<double> zero{0, 0};
+        size_t index{0};
+        std::ostringstream oss;
+        oss << "[[";
+        for (const auto &c : matrix) {
+            if (index == num_cols) {
+                oss << "], [";
+                index = 0;
+            }
+            else if (index) {
+                oss << ", ";
+            }
+            index++;
+
+            if (c == zero) {
+                oss << "0";
+                continue;
+            }
+            oss << std::setprecision(precision) << c.real();
+            oss << std::setprecision(precision) << (c.imag() < 0 ? "" : "+") << c.imag() << "im";
+        }
+        oss << "]]";
+        return oss.str();
+    }
+
+    [[nodiscard]] static auto toOpenQasm(const std::vector<double> &matrix, size_t num_cols,
+                                         size_t precision = 5,
+                                         [[maybe_unused]] const std::string &version = "3.0")
+        -> std::string
+    {
+        size_t index{0};
+
+        std::ostringstream oss;
+        oss << "[[";
+        for (const auto &c : matrix) {
+            if (index == num_cols) {
+                oss << "], [";
+                index = 0;
+            }
+            else if (index) {
+                oss << ", ";
+            }
+            index++;
+
+            oss << std::setprecision(precision) << c;
+        }
+        oss << "]]";
+        return oss.str();
+    }
+};
+
+/**
  * The OpenQasm gate type.
  *
  * @param name The name of the gate to apply from the list of supported gates
@@ -347,6 +412,7 @@ class QasmObs {
     [[nodiscard]] virtual auto getName() const -> std::string = 0;
     [[nodiscard]] virtual auto getWires() const -> std::vector<size_t> = 0;
     [[nodiscard]] virtual auto toOpenQasm(const QasmRegister &qregister,
+                                          [[maybe_unused]] size_t precision = 5,
                                           [[maybe_unused]] const std::string &version = "3.0") const
         -> std::string = 0;
 };
@@ -369,11 +435,52 @@ class QasmNamedObs final : public QasmObs {
     [[nodiscard]] auto getWires() const -> std::vector<size_t> override { return wires; }
 
     [[nodiscard]] auto toOpenQasm(const QasmRegister &qregister,
+                                  [[maybe_unused]] size_t precision = 5,
                                   [[maybe_unused]] const std::string &version = "3.0") const
         -> std::string override
     {
         std::ostringstream oss;
         oss << name << "(" << qregister.toOpenQasm(RegisterMode::Slice, wires) << ")";
+        return oss.str();
+    }
+};
+
+/**
+ * A class for Braket/OpenQasm3 the hermitian observable.
+ *
+ * Note that the support is generic so that it works for any
+ * implementation of the `QasmObs` base class.
+ */
+class QasmHermitianObs final : public QasmObs {
+  private:
+    std::vector<std::complex<double>> matrix;
+    std::vector<size_t> wires;
+    const size_t num_cols;
+
+  public:
+    template <typename T1>
+    QasmHermitianObs(T1 &&_matrix, std::vector<size_t> _wires)
+        : matrix{std::forward<T1>(_matrix)}, wires{std::move(_wires)}, num_cols(1UL << wires.size())
+    {
+        RT_ASSERT(matrix.size() == num_cols * num_cols);
+    }
+
+    [[nodiscard]] auto getMatrix() const -> const std::vector<std::complex<double>> &
+    {
+        return matrix;
+    }
+    [[nodiscard]] auto getName() const -> std::string override { return "QasmHermitianObs"; }
+    [[nodiscard]] auto getWires() const -> std::vector<size_t> override { return wires; }
+
+    [[nodiscard]] auto toOpenQasm(const QasmRegister &qregister, size_t precision = 5,
+                                  const std::string &version = "3.0") const -> std::string override
+    {
+        std::ostringstream oss;
+        oss << "hermitian(";
+        oss << MatrixBuilder::toOpenQasm(matrix, num_cols, precision, version);
+        oss << ") ";
+        oss << qregister.toOpenQasm(RegisterMode::Slice, wires);
+
         return oss.str();
     }
 };
@@ -411,14 +518,13 @@ class QasmTensorObs final : public QasmObs {
     [[nodiscard]] auto getName() const -> std::string override { return "QasmTensorObs"; }
     [[nodiscard]] auto getWires() const -> std::vector<size_t> override { return wires; }
 
-    [[nodiscard]] auto toOpenQasm(const QasmRegister &qregister,
-                                  [[maybe_unused]] const std::string &version = "3.0") const
-        -> std::string override
+    [[nodiscard]] auto toOpenQasm(const QasmRegister &qregister, size_t precision = 5,
+                                  const std::string &version = "3.0") const -> std::string override
     {
         std::ostringstream oss;
         const size_t obs_size = obs.size();
         for (size_t idx = 0; idx < obs_size; idx++) {
-            oss << obs[idx]->toOpenQasm(qregister, version);
+            oss << obs[idx]->toOpenQasm(qregister, precision, version);
             if (idx != obs_size - 1) {
                 oss << " @ ";
             }
@@ -474,14 +580,13 @@ class QasmHamiltonianObs final : public QasmObs {
     }
     [[nodiscard]] auto getCoeffs() const -> std::vector<double> { return coeffs; }
 
-    [[nodiscard]] auto toOpenQasm(const QasmRegister &qregister,
-                                  [[maybe_unused]] const std::string &version = "3.0") const
-        -> std::string override
+    [[nodiscard]] auto toOpenQasm(const QasmRegister &qregister, size_t precision = 5,
+                                  const std::string &version = "3.0") const -> std::string override
     {
         std::ostringstream oss;
         const size_t obs_size = obs.size();
         for (size_t idx = 0; idx < obs_size; idx++) {
-            oss << coeffs[idx] << " * " << obs[idx]->toOpenQasm(qregister, version);
+            oss << coeffs[idx] << " * " << obs[idx]->toOpenQasm(qregister, precision, version);
             if (idx != obs_size - 1) {
                 oss << " + ";
             }
