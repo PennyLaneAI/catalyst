aff323e32b58080c9b5dd849793d1881d792e332
David Ittah <dime10@users.noreply.github.com>
2023-08-16 14:45:24 -0400

    Rename include/Quantum-c to include/CAPI

--- mlir/lib/CAPI/Dialects.cpp
+++ mlir/lib/CAPI/Dialects.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "Quantum-c/Dialects.h"
+#include "CAPI/Dialects.h"
 
 #include "Gradient/IR/GradientDialect.h"
 #include "Quantum/IR/QuantumDialect.h"
