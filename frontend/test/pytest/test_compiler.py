5201be4795bf9b1883e9bb0a0d0657e81e9ce46b
David Ittah <dime10@users.noreply.github.com>
2023-08-25 18:14:42 -0400

    Improve handling of linking errors
    Two issues are addressed:
    - if a user doesn't have the first compiler in the list, they would
      always receive a warning
    - the error raised for the linking process assumes the source is a lack
      of supported compiler

--- frontend/test/pytest/test_compiler.py
+++ frontend/test/pytest/test_compiler.py
@@ -98,7 +98,7 @@ class TestCompilerWarnings:
         """Test that a warning is emitted when a compiler failed."""
         with pytest.warns(UserWarning, match="Compiler .* failed .*"):
             # pylint: disable=protected-access
-            CompilerDriver._attempt_link("cc", [""], "in.o", "out.so", None)
+            CompilerDriver._attempt_link("cc", [""], "in.o", "out.so", CompileOptions(verbose=True))
 
 
 class TestCompilerErrors:
@@ -142,9 +142,8 @@ class TestCompilerErrors:
         with tempfile.NamedTemporaryFile(mode="w+", encoding="utf-8", suffix=".o") as invalid_file:
             invalid_file.write("These are invalid contents.")
             invalid_file.flush()
-            with pytest.raises(EnvironmentError, match="Unable to link .*"):
-                with pytest.warns(UserWarning, match="Compiler cc failed during execution"):
-                    CompilerDriver.run(invalid_file.name, fallback_compilers=["cc"])
+            with pytest.raises(CompileError, match="Unable to link .*"):
+                CompilerDriver.run(invalid_file.name, fallback_compilers=["cc"])
 
     @pytest.mark.parametrize(
         "pipeline",
