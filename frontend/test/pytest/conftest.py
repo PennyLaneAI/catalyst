1fc35211b982d124f8c7a83afd1a8140c43fbf2e
rmoyard <rmoyard@gmail.com>
2023-08-31 14:01:11 -0400

    Update

--- frontend/test/pytest/conftest.py
+++ frontend/test/pytest/conftest.py
@@ -1,3 +1,22 @@
+# Copyright 2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Pytest configuration file for Catalyst test suite.
+"""
+
+import pytest
+
 try:
     import tensorflow as tf
 except (ImportError, ModuleNotFoundError) as e:
@@ -7,7 +26,6 @@ else:
 
 def pytest_runtest_setup(item):
     """Automatically skip tests if interfaces are not installed"""
-    # Autograd is assumed to be installed
     interfaces = {"tf"}
     available_interfaces = {
         "tf": tf_available,
