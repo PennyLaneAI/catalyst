e3b928f566e70a05cdb10165f6c6c541c635926c
rmoyard <rmoyard@gmail.com>
2023-09-20 16:04:44 -0400

    Update

--- mlir/lib/Quantum/Transforms/ScatterPatterns.cpp
+++ mlir/lib/Quantum/Transforms/ScatterPatterns.cpp
@@ -34,15 +34,19 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
     mlir::LogicalResult matchAndRewrite(mhlo::ScatterOp op,
                                         mlir::PatternRewriter &rewriter) const override
     {
+        // Compute operation hash in case they are more than one scatter and they have different
+        // update function
+        auto opHash = OperationEquivalence::computeHash(op);
+        // Create the function to replace the update block from scatter
+        std::string funcName = "__catalyst_update_scatter" + std::to_string(opHash);
+
         Location loc = op.getLoc();
         // Add checks for supported cases (assumptions: no update windows dim, unique indices and
         // sorted indices)
-        if (!op.getUniqueIndices() || !op.getIndicesAreSorted() ||
-            !op.getScatterDimensionNumbers().getUpdateWindowDims().empty()) {
+        if (!op.getUniqueIndices() || !op.getIndicesAreSorted()) {
             return failure();
         }
 
-        op.getScatterDimensionNumbers();
         // Extract the block responsible for update
         Region &region = op.getUpdateComputation();
 
@@ -53,15 +57,18 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
 
         // We create a function from the update block
         FlatSymbolRefAttr updateFn =
-            getOrInsertUpdateFunction(op.getLoc(), moduleOp, rewriter, region);
+            getOrInsertUpdateFunction(op.getLoc(), moduleOp, rewriter, region, funcName);
         func::FuncOp updateFnOp = SymbolTable::lookupNearestSymbolFrom<func::FuncOp>(op, updateFn);
 
         // Get the inputs and updates values
         Value resultsValue = op.getInputs().front();
+        auto inputsShape = resultsValue.getType().cast<RankedTensorType>().getShape();
+
         Value updatesValue = op.getUpdates().front();
 
         // Get the shape of the updates
         ArrayRef<int64_t> updatesShape = updatesValue.getType().cast<TensorType>().getShape();
+        int64_t updatesSize = updatesShape.size();
         std::vector<int64_t> updatesShapeVector(updatesShape.begin(), updatesShape.end());
 
         // Get the scatter indices
@@ -70,27 +77,64 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
         ArrayRef<int64_t> scatterDimsToOperandDims =
             op.getScatterDimensionNumbers().getScatterDimsToOperandDims();
 
+        // Get updated windows dims
+        ArrayRef<int64_t> updatedWindowsDims =
+            op.getScatterDimensionNumbers().getUpdateWindowDims();
+
+        // Get updated inserted windows dims
+        ArrayRef<int64_t> insertedWindowsDims =
+            op.getScatterDimensionNumbers().getInsertedWindowDims();
+
+        std::vector<int64_t> updatedScatterDims;
+        // Separate updated windows dims
+        if (!updatedWindowsDims.empty()) {
+            int64_t start = 0;
+            std::vector<int64_t> dimensions;
+
+            for (int64_t i = start; i <= updatesSize; ++i) {
+                dimensions.push_back(i);
+            }
+
+            std::copy_if(updatedWindowsDims.begin(), updatedWindowsDims.end(),
+                         std::back_inserter(updatedScatterDims), [&dimensions](int64_t element) {
+                             return std::find(dimensions.begin(), dimensions.end(), element) ==
+                                    dimensions.end();
+                         });
+        }
+
         // Generate all possible indices given the shape of updates
         std::vector<int64_t> indices;
         std::vector<std::vector<int64_t>> allUpdatesIndices;
         generateIndicesRecursive(updatesShapeVector, indices, 0, allUpdatesIndices);
 
         // Replace the results with the updated inputs.
-        for (auto updatesIndices : allUpdatesIndices) {
+        for (std::vector<int64_t> updatesIndices : allUpdatesIndices) {
+            // Scatter update
+            std::vector<int64_t> updateScatterIndices;
+            for (int index : updatedScatterDims) {
+                updateScatterIndices.push_back(updatesIndices[index]);
+            }
+            // Windows update
+            std::vector<int64_t> updateWindowsIndices;
+            for (int index : updatedWindowsDims) {
+                updateWindowsIndices.push_back(updatesIndices[index]);
+            }
+            // Get results indices from update indices
+            SmallVector<Value> resultsIndicesValue =
+                getResultsIndices(updatesIndices, updateScatterIndices, updateWindowsIndices,
+                                  inputsShape, insertedWindowsDims, scatterIndices, indexVectorDim,
+                                  scatterDimsToOperandDims, rewriter, loc);
+            // Create Values for indices
             SmallVector<Value> updatesIndicesValue;
             for (int64_t index : updatesIndices) {
                 Value value = rewriter.create<index::ConstantOp>(loc, index);
                 updatesIndicesValue.push_back(value);
             }
-            // Get results indices from update indices
-            ValueRange resultsIndices =
-                getResultsIndices(updatesIndices, scatterIndices, indexVectorDim,
-                                  scatterDimsToOperandDims, rewriter, loc);
             // Set Args (Value range)
             Value updateValue =
                 rewriter.create<tensor::ExtractOp>(loc, updatesValue, updatesIndicesValue);
             Value resultValue =
-                rewriter.create<tensor::ExtractOp>(loc, resultsValue, resultsIndices);
+                rewriter.create<tensor::ExtractOp>(loc, resultsValue, resultsIndicesValue);
 
             // f64 -> tensor<f64> if necessary
             if (!isa<RankedTensorType>(updateValue.getType())) {
@@ -107,7 +151,6 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
 
             // Call the function that computes the update
             Value updated = rewriter.create<func::CallOp>(loc, updateFnOp, args).getResult(0);
-
             // Make it a scalar if necessary tensor<f64> -> f64
             Value updatedExtracted;
             if (isa<RankedTensorType>(updated.getType())) {
@@ -118,7 +161,7 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
             }
             // Insert the update in the results and replace the previous value
             Value res = rewriter.create<tensor::InsertOp>(loc, updatedExtracted, resultsValue,
-                                                          resultsIndices);
+                                                          resultsIndicesValue);
             resultsValue = res;
         }
         // Replace the results with the updated one
@@ -127,12 +170,9 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
     }
 
     FlatSymbolRefAttr getOrInsertUpdateFunction(Location loc, ModuleOp moduleOp, OpBuilder &builder,
-                                                Region &updateRegion) const
+                                                Region &updateRegion, std::string funcName) const
     {
         MLIRContext *ctx = builder.getContext();
-
-        // Create the function to replace the update block from scatter
-        std::string funcName = "__catalyst_update_scatter";
 
         if (moduleOp.lookupSymbol<func::FuncOp>(funcName)) {
             return SymbolRefAttr::get(ctx, funcName);
@@ -187,8 +227,12 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
         }
     }
 
-    SmallVector<Value> getResultsIndices(std::vector<int64_t> updatesIndices, Value scatterIndices,
-                                         int64_t indexVectorDim,
+    SmallVector<Value> getResultsIndices(std::vector<int64_t> updatesIndices,
+                                         std::vector<int64_t> updateScatterIndices,
+                                         std::vector<int64_t> updateWindowsIndices,
+                                         ArrayRef<int64_t> inputsShape,
+                                         ArrayRef<int64_t> insertedWindowsDims,
+                                         Value scatterIndices, int64_t indexVectorDim,
                                          ArrayRef<int64_t> scatterDimsToOperandDims,
                                          mlir::PatternRewriter &rewriter, Location loc) const
     {
@@ -199,16 +243,15 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
         int64_t rank = scatterIndicesTensorType.getRank();
         auto shape = scatterIndicesTensorType.getShape();
 
-        if (rank != 1) {
+        // Get the scatter indices
+        if (!updateScatterIndices.empty()) {
             // Offset
-            std::vector<int64_t> offsets = updatesIndices;
-            offsets.push_back(0);
-
+            std::vector<int64_t> offsets(rank, 0);
             std::vector<Value> dynOffsets = {};
 
             // Size
-            std::vector<int64_t> sizes(rank, 0);
-            sizes[-1] = shape[-1];
+            std::vector<int64_t> sizes(rank, 1);
+            sizes[indexVectorDim] = shape[indexVectorDim];
             std::vector<Value> dynSizes = {};
 
             // Stides
@@ -223,19 +266,63 @@ struct ScatterOpRewritePattern : public mlir::OpRewritePattern<mhlo::ScatterOp> 
                 loc, resultType, scatterIndices, dynOffsets, dynSizes, dynStrides, offsets, sizes,
                 strides);
         }
-        // Sort
-        SmallVector<Value> results(rank);
-        for (auto index : scatterDimsToOperandDims) {
-            Value indexValue = rewriter.create<index::ConstantOp>(loc, index);
-            Value value = rewriter.create<tensor::ExtractOp>(loc, scatterIndices, indexValue);
-            Value valueCasted =
-                rewriter.create<arith::IndexCastOp>(loc, rewriter.getIndexType(), value);
-            // Add to tensor
-            results[index] = valueCasted;
-        }
 
-        // Results indices
-        return results;
+        if (insertedWindowsDims.empty()) {
+            // Add Scatter in Windows Indices
+            SmallVector<Value> results;
+            for (size_t i = 0; i < updateWindowsIndices.size(); ++i) {
+                int64_t indexUpdate = updateWindowsIndices[i];
+                Value index = rewriter.create<index::ConstantOp>(loc, i);
+                auto indexScatter = rewriter.create<tensor::ExtractOp>(loc, scatterIndices, index);
+
+                TypedAttr indexAttr = rewriter.getI32IntegerAttr(indexUpdate);
+                Value indexValue = rewriter.create<arith::ConstantOp>(loc, indexAttr);
+
+                Value addValue = rewriter.create<arith::AddIOp>(loc, indexScatter, indexValue);
+                Value addValueCasted =
+                    rewriter.create<arith::IndexCastOp>(loc, rewriter.getIndexType(), addValue);
+                results.push_back(addValueCasted);
+            }
+            return results;
+        }
+        else {
+            SmallVector<Value> fullStartIndex;
+            for (size_t i = 0; i < inputsShape.size(); ++i) {
+                // Full start indices (use scatter dims op)
+                auto itScatter =
+                    std::find(scatterDimsToOperandDims.begin(), scatterDimsToOperandDims.end(), i);
+                if (itScatter != scatterDimsToOperandDims.end()) {
+                    Value index = rewriter.create<index::ConstantOp>(loc, i);
+                    auto indexScatter =
+                        rewriter.create<tensor::ExtractOp>(loc, scatterIndices, index);
+                    fullStartIndex.push_back(indexScatter);
+                }
+                else {
+                    Value index = rewriter.create<index::ConstantOp>(loc, 0);
+                    fullStartIndex.push_back(index);
+                }
+            }
+            SmallVector<Value> fullWindowIndex;
+            // Full windows indices
+            for (auto insertedDim : insertedWindowsDims) {
+                updateWindowsIndices.insert(updateWindowsIndices.begin() + insertedDim, 0);
+            }
+            // Add
+            SmallVector<Value> results;
+            for (size_t i = 0; i < updateWindowsIndices.size(); ++i) {
+                Value indexScatter = fullStartIndex[i];
+                int64_t indexUpdate = updateWindowsIndices[i];
+
+                TypedAttr indexAttr = rewriter.getI32IntegerAttr(indexUpdate);
+                Value indexValue = rewriter.create<arith::ConstantOp>(loc, indexAttr);
+
+                Value addValue = rewriter.create<arith::AddIOp>(loc, indexScatter, indexValue);
+                Value addValueCasted =
+                    rewriter.create<arith::IndexCastOp>(loc, rewriter.getIndexType(), addValue);
+                results.push_back(addValueCasted);
+            }
+            return results;
+        }
     }
 };
 
--- mlir/test/Catalyst/ScatterTest.mlir
+++ mlir/test/Catalyst/ScatterTest.mlir
@@ -36,3 +36,71 @@ func.func public @scatter_multiply(%arg0: tensor<3xf64>, %arg1: tensor<i64>) -> 
     }) {indices_are_sorted = true, scatter_dimension_numbers = #mhlo.scatter<inserted_window_dims = [0], scatter_dims_to_operand_dims = [0]>, unique_indices = true} : (tensor<3xf64>, tensor<1xi32>, tensor<f64>) -> tensor<3xf64>
     return %4 : tensor<3xf64>
   }
+
+#map = affine_map<(d0) -> (d0)>
+func.func public @two_scatter(%arg0: tensor<3xf64>, %arg1: tensor<i64>) -> tensor<3xf64> attributes {llvm.emit_c_interface} {
+  %c0_i64 = arith.constant 0 : i64
+  %c3_i64 = arith.constant 3 : i64
+  %cst = arith.constant dense<2.000000e+00> : tensor<f64>
+  %cst_0 = arith.constant dense<3.000000e+00> : tensor<f64>
+  %extracted = tensor.extract %arg1[] : tensor<i64>
+  %0 = arith.cmpi slt, %extracted, %c0_i64 : i64
+  %extracted_1 = tensor.extract %arg1[] : tensor<i64>
+  %1 = arith.addi %extracted_1, %c3_i64 : i64
+  %extracted_2 = tensor.extract %arg1[] : tensor<i64>
+  %2 = arith.select %0, %1, %extracted_2 : i64
+  %3 = arith.trunci %2 : i64 to i32
+  %from_elements = tensor.from_elements %3 : tensor<1xi32>
+  %4 = "mhlo.scatter"(%arg0, %from_elements, %cst_0) ({
+  ^bb0(%arg2: tensor<f64>, %arg3: tensor<f64>):
+    %extracted_7 = tensor.extract %arg2[] : tensor<f64>
+    %extracted_8 = tensor.extract %arg3[] : tensor<f64>
+    %12 = arith.mulf %extracted_7, %extracted_8 : f64
+    %from_elements_9 = tensor.from_elements %12 : tensor<f64>
+    mhlo.return %from_elements_9 : tensor<f64>
+  }) {indices_are_sorted = true, scatter_dimension_numbers = #mhlo.scatter<inserted_window_dims = [0], scatter_dims_to_operand_dims = [0]>, unique_indices = true} : (tensor<3xf64>, tensor<1xi32>, tensor<f64>) -> tensor<3xf64>
+  %extracted_3 = tensor.extract %arg1[] : tensor<i64>
+  %5 = arith.cmpi slt, %extracted_3, %c0_i64 : i64
+  %extracted_4 = tensor.extract %arg1[] : tensor<i64>
+  %6 = arith.addi %extracted_4, %c3_i64 : i64
+  %extracted_5 = tensor.extract %arg1[] : tensor<i64>
+  %7 = arith.select %5, %6, %extracted_5 : i64
+  %8 = arith.trunci %7 : i64 to i32
+  %from_elements_6 = tensor.from_elements %8 : tensor<1xi32>
+  %9 = "mhlo.scatter"(%arg0, %from_elements_6, %cst) ({
+  ^bb0(%arg2: tensor<f64>, %arg3: tensor<f64>):
+    %extracted_7 = tensor.extract %arg2[] : tensor<f64>
+    %extracted_8 = tensor.extract %arg3[] : tensor<f64>
+    %12 = arith.addf %extracted_7, %extracted_8 : f64
+    %from_elements_9 = tensor.from_elements %12 : tensor<f64>
+    mhlo.return %from_elements_9 : tensor<f64>
+  }) {indices_are_sorted = true, scatter_dimension_numbers = #mhlo.scatter<inserted_window_dims = [0], scatter_dims_to_operand_dims = [0]>, unique_indices = true} : (tensor<3xf64>, tensor<1xi32>, tensor<f64>) -> tensor<3xf64>
+  %10 = tensor.empty() : tensor<3xf64>
+  %11 = linalg.generic {indexing_maps = [#map, #map, #map], iterator_types = ["parallel"]} ins(%4, %9 : tensor<3xf64>, tensor<3xf64>) outs(%10 : tensor<3xf64>) {
+  ^bb0(%in: f64, %in_7: f64, %out: f64):
+    %12 = arith.addf %in, %in_7 : f64
+    linalg.yield %12 : f64
+  } -> tensor<3xf64>
+  return %11 : tensor<3xf64>
+}
+
+// func.func public @full_example_scatter(%input: tensor<3x4x2xi64>, %update: tensor<2x3x2x2xi64>) -> tensor<3x4x2xi64> attributes {llvm.emit_c_interface} {
+//   %scatter_indices = arith.constant dense<2> : tensor<2x3x2xi64>
+//   %result = "mhlo.scatter"(%input, %scatter_indices, %update) ({
+//     ^bb0(%arg2: tensor<i64>, %arg3: tensor<i64>):
+//       %extracted_1 = tensor.extract %arg2[] : tensor<i64>
+//       %extracted_2 = tensor.extract %arg3[] : tensor<i64>
+//       %1 = arith.addi %extracted_1, %extracted_2 : i64
+//       %from_elements = tensor.from_elements %1 : tensor<i64>
+//       mhlo.return %from_elements : tensor<i64>
+//   }) {
+//     scatter_dimension_numbers = #mhlo.scatter<
+//       update_window_dims = [2, 3],
+//       inserted_window_dims = [0],
+//       scatter_dims_to_operand_dims = [1, 0],
+//       index_vector_dim = 2>,
+//     indices_are_sorted = true,
+//     unique_indices = true
+//   } : (tensor<3x4x2xi64>, tensor<2x3x2xi64>, tensor<2x3x2x2xi64>) -> tensor<3x4x2xi64>
+//   return %result : tensor<3x4x2xi64>
+// }
