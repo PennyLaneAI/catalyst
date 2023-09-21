2a9ed99f03af5e0a449c937632ae529651f51005
Sergei Mironov <sergei.mironov@xanadu.ai>
2023-09-21 17:16:15 +0000

    Add quinst to the set of order-preserved ops

--- frontend/catalyst/jax_tracer.py
+++ frontend/catalyst/jax_tracer.py
@@ -108,7 +108,7 @@ class Function:
 
 KNOWN_NAMED_OBS = (qml.Identity, qml.PauliX, qml.PauliY, qml.PauliZ, qml.Hadamard)
 
-FORCED_ORDER_PRIMITIVES = {qdevice_p, qextract_p}
+FORCED_ORDER_PRIMITIVES = {qdevice_p, qextract_p, qinst_p}
 
 PAULI_NAMED_MAP = {
     "I": "Identity",
