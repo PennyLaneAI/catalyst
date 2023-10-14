8821eeac00112163a4ff99ff1d032d2028a9f26e
Josh Izaac <josh146@gmail.com>
2023-10-13 23:37:07 -0400

    more

--- frontend/catalyst/__init__.py
+++ frontend/catalyst/__init__.py
@@ -77,7 +77,81 @@ from catalyst.pennylane_extensions import (
 from catalyst.utils.exceptions import CompileError
 
 autograph_ignore_fallbacks = False
+"""bool: Specify whether AutoGraph should avoid raising
+errors when conversion fails and control flow instead falls back
+to being interpreted by Python at compile-time.
+
+**Example**
+
+In certain cases, AutoGraph will fail to convert control flow(for example,
+when an object that can not be converted to a JAX array is indexed in a
+loop), and will raise a warning informing of the failure.
+
+>>> @qjit(autograph=True)
+... @qml.qnode(dev)
+... def f():
+...     x = ["0.1", "0.2", "0.3"]
+...     for i in range(3):
+...         qml.RX(float(x[i]), wires=i)
+...     return qml.expval(qml.PauliZ(0))
+Warning: If you intended for the conversion to happen, make sure that the(now dynamic) loop variable is not used in tracing-incompatible ways,
+for instance by indexing a Python list with it. In that case, the list should be wrapped into an array.
+To understand different types of JAX tracing errors, please refer to the guide at: https://jax.readthedocs.io/en/latest/errors.html
+If you did not intend for the conversion to happen, you may safely ignore this warning.
+
+Setting this variable to ``True`` will suppress warning messages:
+
+>>> catalyst.autograph_strict_conversion = False
+>>> catalyst.autograph_ignore_fallbacks = True
+>>> @qjit(autograph=True)
+... @qml.qnode(dev)
+... def f():
+...     x = ["0.1", "0.2", "0.3"]
+...     for i in range(3):
+...         qml.RX(float(x[i]), wires=i)
+...     return qml.expval(qml.PauliZ(0))
+>>> f()
+array(0.99500417)
+"""
+
 autograph_strict_conversion = False
+"""bool: Specify whether AutoGraph should raise exceptions
+when conversion fails, rather than falling back to interpreting
+control flow by Python at compile-time.
+
+**Example**
+
+In certain cases, AutoGraph will fail to convert control flow(for example,
+when an object that can not be converted to a JAX array is indexed in a
+loop), and will automatically fallback to interpreting the control
+logic at compile-time via Python:
+
+>>> dev = qml.device("lightning.qubit", wires=1)
+>>> @qjit(autograph=True)
+... @qml.qnode(dev)
+... def f():
+...     params = ["0", "1", "2"]
+...     for x in params:
+...         qml.RY(int(x) * jnp.pi / 4, wires=0)
+...     return qml.expval(qml.PauliZ(0))
+>>> f()
+array(-0.70710678)
+
+Setting this variable to ``True`` will cause AutoGraph
+to error rather than fallback when conversion fail:
+
+>>> catalyst.autograph_strict_conversion = True
+>>> @qjit(autograph=True)
+... @qml.qnode(dev)
+... def f():
+...     params = ["0", "1", "2"]
+...     for x in params:
+...         qml.RY(int(x) * jnp.pi / 4, wires=0)
+...     return qml.expval(qml.PauliZ(0))
+AutoGraphError: Could not convert the iteration target ['0', '1', '2'] to array while processing the following with AutoGraph:
+  File "<ipython-input-44-dbae11e6d745>", line 7, in f
+    for x in params:
+"""
 
 
 __all__ = (
