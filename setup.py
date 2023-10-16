4a7f52693c33237695ff9711edaa939c0877647b
rmoyard <rmoyard@gmail.com>
2023-10-16 17:13:31 -0400

    Add more

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open(".dep-versions") as f:
     jax_version = [line[4:].strip() for line in f.readlines() if "jax=" in line][0]
 
 requirements = [
-    "pennylane>=0.32",
+    "git+https://github.com/PennyLaneAI/pennylane.git@master",
     f"jax=={jax_version}",
     f"jaxlib=={jax_version}",
 ]
