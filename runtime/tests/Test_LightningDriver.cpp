2786ce1bbdc73cdfbaecfff455326d7f7091e557
Erick Eduardo Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2023-10-24 17:12:54 +0000

    f

--- runtime/tests/Test_LightningDriver.cpp
+++ runtime/tests/Test_LightningDriver.cpp
@@ -68,7 +68,6 @@ TEST_CASE("Test Driver", "[Driver]")
     // check device specs update
     driver->setDeviceRecorder(true);
     driver->setDeviceKwArgs("execute=openmp;");
-    CHECK(driver->initDevice("default") == false);
     CHECK(driver->getDevice() == nullptr);
     CHECK(driver->getDeviceKwArgs() == "execute=openmp;");
     CHECK(driver->getDeviceRecorderStatus() == true);
