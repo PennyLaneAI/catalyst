7e4cabcb4e061dfaa23763996e01d0ed714359b8
Josh Izaac <josh146@gmail.com>
2023-10-31 11:23:22 -0400

    Update doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -129,7 +129,7 @@ extensions = [
 intersphinx_mapping = {"https://docs.pennylane.ai/en/stable/": None}
 
 # opengraph details
-ogp_site_url = "https://pennylane.ai/project/catalyst"
+ogp_site_url = "https://docs.pennylane.ai/projects/catalyst/en/stable"
 ogp_image = "https://docs.pennylane.ai/projects/catalyst/en/stable/_static/catalyst.png"
 ogp_type = "article"
 ogp_enable_meta_description = True
