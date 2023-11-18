7a4ce6bd38b18b07d82d07e506d926c92c6ba9b0
David Ittah <dime10@users.noreply.github.com>
2023-11-17 22:33:02 -0500

    Enable address sanitizers on macOS in our build scripts (#368)
    Adjusted configuration in Makefiles and lit config files to successfully
    run tests with ASAN on macOS.
    
    Tested: `make dialects`, `make lit`, `make pytest`, `make test-demos`
    (non-functional), `make runtime`, `make runtime-all` (new), `make
    test-runtime`, `make test-runtime-all` (new), `make test-mlir` (new),
    `make coverage-frontend`
    
    The main issue on this platform is related to the need to preload the
    ASAN runtime library for the process that will end up loading a
    sanitized shared library. Similar to `LD_PRELOAD` on Linux, this is
    achieved via the environment variable
    ```
    DYLD_INSERT_LIBRARIES="$(clang -print-file-name=libclang_rt.asan_osx_dynamic.dylib)"
    ```
    
    However, contrary to `LD_PRELOAD`, the preloaded library is not
    propagated to any subprocesses spawned by the parent process. This can
    also manifest when running Jupyter Notebook tests, or when running
    PyTests in parallel with `xdist`, both of which are **disabled** now on
    macOS with ASAN enabled.
    
    Additionally, it is **necessary** that the default Python executable
    discoverable in the environment is the "real" executable for the Python
    interpreter (one that won't spawn additional processes). On macOS, there
    seems to exist additional Python executables which are wrappers around
    the interpreter executable. For example, on my system, using the
    official Python installer, I can find the following executables (not
    symlinks):
    - `/Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11`:
    seems to spawn a new process with the executable below
    -
    `/Library/Frameworks/Python.framework/Versions/3.11/Resources/Python.app/Contents/MacOS/Python`:
    actual interpreter binary
    
    Refer to this issue for more information:
    https://stackoverflow.com/a/47853433
    
    I also discovered "container overflow" sanitizer errors in our tests,
    but fixing these is outside the scope of this PR.
    
    [sc-50453]

--- frontend/test/lit/lit.cfg.py
+++ frontend/test/lit/lit.cfg.py
@@ -32,19 +32,25 @@ config.test_source_root = path.dirname(__file__)
 config.test_exec_root = getattr(config, "frontend_test_dir", ".lit")
 
 # TODO: Ideally we would test with leak detection, but this may not be possible from Python.
-config.environment["ASAN_OPTIONS"] = "detect_leaks=0"
+# TODO: Find out why we have container overflow on macOS.
+config.environment["ASAN_OPTIONS"] = "detect_leaks=0,detect_container_overflow=0"
 
 # Define substitutions used at the top of lit test files, e.g. %PYTHON.
 python_executable = getattr(config, "python_executable", "python3.10")
 
 if "Address" in getattr(config, "llvm_use_sanitizer", ""):
     # With sanitized builds, Python tests require some preloading magic to run.
-    assert "Linux" in config.host_os, "Testing with sanitized builds requires Linux/Clang"
-
-    python_executable = (
-        f"LD_PRELOAD=$({config.host_cxx} -print-file-name=libclang_rt.asan-{config.host_arch}.so)"
-        f" {python_executable}"
-    )
+    if "Linux" in config.host_os:
+        python_executable = f"LD_PRELOAD=$({config.host_cxx} -print-file-name=libclang_rt.asan-{config.host_arch}.so) {python_executable}"
+    elif "Darwin" in config.host_os:
+        # It's important that the python executable discovered by CMake is the true interpreter
+        # binary. On macOS, it appears that there can be multiple "python" executables which are
+        # just wrappers around the actual interpreter. In that case, the DYLD_INSERT_LIBRARIES trick
+        # will not work, as it will not be forwarded to the final process.
+        # See https://stackoverflow.com/a/47853433 for more information.
+        python_executable = f"DYLD_INSERT_LIBRARIES=$({config.host_cxx} -print-file-name=libclang_rt.asan_osx_dynamic.dylib) {python_executable}"
+    else:
+        assert False, "Testing with sanitized builds requires Linux or MacOS"
 
 config.substitutions.append(("%PYTHON", python_executable))
 
