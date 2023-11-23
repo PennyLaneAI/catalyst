d47e9952ba04b82212e868765a6ac5376929de2d
rmoyard <rmoyard@gmail.com>
2023-11-23 15:46:33 -0500

    Finish encoding arg

--- mlir/lib/Catalyst/Transforms/catalyst_to_llvm.cpp
+++ mlir/lib/Catalyst/Transforms/catalyst_to_llvm.cpp
@@ -15,6 +15,7 @@
 #include <unordered_map>
 
 #include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
+#include "mlir/Conversion/LLVMCommon/MemRefBuilder.h"
 #include "mlir/Conversion/LLVMCommon/TypeConverter.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
@@ -232,11 +233,6 @@ struct PrintOpPattern : public OpConversionPattern<PrintOp> {
     }
 };
 
-struct EncodedArguments {
-    LLVM::AllocaOp encoded; // `args` argument
-    SmallVector<LLVM::AllocaOp> values;
-};
-
 struct CustomCallOpPattern : public OpConversionPattern<CustomCallOp> {
     using OpConversionPattern::OpConversionPattern;
 
@@ -265,44 +261,46 @@ struct CustomCallOpPattern : public OpConversionPattern<CustomCallOp> {
                                            operandsConverted.end()};
 
         // Encode args
-        EncodedArguments arguments;
-
-        for (auto tuple : llvm::drop_begin(llvm::zip(operands, operandsConverted))) {
-            auto encoded_arg = encodeMemref(std::get<0>(tuple), std::get<1>(tuple));
-            encoded.push_back(*encoded_arg);
+        Value c1 = rewriter.create<LLVM::ConstantOp>(loc, rewriter.getI64IntegerAttr(1));
+        SmallVector<mlir::LLVM::AllocaOp> encoded;
+        for (auto tuple : llvm::drop_begin(llvm::zip(args, argsConverted))) {
+            auto memref_type = std::get<0>(tuple).getType().cast<MemRefType>();
+            auto encoded_arg = EncodeMemRef(loc, rewriter, memref_type, std::get<1>(tuple));
+            LLVM::AllocaOp alloca = rewriter.create<LLVM::AllocaOp>(loc, ptr, type, c1, 0);
+            // Start the lifetime of encoded value.
+            rewriter.create<LLVM::LifetimeStartOp>(loc, rewriter.getI64IntegerAttr(-1), alloca);
+            // Use volatile store to suppress expensive LLVM optimizations.
+            rewriter.create<LLVM::StoreOp>(loc, encoded_arg, alloca, /*alignment=*/0, /*isVolatile=*/true);
+            encoded.push_back(alloca);
         }
 
         // We store encoded arguments as `!llvm.array<ptr x len>`.
-        size_t len = 2 + encoded.size();
+        size_t len = encoded.size();
         Type ptr = LLVM::LLVMPointerType::get(rewriter.getContext());
         Type type = LLVM::LLVMArrayType::get(ptr, len);
 
         // Prepare an array for encoded arguments.
-        Value arr = rewriter.create<LLVM::UndefOp>(type);
+        Value arr = rewriter.create<LLVM::UndefOp>(loc, type);
         auto insert_value = [&](Value value, int64_t offset) {
-            arr = rewriter.create<LLVM::InsertValueOp>(arr, value, offset);
+            arr = rewriter.create<LLVM::InsertValueOp>(loc, arr, value, offset);
         };
-
         // Store pointer to encoded arguments into the allocated storage.
         for (const auto &pair : llvm::enumerate(encoded)) {
-            CustomCallArgEncoding::Encoded encoded = pair.value();
-            int64_t offset = 2 + pair.index();
-            insert_value(AsPtr(b, encoded.value), offset);
-            arguments.values.push_back(encoded.value);
+            int64_t offset = pair.index();
+            insert_value(pair.value(), offset);
         }
-
         // Get allocation for packed arguments pointers.
-        LLVM::AllocaOp alloca = a.GetOrCreate(b, type);
+        LLVM::AllocaOp alloca = rewriter.create<LLVM::AllocaOp>(loc, ptr, type, c1, 0);
 
         // Start the lifetime of the encoded arguments pointers.
-        b.create<LLVM::LifetimeStartOp>(b.getI64IntegerAttr(-1), alloca);
+        rewriter.create<LLVM::LifetimeStartOp>(loc, rewriter.getI64IntegerAttr(-1), alloca);
 
         // Store constructed arguments pointers array into the alloca. Use volatile
         // store to suppress expensive LLVM optimizations.
-        b.create<LLVM::StoreOp>(arr, alloca, /*alignment=*/0, /*isVolatile=*/true);
+        rewriter.create<LLVM::StoreOp>(loc, arr, alloca, /*alignment=*/0, /*isVolatile=*/true);
 
         // Alloca that encodes the custom call arguments.
-        arguments.encoded = alloca;
+        auto encodedArguments = alloca.getResult();
 
         // Encode res
 
@@ -313,6 +311,175 @@ struct CustomCallOpPattern : public OpConversionPattern<CustomCallOp> {
         return success();
     }
 };
+
+enum class PrimitiveType : uint8_t {
+    // Invalid primitive type to serve as default.
+    PRIMITIVE_TYPE_INVALID = 0,
+
+    // Predicates are two-state booleans.
+    PRED = 1,
+
+    // Signed integral values of fixed width.
+    S8 = 2,
+    S16 = 3,
+    S32 = 4,
+    S64 = 5,
+
+    // Unsigned integral values of fixed width.
+    U8 = 6,
+    U16 = 7,
+    U32 = 8,
+    U64 = 9,
+
+    // Floating-point values of fixed width.
+    F16 = 10,
+    F32 = 11,
+    F64 = 12,
+};
+
+PrimitiveType SignedIntegralTypeForBitWidth(int64_t src_bitwidth)
+{
+    switch (src_bitwidth) {
+    case 8:
+        return PrimitiveType::S8;
+    case 16:
+        return PrimitiveType::S16;
+    case 32:
+        return PrimitiveType::S32;
+    case 64:
+        return PrimitiveType::S64;
+    default:
+        return PrimitiveType::PRIMITIVE_TYPE_INVALID;
+    }
+}
+
+PrimitiveType UnsignedIntegralTypeForBitWidth(int64_t src_bitwidth)
+{
+    switch (src_bitwidth) {
+    case 8:
+        return PrimitiveType::U8;
+    case 16:
+        return PrimitiveType::U16;
+    case 32:
+        return PrimitiveType::U32;
+    case 64:
+        return PrimitiveType::U64;
+    default:
+        return PrimitiveType::PRIMITIVE_TYPE_INVALID;
+    }
+}
+static PrimitiveType ScalarPrimitiveType(Type type)
+{
+    // Integer types.
+    if (type.isInteger(1))
+        return PrimitiveType::PRED;
+    if (auto int_type = type.dyn_cast<mlir::IntegerType>()) {
+        unsigned int width = int_type.getWidth();
+        auto primitive_type = int_type.isUnsigned()
+                                  // Unsigned integer types.
+                                  ? UnsignedIntegralTypeForBitWidth(width)
+                                  // Signed integer types.
+                                  : SignedIntegralTypeForBitWidth(width);
+        return primitive_type;
+    }
+
+    // Floating point types.
+    if (type.isF16())
+        return PrimitiveType::F16;
+    if (type.isF32())
+        return PrimitiveType::F32;
+    if (type.isF64())
+        return PrimitiveType::F64;
+
+    assert(false && "unsupported type id");
+    return PrimitiveType::PRIMITIVE_TYPE_INVALID;
+}
+
+// Encodes memref as LLVM struct value:
+//
+//   { i8: dtype, i8: rank, ptr<i8>: data,
+//     array<2*rank x i64>: sizes_and_strides }
+//
+// This is a type erased version of the MLIR memref descriptor without base
+// pointer. We pack sizes and strides as a single array member, so that on
+// the runtime side we can read it back using C flexible array member.
+// If the descriptor value is null, we only encode statically known info: dtype,
+// rank, and dims, otherwise we also encode dynamic info
+Value EncodeMemRef(Location loc, PatternRewriter &rewriter, MemRefType memref_ty, Value descriptor)
+{
+    auto ctx = rewriter.getContext();
+    // Encode sizes together with strides as a single array.
+    int64_t sizes_and_strides_size = 2 * memref_ty.getRank();
+
+    // Encoded memref type: !llvm.struct<(i8, i8, ptr<i8>, array<... x i64>)>.
+    Type i8 = rewriter.getI8Type();
+    Type ptr = LLVM::LLVMPointerType::get(ctx);
+    Type arr = LLVM::LLVMArrayType::get(rewriter.getI64Type(), sizes_and_strides_size);
+    auto type = LLVM::LLVMStructType::getLiteral(ctx, {i8, i8, ptr, arr});
+
+    // Helper to unpack MLIR strided memref descriptor value.
+    std::optional<MemRefDescriptor> desc = std::nullopt;
+    if (descriptor) {
+        desc = MemRefDescriptor(descriptor);
+    }
+
+    PrimitiveType element_dtype = ScalarPrimitiveType(memref_ty.getElementType());
+
+    // Create values for filling encoded memref struct.
+    Value dtype = rewriter.create<LLVM::ConstantOp>(
+        loc, rewriter.getI8IntegerAttr(static_cast<uint8_t>(element_dtype)));
+    Value rank =
+        rewriter.create<LLVM::ConstantOp>(loc, rewriter.getI8IntegerAttr(memref_ty.getRank()));
+
+    auto i64 = [&](int64_t i) { return rewriter.getI64IntegerAttr(i); };
+
+    // Get the statically known strides and offset from the memref type.
+    llvm::SmallVector<int64_t> strides;
+    int64_t memref_offset;
+    if (failed(getStridesAndOffset(memref_ty, strides, memref_offset)))
+        strides.resize(memref_ty.getRank(), ShapedType::kDynamic);
+
+    // Build encoded memref sizes + strides: !llvm.array<... x i64>
+    Value payload = rewriter.create<LLVM::UndefOp>(loc, type.getBody()[3]);
+    for (unsigned i = 0; i < memref_ty.getRank(); ++i) {
+        int64_t dim_size = memref_ty.getDimSize(i);
+        int64_t stride_size = strides[i];
+
+        Value dim = ShapedType::isDynamic(dim_size) && desc.has_value()
+                        ? desc->size(rewriter, loc, i)
+                        : rewriter.create<LLVM::ConstantOp>(loc, i64(dim_size));
+
+        Value stride = ShapedType::isDynamic(stride_size) && desc.has_value()
+                           ? desc->stride(rewriter, loc, i)
+                           : rewriter.create<LLVM::ConstantOp>(loc, i64(stride_size));
+
+        auto stride_pos = memref_ty.getRank() + i;
+
+        payload = rewriter.create<LLVM::InsertValueOp>(loc, payload, dim, i);
+        payload = rewriter.create<LLVM::InsertValueOp>(loc, payload, stride, stride_pos);
+    }
+
+    // Construct encoded memref value.
+    Value memref = rewriter.create<LLVM::UndefOp>(loc, type);
+    memref = rewriter.create<LLVM::InsertValueOp>(loc, memref, dtype, 0);
+    memref = rewriter.create<LLVM::InsertValueOp>(loc, memref, rank, 1);
+    memref = rewriter.create<LLVM::InsertValueOp>(loc, memref, payload, 3);
+
+    // Previous values almost always are known at compile time, and inserting
+    // dynamic values into the struct after all statically know values leads to a
+    // better canonicalization and cleaner final LLVM IR.
+    if (desc.has_value()) {
+        Value offset = (memref_offset == ShapedType::kDynamic)
+                           ? desc->offset(rewriter, loc)
+                           : rewriter.create<LLVM::ConstantOp>(loc, i64(memref_offset)).getResult();
+        auto ptr = LLVM::LLVMPointerType::get(rewriter.getContext());
+        Value data = rewriter.create<LLVM::GEPOp>(loc, ptr, memref_ty.getElementType(),
+                                                  desc->alignedPtr(rewriter, loc), offset);
+        memref = rewriter.create<LLVM::InsertValueOp>(loc, memref, data, 2);
+    }
+
+    return memref;
+}
 
 } // namespace
 
