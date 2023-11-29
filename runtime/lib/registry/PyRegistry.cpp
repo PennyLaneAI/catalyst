b2a37b60b39fdeaaf7351b595b8772d1c0f89e9e
Erick <erick.ochoalopez@xanadu.ai>
2023-11-29 16:40:02 -0500

    Add build commands.

--- runtime/lib/registry/PyRegistry.cpp
+++ runtime/lib/registry/PyRegistry.cpp
@@ -6,7 +6,7 @@ int add(int i, int j) {
 	return i + j;
 }
 
-PYBIND11_MODULE(PyRegistry, m) {
+PYBIND11_MODULE(pyregistry, m) {
 	m.doc() = "pybind11 example plugin"; // optional module docstring.
 	m.def("add", &add, "A function that adds two numbers");
 }
