48e1a84c7ca09691b361ea8b5a1af0b3af8dbae1
Erick <erick.ochoalopez@xanadu.ai>
2023-12-14 13:03:35 -0500

    Remove func.func to async.func

--- mlir/lib/Catalyst/Transforms/QnodeToAsyncPatterns.cpp
+++ mlir/lib/Catalyst/Transforms/QnodeToAsyncPatterns.cpp
@@ -27,59 +27,6 @@ using namespace mlir;
 
 namespace catalyst {
 
-struct FuncOpToAsyncOPRewritePattern : public mlir::OpRewritePattern<func::FuncOp> {
-    using mlir::OpRewritePattern<func::FuncOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(func::FuncOp op,
-                                        mlir::PatternRewriter &rewriter) const override
-    {
-        if (op->hasAttrOfType<UnitAttr>("qnode")) {
-            FunctionType opType = op.getFunctionType();
-
-            auto inputTypes = opType.getInputs();
-            auto resultTypes = opType.getResults();
-
-            // Convert results to async values
-            SmallVector<Type> asyncResultTypes;
-            for (Type resultType : resultTypes) {
-                Type asyncType = async::ValueType::get(resultType);
-                asyncResultTypes.push_back(asyncType);
-            }
-            FunctionType asyncFunctionType = FunctionType::get(op.getContext(), /*inputs=*/
-                                                               inputTypes,
-                                                               /*outputs=*/asyncResultTypes);
-            StringRef nameOp = op.getName();
-            auto asyncFunc = rewriter.create<async::FuncOp>(op.getLoc(), nameOp, asyncFunctionType);
-            asyncFunc.setPrivate();
-            // Create the block of the function
-            Block *asyncFuncBody = asyncFunc.addEntryBlock();
-
-            auto asyncFuncBlockArgs = asyncFuncBody->getArguments();
-            rewriter.setInsertionPointToEnd(asyncFuncBody);
-
-            // Merge the two blocks and delete the first one
-            Region *body = &op.getFunctionBody();
-            Block *originalBlock = &body->front();
-            Operation *originalTerminator = originalBlock->getTerminator();
-            rewriter.mergeBlocks(originalBlock, asyncFuncBody, asyncFuncBlockArgs);
-            // Replace the terminator with async return
-            rewriter.create<async::ReturnOp>(op.getLoc(), originalTerminator->getResultTypes(),
-                                             originalTerminator->getOperands());
-            rewriter.eraseOp(originalTerminator);
-
-            // Removing the function_type attribute as we don't want that overwritten.
-            op->removeAttr("function_type");
-            auto oldAttrs = op->getAttrs();
-            asyncFunc->setAttrs(oldAttrs);
-
-            rewriter.replaceOp(op, asyncFunc);
-
-            return success();
-        }
-        return failure();
-    }
-};
-
 struct CallOpToAsyncOPRewritePattern : public mlir::OpRewritePattern<func::CallOp> {
     using mlir::OpRewritePattern<func::CallOp>::OpRewritePattern;
 
@@ -125,7 +72,6 @@ struct CallOpToAsyncOPRewritePattern : public mlir::OpRewritePattern<func::CallO
 
 void populateQnodeToAsyncPatterns(RewritePatternSet &patterns)
 {
-    patterns.add<catalyst::FuncOpToAsyncOPRewritePattern>(patterns.getContext(), 1);
     patterns.add<catalyst::CallOpToAsyncOPRewritePattern>(patterns.getContext(), 1);
 }
 
