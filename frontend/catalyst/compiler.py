48e51d81d8150fec40fbfc34106711bcc60a9d5d
Romain Moyard <rmoyard@gmail.com>
2024-01-10 14:56:41 -0500

    Update module ctx Jax

--- frontend/catalyst/compiler.py
+++ frontend/catalyst/compiler.py
@@ -116,7 +116,6 @@ HLO_LOWERING_PASS = (
         "func.func(hlo-legalize-to-linalg)",
         "func.func(mhlo-legalize-to-std)",
         "convert-to-signless",
-        "func.func(scalarize)",
         "canonicalize",
         "scatter-lowering",
         "hlo-custom-call-lowering",
