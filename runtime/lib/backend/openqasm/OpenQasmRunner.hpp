754a2808aaad24f285dc0b6d126a057ca37cb31f
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-01-17 17:40:06 -0500

    Enable LLVM ZSTD

--- runtime/lib/backend/openqasm/OpenQasmRunner.hpp
+++ runtime/lib/backend/openqasm/OpenQasmRunner.hpp
@@ -114,9 +114,7 @@ struct BraketRunner : public OpenQasmRunner {
         using namespace py::literals;
 
         RT_FAIL_IF(!Py_IsInitialized(), "The Python interpreter is not initialized");
-        py::module sys = py::module::import("sys");
-        py::print("Rashid -> Printing Python executable");
-        py::print(sys.attr("executable"));
+
         auto locals = py::dict("circuit"_a = circuit, "braket_device"_a = device,
                                "kwargs"_a = kwargs, "shots"_a = shots, "msg"_a = "");
 
