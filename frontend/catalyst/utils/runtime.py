29aee8857362a9ad6c19d8e20b3d8b47fd3d0d5d
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-01-26 11:19:35 -0500

    CodeCoverage

--- frontend/catalyst/utils/runtime.py
+++ frontend/catalyst/utils/runtime.py
@@ -266,8 +266,9 @@ def extract_backend_info(device):
     if dname == "braket.local.qubit":  # pragma: no cover
         device_kwargs["device_type"] = dname
         device_kwargs["backend"] = (
+            # pylint: disable=protected-access
             device._device._delegate.DEVICE_ID
-        )  # pylint: disable=protected-access
+        )
     elif dname == "braket.aws.qubit":  # pragma: no cover
         device_kwargs["device_type"] = dname
         device_kwargs["device_arn"] = device._device._arn  # pylint: disable=protected-access
