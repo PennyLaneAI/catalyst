b7401089212e3c70868b12ab9abc7df38bd25b7a
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-02-01 19:05:05 -0500

    Copyright and docstrings.

--- frontend/catalystcuda/__init__.py
+++ frontend/catalystcuda/__init__.py
@@ -1,21 +1,41 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module contains a CudaQDevice and the qjit
+entry point.
+"""
+
 from pathlib import Path
 
 import pennylane as qml
 
 from catalyst import pennylane_extensions
+from catalyst.compilation_pipelines import qjit_cuda
 from catalyst.utils.contexts import EvaluationContext
 
 
 def qjit(fn=None):
-    from catalyst.compilation_pipelines import qjit_cuda
-
+    """Entry point to qjit_cuda."""
     return qjit_cuda(fn)
 
 
 class CudaQDevice(qml.QubitDevice):
+    """CudaQ Device."""
 
     name = "CudaQ Device"
     short_name = "cudaq"
+    # TODO: Once 0.35 is released, remove -dev suffix.
     pennylane_requires = "0.35.0-dev"
     version = "1.0"
     author = "Catalyst authors wrote this wrapper for CUDA Quantum"
@@ -40,9 +60,3 @@ class CudaQDevice(qml.QubitDevice):
     def apply(self, operations, **kwargs):
         """Unused"""
         raise RuntimeError("We are not applying operation by operation.")
-
-
-__all__ = (
-    "qjit",
-    "CudaQDevice",
-)
