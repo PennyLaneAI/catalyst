f945ecea49a330b616786edad10b9b7246111f0e
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2024-02-20 15:53:40 -0500

    Update test to not depend on initial `JAX_DYNAMIC_SHAPES` setting

--- frontend/test/pytest/test_jax_config.py
+++ frontend/test/pytest/test_jax_config.py
@@ -7,7 +7,7 @@ def test_transient_jax_config():
     """Test that the ``transient_jax_config()`` context manager updates and
     restores the value of the JAX dynamic shapes option.
     """
-    assert jax.config.jax_dynamic_shapes is False  # type: ignore
+    jax.config.update("jax_dynamic_shapes", False)
 
     with transient_jax_config():
         assert jax.config.jax_dynamic_shapes is True  # type: ignore
