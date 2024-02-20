e1add21f173a9577f5e661f8ebb320d426091593
Jason Selby <jason.selby@xanadu.ai>
2024-02-20 18:12:19 -0500

    Update formatting

--- frontend/test/pytest/test_measurements_shots_results.py
+++ frontend/test/pytest/test_measurements_shots_results.py
@@ -535,7 +535,10 @@ class TestOtherMeasurements:
         )
 
         # qml.state
-        with pytest.warns(UserWarning, match="Requested state or density matrix with finite shots; the returned state information is analytic and is unaffected by sampling. To silence this warning, set shots=None on the device."):
+        with pytest.warns(
+            UserWarning,
+            match="Requested state or density matrix with finite shots; the returned state information is analytic and is unaffected by sampling. To silence this warning, set shots=None on the device.",
+        ):
             assert np.allclose(result[5], expected(x, qml.state()))
 
 
