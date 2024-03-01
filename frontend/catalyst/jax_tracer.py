20cc3ed4888b5fefd82180b876d484945261d994
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-03-01 13:39:55 -0500

    Unpack 1-element tuple returned by transforms.

--- frontend/catalyst/jax_tracer.py
+++ frontend/catalyst/jax_tracer.py
@@ -713,7 +713,8 @@ def apply_transform(transform_program, tape, flat_results):
 
     if is_program_transformed:
         is_valid_for_batch = is_transform_valid_for_batch_transforms(tape, flat_results)
-        tapes, post_processing = transform_program([tape])
+        tapes, _post_processing = qnode.transform_program([tape])
+        post_processing = lambda r: _post_processing(r)[0]
         if not is_valid_for_batch and len(tapes) > 1:
             msg = "Multiple tapes are generated, but each run might produce different results."
             raise CompileError(msg)
