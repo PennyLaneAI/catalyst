effe08563d3bc81a338afd412a39c09aedf450e5
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-03-07 10:55:02 -0500

    Style

--- frontend/test/pytest/test_callback.py
+++ frontend/test/pytest/test_callback.py
@@ -1,10 +1,25 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test callbacks"""
+
 import pennylane as qml
-import catalyst
 from catalyst.pennylane_extensions import callback
-import pytest
 
 
-def test_callback_print(capsys):
+def test_callback_no_returns_no_params(capsys):
+    """Test callback no parameters no returns"""
+
     def my_callback():
         print("Hello erick")
 
@@ -15,4 +30,4 @@ def test_callback_print(capsys):
 
     foo()
     captured = capsys.readouterr()
-    assert captured.out == "Hello erick\n"
+    assert captured.out.strip() == "Hello erick"
