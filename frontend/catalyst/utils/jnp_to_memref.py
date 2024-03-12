7367abca93daf2aaad34043cf64c170e8bee32eb
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-03-12 14:42:21 -0400

    Add support for translating ShapedArray's to ctypes unintialized values

---
+++ frontend/catalyst/utils/jnp_to_memref.py
@@ -0,0 +1,45 @@
+#  This file is essentially a re-implementation of np_to_memref.py
+#  But we make sure that all the functions also work with JAX's numpy.
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#  This file contains functions to convert between Memrefs and NumPy arrays and vice-versa.
+
+import ctypes
+
+from jax import numpy as jnp
+from jax.core import ShapedArray
+from mlir_quantum.runtime import as_ctype
+from mlir_quantum.runtime import (
+    get_ranked_memref_descriptor as mlir_get_ranked_memref_descriptor,
+)
+from mlir_quantum.runtime import (
+    make_nd_memref_descriptor,
+    make_zero_d_memref_descriptor,
+)
+
+
+def get_ranked_memref_descriptor_from_shaped_array(array: ShapedArray):
+    """Get a ranked memref descriptor from a shaped array.
+
+    Unlike MLIR's implementation, all values are left uninitialized.
+    This is because the values are not yet known. We only have a description
+    of the type.
+    """
+
+    ctp = as_ctype(array.dtype)
+    if array.ndim == 0:
+        return make_zero_d_memref_descriptor(ctp)()
+
+    return make_nd_memref_descriptor(array.ndim, ctp)()
+
+
+def get_ranked_memref_descriptor(array):
+    """Wrapper around MLIR's get_ranked_memref_descriptor."""
+
+    if not isinstance(array, ShapedArray):
+        # If input is not ShapedArray, use default implementation.
+        return mlir_get_ranked_memref_descriptor(array)
+
+    return get_ranked_memref_descriptor_from_shaped_array(array)
---
+++ frontend/test/pytest/test_np_to_memref.py
@@ -0,0 +1,62 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests memref descriptor functions with JAX's ShapedArrays."""
+
+import ctypes
+
+import jax.numpy as jnp
+import numpy as np
+import pytest
+from jax.core import ShapedArray
+from mlir_quantum.runtime import (
+    C64,
+    C128,
+    F16,
+    as_ctype,
+    make_nd_memref_descriptor,
+    make_zero_d_memref_descriptor,
+)
+
+from catalyst.utils.jnp_to_memref import get_ranked_memref_descriptor
+
+
+@pytest.mark.parametrize(
+    "inp, exp",
+    [
+        (jnp.dtype(jnp.float64), ctypes.c_double),
+        (jnp.dtype(jnp.float32), ctypes.c_float),
+        (jnp.dtype(jnp.int64), ctypes.c_long),
+        (jnp.dtype(jnp.bool_), ctypes.c_bool),
+        (jnp.dtype(jnp.complex128), C128),
+        (jnp.dtype(jnp.complex64), C64),
+    ],
+)
+def test_as_ctype(inp, exp):
+    """Tests that JAX's dtypes behave the same as numpy's dtypes"""
+    obs = as_ctype(inp)
+    assert exp == obs
+
+
+@pytest.mark.parametrize(
+    "inp, exp",
+    [
+        (ShapedArray([], float), make_zero_d_memref_descriptor(ctypes.c_double)),
+        (ShapedArray([1], float), make_nd_memref_descriptor(1, ctypes.c_double)),
+        (ShapedArray([2,2], float), make_nd_memref_descriptor(2, ctypes.c_double)),
+    ],
+)
+def test_get_ranked_memref_descriptor(inp, exp):
+    """Tests that the structure has the expected fields."""
+    obs = get_ranked_memref_descriptor(inp)
+    assert exp._fields_ == obs._fields_
