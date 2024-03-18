581c2106caee2af01daa465ff0a51d51859850c9
Romain Moyard <rmoyard@gmail.com>
2024-03-18 11:39:02 -0400

    More

--- frontend/catalyst/oqc/oqc_device.py
+++ frontend/catalyst/oqc/oqc_device.py
@@ -1,3 +1,19 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the OQC device."""
+
 import pathlib
 
 from pennylane.devices import DefaultExecutionConfig, Device, ExecutionConfig
@@ -26,20 +42,22 @@ class OQCDevice(Device):
     def __init__(self, wires, backend, shots=1024, **kwargs):
         self._backend = backend
         _check_backend(backend=backend)
-        super().__init__(wires=wires, shots=shots)
+        super().__init__(wires=wires, shots=shots, **kwargs)
 
     @property
     def backend(self):
+        """Backend property of the device."""
         return self._backend
 
     def preprocess(
         self,
         execution_config: ExecutionConfig = DefaultExecutionConfig,
     ):
-        """This function defines the device transform program to be applied and an updated device configuration."""
+        """This function defines the device transform program to be applied and an
+        updated device configuration."""
         transform_program = TransformProgram()
-
-        # TODO: Add transforms (check wires, check shots, no sample, only commuting measurements, measurement from counts)
+        # TODO: Add transforms (check wires, check shots, no sample, only commuting measurements,
+        # measurement from counts)
         return transform_program, execution_config
 
     def execute(self, circuits, execution_config):
@@ -48,5 +66,6 @@ class OQCDevice(Device):
 
 
 def _check_backend(backend):
+    """Helper function to check the backend."""
     if backend not in BACKENDS:
         raise ValueError(f"The backend {backend} is not supported. Valid devices are {BACKENDS}")
