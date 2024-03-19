7cb68933f0cd687a0628fbf84b803b80838eefa6
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-03-19 13:37:26 -0400

    Pragma once

--- runtime/utils/Python.hpp
+++ runtime/utils/Python.hpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma once
+
 #include <mutex>
 
 #if __has_include("pybind11/embed.h")
