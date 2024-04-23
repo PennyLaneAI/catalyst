df3bc60f37e4433a6adf342709f424e88a8c0548
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-04-23 11:41:22 -0400

    Style

--- frontend/catalyst/callback/debug/implementation.py
+++ frontend/catalyst/callback/debug/implementation.py
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 Xanadu Quantum Technologies Inc.
+# Copyright 2024 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,10 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""debug.callback module"""
 
 from ..implementation import callback as base_callback
-
-"""debug.callback module"""
 
 
 def callback(callback_fn):
