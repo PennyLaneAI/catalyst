531404375b0b84885afe744a7dc460940a384dc6
Lillian <lillian542@gmail.com>
2024-05-09 10:51:44 -0400

    import _operator_decomposition_gen from PL instead of redefining

--- frontend/catalyst/preprocess.py
+++ frontend/catalyst/preprocess.py
@@ -16,6 +16,7 @@
 import jax
 import pennylane as qml
 from pennylane import transform
+from pennylane.devices.preprocess import _operator_decomposition_gen
 from pennylane.measurements import (
     CountsMP,
     ExpectationMP,
@@ -34,43 +35,6 @@ from catalyst.api_extensions.quantum_operators import Adjoint, MidCircuitMeasure
 from catalyst.jax_tracer import HybridOpRegion, has_nested_tapes
 from catalyst.tracing.contexts import EvaluationContext
 from catalyst.utils.exceptions import CompileError
-
-
-# identical to PL version except for error type, can be removed and imported from PL as soon as PR#5669 is merged
-def _operator_decomposition_gen(
-    op: qml.operation.Operator,
-    acceptance_function,
-    decomposer,
-    max_expansion=None,
-    current_depth=0,
-    name: str = "device",
-    error: Exception = RuntimeError,
-):
-    """A generator that yields the next operation that is accepted."""
-    max_depth_reached = False
-    if max_expansion is not None and max_expansion <= current_depth:  # pragma: no cover
-        max_depth_reached = True
-    if acceptance_function(op) or max_depth_reached:
-        yield op
-    else:
-        try:
-            decomp = decomposer(op)
-            current_depth += 1
-        except qml.operation.DecompositionUndefinedError as e:  # pragma: no cover
-            raise error(
-                f"Operator {op} not supported with {name} and does not provide a decomposition."
-            ) from e
-
-        for sub_op in decomp:
-            yield from _operator_decomposition_gen(
-                sub_op,
-                acceptance_function,
-                decomposer=decomposer,
-                max_expansion=max_expansion,
-                current_depth=current_depth,
-                name=name,
-                error=error,
-            )
 
 
 def catalyst_decomposer(op):
