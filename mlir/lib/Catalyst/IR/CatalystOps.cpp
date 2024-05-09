f8707aca112efd1352ab72d8a00005395091d055
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-05-09 14:19:39 -0400

    Add ActiveCallbackOp

--- mlir/lib/Catalyst/IR/CatalystOps.cpp
+++ mlir/lib/Catalyst/IR/CatalystOps.cpp
@@ -45,3 +45,13 @@ void InactiveCallbackOp::getEffects(
     effects.emplace_back(mlir::MemoryEffects::Write::get());
     effects.emplace_back(mlir::MemoryEffects::Read::get());
 }
+
+void ActiveCallbackOp::getEffects(
+    llvm::SmallVectorImpl<mlir::SideEffects::EffectInstance<mlir::MemoryEffects::Effect>> &effects)
+{
+    // Assume all effects
+    effects.emplace_back(mlir::MemoryEffects::Allocate::get());
+    effects.emplace_back(mlir::MemoryEffects::Free::get());
+    effects.emplace_back(mlir::MemoryEffects::Write::get());
+    effects.emplace_back(mlir::MemoryEffects::Read::get());
+}
