e363f84946cf556d8bc98c07d8ccd7f650eab067
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-05-10 17:38:57 -0400

    Use all parameters.

--- mlir/lib/Catalyst/Transforms/SpecializeActiveCallbackPass.cpp
+++ mlir/lib/Catalyst/Transforms/SpecializeActiveCallbackPass.cpp
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/FunctionCallUtils.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #include "Catalyst/IR/CatalystOps.h"
@@ -30,6 +32,70 @@ std::string getSpecializedName(ActiveCallbackOp op)
     return "active_callback_" + id;
 }
 
+LLVM::LLVMFuncOp lookupOrDeclareInactiveCallback(ActiveCallbackOp op, PatternRewriter &rewriter)
+{
+    std::string name = "inactive_callback";
+    auto moduleOp = op->getParentOfType<ModuleOp>();
+    auto maybeFuncOp = moduleOp.lookupSymbol<LLVM::LLVMFuncOp>(name);
+    if (maybeFuncOp) {
+        return maybeFuncOp;
+    }
+
+    auto ctx = rewriter.getContext();
+    Type i64 = rewriter.getI64Type();
+
+    auto point = rewriter.saveInsertionPoint();
+    rewriter.setInsertionPointToStart(moduleOp.getBody());
+    bool isVarArg = true;
+    Type voidType = LLVM::LLVMVoidType::get(ctx);
+    LLVM::LLVMFuncOp inactiveCallback = mlir::LLVM::lookupOrCreateFn(
+        moduleOp, name, {/*args=*/i64, i64, i64}, /*ret_type=*/voidType, isVarArg);
+    // inactiveCallback.setPrivate();
+    rewriter.restoreInsertionPoint(point);
+    return inactiveCallback;
+}
+
+func::FuncOp lookupOrCreateSpecialized(ActiveCallbackOp op, PatternRewriter &rewriter)
+{
+    auto name = getSpecializedName(op);
+    auto moduleOp = op->getParentOfType<ModuleOp>();
+    auto maybeFuncOp = moduleOp.lookupSymbol<func::FuncOp>(name);
+    if (maybeFuncOp) {
+        return maybeFuncOp;
+    }
+
+    Type i64 = rewriter.getI64Type();
+    auto ctx = rewriter.getContext();
+    SmallVector<Type> inputs = {i64, i64, i64};
+    for (auto input : op.getInputs()) {
+        inputs.push_back(input.getType());
+    }
+
+    auto funcTy = FunctionType::get(ctx, inputs, {});
+    auto loc = op.getLoc();
+    func::FuncOp funcSpecialized = nullptr;
+
+    {
+        OpBuilder::InsertionGuard guard(rewriter);
+        rewriter.setInsertionPointToStart(moduleOp.getBody());
+        funcSpecialized = rewriter.create<func::FuncOp>(loc, name, funcTy);
+    }
+
+    Block *entryBlock = funcSpecialized.addEntryBlock();
+    rewriter.setInsertionPointToStart(entryBlock);
+
+    SmallVector<Value> args;
+    for (auto arg : entryBlock->getArguments()) {
+        args.push_back(arg);
+    }
+
+    auto inactive = lookupOrDeclareInactiveCallback(op, rewriter);
+    rewriter.create<LLVM::CallOp>(loc, inactive, args);
+    rewriter.create<func::ReturnOp>(loc);
+
+    return funcSpecialized;
+}
+
 struct AddDeclarationToModulePattern : public OpRewritePattern<ActiveCallbackOp> {
     using OpRewritePattern<ActiveCallbackOp>::OpRewritePattern;
 
@@ -44,6 +110,7 @@ LogicalResult AddDeclarationToModulePattern::match(ActiveCallbackOp op) const
 
 void AddDeclarationToModulePattern::rewrite(ActiveCallbackOp op, PatternRewriter &rewriter) const
 {
+    lookupOrCreateSpecialized(op, rewriter);
     auto specializedName = getSpecializedName(op);
     auto specializedNameAttr = rewriter.getStringAttr(specializedName);
     rewriter.updateRootInPlace(op, [&] {
