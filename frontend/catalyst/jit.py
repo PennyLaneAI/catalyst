e729752ecb7e40f8f0fbda0b55275d9f80c006e3
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-05-28 16:39:48 -0400

    Added QJIT.jaxpr, .mlir and .qir to the QJIT API class documentation alongside QJIT.original_function.

--- frontend/catalyst/jit.py
+++ frontend/catalyst/jit.py
@@ -450,7 +450,11 @@ class QJIT:
         compile_options (CompileOptions): compilation options to use
 
     :ivar original_function: This attribute stores `fn`, the quantum or classical function
-                             to compile, as is, without any modifications
+                             object to compile, as is, without any modifications
+    :ivar jaxpr: This attribute stores the Jaxpr compiled from the function as a string. 
+    :ivar mlir: This attribute stores the MLIR compiled from the function as a string. 
+    :ivar qir: This attribute stores the QIR in LLVM IR form compiled from the function as a string. 
+    
     """
 
     def __init__(self, fn, compile_options):
