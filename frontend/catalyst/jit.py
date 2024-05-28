c3c20eaa90cfc2c60fa8fa42878d38d2471eb207
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-05-28 11:53:18 -0400

    Added `original_function` as an instance variable in the documentation of `qjit.py/class QJIT`

--- frontend/catalyst/jit.py
+++ frontend/catalyst/jit.py
@@ -448,6 +448,8 @@ class QJIT:
     Args:
         fn (Callable): the quantum or classical function to compile
         compile_options (CompileOptions): compilation options to use
+
+    :ivar original_function: This attribute stores `fn`, the quantum or classical function to compile, as is, without any modifications
     """
 
     def __init__(self, fn, compile_options):
