78c3608553de16fc623651a727619d473bc16f17
Sergei Mironov <sergei.mironov@xanadu.ai>
2024-05-29 09:36:11 +0000

    Fix device name

--- frontend/catalyst/device/qjit_device.py
+++ frontend/catalyst/device/qjit_device.py
@@ -490,12 +490,13 @@ def validate_device_capabilities(
         # Lightning-kokkis might support C(GlobalPhase) in Python, but not in C++. We remove this
         # gate before calling the validation.
         # See https://github.com/PennyLaneAI/pennylane-lightning/pull/642#discussion_r1535478642
-        if device_name == "lightning-kokkos":
+        if device_name == "lightning.kokkos":
             device_gates = device_gates - {"C(GlobalPhase)"}
         spec_gates = filter_out_adjoint(set.union(native, matrix, decomposable))
         if device_gates != spec_gates:
             raise CompileError(
-                "Gates in qml.device.operations and specification file do not match.\n"
+                "Gates in qml.device.operations and specification file do not match for "
+                f"\"{device_name}\".\n"
                 f"Gates that present only in the device: {device_gates - spec_gates}\n"
                 f"Gates that present only in spec: {spec_gates - device_gates}\n"
             )
