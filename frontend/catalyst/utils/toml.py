3821801f95898061354bbd36e1023c5fb4fa8df3
Lillian <lillian542@gmail.com>
2024-05-30 11:55:39 -0400

    put observable_props back in patch_schema1_collections

--- frontend/catalyst/utils/toml.py
+++ frontend/catalyst/utils/toml.py
@@ -256,7 +256,7 @@ def get_operation_properties(config_props: dict) -> OperationProperties:
 
 
 def patch_schema1_collections(
-    config, _device_name, native_gate_props, matrix_decomp_props, decomp_props
+    config, _device_name, native_gate_props, matrix_decomp_props, decomp_props, observable_props
 ):  # pylint: disable=too-many-arguments, too-many-branches
     """For old schema1 config files we deduce some information which was not explicitly encoded."""
 
@@ -359,6 +359,7 @@ def load_device_capabilities(
             native_gate_props,
             matrix_decomp_props,
             decomp_props,
+            observable_props,
         )
 
     return DeviceCapabilities(
