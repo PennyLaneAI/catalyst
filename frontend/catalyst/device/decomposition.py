f83b2c397d97079f12e0fb557ee15d61a397ba5d
Lee James O'Riordan <mlxd@users.noreply.github.com>
2024-06-05 09:36:58 -0400

    Add Python-native logging to Catalyst frontend (#660)
    ### Before submitting
    
    Please complete the following checklist when submitting a PR:
    
    - [x] All new functions and code must be clearly commented and
    documented.
    
    - [x] Ensure that code is properly formatted by running `make format`.
    The latest version of black and `clang-format-14` are used in CI/CD to
    check formatting.
    
    - [ ] All new features must include a unit test.
    Integration and frontend tests should be added to
    [`frontend/test`](../frontend/test/),
    Quantum dialect and MLIR tests should be added to
    [`mlir/test`](../mlir/test/), and
    Runtime tests should be added to [`runtime/tests`](../runtime/tests/).
    
    When all the above are checked, delete everything above the dashed
    line and fill in the pull request template.
    
    
    ------------------------------------------------------------------------------------------------------------
    
    **Context:** PennyLane's [logging
    support](https://docs.pennylane.ai/en/stable/introduction/logging.html)
    allows developers to explore the execution pipeline end-to-end, while
    tying into the Python-native logging framework. This has the advantage
    of being enterprise ready, consumable by a variety of log sinks, and
    unifies/removes the need for adding `print` statements throughout a
    code-base, with all control to be handled by the Python [language
    standard libraries](https://docs.python.org/3/library/logging.html).
    This PR aims to unify Catalyst to follow the same design as PennyLane to
    support logging with the same unified configuration, allowing ease of
    examining of function entry points at the `DEBUG` level, and a custom
    `TRACE` method for expanding functions passed as arguments.
    
    **Description of the Change:** Adds preliminary support to a variety of
    Catalyst frontend public API entry-points. Any workload using
    `qml.logging.enable_logging()` allows the function entry data to be
    streamed to the pre-configured log-handlers from PennyLane.
    
    **Benefits:** More easily allows time-order and standardised data
    formatting for consumption with complex application execution,
    debugging, and end-to-end validation of call-points.
    
    **Possible Drawbacks:** 
    
    **Related GitHub Issues:**
    https://github.com/PennyLaneAI/pennylane/pull/5528
    
    ---------
    
    Co-authored-by: David Ittah <dime10@users.noreply.github.com>

--- frontend/catalyst/device/decomposition.py
+++ frontend/catalyst/device/decomposition.py
@@ -16,7 +16,7 @@
 This module contains the decomposition functions to pre-process tapes for
 compilation & execution on devices.
 """
-
+import logging
 from functools import partial
 
 import jax
@@ -37,9 +37,13 @@ from pennylane.tape.tape import (
 
 from catalyst.api_extensions.quantum_operators import QCtrl
 from catalyst.jax_tracer import HybridOpRegion, has_nested_tapes
+from catalyst.logging import debug_logger
 from catalyst.tracing.contexts import EvaluationContext
 from catalyst.utils.exceptions import CompileError
 from catalyst.utils.toml import DeviceCapabilities
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 def catalyst_decomposer(op, capabilities: DeviceCapabilities):
@@ -54,6 +58,7 @@ def catalyst_decomposer(op, capabilities: DeviceCapabilities):
 
 
 @transform
+@debug_logger
 def catalyst_decompose(
     tape: qml.tape.QuantumTape,
     ctx,
@@ -132,6 +137,7 @@ def _decompose_nested_tapes(op, ctx, stopping_condition, capabilities, max_expan
 
 
 @transform
+@debug_logger
 def decompose_ops_to_unitary(tape, convert_to_matrix_ops):
     r"""Quantum transform that decomposes operations to unitary given a list of operations name.
 
@@ -173,6 +179,7 @@ def catalyst_acceptance(op: qml.operation.Operator, operations) -> bool:
 
 
 @transform
+@debug_logger
 def measurements_from_counts(tape):
     r"""Replace all measurements from a tape with a single count measurement, it adds postprocessing
     functions for each original measurement.
