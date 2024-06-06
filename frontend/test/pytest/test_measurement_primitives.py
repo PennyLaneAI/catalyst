5f407c283be6ec5421c2c1351763a576dd1e06b5
albi3ro <chrissie.c.l@gmail.com>
2024-06-06 14:33:53 -0400

    test docstring

--- frontend/test/pytest/test_measurement_primitives.py
+++ frontend/test/pytest/test_measurement_primitives.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+This file contains a couple of tests for the capture of measurement primitives into jaxpr.
+"""
 import jax
 
 from catalyst.jax_primitives import (
