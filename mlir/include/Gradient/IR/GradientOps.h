8c6c8b70681d1a9720a511518adb18cd66d3f9c2
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-06-10 15:35:00 -0400

    format

--- mlir/include/Gradient/IR/GradientOps.h
+++ mlir/include/Gradient/IR/GradientOps.h
@@ -14,16 +14,15 @@
 
 #pragma once
 
+#include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/SymbolTable.h"
 #include "mlir/Interfaces/CallInterfaces.h"
-#include "mlir/Bytecode/BytecodeOpInterface.h"
 
 #include "Gradient/IR/GradientInterfaces.h"
 
 #define GET_OP_CLASSES
 #include "Gradient/IR/GradientOps.h.inc"
-
