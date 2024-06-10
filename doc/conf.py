640a6a445869d980ee8247040280fdd066a7dee7
Josh Izaac <josh146@gmail.com>
2024-06-10 10:14:29 -0400

    Show full module path in documentation

--- doc/conf.py
+++ doc/conf.py
@@ -217,7 +217,7 @@ project = "Catalyst"
 copyright = "2023, Xanadu Quantum Technologies"
 author = "Xanadu Inc."
 
-add_module_names = False
+add_module_names = True
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
