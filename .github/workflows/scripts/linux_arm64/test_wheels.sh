f5bb66707a8f0fa4dea0bc45ffdc5b85239250eb
Raul Torres <raul_torres80@hotmail.com>
2024-06-11 13:18:37 -0500

    Move Catalyst test wheels bash string to a script

---
+++ .github/workflows/scripts/linux_arm64/test_wheels.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e -x
+cd /catalyst
+
+# Process args
+export GCC_VERSION=$1
+export PYTHON_VERSION=$2
+export PYTHON_SUBVERSION=$3
+
+# Install system dependencies
+dnf update -y 
+dnf install -y libzstd-devel gcc-toolset-${GCC_VERSION} python39 python39-devel gcc
+dnf clean all -y
+
+# Make GCC the default compiler
+source /opt/rh/gcc-toolset-${GCC_VERSION}/enable -y 
+export C_COMPILER=/opt/rh/gcc-toolset-${GCC_VERSION}/root/usr/bin/gcc 
+export CXX_COMPILER=/opt/rh/gcc-toolset-${GCC_VERSION}/root/usr/bin/g++
+
+# Set the right Python interpreter
+rm -rf /usr/bin/python3
+ln -s /opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/bin/python3 /usr/bin/python3
+export PYTHON=/usr/bin/python3
+
+# Set llvm-symbolizer
+ls -la /catalyst/llvm-build/bin/llvm-symbolizer
+export LLVM_SYMBOLIZER_PATH=/catalyst/llvm-build/bin/llvm-symbolizer
+
+# Add LLVM, Python and GCC to the PATH env var
+export PATH=/catalyst/llvm-build/bin:/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/bin:/opt/rh/gcc-toolset-${GCC_VERSION}/root/usr/bin:$PATH
+
+# Install python dependencies
+/usr/bin/python3 -m pip install numpy pybind11 PyYAML cmake ninja pytest pytest-xdist autoray oqc-qcaas-client PennyLane-Lightning-Kokkos 'amazon-braket-pennylane-plugin>1.27.1'
+
+# Install Catalyst wheel
+/usr/bin/python3 -m pip install /catalyst/dist/*.whl --extra-index-url https://test.pypi.org/simple
+
+# Test Catalyst
+/usr/bin/python3 -m pytest -v /catalyst/frontend/test/pytest -n auto
+/usr/bin/python3 -m pytest -v /catalyst/frontend/test/pytest --backend="lightning.kokkos" -n auto
+/usr/bin/python3 -m pytest /catalyst/frontend/test/async_tests
+/usr/bin/python3 -m pytest -v /catalyst/frontend/test/pytest --runbraket=LOCAL -n auto
+/usr/bin/python3 -m pytest /catalyst/frontend/test/test_oqc/oqc -n auto
