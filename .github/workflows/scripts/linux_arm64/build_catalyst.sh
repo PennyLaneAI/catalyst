d99402182a68ded6b7eb9ceab9c9e1cd902008bf
Raul Torres <raul_torres80@hotmail.com>
2024-06-12 19:16:42 -0500

    [NFC] Add spacing to scripts

--- .github/workflows/scripts/linux_arm64/build_catalyst.sh
+++ .github/workflows/scripts/linux_arm64/build_catalyst.sh
@@ -15,7 +15,8 @@ export PYTHON_ALTERNATIVE_VERSION=$4
 # Install system dependencies
 dnf update -y 
 dnf install -y libzstd-devel gcc-toolset-${GCC_VERSION} python${PYTHON_ALTERNATIVE_VERSION} python${PYTHON_ALTERNATIVE_VERSION}-devel
-dnf clean all -y 
+dnf clean all -y
+
 # Make GCC the default compiler
 source /opt/rh/gcc-toolset-${GCC_VERSION}/enable -y 
 export C_COMPILER=/opt/rh/gcc-toolset-${GCC_VERSION}/root/usr/bin/gcc 
