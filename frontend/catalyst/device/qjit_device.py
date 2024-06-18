05cad43919ff734e41a5ff232a8ba0ad0a66dce5
Lillian <lillian542@gmail.com>
2024-06-18 12:04:19 -0400

    remove accidental duplication from merge

--- frontend/catalyst/device/qjit_device.py
+++ frontend/catalyst/device/qjit_device.py
@@ -469,14 +469,6 @@ class QJITDeviceNewAPI(qml.devices.Device):
 
         if config.gradient_method is not None:
             program.add_transform(verify_no_state_variance_returns)
-
-        if config.gradient_method == "adjoint":
-            program.add_transform(validate_observables_adjoint_diff, qjit_device=self)
-        elif config.gradient_method == "parameter-shift":
-            program.add_transform(validate_observables_parameter_shift)
-
-        if config.gradient_method is not None:
-            program.add_transform(verify_no_state_variance_returns)
         if config.gradient_method == "adjoint":
             program.add_transform(validate_observables_adjoint_diff, qjit_device=self)
         elif config.gradient_method == "parameter-shift":
