22a1324dcc586eab89db2d1ff9b189372eedd6d7
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-06-28 09:56:23 -0400

    Refactoring out the boilerplate patterns for all the gates

--- mlir/lib/Quantum/Transforms/ChainedSelfInversePatterns.cpp
+++ mlir/lib/Quantum/Transforms/ChainedSelfInversePatterns.cpp
@@ -27,8 +27,12 @@ using namespace catalyst::quantum;
 
 namespace {
 
-// We check if the operation and it's parent are a pair of self-inverse operations.
-// If so, replace op with it's "grandparent".
+llvm::SmallVector<StringRef> SelfInverseGates{
+    "PauliX", "PauliY", "PauliZ", "Hadamard", "CNOT", "CY", "CZ", "SWAP", "Toffoli",
+};
+
+// We check if the operation and its parent are a pair of self-inverse operations.
+// If so, replace op with its "grandparent".
 // e.g.
 //    %1 = (a qubit value produced from somewhere)
 //    %out_qubits_1 = quantum.custom "PauliZ"() %1 : !quantum.bit
@@ -37,120 +41,56 @@ namespace {
 // The Value %out_qubits_2 can be replaced by the Value %1 (which is its grandparent) in all uses
 
 mlir::LogicalResult matchAndRewriteImpl(CustomOp op, mlir::PatternRewriter &rewriter,
-                                        StringRef OpName)
+                                        SmallVector<StringRef> SelfInverseGates)
 {
-    LLVM_DEBUG(dbgs() << "Simplifying the following " << OpName << " operation:\n" << op << "\n");
-    if (op.getGateName().str() != OpName)
-        return failure();
+    for (StringRef OpName : SelfInverseGates) {
+        LLVM_DEBUG(dbgs() << "Simplifying the following " << OpName << " operation:\n"
+                          << op << "\n");
+        if (op.getGateName().str() != OpName)
+            continue;
 
-    ValueRange qbs = op.getInQubits();
-    auto parentOp = dyn_cast<CustomOp>(qbs[0].getDefiningOp());
+        ValueRange qbs = op.getInQubits();
+        auto parentOp = dyn_cast<CustomOp>(qbs[0].getDefiningOp());
 
-    if (parentOp == nullptr) {
-        return failure();
-    }
+        if (parentOp == nullptr) {
+            continue;
+        }
 
-    if (parentOp.getGateName().str() != OpName) {
-        return failure();
-    }
+        if (parentOp.getGateName().str() != OpName) {
+            continue;
+        }
 
-    // for multiple qubit gates, the wires need to be in order
-    // since the cancelled inverses need to have matching control/target wires
-    // e.g. The following pair of neighbouring CNOTs should NOT be eliminated
-    //    %out_qubits:2 = quantum.custom "CNOT"() %1, %2 : !quantum.bit, !quantum.bit
-    //    %out_qubits_0:2 = quantum.custom "CNOT"() %out_qubits#1, %out_qubits#0 : !quantum.bit,
-    //    !quantum.bit
-    if (qbs.size() > 1) {
+        // for multiple qubit gates, the wires need to be in order
+        // since the cancelled inverses need to have matching control/target wires
+        // e.g. The following pair of neighbouring CNOTs should NOT be eliminated
+        //    %out_qubits:2 = quantum.custom "CNOT"() %1, %2 : !quantum.bit, !quantum.bit
+        //    %out_qubits_0:2 = quantum.custom "CNOT"() %out_qubits#1, %out_qubits#0 : !quantum.bit,
+        //    !quantum.bit
+
+        bool failed = false; // we want to continue to the next gate to check for in the outer loop,
+                             // not this small loop
         for (size_t i = 0; i < qbs.size(); i++) {
             if ((parentOp.getOutQubits())[i] != qbs[i]) {
-                return failure();
+                failed = true;
             }
         }
-    }
+        if (failed) {
+            continue;
+        }
 
-    ValueRange simplifiedVal = parentOp.getInQubits();
-    rewriter.replaceOp(op, simplifiedVal);
-    return success();
+        ValueRange simplifiedVal = parentOp.getInQubits();
+        rewriter.replaceOp(op, simplifiedVal);
+        return success();
+    }
+    return failure();
 }
 
-struct ChainedPauliXOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
+struct ChainedSelfInverseOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
     using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
 
     mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
     {
-        return matchAndRewriteImpl(op, rewriter, "PauliX");
-    }
-};
-
-struct ChainedPauliYOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "PauliY");
-    }
-};
-
-struct ChainedPauliZOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "PauliZ");
-    }
-};
-
-struct ChainedHadamardOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "Hadamard");
-    }
-};
-
-struct ChainedCNOTOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "CNOT");
-    }
-};
-
-struct ChainedCYOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "CY");
-    }
-};
-
-struct ChainedCZOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "CZ");
-    }
-};
-
-struct ChainedSWAPOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "SWAP");
-    }
-};
-
-struct ChainedToffoliOpRewritePattern : public mlir::OpRewritePattern<CustomOp> {
-    using mlir::OpRewritePattern<CustomOp>::OpRewritePattern;
-
-    mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        return matchAndRewriteImpl(op, rewriter, "Toffoli");
+        return matchAndRewriteImpl(op, rewriter, SelfInverseGates);
     }
 };
 
@@ -161,15 +101,7 @@ namespace quantum {
 
 void populateSelfInversePatterns(RewritePatternSet &patterns)
 {
-    patterns.add<ChainedPauliXOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedPauliYOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedPauliZOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedHadamardOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedCNOTOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedCYOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedCZOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedSWAPOpRewritePattern>(patterns.getContext(), 1);
-    patterns.add<ChainedToffoliOpRewritePattern>(patterns.getContext(), 1);
+    patterns.add<ChainedSelfInverseOpRewritePattern>(patterns.getContext(), 1);
 }
 
 } // namespace quantum
