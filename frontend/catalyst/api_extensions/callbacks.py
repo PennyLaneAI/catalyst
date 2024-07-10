d380a70605226ce84bea0fd9e28576c7be5b6392
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-07-10 09:53:45 -0400

    pylint

--- frontend/catalyst/api_extensions/callbacks.py
+++ frontend/catalyst/api_extensions/callbacks.py
@@ -377,6 +377,7 @@ def pure_callback_impl(callback_fn: AnnotatedFunction):
     return CallbackWithPotentialCustomGrad(callback_fn)
 
 
+# pylint: disable-next=too-many-instance-attributes)
 class CallbackWithCustomGrad(AnnotatedFunction):
     """A callback with a custom grad"""
 
