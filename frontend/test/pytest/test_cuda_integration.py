2225af44612c2c2472ac924dfa10756e9dda48c9
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-07-12 13:30:19 -0400

    Do not skip tests

--- frontend/test/pytest/test_cuda_integration.py
+++ frontend/test/pytest/test_cuda_integration.py
@@ -238,7 +238,6 @@ class TestCudaQ:
         observed = cuda_compiled(jax.numpy.array([3.14, 0.0]))
         assert_allclose(expected, observed)
 
-    @pytest.mark.skipif("0.35" not in qml.version(), reason="Unsupported in pennylane version")
     def test_cuda_device_entry_point(self):
         """Test the entry point for SoftwareQQPP"""
 
@@ -258,7 +257,6 @@ class TestCudaQ:
         observed = cuda_compiled(3.14)
         assert_allclose(expected["a"], observed["a"])
 
-    @pytest.mark.skipif("0.35" not in qml.version(), reason="Unsupported in pennylane version")
     def test_cuda_device_entry_point_compiler(self):
         """Test the entry point for cudaq.qjit"""
 
