00c992c85d6b9c209bdf2c08e1687530e160737b
Tzung-Han Juang <tzunghan.juang@gmail.com>
2024-07-12 13:39:31 -0400

    Fix a missing argument for timer

--- mlir/lib/Driver/CompilerDriver.cpp
+++ mlir/lib/Driver/CompilerDriver.cpp
@@ -577,7 +577,7 @@ LogicalResult QuantumDriverMain(const CompilerOptions &options, CompilerOutput &
 
         if (options.lowerToLLVM) {
             llvmModule = timer::timer(translateModuleToLLVMIR, "translateModuleToLLVMIR",
-                                      /* add_endl */ false, *op, llvmContext, "LLVMDialectModule");
+                                      /* add_endl */ false, *op, llvmContext, "LLVMDialectModule", false);
             if (!llvmModule) {
                 CO_MSG(options, Verbosity::Urgent, "Failed to translate LLVM module\n");
                 return failure();
