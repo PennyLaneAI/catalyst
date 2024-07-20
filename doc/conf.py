c2c733d722c4557ff826c263f73d875ca37db52c
Josh Izaac <josh146@gmail.com>
2024-07-20 16:52:51 -0400

    Test MLIR lexer

--- doc/conf.py
+++ doc/conf.py
@@ -25,6 +25,7 @@ from sphinx.highlighting import lexers
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(""))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath("_ext"))
 sys.path.insert(0, os.path.join(os.getcwd(), "doc"))
 
