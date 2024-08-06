f624e6670bb3e9e1682e0dcfbd69a279de9df2fc
Tzung-Han Juang <tzunghan.juang@gmail.com>
2024-08-06 15:02:19 -0400

    Raise error if keep_intermediate is not True

--- frontend/catalyst/debug/compiler_functions.py
+++ frontend/catalyst/debug/compiler_functions.py
@@ -173,6 +173,8 @@ def get_pipeline_output(fn, pass_name):
     Returns:
         str: output ir from the target compiler pass
     """
+    if not fn.compiler.options.keep_intermediate:
+        raise RuntimeError("keep_intermediate must be set to True to get pipeline's output")
     if pass_name == "last":
         return fn.compiler.last_compiler_output.get_output_ir()
     return fn.compiler.get_output_of(pass_name)
