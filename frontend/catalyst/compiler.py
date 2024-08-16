4e9cc289d44eb1490583c6defca60bad0dae7866
Tzung-Han Juang <tzunghan.juang@gmail.com>
2024-08-16 14:36:22 -0400

    Comment out retired bufferization passes

--- frontend/catalyst/compiler.py
+++ frontend/catalyst/compiler.py
@@ -198,13 +198,13 @@ BUFFERIZATION_PASS = (
         "scf-bufferize",
         "convert-tensor-to-linalg",  # tensor.pad
         "convert-elementwise-to-linalg",  # Must be run before --arith-bufferize
-        "arith-bufferize",
+        #"arith-bufferize",
         "empty-tensor-to-alloc-tensor",
-        "func.func(bufferization-bufferize)",
-        "func.func(tensor-bufferize)",
+        #"func.func(bufferization-bufferize)",
+        #"func.func(tensor-bufferize)",
         "catalyst-bufferize",  # Must be run before -- func.func(linalg-bufferize)
-        "func.func(linalg-bufferize)",
-        "func.func(tensor-bufferize)",
+        #"func.func(linalg-bufferize)",
+        #"func.func(tensor-bufferize)",
         "quantum-bufferize",
         "func-bufferize",
         "func.func(finalizing-bufferize)",
