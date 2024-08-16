PennyLaneAI/catalyst

commit 9cb2e22969acb1f1a0097d89605f15e2789d42b7

[MLIR] Add type checking in JVPOp::verifySymbolUses (#1020)
**Context:** Add type checking in the JVP MLIR operation like we have in
the equivalent VJP operation.

**Description of the Change:** In `JVPOp::verifySymbolUses()`, gather up
the data types of the tangent parameters and check them one by one
against the data types of the corresponding callee input types, in a
similar manner as is currently done in `VJPOp::verifySymbolUses()`.

**Benefits:** The goal is to avoid triggering [this
assert](https://github.com/PennyLaneAI/catalyst/blob/6c0ed0b528119b78bc32172780350ff1bc760424/mlir/lib/Gradient/Utils/EinsumLinalgGeneric.cpp#L100)
by doing the type checking earlier and printing a more descriptive error
message.

**Possible Drawbacks:** None.

**Related GitHub Issues:**

[sc-48792]

---------

Co-authored-by: Romain Moyard <rmoyard@gmail.com>

mlir/lib/Gradient/IR/GradientOps.cpp
@@ -317,6 +317,28 @@ LogicalResult JVPOp::verifySymbolUses(SymbolTableCollection &symbolTable)
         }
     }
 
+    std::vector<Type> tanTypes;
+    {
+        auto tanOperands = OperandRange(
+            this->operand_begin() + callee.getFunctionType().getNumInputs(), this->operand_end());
+        for (auto c : tanOperands) {
+            tanTypes.push_back(c.getType());
+        }
+    }
+
+    auto calleeInputTypes = callee.getFunctionType().getInputs();
+
+    // Check that callee inputs have the same types as tangent inputs
+    for (size_t i = 0; i < tanTypes.size(); i++) {
+        auto tanType = tanTypes[i];
+        auto cIType = calleeInputTypes[i];
+        if (tanType != cIType) {
+            return this->emitOpError("callee input type does not match the tangent type")
+                   << " callee input " << i << " was expected to be of type " << tanType
+                   << " but got " << cIType;
+        }
+    }
+
     return success();
 }
 

mlir/test/Gradient/VerifierTest.mlir
@@ -268,6 +268,37 @@ gradient.jvp "fd" @measure(%cst0) tangents(%cst1) : (tensor<2xf64>, tensor<2xf64
 
 // -----
 
+func.func @foo(%arg0: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
+
+    %cst = stablehlo.constant dense<2.000000e+00> : tensor<f64>
+    %0 = stablehlo.multiply %cst, %arg0 : tensor<f64>
+    %1 = stablehlo.multiply %arg0, %arg0 : tensor<f64>
+    return %0, %1 : tensor<f64>, tensor<f64>
+
+}
+
+%cst0 = arith.constant dense<1.0> : tensor<f64>
+%cst1 = arith.constant dense<1.0> : tensor<f64>
+gradient.jvp "auto" @foo(%cst0) tangents(%cst1) : (tensor<f64>, tensor<f64>) -> (tensor<f64>, tensor<f64>, tensor<f64>, tensor<f64>)
+
+// -----
+
+func.func @foo(%arg0: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
+
+    %cst = stablehlo.constant dense<2.000000e+00> : tensor<f64>
+    %0 = stablehlo.multiply %cst, %arg0 : tensor<f64>
+    %1 = stablehlo.multiply %arg0, %arg0 : tensor<f64>
+    return %0, %1 : tensor<f64>, tensor<f64>
+
+}
+
+%cst0 = arith.constant dense<1.0> : tensor<f64>
+%cst1 = arith.constant dense<1> : tensor<i64>
+// expected-error@+1 {{callee input type does not match the tangent type}}
+gradient.jvp "auto" @foo(%cst0) tangents(%cst1) : (tensor<f64>, tensor<i64>) -> (tensor<f64>, tensor<f64>, tensor<f64>, tensor<f64>)
+
+// -----
+
 func.func @measure(%arg0: tensor<2xf64>) -> tensor<2xf64> {
 
     %c0 = arith.constant 0 : i64
@@ -300,6 +331,39 @@ func.func @measure(%arg0: tensor<2xf64>) -> tensor<2xf64> {
 %cst0 = arith.constant dense<[1.0, 0.0]> : tensor<2xf64>
 %cst1 = arith.constant dense<[1.0, 0.0]> : tensor<2xf64>
 gradient.vjp "fd" @measure(%cst0) cotangents(%cst1) {resultSegmentSizes = array<i32: 1, 1>} : (tensor<2xf64>, tensor<2xf64>) -> (tensor<2xf64>, tensor<2xf64>)
+
+// -----
+
+func.func @foo(%arg0: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
+
+    %cst = stablehlo.constant dense<2.000000e+00> : tensor<f64>
+    %0 = stablehlo.multiply %cst, %arg0 : tensor<f64>
+    %1 = stablehlo.multiply %arg0, %arg0 : tensor<f64>
+    return %0, %1 : tensor<f64>, tensor<f64>
+
+}
+
+%cst0 = arith.constant dense<1.0> : tensor<f64>
+%cst1 = arith.constant dense<1.0> : tensor<f64>
+%cst2 = arith.constant dense<1.0> : tensor<f64>
+gradient.vjp "auto" @foo(%cst0) cotangents(%cst1, %cst2) {resultSegmentSizes = array<i32: 2, 1>} : (tensor<f64>, tensor<f64>, tensor<f64>) -> (tensor<f64>, tensor<f64>, tensor<f64>)
+
+// -----
+
+func.func @foo(%arg0: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
+
+    %cst = stablehlo.constant dense<2.000000e+00> : tensor<f64>
+    %0 = stablehlo.multiply %cst, %arg0 : tensor<f64>
+    %1 = stablehlo.multiply %arg0, %arg0 : tensor<f64>
+    return %0, %1 : tensor<f64>, tensor<f64>
+
+}
+
+%cst0 = arith.constant dense<1.0> : tensor<f64>
+%cst1 = arith.constant dense<1> : tensor<i64>
+%cst2 = arith.constant dense<1> : tensor<i64>
+// expected-error@+1 {{callee result type does not match the cotangent type}}
+gradient.vjp "auto" @foo(%cst0) cotangents(%cst1, %cst2) {resultSegmentSizes = array<i32: 2, 1>} : (tensor<f64>, tensor<i64>, tensor<i64>) -> (tensor<f64>, tensor<f64>, tensor<f64>)
 
 // -----
 

commit 39438c9e01c0847573d6f131e2bc3c25d6e0509e

[MLIR] Fix JVP type-checking when used with argnum (#1030)
**Context:** Improving the type-checking in the JVP and VJP operations
at the MLIR level.

**Description of the Change:** This fixes a minor bug introduced in
#1020, which resulted in the JVP type-checking to be overly strict when
enforcing that the types of the primals and tangents be the same when
using a mixture of differentiable and non-differentiable parameters (as
specified by the `argnum` parameter). For example, in the code sample
below (taken from the [Catalyst JVP
documentation](https://docs.pennylane.ai/projects/catalyst/en/stable/code/api/catalyst.jvp.html)),
the MLIR would enforce that the parameter `n` be a float, even though it
is legal for it to be an integer:

```python
@qjit
@qml.qnode(qml.device("lightning.qubit", wires=2))
def circuit(n, params):
    qml.RX(params[n, 0], wires=n)
    qml.RY(params[n, 1], wires=n)
    return qml.expval(qml.PauliZ(1))

@qjit
def workflow(primals, tangents):
    return catalyst.jvp(circuit, [1, primals], [tangents], argnum=[1])
#                                 ^
#                                 Legal integer input for parameter `n`
```

This is now fixed by comparing only the types of the differentiable
parameters (whose indices are given by `argnum`) to the types of their
corresponding tangent arguments.

**Benefits:** Better error handling and messaging.

**Possible Drawbacks:** None.

**Related GitHub Issues:**

mlir/lib/Gradient/IR/GradientOps.cpp
@@ -318,24 +318,27 @@ LogicalResult JVPOp::verifySymbolUses(SymbolTableCollection &symbolTable)
     }
 
     std::vector<Type> tanTypes;
+    tanTypes.reserve(this->getTangents().size());
     {
-        auto tanOperands = OperandRange(
-            this->operand_begin() + callee.getFunctionType().getNumInputs(), this->operand_end());
-        for (auto c : tanOperands) {
-            tanTypes.push_back(c.getType());
+        for (const auto &tanOps : this->getTangents()) {
+            tanTypes.push_back(tanOps.getType());
         }
     }
 
-    auto calleeInputTypes = callee.getFunctionType().getInputs();
+    const auto calleeInputTypes = callee.getFunctionType().getInputs();
 
-    // Check that callee inputs have the same types as tangent inputs
-    for (size_t i = 0; i < tanTypes.size(); i++) {
-        auto tanType = tanTypes[i];
-        auto cIType = calleeInputTypes[i];
+    // Check that the differentiable callee inputs have the same types as their
+    // corresponding tangent inputs
+    for (size_t i = 0; i < diffArgIndices.size(); i++) {
+        const auto tanType = tanTypes[i];
+        const auto cIType = calleeInputTypes[diffArgIndices[i]];
+
         if (tanType != cIType) {
-            return this->emitOpError("callee input type does not match the tangent type")
-                   << " callee input " << i << " was expected to be of type " << tanType
-                   << " but got " << cIType;
+            return this->emitOpError("callee input and tangent arguments to jvp do not match; ")
+                   << "dtypes must be equal. "
+                   << "Got callee input type " << cIType << " for argument " << diffArgIndices[i]
+                   << " and so expected corresponding tangent type " << cIType << " but got "
+                   << tanType;
         }
     }
 
@@ -412,12 +415,14 @@ LogicalResult VJPOp::verifySymbolUses(SymbolTableCollection &symbolTable)
 
     // Check that callee results have the same types as cotangent inputs
     for (size_t i = 0; i < cotTypes.size(); i++) {
-        auto cotType = cotTypes[i];
-        auto crType = calleeResultTypes[i];
-        if (cotType != crType) {
-            return this->emitOpError("callee result type does not match the cotangent type")
-                   << " callee result " << i << " was expected to be of type " << cotType
-                   << " but got " << crType;
+        const auto cotType = cotTypes[i];
+        const auto cRType = calleeResultTypes[i];
+        if (cotType != cRType) {
+            return this->emitOpError("callee result and cotangent argument to vjp do not match; ")
+                   << "dtypes must be equal. "
+                   << "Got callee result type " << cRType << " for output " << i
+                   << " and so expected "
+                   << "corresponding cotangent type " << cRType << " but got " << cotType;
         }
     }
 

mlir/test/Gradient/VerifierTest.mlir
@@ -294,8 +294,41 @@ func.func @foo(%arg0: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
 
 %cst0 = arith.constant dense<1.0> : tensor<f64>
 %cst1 = arith.constant dense<1> : tensor<i64>
-// expected-error@+1 {{callee input type does not match the tangent type}}
+// expected-error@+1 {{callee input and tangent arguments to jvp do not match}}
 gradient.jvp "auto" @foo(%cst0) tangents(%cst1) : (tensor<f64>, tensor<i64>) -> (tensor<f64>, tensor<f64>, tensor<f64>, tensor<f64>)
+
+// -----
+
+func.func @foo(%arg0: tensor<i64>, %arg1: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
+
+    %cst = stablehlo.constant dense<2.000000e+00> : tensor<f64>
+    %0 = stablehlo.multiply %cst, %arg1 : tensor<f64>
+    %1 = stablehlo.multiply %arg1, %arg1 : tensor<f64>
+    return %0, %1 : tensor<f64>, tensor<f64>
+
+}
+
+%cst0 = arith.constant dense<1> : tensor<i64>
+%cst1 = arith.constant dense<1.0> : tensor<f64>
+%cst2 = arith.constant dense<1.0> : tensor<f64>
+gradient.jvp "auto" @foo(%cst0, %cst1) tangents(%cst2) {diffArgIndices = dense<1> : tensor<1xi64>} : (tensor<i64>, tensor<f64>, tensor<f64>) -> (tensor<f64>, tensor<f64>, tensor<f64>, tensor<f64>)
+
+// -----
+
+func.func @foo(%arg0: tensor<i64>, %arg1: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
+
+    %cst = stablehlo.constant dense<2.000000e+00> : tensor<f64>
+    %0 = stablehlo.multiply %cst, %arg1 : tensor<f64>
+    %1 = stablehlo.multiply %arg1, %arg1 : tensor<f64>
+    return %0, %1 : tensor<f64>, tensor<f64>
+
+}
+
+%cst0 = arith.constant dense<1> : tensor<i64>
+%cst1 = arith.constant dense<1.0> : tensor<f64>
+%cst2 = arith.constant dense<1> : tensor<i64>
+// expected-error@+1 {{callee input and tangent arguments to jvp do not match}}
+gradient.jvp "auto" @foo(%cst0, %cst1) tangents(%cst2) {diffArgIndices = dense<1> : tensor<1xi64>} : (tensor<i64>, tensor<f64>, tensor<i64>) -> (tensor<f64>, tensor<f64>, tensor<f64>, tensor<f64>)
 
 // -----
 
@@ -362,7 +395,7 @@ func.func @foo(%arg0: tensor<f64>) -> (tensor<f64>, tensor<f64>) {
 %cst0 = arith.constant dense<1.0> : tensor<f64>
 %cst1 = arith.constant dense<1> : tensor<i64>
 %cst2 = arith.constant dense<1> : tensor<i64>
-// expected-error@+1 {{callee result type does not match the cotangent type}}
+// expected-error@+1 {{callee result and cotangent argument to vjp do not match}}
 gradient.vjp "auto" @foo(%cst0) cotangents(%cst1, %cst2) {resultSegmentSizes = array<i32: 2, 1>} : (tensor<f64>, tensor<i64>, tensor<i64>) -> (tensor<f64>, tensor<f64>, tensor<f64>)
 
 // -----