360607c57a56e9c21f2ba7a23d455b9d382e86d3
Shuli Shu <08cnbj@gmail.com>
2024-08-17 15:29:25 -0400

    test against 05d9e4d

--- .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
+++ .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
@@ -45,7 +45,7 @@ cmake -S runtime -B runtime-build -G Ninja \
     -DPYTHON_INCLUDE_DIR=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/include/python${PYTHON_VERSION} \
     -DPYTHON_LIBRARY=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/lib \
     -Dpybind11_DIR=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/lib/python${PYTHON_VERSION}/site-packages/pybind11/share/cmake/pybind11 \
-    -DLIGHTNING_GIT_TAG=a599094 \
+    -DLIGHTNING_GIT_TAG=05d9e4d \
     -DENABLE_WARNINGS=OFF \
     -DENABLE_OPENQASM=ON \
     -DENABLE_OPENMP=OFF \
