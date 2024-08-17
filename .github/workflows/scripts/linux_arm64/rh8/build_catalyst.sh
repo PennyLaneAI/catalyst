c0b1564a0e82ee8607c70b51c277f675a2953520
Shuli Shu <08cnbj@gmail.com>
2024-08-17 19:49:27 -0400

    test against 4b40406

--- .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
+++ .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
@@ -45,7 +45,7 @@ cmake -S runtime -B runtime-build -G Ninja \
     -DPYTHON_INCLUDE_DIR=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/include/python${PYTHON_VERSION} \
     -DPYTHON_LIBRARY=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/lib \
     -Dpybind11_DIR=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/lib/python${PYTHON_VERSION}/site-packages/pybind11/share/cmake/pybind11 \
-    -DLIGHTNING_GIT_TAG=4b40406 \
+    -DLIGHTNING_GIT_TAG=3044c13 \
     -DENABLE_WARNINGS=OFF \
     -DENABLE_OPENQASM=ON \
     -DENABLE_OPENMP=OFF \
