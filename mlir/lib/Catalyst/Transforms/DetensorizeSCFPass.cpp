b127a272a0c2b0ba3f845c29f71c63fdfaca2c57
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-08-23 19:18:38 +0000

    Add scf.for pass.

--- mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
+++ mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
@@ -71,8 +71,84 @@ struct DetensorizeSCFPass : public impl::DetensorizeSCFPassBase<DetensorizeSCFPa
             });
         });
 
+        // Find scf.for
+        root->walk([&](scf::ForOp for_op) {
+            // Find scf.yield
+            for_op->walk([&](scf::YieldOp yield_op) {
+                // Loop over results
+                std::size_t i_result = 0;
+                for (mlir::Value result : yield_op.getResults()) {
+                    if (isFromElementScalarTensor(result)) {
+                        // Detensorize operand and modify type
+                        {
+                            auto iter_arg = for_op.getOperand(3 + i_result);
+                            // llvm::errs() << iter_arg << '\n'; 
+                            mlir::Operation *from_elem_op = iter_arg.getDefiningOp();
+                            rewriter.replaceAllUsesWith(iter_arg, from_elem_op->getOperand(0));
+                            rewriter.modifyOpInPlace(for_op, [&]() {
+                                for_op.getRegionIterArg(i_result).setType(
+                                    from_elem_op->getOperand(0).getType());
+                            });
+                            rewriter.eraseOp(from_elem_op);
+                        }
+                        // Tensorize argument in the for loop block
+                        {
+                            // llvm::errs() << for_op.getBody() << '\n';
+                            rewriter.setInsertionPointToStart(for_op.getBody());
+                            auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
+                                for_op->getLoc(),
+                                RankedTensorType::get({},
+                                                      for_op.getRegionIterArg(i_result).getType()),
+                                for_op.getRegionIterArg(i_result));
+                            // Substitute scf.for arguments with from_element results
+                            rewriter.replaceUsesWithIf(
+                                for_op.getRegionIterArg(i_result), from_elem_op->getResult(0),
+                                [&](OpOperand &operand) {
+                                    return operand.getOwner() != from_elem_op;
+                                });
+                        }
+                        // Remove from_element op and substitute argument and operand
+                        {
+                            mlir::Operation *from_elem_op = result.getDefiningOp();
+                            rewriter.replaceAllUsesWith(result, from_elem_op->getOperand(0));
+                            rewriter.modifyOpInPlace(for_op, [&]() {
+                                for_op.getResult(i_result).setType(
+                                    from_elem_op->getOperand(0).getType());
+                            });
+                            rewriter.eraseOp(from_elem_op);
+                        }
+                        // Create a from_element op after scf.for
+                        { 
+                            rewriter.setInsertionPointAfter(for_op);
+                            auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
+                                for_op->getLoc(),
+                                RankedTensorType::get({}, for_op->getResult(i_result).getType()),
+                                for_op->getResult(i_result));
+                            // Substitute scf.for results with from_element results
+                            rewriter.replaceUsesWithIf(
+                                for_op->getResult(i_result), from_elem_op->getResult(0),
+                                [&](OpOperand &operand) {
+                                    return operand.getOwner() != from_elem_op;
+                                });
+                        }
+                    }
+                    i_result += 1;
+                }
+            });
+            // llvm::errs() << for_op << '\n';
+            // for (auto args : for_op.getOperands()) {
+            //     llvm::errs() << args << '\n';
+            // }
+            // for (auto args : for_op.getRegionIterArgs()) {
+            //     llvm::errs() << args << '\n';
+            // }
+            // for (auto args : for_op.getResults()) {
+            //     llvm::errs() << args << '\n';
+            // }
+        });
+
         // llvm::errs() << "|||||||||||||||||| Final IR ||||||||||||||||||\n";
-        // root->walk([&](mlir::Operation *if_op) { llvm::errs() << *if_op << '\n'; });
+        // root->walk([&](mlir::Operation *op) { llvm::errs() << *op << '\n'; });
         // llvm::errs() << "|||||||||||||||||| Final IR ||||||||||||||||||\n";
     }
 };
