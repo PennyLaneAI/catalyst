2f56833c5d5009c213857f52104297ff116fc833
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2024-08-26 09:51:38 -0400

    added module docstring to test

--- frontend/test/pytest/test_jax_primitives.py
+++ frontend/test/pytest/test_jax_primitives.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""Tests related to jax premitives in Catalyst"""
 
 import pytest
 from jax import make_jaxpr
