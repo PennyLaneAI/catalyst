863e385e6c3f171929b2cd73709ee84743e01e0c
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-08-27 19:14:34 +0000

    Clean up scf pass.

--- mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
+++ mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
@@ -77,93 +77,52 @@ struct DetensorizeSCFPass : public impl::DetensorizeSCFPassBase<DetensorizeSCFPa
             });
         });
 
-        // llvm::errs() << *root << '\n';
-
         // Find scf.for
         root->walk([&](scf::ForOp for_op) {
-            // llvm::errs() << for_op << '\n';
-
+            // Loop over operands
             std::size_t i_operand = 0;
             for (mlir::Value operand : for_op.getOperands()) {
                 if (isScalarTensor(operand)) {
-                    // llvm::errs() << operand << '(' << i_operand << ')' << '\n';
-
-                    // Detensorize operand and modify type
+                    const std::size_t i_argument = i_operand - 3;
+                    // Detensorize operand: extract tensor element before for operation and replace
+                    // operand
                     {
-                        // auto operand = for_op.getOperand(3 + i_operand);
-                        // llvm::errs() << operand << '\n';
-                        // mlir::Operation *operand_def = operand.getDefiningOp();
-                        // rewriter.setInsertionPointAfter(operand_def);
                         rewriter.setInsertionPoint(for_op);
-                        mlir::Operation *extract_op = rewriter.create<tensor::ExtractOp>(for_op->getLoc(), operand, ValueRange{});
-                        // rewriter.replaceAllUsesWith(operand, extract_op->getResult(0));
+                        mlir::Operation *extract_op = rewriter.create<tensor::ExtractOp>(
+                            for_op->getLoc(), operand, ValueRange{});
                         rewriter.replaceUsesWithIf(
                             operand, extract_op->getResult(0),
-                            [&](OpOperand &op) { return op.getOwner() != extract_op; });
+                            [&](OpOperand &op) { return op.getOwner() == for_op; });
                         rewriter.modifyOpInPlace(for_op, [&]() {
-                            for_op.getRegionIterArg(i_operand - 3).setType(
-                                extract_op->getResult(0).getType());
+                            for_op.getRegionIterArg(i_argument)
+                                .setType(extract_op->getResult(0).getType());
                         });
                     }
-                    // Tensorize argument in the for loop block
+                    // Retensorize argument: reconstruct tensor at the beginning of the for body
                     {
-                        // llvm::errs() << for_op.getBody() << '\n';
                         rewriter.setInsertionPointToStart(for_op.getBody());
                         auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
                             for_op->getLoc(),
-                            RankedTensorType::get({}, for_op.getRegionIterArg(i_operand - 3).getType()),
-                            for_op.getRegionIterArg(i_operand - 3));
-                        // Substitute scf.for arguments with from_element results
+                            RankedTensorType::get({},
+                                                  for_op.getRegionIterArg(i_argument).getType()),
+                            for_op.getRegionIterArg(i_argument));
                         rewriter.replaceUsesWithIf(
-                            for_op.getRegionIterArg(i_operand - 3), from_elem_op->getResult(0),
-                            [&](OpOperand &operand) { return operand.getOwner() != from_elem_op; });
+                            for_op.getRegionIterArg(i_argument), from_elem_op->getResult(0),
+                            [&](OpOperand &op) { return op.getOwner() != from_elem_op; });
                     }
                 }
                 i_operand += 1;
             }
-            // llvm::errs() << for_op << '\n';
-
             scf::YieldOp yield_op = cast<scf::YieldOp>(*for_op.getBody()->getTerminator());
-            // llvm::errs() << yield_op << '\n';
             // Loop over results
             std::size_t i_result = 0;
             for (mlir::Value result : yield_op.getOperands()) {
-                // llvm::errs() << result << '\n';
                 if (isScalarTensor(result)) {
-                    // llvm::errs() << "Is a scalar tensor" << '\n';
-                    // // Detensorize operand and modify type
-                    // {
-                    //     auto iter_arg = for_op.getOperand(3 + i_result);
-                    //     // llvm::errs() << iter_arg << '\n';
-                    //     mlir::Operation *from_elem_op = iter_arg.getDefiningOp();
-                    //     rewriter.replaceAllUsesWith(iter_arg, from_elem_op->getOperand(0));
-                    //     rewriter.modifyOpInPlace(for_op, [&]() {
-                    //         for_op.getRegionIterArg(i_result).setType(
-                    //             from_elem_op->getOperand(0).getType());
-                    //     });
-                    //     rewriter.eraseOp(from_elem_op);
-                    // }
-                    // // Tensorize argument in the for loop block
-                    // {
-                    //     // llvm::errs() << for_op.getBody() << '\n';
-                    //     rewriter.setInsertionPointToStart(for_op.getBody());
-                    //     auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
-                    //         for_op->getLoc(),
-                    //         RankedTensorType::get({},
-                    //         for_op.getRegionIterArg(i_result).getType()),
-                    //         for_op.getRegionIterArg(i_result));
-                    //     // Substitute scf.for arguments with from_element results
-                    //     rewriter.replaceUsesWithIf(
-                    //         for_op.getRegionIterArg(i_result), from_elem_op->getResult(0),
-                    //         [&](OpOperand &operand) { return operand.getOwner() != from_elem_op;
-                    //         });
-                    // }
-
-                    // Remove from_element op and substitute argument and operand
+                    // Detensorize result: extract tensor element before yielding
                     {
-                        
                         rewriter.setInsertionPoint(yield_op);
-                        mlir::Operation *extract_op = rewriter.create<tensor::ExtractOp>(yield_op->getLoc(), result, ValueRange{});
+                        mlir::Operation *extract_op = rewriter.create<tensor::ExtractOp>(
+                            yield_op->getLoc(), result, ValueRange{});
                         rewriter.replaceUsesWithIf(
                             result, extract_op->getResult(0),
                             [&](OpOperand &op) { return op.getOwner() != extract_op; });
@@ -171,39 +130,25 @@ struct DetensorizeSCFPass : public impl::DetensorizeSCFPassBase<DetensorizeSCFPa
                             yield_op->getOperand(i_result).setType(
                                 extract_op->getResult(0).getType());
                         });
-
-                        // mlir::Operation *from_elem_op = result.getDefiningOp();
-                        // rewriter.replaceAllUsesWith(result, from_elem_op->getOperand(0));
                         rewriter.modifyOpInPlace(for_op, [&]() {
-                            for_op.getResult(i_result).setType(
-                                extract_op->getResult(0).getType());
+                            for_op.getResult(i_result).setType(extract_op->getResult(0).getType());
                         });
-                        // rewriter.eraseOp(from_elem_op);
                     }
-                    // Create a from_element op after scf.for
+                    // Retensorize result: reconstruct tensor after the for body
                     {
                         rewriter.setInsertionPointAfter(for_op);
                         auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
                             for_op->getLoc(),
                             RankedTensorType::get({}, for_op->getResult(i_result).getType()),
                             for_op->getResult(i_result));
-                        // Substitute scf.for results with from_element results
                         rewriter.replaceUsesWithIf(
                             for_op->getResult(i_result), from_elem_op->getResult(0),
-                            [&](OpOperand &operand) { return operand.getOwner() != from_elem_op;
-                            });
+                            [&](OpOperand &operand) { return operand.getOwner() != from_elem_op; });
                     }
                 }
                 i_result += 1;
             }
-
-            // llvm::errs() << for_op << '\n';
-            // llvm::errs() << *root << '\n';
         });
-
-        // llvm::errs() << "|||||||||||||||||| Final IR ||||||||||||||||||\n";
-        // root->walk([&](mlir::Operation *op) { llvm::errs() << *op << '\n'; });
-        // llvm::errs() << "|||||||||||||||||| Final IR ||||||||||||||||||\n";
     }
 };
 
