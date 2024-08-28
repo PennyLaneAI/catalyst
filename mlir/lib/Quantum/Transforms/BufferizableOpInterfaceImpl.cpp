f02870939bed617fc7b33edd405222d53c982e9f
Tzung-Han Juang <tzunghan.juang@gmail.com>
2024-08-28 11:47:02 -0400

    Add new bufferization pass for ProbsOp

--- mlir/lib/Quantum/Transforms/BufferizableOpInterfaceImpl.cpp
+++ mlir/lib/Quantum/Transforms/BufferizableOpInterfaceImpl.cpp
@@ -10,7 +10,8 @@ using namespace catalyst::quantum;
 
 namespace {
 
-/// Bufferization of tensor.extract. Replace with memref.load.
+/// Bufferization of catalyst.quantum.state. Replace with memref.alloc and a new
+/// catalyst.quantum.state that uses the memory allocated by memref.alloc.
 struct StateOpInterface
     : public mlir::bufferization::BufferizableOpInterface::ExternalModel<StateOpInterface,
                                                     catalyst::quantum::StateOp> {
@@ -45,11 +46,48 @@ struct StateOpInterface
   }
 };
 
-}
+/// Bufferization of catalyst.quantum.probs. Replace with memref.alloc and a new
+/// catalyst.quantum.probs that uses the memory allocated by memref.alloc.
+struct ProbsOpInterface
+    : public mlir::bufferization::BufferizableOpInterface::ExternalModel<ProbsOpInterface,
+                                                    catalyst::quantum::ProbsOp> {
+  bool bufferizesToMemoryRead(mlir::Operation *op, mlir::OpOperand &opOperand,
+                              const mlir::bufferization::AnalysisState &state) const {
+    return true;
+  }
+
+  bool bufferizesToMemoryWrite(mlir::Operation *op, mlir::OpOperand &opOperand,
+                               const mlir::bufferization::AnalysisState &state) const {
+    return false;
+  }
+
+  mlir::bufferization::AliasingValueList getAliasingValues(mlir::Operation *op,
+                                      mlir::OpOperand &opOperand,
+                                      const mlir::bufferization::AnalysisState &state) const {
+    return {};
+  }
+
+  LogicalResult bufferize(mlir::Operation *op, RewriterBase &rewriter,
+                          const mlir::bufferization::BufferizationOptions &options) const {
+    auto probsOp = cast<ProbsOp>(op);
+    Location loc = op->getLoc();
+    auto tensorType = cast<RankedTensorType>(probsOp.getProbabilities().getType());
+    MemRefType resultType = MemRefType::get(tensorType.getShape(), tensorType.getElementType());
+
+    Value allocVal = rewriter.create<memref::AllocOp>(loc, resultType);
+    rewriter.create<ProbsOp>(loc, TypeRange{}, ValueRange{probsOp.getObs(), allocVal});
+    mlir::bufferization::replaceOpWithBufferizedValues(rewriter, op, allocVal);
+
+    return success();
+  }
+};
+
+} // namespace
 
 void catalyst::quantum::registerBufferizableOpInterfaceExternalModels(
     DialectRegistry &registry) {
   registry.addExtension(+[](MLIRContext *ctx, catalyst::quantum::QuantumDialect *dialect) {
     StateOp::attachInterface<StateOpInterface>(*ctx);
+    ProbsOp::attachInterface<ProbsOpInterface>(*ctx);
   });
 }
