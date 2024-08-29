7815467d8e32d494f3f6b40782d7b1852b58cfae
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-08-29 15:54:45 -0400

    test

--- frontend/test/pytest/test_jax_numerical.py
+++ frontend/test/pytest/test_jax_numerical.py
@@ -20,7 +20,7 @@ import pytest
 from jax import numpy as jnp
 from jax import scipy as jsp
 
-from catalyst import qjit
+from catalyst import accelerate, qjit
 
 
 class TestExpmNumerical:
@@ -32,14 +32,26 @@ class TestExpmNumerical:
             jnp.array([[0.1, 0.2], [5.3, 1.2]]),
             jnp.array([[1, 2], [3, 4]]),
             jnp.array([[1.0, -1.0j], [1.0j, -1.0]]),
+            (jnp.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [3.0, 2.0, 1.0]]), "wrong numbers"),
         ],
     )
     def test_expm_numerical(self, inp):
         """Test basic numerical correctness for jax.scipy.linalg.expm for float, int, complex"""
 
-        @qjit
-        def f(x):
-            return jsp.linalg.expm(x)
+        if (type(inp) == tuple) and (inp[1] == "wrong numbers"):
+            # This particulat matrix has wrong result numbers. Use callback.
+            # https://github.com/PennyLaneAI/catalyst/issues/1071
+            @qjit
+            def f(x):
+                return accelerate(jsp.linalg.expm)(x)
+
+            inp = inp[0]
+
+        else:
+
+            @qjit
+            def f(x):
+                return jsp.linalg.expm(x)
 
         observed = f(inp)
         expected = jsp.linalg.expm(inp)
