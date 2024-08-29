77a8a61b910cbdf83b8a26830a3ae4cda6673029
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-08-29 11:38:51 -0400

    Remove detensor-if

--- mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
+++ mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
@@ -30,53 +30,6 @@ struct DetensorizeSCFPass : public impl::DetensorizeSCFPassBase<DetensorizeSCFPa
             }
             return dyn_cast<TensorType>(v.getType()).getRank() == 0;
         };
-        auto isFromElementScalarTensor = [](Value v) {
-            if (!::llvm::isa<TensorType>(v.getType())) {
-                return false;
-            }
-            if (dyn_cast<TensorType>(v.getType()).getRank() != 0) {
-                return false;
-            }
-            return isa<tensor::FromElementsOp>(*v.getDefiningOp());
-        };
-        // Find scf.if
-        root->walk([&](scf::IfOp if_op) {
-            bool first_yield = true;
-            // Find scf.yield
-            if_op->walk([&](scf::YieldOp yield_op) {
-                // Loop over results
-                std::size_t i_result = 0;
-                for (mlir::Value result : yield_op.getResults()) {
-                    if (isFromElementScalarTensor(result)) {
-                        // Remove from_element op and substitute operand
-                        mlir::Operation *from_element_op = result.getDefiningOp();
-                        rewriter.replaceAllUsesWith(result, from_element_op->getOperand(0));
-                        rewriter.modifyOpInPlace(if_op, [&]() {
-                            if_op.getResult(i_result).setType(
-                                from_element_op->getOperand(0).getType());
-                        });
-                        rewriter.eraseOp(from_element_op);
-                        if (first_yield) {
-                            // Create a from_element op after scf.if
-                            rewriter.setInsertionPointAfter(if_op);
-                            auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
-                                if_op->getLoc(),
-                                RankedTensorType::get({}, if_op->getResult(i_result).getType()),
-                                if_op->getResult(i_result));
-                            // Substitute scf.if results with from_element results
-                            rewriter.replaceUsesWithIf(
-                                if_op->getResult(i_result), from_elem_op->getResult(0),
-                                [&](OpOperand &operand) {
-                                    return operand.getOwner() != from_elem_op;
-                                });
-                        }
-                    }
-                    i_result += 1;
-                }
-                first_yield = false;
-            });
-        });
-
         // Find scf.for
         root->walk([&](scf::ForOp for_op) {
             // Loop over operands
