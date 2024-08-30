ec39190644f9b53f057fd014205c1ae58cb98ead
Romain Moyard <rmoyard@gmail.com>
2024-08-30 09:52:53 -0400

    Move test in gradient

--- frontend/test/pytest/test_gradient.py
+++ frontend/test/pytest/test_gradient.py
@@ -34,6 +34,7 @@ from catalyst import (
     pure_callback,
     qjit,
     value_and_grad,
+    vmap,
 )
 
 # pylint: disable=too-many-lines
@@ -1501,6 +1502,62 @@ def test_gradient_slice(backend):
     )(data, params["weights"], params["bias"])
     jax_res = jax.jacobian(my_model, argnums=1)(data, params["weights"], params["bias"])
     assert np.allclose(cat_res, jax_res)
+
+
+@pytest.mark.xfail(reason="Vmap yields wrong results when differentiated")
+def test_vmap_worflow_derivation(backend):
+    """Check the gradient of a vmap workflow"""
+    n_wires = 5
+    data = jnp.sin(jnp.mgrid[-2:2:0.2].reshape(n_wires, -1)) ** 3
+
+    targets = jnp.array([-0.2, 0.4, 0.35, 0.2], dtype=jax.numpy.float64)
+
+    dev = qml.device(backend, wires=n_wires)
+
+    @qml.qnode(dev, diff_method="adjoint")
+    def circuit(data, weights):
+        """Quantum circuit ansatz"""
+
+        @for_loop(0, n_wires, 1)
+        def data_embedding(i):
+            qml.RY(data[i], wires=i)
+
+        data_embedding()
+
+        @for_loop(0, n_wires, 1)
+        def ansatz(i):
+            qml.RX(weights[i, 0], wires=i)
+            qml.RY(weights[i, 1], wires=i)
+            qml.RX(weights[i, 2], wires=i)
+            qml.CNOT(wires=[i, (i + 1) % n_wires])
+
+        ansatz()
+
+        return qml.expval(qml.sum(*[qml.PauliZ(i) for i in range(n_wires)]))
+
+    circuit = vmap(circuit, in_axes=(1, None))
+
+    def my_model(data, weights, bias):
+        return circuit(data, weights) + bias
+
+    def loss_fn(params, data, targets):
+        predictions = my_model(data, params["weights"], params["bias"])
+        loss = jnp.sum((targets - predictions) ** 2 / len(data))
+        return loss
+
+    weights = jnp.ones([n_wires, 3])
+    bias = jnp.array(0.0, dtype=jax.numpy.float64)
+    params = {"weights": weights, "bias": bias}
+
+    results_enzyme = qjit(grad(loss_fn))(params, data, targets)
+    results_jax = jax.grad(loss_fn)(params, data, targets)
+
+    data_enzyme, pytree_enzyme = tree_flatten(results_enzyme)
+    data_jax, pytree_fd = tree_flatten(results_jax)
+
+    assert pytree_enzyme == pytree_fd
+    assert jnp.allclose(data_enzyme[0], data_jax[0])
+    assert jnp.allclose(data_enzyme[1], data_jax[1])
 
 
 class TestGradientErrors:
