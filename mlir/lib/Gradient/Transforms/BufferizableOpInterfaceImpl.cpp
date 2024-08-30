c01dd1f019d970e1aea865430c98980dff4284fd
Tzung-Han Juang <tzunghan.juang@gmail.com>
2024-08-30 18:05:34 -0400

    Add AdjointOp bufferization

--- mlir/lib/Gradient/Transforms/BufferizableOpInterfaceImpl.cpp
+++ mlir/lib/Gradient/Transforms/BufferizableOpInterfaceImpl.cpp
@@ -1,21 +1,139 @@
+#include "mlir/Conversion/LLVMCommon/MemRefBuilder.h"
+#include "mlir/Conversion/LLVMCommon/Pattern.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
 #include "mlir/Dialect/Bufferization/IR/BufferizableOpInterface.h"
 #include "mlir/Dialect/Bufferization/IR/Bufferization.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/FunctionCallUtils.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Transforms/DialectConversion.h"
 
 #include "Gradient/IR/GradientOps.h"
 #include "Gradient/Transforms/BufferizableOpInterfaceImpl.h"
+#include "Quantum/IR/QuantumOps.h"
 
 using namespace mlir;
 using namespace catalyst::gradient;
 
 namespace {
 
+constexpr int64_t UNKNOWN = ShapedType::kDynamic;
+
+LLVM::LLVMFuncOp ensureFunctionDeclaration(RewriterBase &rewriter, Operation *op,
+                                           StringRef fnSymbol, Type fnType)
+{
+    Operation *fnDecl = SymbolTable::lookupNearestSymbolFrom(op, rewriter.getStringAttr(fnSymbol));
+
+    if (!fnDecl) {
+        RewriterBase::InsertionGuard insertGuard(rewriter);
+        ModuleOp mod = op->getParentOfType<ModuleOp>();
+        rewriter.setInsertionPointToStart(mod.getBody());
+
+        fnDecl = rewriter.create<LLVM::LLVMFuncOp>(op->getLoc(), fnSymbol, fnType);
+    }
+    else {
+        assert(isa<LLVM::LLVMFuncOp>(fnDecl) && "QIR function declaration is not a LLVMFuncOp");
+    }
+
+    return cast<LLVM::LLVMFuncOp>(fnDecl);
+}
+
+struct AdjointOpInterface
+    : public bufferization::BufferizableOpInterface::ExternalModel<AdjointOpInterface,
+                                                    AdjointOp> {
+    bool bufferizesToMemoryRead(Operation *op, OpOperand &opOperand,
+                                const bufferization::AnalysisState &state) const {
+        return false;
+    }
+
+    bool bufferizesToMemoryWrite(Operation *op, OpOperand &opOperand,
+                                 const bufferization::AnalysisState &state) const {
+        return false;
+    }
+
+    bufferization::AliasingValueList getAliasingValues(Operation *op,
+                                      OpOperand &opOperand,
+                                      const bufferization::AnalysisState &state) const {
+        return {};
+    }
+
+    LogicalResult bufferize(Operation *op, RewriterBase &rewriter,
+                            const bufferization::BufferizationOptions &options) const {
+        auto adjointOp = cast<AdjointOp>(op);
+        MLIRContext *ctx = rewriter.getContext();
+        Location loc = op->getLoc();
+        LLVMTypeConverter typeConverter(ctx);
+        Type vectorType = typeConverter.convertType(MemRefType::get({UNKNOWN}, Float64Type::get(ctx)));
+
+        for (Type type : adjointOp.getResultTypes()) {
+            if (!isa<MemRefType>(type))
+                return adjointOp.emitOpError("must be bufferized before lowering");
+
+            // Currently only expval gradients are supported by the runtime,
+            // leading to tensor<?xf64> return values.
+            if (dyn_cast<MemRefType>(type) != MemRefType::get({UNKNOWN}, Float64Type::get(ctx)))
+                return adjointOp.emitOpError("adjoint can only return MemRef<?xf64> or tuple thereof");
+        }
+
+        // The callee of the adjoint op must return as a single result the quantum register.
+        func::FuncOp callee =
+            SymbolTable::lookupNearestSymbolFrom<func::FuncOp>(adjointOp, adjointOp.getCalleeAttr());
+        assert(callee && callee.getNumResults() == 1 && "invalid qfunc symbol in adjoint op");
+
+        StringRef cacheFnName = "__catalyst__rt__toggle_recorder";
+        StringRef gradFnName = "__catalyst__qis__Gradient";
+        Type cacheFnSignature =
+            LLVM::LLVMFunctionType::get(LLVM::LLVMVoidType::get(ctx), IntegerType::get(ctx, 1));
+        Type gradFnSignature = LLVM::LLVMFunctionType::get(
+            LLVM::LLVMVoidType::get(ctx), IntegerType::get(ctx, 64), /*isVarArg=*/true);
+
+        LLVM::LLVMFuncOp cacheFnDecl =
+            ensureFunctionDeclaration(rewriter, adjointOp, cacheFnName, cacheFnSignature);
+        LLVM::LLVMFuncOp gradFnDecl =
+            ensureFunctionDeclaration(rewriter, adjointOp, gradFnName, gradFnSignature);
+
+        // Run the forward pass and cache the circuit.
+        Value c_true = rewriter.create<LLVM::ConstantOp>(
+            loc, rewriter.getIntegerAttr(IntegerType::get(ctx, 1), 1));
+        Value c_false = rewriter.create<LLVM::ConstantOp>(
+            loc, rewriter.getIntegerAttr(IntegerType::get(ctx, 1), 0));
+        rewriter.create<LLVM::CallOp>(loc, cacheFnDecl, c_true);
+        Value qreg = rewriter.create<func::CallOp>(loc, callee, adjointOp.getArgs()).getResult(0);
+        if (!isa<catalyst::quantum::QuregType>(qreg.getType()))
+            return callee.emitOpError("qfunc must return quantum register");
+        rewriter.create<LLVM::CallOp>(loc, cacheFnDecl, c_false);
+
+        // We follow the C ABI convention of passing result memrefs as struct pointers in the
+        // arguments to the C function, although in this case as a variadic argument list to allow
+        // for a varying number of results in a single signature.
+        Value c1 = rewriter.create<LLVM::ConstantOp>(loc, rewriter.getI64IntegerAttr(1));
+        Value numResults = rewriter.create<LLVM::ConstantOp>(
+            loc, rewriter.getI64IntegerAttr(adjointOp.getDataIn().size()));
+        SmallVector<Value> args = {numResults};
+
+        // Might need to modify here.
+        for (Value memref : adjointOp.getDataIn()) {
+            Value newArg = rewriter.create<LLVM::AllocaOp>(
+                loc, LLVM::LLVMPointerType::get(rewriter.getContext()), vectorType, c1);
+            rewriter.create<LLVM::StoreOp>(loc, memref, newArg);
+            args.push_back(newArg);
+        }
+
+        rewriter.create<LLVM::CallOp>(loc, gradFnDecl, args);
+        rewriter.create<catalyst::quantum::DeallocOp>(loc, qreg);
+        mlir::bufferization::replaceOpWithNewBufferizedOp<catalyst::quantum::DeviceReleaseOp>(rewriter, adjointOp);
+
+        return success();
+    }
+};
+
 } // namespace
 
 void catalyst::gradient::registerBufferizableOpInterfaceExternalModels(
     DialectRegistry &registry) {
-  registry.addExtension(+[](MLIRContext *ctx, GradientDialect *dialect) {
-
-  });
+    registry.addExtension(+[](MLIRContext *ctx, GradientDialect *dialect) {
+        AdjointOp::attachInterface<AdjointOpInterface>(*ctx);
+    });
 }
