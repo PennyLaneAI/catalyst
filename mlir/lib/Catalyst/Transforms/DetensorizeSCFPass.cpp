d704afc9a74e41ae8e705e30af622bccb84e2707
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-09-06 17:59:24 +0000

    Try OpConversionPattern

--- mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
+++ mlir/lib/Catalyst/Transforms/DetensorizeSCFPass.cpp
@@ -6,6 +6,7 @@
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "llvm/Support/Debug.h"
 
@@ -16,6 +17,151 @@ using namespace catalyst;
 namespace catalyst {
 #define GEN_PASS_DEF_DETENSORIZESCFPASS
 #include "Catalyst/Transforms/Passes.h.inc"
+
+static Value sourceMaterializationCallback(OpBuilder &builder, Type type, ValueRange inputs,
+                                           Location loc)
+{
+    assert(inputs.size() == 1);
+    auto inputType = inputs[0].getType();
+    if (isa<TensorType>(inputType))
+        return nullptr;
+
+    // A detensored value is converted back by creating a new tensor from its
+    // element(s).
+    return builder.create<tensor::FromElementsOp>(loc, RankedTensorType::get({}, inputType),
+                                                  inputs[0]);
+}
+
+class DetensorizeTypeConverter : public TypeConverter {
+  public:
+    DetensorizeTypeConverter()
+    {
+        addConversion([](Type type) { return type; });
+
+        // A TensorType that can be detensorized, is converted to the underlying
+        // element type.
+        addConversion([](TensorType tensorType) -> Type {
+            if (tensorType.getRank() == 0)
+                return tensorType.getElementType();
+
+            return tensorType;
+        });
+
+        // A tensor value is detensorized by extracting its element(s).
+        addTargetMaterialization(
+            [](OpBuilder &builder, Type type, ValueRange inputs, Location loc) -> Value {
+                return builder.create<tensor::ExtractOp>(loc, inputs[0], ValueRange{});
+            });
+
+        addSourceMaterialization(sourceMaterializationCallback);
+        addArgumentMaterialization(sourceMaterializationCallback);
+    }
+};
+
+class DetensorizeForOp : public OpConversionPattern<scf::ForOp> {
+  public:
+    using OpConversionPattern::OpConversionPattern;
+    LogicalResult matchAndRewrite(scf::ForOp forOp, OpAdaptor adaptor,
+                                  ConversionPatternRewriter &rewriter) const override
+    {
+        auto isScalarTensor = [](Value v) {
+            if (!::llvm::isa<TensorType>(v.getType())) {
+                return false;
+            }
+            return dyn_cast<TensorType>(v.getType()).getRank() == 0;
+        };
+
+        // 1. Find scalar tensors and extract element
+        SmallVector<std::size_t> indices;
+        SmallVector<Value> newIterOperands;
+        std::size_t index = 0;
+        for (OpOperand &opOperand : forOp.getInitArgsMutable()) {
+            if (isScalarTensor(opOperand.get())) {
+                rewriter.setInsertionPoint(forOp);
+                mlir::Operation *extract_op = rewriter.create<tensor::ExtractOp>(
+                    forOp->getLoc(), opOperand.get(), ValueRange{});
+                newIterOperands.push_back(extract_op->getResult(0));
+                indices.push_back(index);
+                index += 1;
+                continue;
+            }
+            newIterOperands.push_back(opOperand.get());
+            index += 1;
+        }
+
+        if (newIterOperands.empty()) {
+            return failure();
+        }
+
+        // 2. Create the new ForOp shell.
+        scf::ForOp newForOp =
+            rewriter.create<scf::ForOp>(forOp.getLoc(), forOp.getLowerBound(),
+                                        forOp.getUpperBound(), forOp.getStep(), newIterOperands);
+        newForOp->setAttrs(forOp->getAttrs());
+        Block &newBlock = newForOp.getRegion().front();
+        SmallVector<Value> newBlockTransferArgs(newBlock.getArguments().begin(),
+                                                newBlock.getArguments().end());
+
+        // 3. Copy body
+        Block &oldBlock = forOp.getRegion().front();
+        rewriter.mergeBlocks(&oldBlock, &newBlock, newBlockTransferArgs);
+
+        llvm::errs() << "........................................" << '\n';
+        llvm::errs() << newForOp << '\n';
+        llvm::errs() << "........................................" << '\n';
+
+        // 4. Retensorize arguments at the beginning of the body
+        rewriter.setInsertionPoint(&newBlock, newBlock.begin());
+        for (std::size_t index : indices) {
+            auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
+                newForOp->getLoc(),
+                RankedTensorType::get({}, newForOp.getRegionIterArg(index).getType()),
+                newForOp.getRegionIterArg(index));
+            rewriter.replaceUsesWithIf(
+                newForOp.getRegionIterArg(index), from_elem_op->getResult(0),
+                [&](OpOperand &op) { return !isa<tensor::FromElementsOp>(op.getOwner()); });
+        }
+
+        llvm::errs() << "........................................" << '\n';
+        llvm::errs() << newForOp << '\n';
+        llvm::errs() << "........................................" << '\n';
+
+        // 5. Extract scalar tensor elements and detensorize terminator
+        auto clonedYieldOp = cast<scf::YieldOp>(newBlock.getTerminator());
+        SmallVector<Value> newYieldOperands = clonedYieldOp.getOperands();
+        rewriter.setInsertionPoint(clonedYieldOp);
+        for (std::size_t index : indices) {
+            mlir::Operation *extract_op = rewriter.create<tensor::ExtractOp>(
+                clonedYieldOp->getLoc(), clonedYieldOp->getOperand(index), ValueRange{});
+            newYieldOperands[index] = extract_op->getResult(0);
+        }
+        rewriter.create<scf::YieldOp>(newForOp.getLoc(), newYieldOperands);
+        rewriter.eraseOp(clonedYieldOp);
+
+        llvm::errs() << "........................................" << '\n';
+        llvm::errs() << newForOp << '\n';
+        llvm::errs() << "........................................" << '\n';
+
+        // 6. Retensorize results after for loop
+        rewriter.setInsertionPointAfter(forOp);
+        for (std::size_t index : indices) {
+            mlir::Value for_result = newForOp->getResult(index);
+            auto from_elem_op = rewriter.create<tensor::FromElementsOp>(
+                newForOp->getLoc(), RankedTensorType::get({}, for_result.getType()), for_result);
+            rewriter.replaceUsesWithIf(
+                forOp->getResult(index), from_elem_op->getResult(0),
+                [&](OpOperand &op) { return !isa<tensor::FromElementsOp>(op.getOwner()); });
+        }
+
+        llvm::errs() << "........................................" << '\n';
+        llvm::errs() << newForOp << '\n';
+        llvm::errs() << "........................................" << '\n';
+
+        rewriter.replaceOp(forOp, newForOp);
+
+        return success();
+    }
+};
 
 struct DetensorizeSCFPass : public impl::DetensorizeSCFPassBase<DetensorizeSCFPass> {
     using impl::DetensorizeSCFPassBase<DetensorizeSCFPass>::DetensorizeSCFPassBase;
@@ -148,11 +294,36 @@ struct DetensorizeSCFPass : public impl::DetensorizeSCFPassBase<DetensorizeSCFPa
         mlir::Operation *root = getOperation();
         IRRewriter rewriter(root->getContext());
 
+        // MLIRContext *context = &getContext();
+        // ConversionTarget target(*context);
+        // target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
+        // target.addDynamicallyLegalOp<scf::ForOp>([&](scf::ForOp forOp) {
+        //     auto isScalarTensor = [](Value v) {
+        //         if (!::llvm::isa<TensorType>(v.getType())) {
+        //             return false;
+        //         }
+        //         return dyn_cast<TensorType>(v.getType()).getRank() == 0;
+        //     };
+        //     std::size_t count = 0;
+        //     for (OpOperand &opOperand : forOp.getInitArgsMutable()) {
+        //         if (isScalarTensor(opOperand.get())) {
+        //             count += 1;
+        //             continue;
+        //         }
+        //     }
+        //     return !count;
+        // });
+        // DetensorizeTypeConverter typeConverter;
+
+        // RewritePatternSet patterns(context);
+        // patterns.add<DetensorizeForOp>(typeConverter, patterns.getContext());
+        // if (failed(applyFullConversion(getOperation(), target, std::move(patterns))))
+        //     signalPassFailure();
         // llvm::errs() << *root << '\n';
 
         root->walk([&](scf::ForOp for_op) { detensorizeForOp(for_op, rewriter); });
 
-        // llvm::errs() << *root << '\n';
+        llvm::errs() << *root << '\n';
 
         root->walk([&](scf::IfOp if_op) { detensorizeIfOp(if_op, rewriter); });
     }
