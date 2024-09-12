2b0fada011d165a7ac8777d7aadedb363082b8db
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-09-12 13:58:51 -0400

    license

--- mlir/unittests/Example/Example.cpp
+++ mlir/unittests/Example/Example.cpp
@@ -1,10 +1,34 @@
+#include "mlir/Parser/Parser.h"
+#include "mlir/AsmParser/AsmParser.h"
+#include "mlir/AsmParser/AsmParserState.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "Quantum/IR/QuantumOps.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Verifier.h"
+#include "llvm/Support/SourceMgr.h"
+
 #include "gtest/gtest.h"
-#include <unistd.h>
+
+using namespace mlir;
 
 namespace {
-TEST(Example, example)
-{
-    sleep(10);
-    ASSERT_TRUE(true);
+TEST(MLIRParser, ParseQuantumIR) {
+  std::string moduleStr = R"mlir(
+func.func @test_alloc_dealloc_no_fold() -> !quantum.bit {
+  %r = quantum.alloc(3) : !quantum.reg
+  %q = quantum.extract %r[0] : !quantum.reg -> !quantum.bit
+  quantum.dealloc %r : !quantum.reg
+  return %q : !quantum.bit
 }
-} // namespace
+  )mlir";
+
+  DialectRegistry registry;
+  registry.insert<func::FuncDialect, catalyst::quantum::QuantumDialect>();
+  MLIRContext context(registry);
+
+  ParserConfig config(&context, /*verifyAfterParse=*/false);
+
+  OwningOpRef<ModuleOp> module = parseSourceString<ModuleOp>(moduleStr, config);
+  ASSERT_TRUE(module);
+}
+}
