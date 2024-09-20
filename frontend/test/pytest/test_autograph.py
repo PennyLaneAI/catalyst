47cd5fb9d334cb58d6f9494756f11de697715d2c
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2024-09-20 10:48:51 -0400

    codefactor

--- frontend/test/pytest/test_autograph.py
+++ frontend/test/pytest/test_autograph.py
@@ -2335,7 +2335,7 @@ class TestJaxIndexOperatorUpdate:
         def workflow(x):
 
             def test_multi_dimensional_index(x):
-                """Test that TypeError is raised when updating a JAX array with multi-dimx indexing."""
+                """Test that TypeError is raised when using multi-dim indexing."""
                 x[0, 1] += 5
                 return x
 
@@ -2343,8 +2343,8 @@ class TestJaxIndexOperatorUpdate:
             with pytest.raises(TypeError, match="JAX arrays are immutable"):
                 result = test_multi_dimensional_index(x)
 
-            def test_unsupported_operator(x, i, y):
-                """Test that TypeError is raised when updating a JAX array using an unsupported operator."""
+            def test_unsupported_operator(x):
+                """Test that TypeError is raised when using an unsupported operator."""
                 x[1] %= y
                 return x
 
@@ -2352,10 +2352,10 @@ class TestJaxIndexOperatorUpdate:
             with pytest.raises(TypeError, match="JAX arrays are immutable"):
                 result = test_unsupported_operator(x)
 
-            """Test that TypeError is raised when updating a JAX array using an array as index."""
+            """Test that TypeError is raised when using an array as index."""
 
             def test_array_index(x, i):
-                """Test that TypeError is raised when updating a JAX array using an array as index."""
+                """Test that TypeError is raised when using an array as index."""
                 x[np.array([1, 2])] += 3
                 return x
 
