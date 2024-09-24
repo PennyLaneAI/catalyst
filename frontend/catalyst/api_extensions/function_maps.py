Fix batch size calculation with vmap and static_argnums (#1150)
Resolves a bug accessing argument shapes in the vmap function when those
arguments don't have a shape. This might happen when using vmap together
with `static_argnums` for example:

```py
@qjit(static_argnums=0)
@vmap(in_axes=(None, 0))
def test(n, array):
    for _ in range(n):
        array = array * 2
    return array

test(10, np.random.rand(3,2))
```

Also improves error message for out of bounds axis specifier.

[sc-74291]

---------

Co-authored-by: Joey Carter <joseph.carter@xanadu.ai>

frontend/catalyst/api_extensions/function_maps.py
@@ -331,10 +331,18 @@ class VmapCallable(CatalystCallable):
         """
 
         batch_sizes = []
-        for arg, d in zip(args_flat, axes_flat):
-            shape = np.shape(arg) if arg.shape else (1,)
-            if d is not None and len(shape) > d:
+        for i, (arg, d) in enumerate(zip(args_flat, axes_flat)):
+            if d is None:
+                continue
+
+            shape = np.shape(arg)
+            if len(shape) > d:
                 batch_sizes.append(shape[d])
+            else:
+                raise ValueError(
+                    f"Axis specifier {d} is out of bounds for argument {i}. "
+                    f"The argument only has {len(shape)} dimensions."
+                )
 
         if any(size != batch_sizes[0] for size in batch_sizes[1:]):
             raise ValueError(