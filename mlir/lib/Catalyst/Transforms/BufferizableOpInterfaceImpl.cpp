323b09372db8dd7f4741166335fee975cc18396a
Tzung-Han Juang <tzunghan.juang@xanadu.ai>
2024-10-07 17:50:58 -0400

    Tentatively revert CustomCallOp bufferizesToMemoryWrite to false

--- mlir/lib/Catalyst/Transforms/BufferizableOpInterfaceImpl.cpp
+++ mlir/lib/Catalyst/Transforms/BufferizableOpInterfaceImpl.cpp
@@ -87,7 +87,7 @@ struct CustomCallOpInterface
     bool bufferizesToMemoryWrite(Operation *op, OpOperand &opOperand,
                                  const bufferization::AnalysisState &state) const
     {
-        return true;
+        return false;
     }
 
     bufferization::AliasingValueList
