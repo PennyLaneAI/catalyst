3e216f9880977ec4db1c6c27d93970b2d49ffa17
AmintorDusko <amintor_dusko@hotmail.com>
2024-10-09 13:11:39 -0400

    fix tests(?)

--- frontend/test/pytest/test_custom_devices.py
+++ frontend/test/pytest/test_custom_devices.py
@@ -188,7 +188,7 @@ def test_custom_device_bad_directory():
         """Custom Device"""
 
         name = "Custom Qubit"
-        short_name = "null.qubit"
+        short_name = "null.device"
         pennylane_requires = "0.33.0"
         version = "0.0.1"
         author = "Dummy"
