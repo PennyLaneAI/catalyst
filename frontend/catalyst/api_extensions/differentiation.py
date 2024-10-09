f812c4669290e3f82a3b097ce463e54bdcf7c9e7
Romain Moyard <rmoyard@gmail.com>
2024-10-09 09:02:46 -0400

    Unwanted update removal

--- frontend/catalyst/api_extensions/differentiation.py
+++ frontend/catalyst/api_extensions/differentiation.py
@@ -38,6 +38,7 @@ from catalyst.jax_primitives import (
     grad_p,
     jvp_p,
     probs_p,
+    quantum_kernel_p,
     value_and_grad_p,
     vjp_p,
 )
@@ -833,7 +834,7 @@ def _verify_differentiable_child_qnodes(jaxpr, method):
     def traverse_children(jaxpr):
         for eqn in jaxpr.eqns:
             primitive = eqn.primitive
-            if primitive is func_p:
+            if primitive in {func_p, quantum_kernel_p}:
                 child_jaxpr = eqn.params.get("call_jaxpr")
             elif primitive is grad_p:
                 child_jaxpr = eqn.params.get("jaxpr")
@@ -842,7 +843,7 @@ def _verify_differentiable_child_qnodes(jaxpr, method):
 
             _check_primitive_is_differentiable(primitive, method)
 
-            py_callable = eqn.params.get("fn")
+            py_callable = eqn.params.get("qnode")
             if py_callable not in visited:
                 if isinstance(py_callable, QNode):
                     _check_qnode_against_grad_method(py_callable, method, child_jaxpr)
