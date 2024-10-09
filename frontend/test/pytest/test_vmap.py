9b6120fff04f7e7eebf5455f9b04e0f3f4539d66
David Ittah <dime10@users.noreply.github.com>
2024-10-09 15:31:24 -0400

    Fix autograph not transforming wrapped functions in Catalyst callables (#1190)
    **Context:** Follow up on #835 which while fixing compilation errors
    when autograph is applied to Catalyst callables, it did not ensure the
    AST transformations are actually applied. The following program fails
    AutoGraph conversion:
    
    ```py
    @qml.qnode(qml.device("lightning.qubit", wires=1))
    def circuit(c, data):
        if c:
            qml.RY(data, wires=0)
        return qml.probs()
    
    fn = vmap(circuit, in_axes=(None, 0))
    qjit(autograph=True)(fn)(True, jnp.array([np.pi / 2] * 5))
    ```
    
    The primary issue is that several function transforms in Catalyst, like
    `vmap`, `grad`, `mitigate_with_zne`, etc. generate a new object wrapping
    the target function, and this object is generally excluded for autograph
    transformation because it's source code is in the Catalyst library. The
    existing autograph implementation had a few [special
    cases](https://github.com/PennyLaneAI/catalyst/blob/228a5196c9f7b3ed60680c7daa13cc0ecf8d47cb/frontend/catalyst/autograph/ag_primitives.py#L563-L570)
    programmed in for `QJIT` and `QNode` object, but this is not very
    scalable.
    
    This issue is distinct from the one ensuring the transform functions
    themselves (like `catalyst.jacobian`) work with autograph, which has
    [its own
    fix](https://github.com/PennyLaneAI/catalyst/blob/228a5196c9f7b3ed60680c7daa13cc0ecf8d47cb/frontend/catalyst/autograph/ag_primitives.py#L536-L545),
    and instead is about the callable objects generated by those functions
    (like `GradCallable`).
    
    **Description of the Change:** Introduce a new base class for Catalyst
    callables, `CatalystCallable`, which can be used to identify callable
    objects produced by function transforms. This allows identifying any
    such class in the autograph code without querying the exact class type,
    provided the relevant transforms in Catalyst inherit from this new base
    class.
    In addition, the class allows wrapping the target function in an
    additional function wrapper while invoking its `__call__` method, which
    is useful to apply the autograph transformation transparently to the
    target function.
    
    **Benefits:** Besides getting autograph working with vmap, grad, and
    zne, the following issues were also identified and fixed:
    - `scale_factors` argument in zne, which is mandatory, had a default
    argument
    - `functools.update_wrapper` will by default **overwrite attributes of
    the object** with attributes from the object being wrapped, so this
    command should come first in an `__init__` function. An alternative is
    to disable the member dictionary update all together.
    - vmap callable was missing a name similar to
    [other](https://github.com/PennyLaneAI/catalyst/blob/228a5196c9f7b3ed60680c7daa13cc0ecf8d47cb/frontend/catalyst/api_extensions/differentiation.py#L640)
    [transforms](https://github.com/PennyLaneAI/catalyst/blob/228a5196c9f7b3ed60680c7daa13cc0ecf8d47cb/frontend/catalyst/api_extensions/error_mitigation.py#L168)
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    [sc-75062]
    
    ---------
    
    Co-authored-by: Romain Moyard <rmoyard@gmail.com>

--- frontend/test/pytest/test_vmap.py
+++ frontend/test/pytest/test_vmap.py
@@ -782,3 +782,35 @@ class TestVectorizeMap:
         expected = jnp.array([0.93005586, 0.00498127, -0.88789978])
         assert jnp.allclose(result[0], expected)
         assert jnp.allclose(result[1], expected)
+
+    @pytest.mark.parametrize("arg", [10, "no", object()])
+    def test_vmap_with_shapeless_args(self, arg):
+        """Confirm that vmap works when used with shape-less values."""
+
+        @vmap(in_axes=(None, 0))
+        def f(n, arr):
+            return n * arr
+
+        batch_size = f._get_batch_size((arg, jnp.zeros((2, 5))), (None, 0), None)
+        assert batch_size == 2
+
+    def test_vmap_axis_out_of_bounds(self):
+        """Confirm that vmap works when used with shapeless values."""
+
+        @vmap(in_axes=(0, 2))
+        def f(a, b):
+            return a * b
+
+        with pytest.raises(ValueError, match="2 is out of bounds for argument 1"):
+            f._get_batch_size((jnp.zeros(5), jnp.zeros((2, 5))), (0, 2), None)
+
+    def test_vmap_no_aot(self):
+        """Test that vmap does not allow AOT compilation since type signatures are most likely to
+        be wrong."""
+
+        @qjit(target="mlir")
+        @vmap
+        def f(x: float):
+            return x**2
+
+        assert f.mlir is None
