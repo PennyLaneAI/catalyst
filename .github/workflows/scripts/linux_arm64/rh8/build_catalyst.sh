a3376c12d76e211a9fa9cb8b1c82327a7413cba2
Joey Carter <joseph.carter@xanadu.ai>
2024-10-10 11:59:06 -0400

    [no ci] Revert "[CI test] Install cmake system-wide before pip install"
    This reverts commit 037429701c9a863989470c6f28f68e29fa7c5161.

--- .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
+++ .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
@@ -15,7 +15,7 @@ export PYTHON_ALTERNATIVE_VERSION=$5
 
 # Install system dependencies
 dnf update -y 
-dnf install -y libzstd-devel gcc-toolset-${GCC_VERSION} cmake
+dnf install -y libzstd-devel gcc-toolset-${GCC_VERSION}
 if [ "$PYTHON_VERSION" != "3.10" ]; then
     dnf install -y ${PYTHON_PACKAGE} ${PYTHON_PACKAGE}-devel
 fi
