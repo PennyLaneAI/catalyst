037429701c9a863989470c6f28f68e29fa7c5161
Joey Carter <joseph.carter@xanadu.ai>
2024-10-10 11:24:03 -0400

    [CI test] Install cmake system-wide before pip install

--- .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
+++ .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
@@ -15,7 +15,7 @@ export PYTHON_ALTERNATIVE_VERSION=$5
 
 # Install system dependencies
 dnf update -y 
-dnf install -y libzstd-devel gcc-toolset-${GCC_VERSION}
+dnf install -y libzstd-devel gcc-toolset-${GCC_VERSION} cmake
 if [ "$PYTHON_VERSION" != "3.10" ]; then
     dnf install -y ${PYTHON_PACKAGE} ${PYTHON_PACKAGE}-devel
 fi
