698bdfedf230d295bd959c9cfa0f1988a03a1378
AmintorDusko <amintor_dusko@hotmail.com>
2024-10-11 12:32:27 -0400

    add C++ tests

--- runtime/lib/backend/null_qubit/NullQubit.hpp
+++ runtime/lib/backend/null_qubit/NullQubit.hpp
@@ -1,77 +1,299 @@
-#include <QuantumDevice.hpp>
+// Copyright 2022 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <complex>
+#include <memory>
+#include <optional>
+#include <random>
+#include <vector>
+
+#include "DataView.hpp"
+#include "QuantumDevice.hpp"
+#include "Types.h"
 
 namespace Catalyst::Runtime::Devices {
+
+/**
+ * @brief struct API for a null backend quantum device.
+ *
+ * This device API contains several NULL methods:
+ * - a set of methods to manage qubit allocations and deallocations, device shot
+ *   noise, and quantum tape recording as well as reference values for the result
+ *   data-type; these are used to implement Quantum Runtime (QR) instructions.
+ *
+ * - a set of methods for quantum operations, observables, measurements, and gradient
+ *   of the device; these are used to implement Quantum Instruction Set (QIS) instructions.
+ */
 struct NullQubit final : public Catalyst::Runtime::QuantumDevice {
     NullQubit(const std::string &kwargs = "{}") {}
     ~NullQubit() = default; // LCOV_EXCL_LINE
 
-    NullQubit &operator=(const QuantumDevice &) = delete;
+    NullQubit &operator=(const NullQubit &) = delete;
     NullQubit(const NullQubit &) = delete;
     NullQubit(NullQubit &&) = delete;
-    NullQubit &operator=(QuantumDevice &&) = delete;
+    NullQubit &operator=(NullQubit &&) = delete;
 
-    auto AllocateQubit() -> QubitIdType override { return 0; }
-    auto AllocateQubits(size_t num_qubits) -> std::vector<QubitIdType> override
+    /**
+     * @brief Doesn't Allocate a qubit.
+     *
+     * @return `QubitIdType`
+     */
+    auto AllocateQubit() -> QubitIdType { return 0; }
+
+    /**
+     * @brief Allocate a vector of qubits.
+     *
+     * @param num_qubits The number of qubits to allocate.
+     *
+     * @return `std::vector<QubitIdType>`
+     */
+    auto AllocateQubits(size_t num_qubits) -> std::vector<QubitIdType>
     {
-        return std::vector<QubitIdType>(num_qubits);
+        return std::vector<QubitIdType>(num_qubits, 0);
     }
-    [[nodiscard]] auto Zero() const -> Result override { return NULL; }
-    [[nodiscard]] auto One() const -> Result override { return NULL; }
+
+    /**
+     * @brief Doesn't Release a qubit.
+     */
+    void ReleaseQubit(QubitIdType) {}
+
+    /**
+     * @brief Doesn't Release all qubits.
+     */
+    void ReleaseAllQubits() {}
+
+    /**
+     * @brief Doesn't Get the number of allocated qubits.
+     *
+     * @return `size_t`
+     */
+    [[nodiscard]] auto GetNumQubits() const -> size_t { return 0; }
+
+    /**
+     * @brief Doesn't Set the number of device shots.
+     *
+     * @param shots The number of noise shots
+     */
+    void SetDeviceShots(size_t shots) {}
+
+    /**
+     * @brief Doesn't Get the number of device shots.
+     *
+     * @return `size_t`
+     */
+    [[nodiscard]] auto GetDeviceShots() const -> size_t { return 0; }
+
+    /**
+     * @brief Doesn't Set the PRNG of the device.
+     *
+     * The Catalyst runtime enables seeded program execution on non-hardware devices.
+     * A random number generator instance is managed by the runtime to predictably
+     * generate results for non-deterministic programs, such as those involving `Measure`
+     * calls.
+     * Devices implementing support for this feature do not need to use the provided
+     * PRNG instance as their sole source of random numbers, but it is expected that the
+     * the same instance state will predictable and reproducibly generate the same
+     * program results. It is also expected that the provided PRNG state is evolved
+     * sufficiently so that two device executions sharing the same instance do not produce
+     * identical results.
+     * The provided PRNG instance is not thread-locked, and devices wishing to share it
+     * across threads will need to provide their own thread-safety.
+     */
+    void SetDevicePRNG([[maybe_unused]] std::mt19937 *gen) {}
+
+    /**
+     * @brief Doesn't Start recording a quantum tape if provided.
+     *
+     * @note This is backed by the `Catalyst::Runtime::CacheManager<ComplexT>` property in
+     * the device implementation.
+     */
+    void StartTapeRecording() {}
+
+    /**
+     * @brief Doesn't Stop recording a quantum tape if provided.
+     *
+     * @note This is backed by the `Catalyst::Runtime::CacheManager<ComplexT>` property in
+     * the device implementation.
+     */
+    void StopTapeRecording() {}
+
+    /**
+     * @brief Not the Result value for "Zero" used in the measurement process.
+     *
+     * @return `Result`
+     */
+    [[nodiscard]] auto Zero() const -> Result { return NULL; }
+
+    /**
+     * @brief Not the Result value for "One"  used in the measurement process.
+     *
+     * @return `Result`
+     */
+    [[nodiscard]] auto One() const -> Result { return NULL; }
+
+    /**
+     * @brief Not A helper method to print the state vector of a device.
+     */
+    void PrintState() {}
+
+    /**
+     * @brief Doesn't Prepare subsystems using the given ket vector in the computational basis.
+     */
+    void SetState(DataView<std::complex<double>, 1> &, std::vector<QubitIdType> &) {}
+
+    /**
+     * @brief Doesn't Prepare a single computational basis state.
+     */
+    void SetBasisState(DataView<int8_t, 1> &, std::vector<QubitIdType> &) {}
+
+    /**
+     * @brief Doesn't Apply a single gate to the state vector of a device with its name if this is
+     * supported.
+     */
+    void NamedOperation(const std::string &name, const std::vector<double> &params,
+                        const std::vector<QubitIdType> &wires, bool inverse,
+                        const std::vector<QubitIdType> &controlled_wires = {},
+                        const std::vector<bool> &controlled_values = {})
+    {
+    }
+
+    /**
+     * @brief Doesn't Apply a given matrix directly to the state vector of a device.
+     *
+     */
+    void MatrixOperation(const std::vector<std::complex<double>> &,
+                         const std::vector<QubitIdType> &, bool,
+                         const std::vector<QubitIdType> &controlled_wires = {},
+                         const std::vector<bool> &controlled_values = {})
+    {
+    }
+
+    /**
+     * @brief Doesn't Construct a named (Identity, PauliX, PauliY, PauliZ, and Hadamard)
+     * or Hermitian observable.
+     *
+     * @return `ObsIdType` Index of the constructed observable
+     */
     auto Observable(ObsId, const std::vector<std::complex<double>> &,
-                    const std::vector<QubitIdType> &) -> ObsIdType override
+                    const std::vector<QubitIdType> &) -> ObsIdType
     {
-        return 0;
+        return 0.0;
     }
-    auto TensorObservable(const std::vector<ObsIdType> &) -> ObsIdType override { return 0; }
+
+    /**
+     * @brief Doesn't Construct a tensor product of observables.
+     *
+     * @return `ObsIdType` Index of the constructed observable
+     */
+    auto TensorObservable(const std::vector<ObsIdType> &) -> ObsIdType { return 0.0; }
+
+    /**
+     * @brief Doesn't Construct a Hamiltonian observable.
+     *
+     * @return `ObsIdType` Index of the constructed observable
+     */
     auto HamiltonianObservable(const std::vector<double> &, const std::vector<ObsIdType> &)
-        -> ObsIdType override
+        -> ObsIdType
     {
-        return 0;
+        return 0.0;
     }
-    auto Measure(QubitIdType, std::optional<int32_t>) -> Result override
+
+    /**
+     * @brief Doesn't Compute the expected value of an observable.
+     *
+     * @return `double` The expected value
+     */
+    auto Expval(ObsIdType) -> double { return 0.0; }
+
+    /**
+     * @brief Doesn't Compute the variance of an observable.
+     *
+     * @return `double` The variance
+     */
+    auto Var(ObsIdType) -> double { return 0.0; }
+
+    /**
+     * @brief Doesn't Get the state-vector of a device.
+     */
+    void State(DataView<std::complex<double>, 1> &) {}
+
+    /**
+     * @brief Doesn't Compute the probabilities of each computational basis state.
+     */
+    void Probs(DataView<double, 1> &) {}
+
+    /**
+     * @brief Doesn't Compute the probabilities for a subset of the full system.
+     */
+    void PartialProbs(DataView<double, 1> &, const std::vector<QubitIdType> &) {}
+
+    /**
+     * @brief Doesn't Compute samples with the number of shots on the entire wires,
+     * returing raw samples.
+     */
+    void Sample(DataView<double, 2> &, size_t) {}
+
+    /**
+     * @brief Doesn't Compute partial samples with the number of shots on `wires`,
+     * returing raw samples.
+     *
+     * @param samples The pre-allocated `DataView<double, 2>`representing a matrix of
+     * shape `shots * numWires`. The built-in iterator in `DataView<double, 2>`
+     * iterates over all elements of `samples` row-wise.
+     */
+    void PartialSample(DataView<double, 2> &, const std::vector<QubitIdType> &, size_t) {}
+
+    /**
+     * @brief Doesn't Sample with the number of shots on the entire wires, returning the
+     * number of counts for each sample.
+     */
+    void Counts(DataView<double, 1> &, DataView<int64_t, 1> &, size_t) {}
+
+    /**
+     * @brief Doesn't Partial sample with the number of shots on `wires`, returning the
+     * number of counts for each sample.
+     */
+    void PartialCounts(DataView<double, 1> &, DataView<int64_t, 1> &,
+                       const std::vector<QubitIdType> &, size_t)
+    {
+    }
+
+    /**
+     * @brief This is not A general measurement method that acts on a single wire.
+     *
+     * @return `Result` The measurement result
+     */
+    auto Measure(QubitIdType, std::optional<int32_t>) -> Result
     {
         bool *ret = (bool *)malloc(sizeof(bool));
         *ret = true;
         return ret;
     }
 
-    void ReleaseQubit(QubitIdType) override {}
-    void ReleaseAllQubits() override {}
-    [[nodiscard]] auto GetNumQubits() const -> size_t override { return 0; }
-    void SetDeviceShots(size_t shots) override {}
-    [[nodiscard]] auto GetDeviceShots() const -> size_t override { return 0; }
-    void StartTapeRecording() override {}
-    void StopTapeRecording() override {}
-    void PrintState() override {}
+    /**
+     * @brief Doesn't Compute the gradient of a quantum tape, that is cached using
+     * `Catalyst::Runtime::Simulator::CacheManager`, for a specific set of trainable
+     * parameters.
+     */
+    void Gradient(std::vector<DataView<double, 1>> &, const std::vector<size_t> &) {}
 
-    void NamedOperation(const std::string &name, const std::vector<double> &params,
-                        const std::vector<QubitIdType> &wires, bool inverse,
-                        const std::vector<QubitIdType> &controlled_wires,
-                        const std::vector<bool> &controlled_values) override
+    auto CacheManagerInfo()
+        -> std::tuple<size_t, size_t, size_t, std::vector<std::string>, std::vector<ObsIdType>>
     {
+        return {0, 0, 0, {}, {}};
     }
-
-    void MatrixOperation(const std::vector<std::complex<double>> &,
-                         const std::vector<QubitIdType> &, bool,
-                         const std::vector<QubitIdType> &controlled_wires,
-                         const std::vector<bool> &controlled_values) override
-    {
-    }
-
-    auto Expval(ObsIdType) -> double override { return 0.0; }
-    auto Var(ObsIdType) -> double override { return 0.0; }
-    void State(DataView<std::complex<double>, 1> &) override {}
-    void Probs(DataView<double, 1> &) override {}
-    void PartialProbs(DataView<double, 1> &, const std::vector<QubitIdType> &) override {}
-    void Sample(DataView<double, 2> &, size_t) override {}
-    void PartialSample(DataView<double, 2> &, const std::vector<QubitIdType> &, size_t) override {}
-    void Counts(DataView<double, 1> &, DataView<int64_t, 1> &, size_t) override {}
-
-    void PartialCounts(DataView<double, 1> &, DataView<int64_t, 1> &,
-                       const std::vector<QubitIdType> &, size_t) override
-    {
-    }
-
-    void Gradient(std::vector<DataView<double, 1>> &, const std::vector<size_t> &) override {}
 };
 } // namespace Catalyst::Runtime::Devices
