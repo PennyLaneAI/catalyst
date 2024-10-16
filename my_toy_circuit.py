02157077df5a6c0cb676ff3ea7e1c18344898a4e
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-10-16 13:56:10 -0400

    use instrumentation

--- my_toy_circuit.py
+++ my_toy_circuit.py
@@ -4,33 +4,51 @@ import pennylane as qml
 
 import catalyst
 from catalyst import qjit
+from catalyst.debug import instrumentation
 
-# dev = qml.device("lightning.qubit", wires=2, shots=5)
+import os
+
 dev = qml.device("lightning.qubit", wires=3)
-U = 1j / np.sqrt(2) * np.array([[1, 1], [1, -1]])
 
-pipeline = {"cancel_inverses":{}, "merge_rotations":{}}
+peephole_pipeline = {"cancel_inverses":{}, "merge_rotations":{}}
+
 
 # @qjit(seed=37, keep_intermediate=True)
 #@qjit(keep_intermediate=True)
-@qjit(circuit_transform_pipeline = pipeline)
+"""
+@qjit(pipelines = [
+    ("0_canonicalize", ["canonicalize"]),
+    ("peephole",
+    [
+    "builtin.module(remove-chained-self-inverse{func-name=circuit})",
+    "builtin.module(merge-rotations{func-name=circuit})"
+    ]
+    ), # peephole
+    ("inline_nested_module", ["inline-nested-module"]),
+    ], # pipelines =
+    #circuit_transform_pipeline = peephole_pipeline,
+    autograph=True,
+    keep_intermediate=True)
+"""
+@qjit(autograph=True,
+    keep_intermediate=True,
+    #circuit_transform_pipeline = peephole_pipeline,
+    )
 @qml.qnode(dev)
-def circuit(theta):
-    qml.ctrl(qml.GlobalPhase(0.123), 0)
-    qml.ctrl(qml.adjoint(qml.GlobalPhase(0.123)), 0)
-    qml.GlobalPhase(1.23)
-    qml.adjoint(qml.GlobalPhase(1.23))
-    qml.Hadamard(wires=[0])
-    qml.RX(theta, wires=[1])
-    qml.adjoint(qml.RX(theta, wires=[1]))
-    qml.QubitUnitary(U, wires=[2])
-    qml.adjoint(qml.QubitUnitary(U, wires=[2]))
-    qml.adjoint(qml.RY(12.34, wires=[1]))
-    qml.RY(12.34, wires=[1])
-    # return qml.expval(qml.PauliZ(wires=[0]))
-    # return qml.sample()
+def circuit(theta, loop_size):
+    for i in range(loop_size):
+        qml.Hadamard(0)
+        qml.Hadamard(0)
+        qml.RX(theta, wires=1)
+        qml.RX(-theta, wires=1)
     return qml.probs()
 
 
-res = circuit(12.3)
-print(res)
+os.remove("my_toy_circuit.yml")
+with instrumentation("my_toy_circuit", filename="my_toy_circuit.yml", detailed=True):
+    res = circuit(12.3, 10000000)
+    print(res)
+
+with open('my_toy_circuit.yml', 'r') as f:
+    print(f.read())
+
