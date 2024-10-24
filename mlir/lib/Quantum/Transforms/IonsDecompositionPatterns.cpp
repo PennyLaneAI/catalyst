9ce4f6ac704f52dcc2b310a92c9472f499594a55
Romain Moyard <rmoyard@gmail.com>
2024-10-24 13:46:53 -0400

    Update

--- mlir/lib/Quantum/Transforms/IonsDecompositionPatterns.cpp
+++ mlir/lib/Quantum/Transforms/IonsDecompositionPatterns.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define DEBUG_TYPE "merge-rotations"
+#define DEBUG_TYPE "ions-decomposition"
 
 #include "Quantum/IR/QuantumOps.h"
 #include "Quantum/Transforms/Patterns.h"
@@ -26,8 +26,9 @@ using llvm::dbgs;
 using namespace mlir;
 using namespace catalyst::quantum;
 
-static const mlir::StringSet<> rotationsSet = {"RX",  "RY",  "RZ",  "PhaseShift",
-                                               "CRX", "CRY", "CRZ", "ControlledPhaseShift"};
+constexpr double PI = 3.14159265358979323846;
+
+// Define map, name to function creating decomp
 
 namespace {
 
@@ -36,40 +37,6 @@ struct IonsDecompositionRewritePattern : public mlir::OpRewritePattern<CustomOp>
 
     mlir::LogicalResult matchAndRewrite(CustomOp op, mlir::PatternRewriter &rewriter) const override
     {
-        LLVM_DEBUG(dbgs() << "Simplifying the following operation:\n" << op << "\n");
-        auto loc = op.getLoc();
-        StringRef opGateName = op.getGateName();
-
-        if (!rotationsSet.contains(opGateName))
-            return failure();
-        ValueRange inQubits = op.getInQubits();
-        auto parentOp = dyn_cast_or_null<CustomOp>(inQubits[0].getDefiningOp());
-
-        VerifyParentGateAndNameAnalysis vpga(op);
-        if (!vpga.getVerifierResult()) {
-            return failure();
-        }
-
-        TypeRange outQubitsTypes = op.getOutQubits().getTypes();
-        TypeRange outQubitsCtrlTypes = op.getOutCtrlQubits().getTypes();
-        ValueRange parentInQubits = parentOp.getInQubits();
-        ValueRange parentInCtrlQubits = parentOp.getInCtrlQubits();
-        ValueRange parentInCtrlValues = parentOp.getInCtrlValues();
-
-        auto parentParams = parentOp.getParams();
-        auto params = op.getParams();
-        SmallVector<mlir::Value> sumParams;
-        for (auto [param, parentParam] : llvm::zip(params, parentParams)) {
-            mlir::Value sumParam =
-                rewriter.create<arith::AddFOp>(loc, parentParam, param).getResult();
-            sumParams.push_back(sumParam);
-        };
-        auto mergeOp = rewriter.create<CustomOp>(loc, outQubitsTypes, outQubitsCtrlTypes, sumParams,
-                                                 parentInQubits, opGateName, nullptr,
-                                                 parentInCtrlQubits, parentInCtrlValues);
-
-        op.replaceAllUsesWith(mergeOp);
-
         return success();
     }
 };
