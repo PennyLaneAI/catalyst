0802c60c9a59560fe9ecb939a08bd197151f0ff5
Romain Moyard <rmoyard@gmail.com>
2024-10-24 16:43:35 -0400

    Update header

--- mlir/lib/Quantum/Transforms/ions_decompositions.cpp
+++ mlir/lib/Quantum/Transforms/ions_decompositions.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Xanadu Quantum Technologies Inc.
+// Copyright 2024 Xanadu Quantum Technologies Inc.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define DEBUG_TYPE "merge-rotation"
+#define DEBUG_TYPE "ions-decomposition"
 
 #include "Catalyst/IR/CatalystDialect.h"
 #include "Quantum/IR/QuantumOps.h"
