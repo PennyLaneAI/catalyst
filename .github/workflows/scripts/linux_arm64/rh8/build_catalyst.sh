6275e1dd272f933c248b2f6d9df6e9b579ac9160
Raul Torres <raul_torres80@hotmail.com>
2024-10-25 17:35:07 +0300

    Update Lightning version

--- .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
+++ .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
@@ -45,7 +45,7 @@ cmake -S runtime -B runtime-build -G Ninja \
     -DPYTHON_INCLUDE_DIR=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/include/python${PYTHON_VERSION} \
     -DPYTHON_LIBRARY=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/lib \
     -Dpybind11_DIR=/opt/_internal/cpython-${PYTHON_VERSION}.${PYTHON_SUBVERSION}/lib/python${PYTHON_VERSION}/site-packages/pybind11/share/cmake/pybind11 \
-    -DLIGHTNING_GIT_TAG=73b5b496e400286beb7fe47a2895edaf562bd174 \
+    -DLIGHTNING_GIT_TAG=bb07e2d5c98445175427a40ddd0525bb92e0e11a \
     -DENABLE_LAPACK=OFF \
     -DENABLE_WARNINGS=OFF \
     -DENABLE_OPENQASM=ON \
