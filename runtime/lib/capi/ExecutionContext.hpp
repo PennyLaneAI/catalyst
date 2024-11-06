75dc517a6e2f2583da890b9e8198241933a7aef5
erick-xanadu <110487834+erick-xanadu@users.noreply.github.com>
2024-11-06 16:26:17 -0500

    [Runtime] Use GIL lock in the runtime instead of custom mutexes. (#624)
    **Context:** We have a custom mutex that denotes current use of the
    Python interpreter (a shared resource among threads). This custom mutex
    may be replaced by the lock found in pybind11.
    
    **Description of the Change:** Use the scope acquire lock.
    
    **Benefits:** No custom mutex, better DX with third party libraries. (No
    need to determine location of the mutex library).
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    TODO:
    
    - [ ] ~~add a python file and outline all strings that are input to
    exec~~
    - [x] OQC device
    - [x] release gil when executing the memory transfer
    - [x] write functions and classes for dlopen dlclose
    
    ---------
    
    Co-authored-by: Joey Carter <joseph.carter@xanadu.ai>

--- runtime/lib/capi/ExecutionContext.hpp
+++ runtime/lib/capi/ExecutionContext.hpp
@@ -28,7 +28,6 @@
 #include <unordered_set>
 
 #include "Exception.hpp"
-#include "Python.hpp"
 #include "QuantumDevice.hpp"
 #include "Types.h"
 
@@ -278,7 +277,6 @@ class ExecutionContext final {
 
     // ExecutionContext pointers
     std::unique_ptr<MemoryManager> memory_man_ptr{nullptr};
-    std::unique_ptr<PythonInterpreterGuard> py_guard{nullptr};
 
     // PRNG
     uint32_t *seed;
@@ -340,12 +338,6 @@ class ExecutionContext final {
             device->getQuantumDevicePtr()->SetDevicePRNG(nullptr);
         }
         device_pool.push_back(device);
-
-#ifdef __build_with_pybind11
-        if (!py_guard && device->getDeviceName() == "OpenQasmDevice" && !Py_IsInitialized()) {
-            py_guard = std::make_unique<PythonInterpreterGuard>(); // LCOV_EXCL_LINE
-        }
-#endif
 
         return device_pool[key];
     }
