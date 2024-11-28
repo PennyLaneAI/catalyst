f111e8c2cb2a6751a2209627c55dd6b7e3a9a9b3
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2024-11-28 16:11:45 -0500

    Remove undefined symbol

--- frontend/catalyst/jit.py
+++ frontend/catalyst/jit.py
@@ -38,7 +38,7 @@ from catalyst.debug.instruments import instrument
 from catalyst.from_plxpr import trace_from_pennylane
 from catalyst.jax_tracer import lower_jaxpr_to_mlir, trace_to_jaxpr
 from catalyst.logging import debug_logger, debug_logger_init
-from catalyst.passes import PipelineNameUniquer, _inject_transform_named_sequence
+from catalyst.passes import PipelineNameUniquer
 from catalyst.qfunc import QFunc
 from catalyst.tracing.contexts import EvaluationContext
 from catalyst.tracing.type_signatures import (
