e23c3517f29ce0a7489bdafe1a9eb349b0679010
Raul Torres <raul_torres80@hotmail.com>
2024-12-10 23:48:19 -0500

    Change to a decorator

--- standalone_plugin_wheel/standalone_plugin/__init__.py
+++ standalone_plugin_wheel/standalone_plugin/__init__.py
@@ -1,8 +1,27 @@
 import platform
 from pathlib import Path
 
-StandalonePluginPathExt = "so" if platform.system() == "Linux" else "dylib"
-StandalonePluginPath = Path(
-    Path(__file__).parent, f"lib/StandalonePlugin.{StandalonePluginPathExt}"
-)
-SwitchBarToFoo = "standalone-switch-bar-foo"
+from catalyst.passes import PassPlugin
+
+
+def SwitchBarToFoo(*flags, **valued_options):
+    """Applies the "standalone-switch-bar-foo" pass"""
+
+    SwitchBarToFoo.ext = "so" if platform.system() == "Linux" else "dylib"
+    SwitchBarToFoo.path = Path(Path(__file__).parent, f"lib/StandalonePlugin.{SwitchBarToFoo.ext}")
+
+    def decorator(qnode):
+
+        def qnode_call(*args, **kwargs):
+            pass_pipeline = kwargs.get("pass_pipeline", [])
+            pass_pipeline.append(
+                PassPlugin(
+                    SwitchBarToFoo.path, "standalone-switch-bar-foo", *flags, **valued_options
+                )
+            )
+            kwargs["pass_pipeline"] = pass_pipeline
+            return qnode(*args, **kwargs)
+
+        return qnode_call
+
+    return decorator
