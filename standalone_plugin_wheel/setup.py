d28d42eeb4e82efade2acf13bd4554b23ae46b96
Raul Torres <raul_torres80@hotmail.com>
2024-12-10 23:48:19 -0500

    Change to a decorator

--- standalone_plugin_wheel/setup.py
+++ standalone_plugin_wheel/setup.py
@@ -6,7 +6,7 @@ setup(
     description="The standalone plugin example as a python package",
     packages=["standalone_plugin"],
     include_package_data=True,
-    install_requires=["PennyLane-Catalyst"],
+    install_requires=["PennyLane", "PennyLane-Catalyst"],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Science/Research",
--- standalone_plugin_wheel/standalone_plugin/__init__.py
+++ standalone_plugin_wheel/standalone_plugin/__init__.py
@@ -1,8 +1,34 @@
 import platform
 from pathlib import Path
 
-StandalonePluginPathExt = "so" if platform.system() == "Linux" else "dylib"
-StandalonePluginPath = Path(
-    Path(__file__).parent, f"lib/StandalonePlugin.{StandalonePluginPathExt}"
-)
-SwitchBarToFoo = "standalone-switch-bar-foo"
+import pennylane as qml
+
+from catalyst.passes import PassPlugin
+
+
+def SwitchBarToFoo(*flags, **valued_options):
+    """Applies the "standalone-switch-bar-foo" pass"""
+
+    SwitchBarToFoo.ext = "so" if platform.system() == "Linux" else "dylib"
+    SwitchBarToFoo.path = Path(Path(__file__).parent, f"lib/StandalonePlugin.{SwitchBarToFoo.ext}")
+
+    def decorator(qnode):
+        if not isinstance(qnode, qml.QNode):
+            # Technically, this apply pass is general enough that it can apply to
+            # classical functions too. However, since we lack the current infrastructure
+            # to denote a function, let's limit it to qnodes
+            raise TypeError(f"A QNode is expected, got the classical function {qnode}")
+
+        def qnode_call(*args, **kwargs):
+            pass_pipeline = kwargs.get("pass_pipeline", [])
+            pass_pipeline.append(
+                PassPlugin(
+                    SwitchBarToFoo.path, "standalone-switch-bar-foo", *flags, **valued_options
+                )
+            )
+            kwargs["pass_pipeline"] = pass_pipeline
+            return qnode(*args, **kwargs)
+
+        return qnode_call
+
+    return decorator
