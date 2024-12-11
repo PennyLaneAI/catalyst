3b4fb1bb3ec87044a5eada1a375adc18609495aa
Raul Torres <raul_torres80@hotmail.com>
2024-12-11 00:34:04 -0500

    Use partial

--- standalone_plugin_wheel/standalone_plugin/__init__.py
+++ standalone_plugin_wheel/standalone_plugin/__init__.py
@@ -1,27 +1,11 @@
 import platform
+from functools import partial
 from pathlib import Path
 
-from catalyst.passes import PassPlugin
+from catalyst.passes import apply_pass_plugin
 
-
-def SwitchBarToFoo(*flags, **valued_options):
-    """Applies the "standalone-switch-bar-foo" pass"""
-
-    SwitchBarToFoo.ext = "so" if platform.system() == "Linux" else "dylib"
-    SwitchBarToFoo.path = Path(Path(__file__).parent, f"lib/StandalonePlugin.{SwitchBarToFoo.ext}")
-
-    def decorator(qnode):
-
-        def qnode_call(*args, **kwargs):
-            pass_pipeline = kwargs.get("pass_pipeline", [])
-            pass_pipeline.append(
-                PassPlugin(
-                    SwitchBarToFoo.path, "standalone-switch-bar-foo", *flags, **valued_options
-                )
-            )
-            kwargs["pass_pipeline"] = pass_pipeline
-            return qnode(*args, **kwargs)
-
-        return qnode_call
-
-    return decorator
+SwitchBarToFooExt = "so" if platform.system() == "Linux" else "dylib"
+SwitchBarToFooPath = Path(Path(__file__).parent, f"lib/StandalonePlugin.{SwitchBarToFooExt}")
+SwitchBarToFoo = partial(
+    apply_pass_plugin, plugin_name=SwitchBarToFooPath, pass_name="standalone-switch-bar-foo"
+)
