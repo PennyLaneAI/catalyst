PennyLaneAI/catalyst

commit 50781538350b659bdf5e708207daed8ba1bf4a15

[frontend] Add entry_points support for MLIR plugins (#1361)
**Context:** Add support for adding passes via entry_points.

**Description of the Change:** Adds support for the following syntax:

```python
def test_standalone_entry_point():
    """Generate MLIR for the standalone plugin via entry-point"""

    @apply_pass("standalone.standalone-switch-bar-foo") # notice that this is namespace.pass-name
    @qml.qnode(qml.device("lightning.qubit", wires=0))
    def qnode():
        return qml.state()

    @qml.qjit(target="mlir")
    def module():
        return qnode()

    assert "standalone-switch-bar-foo" in module.mlir
    print(module.mlir)
```

Also tests for support for the dictionary interface:

```python
def test_standalone_dictionary():
    """Generate MLIR for the standalone plugin via entry-point"""

    @pipeline({"standalone.standalone-switch-bar-foo": {}})
    @qml.qnode(qml.device("lightning.qubit", wires=0))
    def qnode():
        return qml.state()

    @qml.qjit(target="mlir")
    def module():
        return qnode()

    assert "standalone-switch-bar-foo" in module.mlir
```

**Benefits:** Entry Points!

**Possible Drawbacks:** Having entry points is essentially adding
dynamism, which means it may impact compile time.

**Related GitHub Issues:**

[sc-73572]

frontend/catalyst/example_entry_point/__init__.py
@@ -1,0 +1,26 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example module with entry points"""
+
+from pathlib import Path
+
+from catalyst.utils.runtime_environment import get_bin_path
+
+
+def name2pass(_name):
+    """Example entry point for standalone plugin"""
+    plugin_path = get_bin_path("cli", "CATALYST_BIN_DIR") + "/../lib/StandalonePlugin.so"
+    plugin = Path(plugin_path)
+    return plugin, "standalone-switch-bar-foo"

frontend/test/pytest/test_mlir_plugin.py
@@ -22,7 +22,7 @@ from pathlib import Path
 
 import pennylane as qml
 
-from catalyst.passes import apply_pass, apply_pass_plugin
+from catalyst.passes import apply_pass, apply_pass_plugin, pipeline
 from catalyst.utils.runtime_environment import get_bin_path
 
 ext = "so" if platform.system() == "Linux" else "dylib"
@@ -53,6 +53,40 @@ def test_standalone_plugin_no_preregistration():
     plugin ahead of time in the qjit decorator"""
 
     @apply_pass_plugin(plugin, "standalone-switch-bar-foo")
+    @qml.qnode(qml.device("lightning.qubit", wires=0))
+    def qnode():
+        return qml.state()
+
+    @qml.qjit(target="mlir")
+    def module():
+        return qnode()
+
+    # It would be nice if we were able to combine lit tests with
+    # pytest
+    assert "standalone-switch-bar-foo" in module.mlir
+
+
+def test_standalone_entry_point():
+    """Generate MLIR for the standalone plugin via entry-point"""
+
+    @apply_pass("standalone.standalone-switch-bar-foo")
+    @qml.qnode(qml.device("lightning.qubit", wires=0))
+    def qnode():
+        return qml.state()
+
+    @qml.qjit(target="mlir")
+    def module():
+        return qnode()
+
+    # It would be nice if we were able to combine lit tests with
+    # pytest
+    assert "standalone-switch-bar-foo" in module.mlir
+
+
+def test_standalone_dictionary():
+    """Generate MLIR for the standalone plugin via entry-point"""
+
+    @pipeline({"standalone.standalone-switch-bar-foo": {}})
     @qml.qnode(qml.device("lightning.qubit", wires=0))
     def qnode():
         return qml.state()

frontend/catalyst/passes.py
@@ -34,6 +34,7 @@ individual Catalyst MLIR compiler passes.
 
 import copy
 import functools
+from importlib.metadata import entry_points
 from pathlib import Path
 from typing import TypeAlias
 
@@ -48,9 +49,18 @@ class Pass:
     """Class intended to hold options for passes"""
 
     def __init__(self, name, *options, **valued_options):
-        self.name = name
         self.options = options
         self.valued_options = valued_options
+        if "." in name:
+            resolution_functions = entry_points(group="catalyst.passes_resolution")
+            key, passname = name.split(".")
+            resolution_function = resolution_functions[key + ".passes"]
+            module = resolution_function.load()
+            path, name = module.name2pass(passname)
+            assert EvaluationContext.is_tracing()
+            EvaluationContext.add_plugin(path)
+
+        self.name = name
 
     def __repr__(self):
         return (
@@ -81,7 +91,7 @@ def dictionary_to_tuple_of_passes(pass_pipeline: PipelineDict):
     passes = tuple()
     pass_names = _API_name_to_pass_name()
     for API_name, pass_options in pass_pipeline.items():
-        name = pass_names[API_name]
+        name = pass_names.get(API_name, API_name)
         passes += (Pass(name, **pass_options),)
     return passes
 

setup.py
@@ -96,6 +96,9 @@ entry_points = {
         "cuda_quantum.ops = catalyst.api_extensions",
         "cuda_quantum.qjit = catalyst.third_party.cuda:cudaqjit",
     ],
+    "catalyst.passes_resolution": [
+        "standalone.passes = catalyst.example_entry_point",
+    ],
 }
 
 classifiers = [

commit ef066f30aa2046019430342d4945c0c096ef5caf

Minor fixes to standalone plugin and makefile (#1375)
Cleans up some duplication in the make targets and updates the clean
command with all the latest artifacts that may be generated during
development.

Also fixes a bug on macOS introduced in #1361

.github/workflows/build-wheel-linux-x86_64.yaml
@@ -393,7 +393,7 @@ jobs:
         CCACHE_DIR="$(pwd)/.ccache" \
         MLIR_DIR="$GITHUB_WORKSPACE/llvm-build/lib/cmake/mlir" \
         LLVM_BUILD_DIR="$GITHUB_WORKSPACE/llvm-build" \
-        make standalone-plugin
+        make plugin
         mkdir -p quantum-build/lib
         mv mlir/standalone/build/lib/StandalonePlugin.so quantum-build/lib/
 

.github/workflows/build-wheel-macos-arm64.yaml
@@ -366,7 +366,7 @@ jobs:
       run: |
         MLIR_DIR="$GITHUB_WORKSPACE/llvm-build/lib/cmake/mlir" \
         LLVM_BUILD_DIR="$GITHUB_WORKSPACE/llvm-build" \
-        make standalone-plugin
+        make plugin
         mkdir -p quantum-build/lib
         mv mlir/standalone/build/lib/StandalonePlugin.* quantum-build/lib
 

.github/workflows/build-wheel-macos-x86_64.yaml
@@ -345,7 +345,7 @@ jobs:
         CCACHE_DIR="$(pwd)/.ccache" \
         LLVM_BUILD_DIR="$GITHUB_WORKSPACE/llvm-build" \
         MLIR_DIR="$(pwd)/llvm-build/lib/cmake/mlir" \
-        make standalone-plugin
+        make plugin
         mkdir -p quantum-build/lib
         mv mlir/standalone/build/lib/StandalonePlugin.* quantum-build/lib
 

.github/workflows/check-catalyst.yaml
@@ -391,7 +391,7 @@ jobs:
         CXX_COMPILER=$(which ${{ needs.constants.outputs[format('cxx_compiler.{0}', matrix.compiler)] }}) \
         LLVM_BUILD_DIR="$(pwd)/llvm-build" \
         MLIR_DIR="$(pwd)/llvm-build/lib/cmake/mlir" \
-        make standalone-plugin
+        make plugin
         mkdir -p $(pwd)/quantum-build/lib
         mv mlir/standalone/build/lib/StandalonePlugin.so "$(pwd)/quantum-build/lib"
 
@@ -457,7 +457,7 @@ jobs:
       with:
         name: runtime-build-${{ matrix.compiler }}
         path: runtime-build/lib
-    
+
     - name: Download OQC-Runtime Artifact
       uses: actions/download-artifact@v4
       with:

.github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
@@ -92,7 +92,7 @@ export OQC_BUILD_DIR=/catalyst/oqc-build
 export OQD_BUILD_DIR=/catalyst/oqd-build
 export ENZYME_BUILD_DIR=/catalyst/enzyme-build
 export PYTHON=/usr/bin/python3
-make standalone-plugin
+make plugin
 make wheel
 
 # Exclude libopenblas as we rely on the openblas/lapack library shipped by scipy

Makefile
@@ -86,7 +86,7 @@ help:
 
 .PHONY: all catalyst
 all: runtime oqc oqd mlir frontend
-catalyst: runtime dialects frontend
+catalyst: runtime dialects plugin frontend
 
 .PHONY: frontend
 frontend:
@@ -123,7 +123,7 @@ oqd:
 	$(MAKE) -C frontend/catalyst/third_party/oqd/src oqd
 
 .PHONY: test test-runtime test-frontend lit pytest test-demos test-oqc test-oqd test-toml-spec
-test: test-runtime standalone-plugin test-frontend test-demos
+test: test-runtime test-frontend test-demos
 
 test-toml-spec:
 	$(PYTHON) ./bin/toml-check.py $(TOML_SPECS)
@@ -142,7 +142,7 @@ test-oqc:
 test-oqd:
 	$(MAKE) -C frontend/catalyst/third_party/oqd/src test
 
-lit: standalone-plugin
+lit:
 ifeq ($(ENABLE_ASAN),ON)
 ifneq ($(findstring clang,$(C_COMPILER)),clang)
 	@echo "Build and Test with Address Sanitizer are only supported by Clang, but provided $(C_COMPILER)"
@@ -209,36 +209,31 @@ wheel:
 	$(PYTHON) -m pip wheel --no-deps . -w dist
 
 	rm -r $(MK_DIR)/build
+	rm -r frontend/PennyLane_Catalyst.egg-info
 
 .PHONY: clean clean-all
 clean:
 	@echo "uninstall catalyst and delete all temporary and cache files"
 	$(PYTHON) -m pip uninstall -y pennylane-catalyst
-	rm -rf $(MK_DIR)/frontend/mlir_quantum $(MK_DIR)/frontend/catalyst/lib
+	find frontend/catalyst -name "*.so" -exec rm -v {} +
+	git restore frontend/catalyst/_configuration.py
+	rm -rf $(MK_DIR)/frontend/catalyst/_revision.py
+	rm -rf $(MK_DIR)/frontend/mlir_quantum $(MK_DIR)/frontend/catalyst/lib $(MK_DIR)/frontend/catalyst/bin
 	rm -rf dist __pycache__
 	rm -rf .coverage coverage_html_report
+	rm -rf .benchmarks
 
-clean-all: clean-frontend clean-mlir clean-runtime clean-oqc clean-oqd clean-standalone-plugin
-	@echo "uninstall catalyst and delete all temporary, cache, and build files"
-	$(PYTHON) -m pip uninstall -y pennylane-catalyst
-	rm -rf dist __pycache__
-	rm -rf .coverage coverage_html_report/
+clean-all: clean clean-mlir clean-runtime clean-oqc clean-oqd
 
-.PHONY: clean-standalone-plugin
-clean-standalone-plugin:
-	rm -rf  $(MK_DIR)/mlir/build/lib/StandalonePlugin.*
-	rm -rf  $(MK_DIR)/mlir/standalone/build/lib/StandalonePlugin.*
-
-.PHONY: clean-frontend
-clean-frontend:
-	find frontend/catalyst -name "*.so" -exec rm -v {} +
-
-.PHONY: clean-mlir clean-dialects clean-llvm clean-mhlo clean-enzyme
+.PHONY: clean-mlir clean-dialects clean-plugin clean-llvm clean-mhlo clean-enzyme
 clean-mlir:
 	$(MAKE) -C mlir clean
 
 clean-dialects:
 	$(MAKE) -C mlir clean-dialects
+
+clean-plugin:
+	$(MAKE) -C mlir clean-plugin
 
 clean-llvm:
 	$(MAKE) -C mlir clean-llvm
@@ -274,9 +269,9 @@ endif
 coverage-runtime:
 	$(MAKE) -C runtime coverage
 
-.PHONY: standalone-plugin
-standalone-plugin:
-	$(MAKE) -C mlir standalone-plugin
+.PHONY: plugin
+plugin:
+	$(MAKE) -C mlir plugin
 
 .PHONY: format
 format:

frontend/catalyst/example_entry_point/__init__.py
@@ -14,6 +14,7 @@
 
 """Example module with entry points"""
 
+import platform
 from pathlib import Path
 
 from catalyst.utils.runtime_environment import get_bin_path
@@ -21,6 +22,7 @@ from catalyst.utils.runtime_environment import get_bin_path
 
 def name2pass(_name):
     """Example entry point for standalone plugin"""
-    plugin_path = get_bin_path("cli", "CATALYST_BIN_DIR") + "/../lib/StandalonePlugin.so"
+    ext = "so" if platform.system() == "Linux" else "dylib"
+    plugin_path = get_bin_path("cli", "CATALYST_BIN_DIR") + f"/../lib/StandalonePlugin.{ext}"
     plugin = Path(plugin_path)
     return plugin, "standalone-switch-bar-foo"

mlir/Makefile
@@ -50,7 +50,7 @@ help:
 	@echo "  format [version=?] to apply C++ formatter; use with 'version={version}' to run clang-format-{version} instead of clang-format"
 
 .PHONY: all
-all: llvm mhlo enzyme dialects standalone-plugin
+all: llvm mhlo enzyme dialects plugin
 
 .PHONY: llvm
 llvm:
@@ -121,8 +121,8 @@ enzyme:
 
 	cmake --build $(ENZYME_BUILD_DIR) --target EnzymeStatic-19
 
-.PHONY: standalone-plugin
-standalone-plugin:
+.PHONY: plugin
+plugin:
 	[ -f $(MK_DIR)/standalone ] || cp -r $(MK_DIR)/llvm-project/mlir/examples/standalone .
 	@if patch -p0 --dry-run -N < $(MK_DIR)/patches/test-plugin-with-catalyst.patch > /dev/null 2>&1; then \
 		patch -p0 < $(MK_DIR)/patches/test-plugin-with-catalyst.patch; \
@@ -193,6 +193,7 @@ clean-enzyme:
 clean-plugin:
 	@echo "clean plugin"
 	rm -rf standalone/build
+	rm -rf $(DIALECTS_BUILD_DIR)/lib/StandalonePlugin.*
 
 .PHONY: format
 format:

frontend/test/pytest/test_mlir_plugin.py
@@ -21,6 +21,7 @@ import platform
 from pathlib import Path
 
 import pennylane as qml
+import pytest
 
 from catalyst.passes import apply_pass, apply_pass_plugin, pipeline
 from catalyst.utils.runtime_environment import get_bin_path
@@ -98,3 +99,7 @@ def test_standalone_dictionary():
     # It would be nice if we were able to combine lit tests with
     # pytest
     assert "standalone-switch-bar-foo" in module.mlir
+
+
+if __name__ == "__main__":
+    pytest.main(["-x", __file__])