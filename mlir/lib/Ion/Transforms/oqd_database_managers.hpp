4e2611f7d6f49a319bdd24513681a50fddffea68
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-12-12 15:38:16 -0500

    toml array to std vector utility docstring

--- mlir/lib/Ion/Transforms/oqd_database_managers.hpp
+++ mlir/lib/Ion/Transforms/oqd_database_managers.hpp
@@ -33,6 +33,13 @@ static const std::string oqd_gate_decomposition_parameters_toml_file_path =
 
 template <typename T> std::vector<T> tomlArray2StdVector(const toml::array &arr)
 {
+    // A toml node can contain toml objects of arbitrary types, even other toml nodes
+    // i.e. toml nodes are similar to pytrees
+    // Therefore, toml++ does not provide a simple "toml array to std vector" converter
+    //
+    // For a "leaf" array node, whose contents are now simple values,
+    // such a utility would come in handy.
+
     std::vector<T> vec;
 
     if constexpr (std::is_same_v<T, int64_t>) {
