09e23ac2923c2513f20953924b29e4cea6561986
Joey Carter <joseph.carter@xanadu.ai>
2024-12-18 09:34:20 -0500

    Add missing includes

--- mlir/include/Ion/Transforms/oqd_database_managers.hpp
+++ mlir/include/Ion/Transforms/oqd_database_managers.hpp
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <algorithm>
 #include <cassert>
 
 #include <toml++/toml.hpp>
--- mlir/include/Ion/Transforms/oqd_database_types.hpp
+++ mlir/include/Ion/Transforms/oqd_database_types.hpp
@@ -14,6 +14,8 @@
 
 #pragma once
 
+#include <cstdint>
+#include <string>
 #include <vector>
 
 namespace {
