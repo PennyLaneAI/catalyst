9578f0c7476567d3302da4da1eda125062ee35e4
Joey Carter <joseph.carter@xanadu.ai>
2025-01-09 17:58:02 -0500

    [WIP] Add docstring for `ions_decomposition` pass

--- frontend/catalyst/passes.py
+++ frontend/catalyst/passes.py
@@ -519,7 +519,131 @@ def _API_name_to_pass_name():
 
 
 def ions_decomposition(qnode=None):  # pragma: nocover
-    """Apply decomposition pass at the MLIR level"""
+    """
+    Specify that the ``--ions-decomposition`` MLIR compiler pass should be
+    applied to the decorated QNode during :func:`~.qjit` compilation.
+
+    This compiler pass decomposes the gates from the set {
+    :class:`qml.T <pennylane.T>`,
+    :class:`qml.S <pennylane.S>`,
+    :class:`qml.Z <pennylane.Z>`,
+    :class:`qml.Hadamard <pennylane.Hadamard>`,
+    :class:`qml.PhaseShift <pennylane.PhaseShift>`,
+    :class:`qml.RZ <pennylane.RZ>`,
+    :class:`qml.CNOT <pennylane.CNOT>`
+    }
+    into gates from the set {RX, RY, MS}, where MS is the Mølmer–Sørensen gate,
+    commonly used by trapped-ion quantum devices.
+
+    .. note::
+
+        Unlike PennyLane :doc:`circuit transformations <introduction/compiling_circuits>`,
+        the QNode itself will not be changed or transformed by applying these
+        decorators.
+
+        As a result, circuit inspection tools such as :func:`~.draw` will continue
+        to display the circuit as written in Python.
+
+        To instead view the optimized circuit, the MLIR must be viewed
+        after the ``"QuantumCompilationPass"`` stage via the
+        :func:`~.get_compilation_stage` function.
+
+    Args:
+        fn (QNode): the QNode to apply the ions-decomposition pass to
+
+    Returns:
+        ~.QNode:
+
+    **Example**
+
+    .. code-block:: python
+
+        import pennylane as qml
+        from pennylane.devices import NullQubit
+
+        import catalyst
+        from catalyst import qjit
+        from catalyst.debug import get_compilation_stage
+
+
+        @qjit(keep_intermediate=True)
+        @catalyst.passes.ions_decomposition
+        @qml.qnode(NullQubit(2))
+        def circuit():
+            qml.Hadamard(wires=[0])
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliY(wires=0))
+
+
+    >>> print(get_compilation_stage(circuit, stage="QuantumCompilationPass"))
+    module @circuit {
+      func.func public @jit_circuit() -> tensor<f64> attributes {llvm.emit_c_interface} {
+        %0 = call @circuit_0() : () -> tensor<f64>
+        return %0 : tensor<f64>
+      }
+      func.func public @circuit_0() -> tensor<f64> attributes {diff_method = "parameter-shift", llvm.linkage = #llvm.linkage<internal>, qnode} {
+        %c0_i64 = arith.constant 0 : i64
+        %cst = arith.constant 0.000000e+00 : f64
+        %cst_0 = arith.constant 1.5707963267948966 : f64
+        %cst_1 = arith.constant 3.1415926535897931 : f64
+        %cst_2 = arith.constant -1.5707963267948966 : f64
+        quantum.device shots(%c0_i64) ["catalyst/runtime/build/lib/librtd_null_qubit.so", "NullQubit", "{'shots': 0}"]
+        %0 = quantum.alloc( 2) : !quantum.reg
+        %1 = quantum.extract %0[ 0] : !quantum.reg -> !quantum.bit
+        %out_qubits = quantum.custom "RX"(%cst) %1 : !quantum.bit
+        %out_qubits_3 = quantum.custom "RY"(%cst_0) %out_qubits : !quantum.bit
+        %out_qubits_4 = quantum.custom "RX"(%cst_1) %out_qubits_3 : !quantum.bit
+        %2 = quantum.extract %0[ 1] : !quantum.reg -> !quantum.bit
+        %out_qubits_5 = quantum.custom "RY"(%cst_0) %out_qubits_4 : !quantum.bit
+        %out_qubits_6:2 = quantum.custom "MS"(%cst_0) %out_qubits_5, %2 : !quantum.bit, !quantum.bit
+        %out_qubits_7 = quantum.custom "RX"(%cst_2) %out_qubits_6#0 : !quantum.bit
+        %out_qubits_8 = quantum.custom "RY"(%cst_2) %out_qubits_6#1 : !quantum.bit
+        %out_qubits_9 = quantum.custom "RY"(%cst_2) %out_qubits_7 : !quantum.bit
+        %3 = quantum.namedobs %out_qubits_8[ PauliY] : !quantum.obs
+        %4 = quantum.expval %3 : f64
+        %from_elements = tensor.from_elements %4 : tensor<f64>
+        %5 = quantum.insert %0[ 0], %out_qubits_8 : !quantum.reg, !quantum.bit
+        %6 = quantum.insert %5[ 1], %out_qubits_9 : !quantum.reg, !quantum.bit
+        quantum.dealloc %6 : !quantum.reg
+        quantum.device_release
+        return %from_elements : tensor<f64>
+      }
+      func.func @setup() {
+        quantum.init
+        return
+      }
+      func.func @teardown() {
+        quantum.finalize
+        return
+      }
+    }
+
+    You can see that the Hadamard gate has been decomposed to RX(0)RY(pi/2)RX(pi):
+
+    .. code-block:: mlir
+
+        %cst = arith.constant 0.000000e+00 : f64
+        %cst_0 = arith.constant 1.5707963267948966 : f64
+        %cst_1 = arith.constant 3.1415926535897931 : f64
+        ...
+        %out_qubits = quantum.custom "RX"(%cst) %1 : !quantum.bit
+        %out_qubits_3 = quantum.custom "RY"(%cst_0) %out_qubits : !quantum.bit
+        %out_qubits_4 = quantum.custom "RX"(%cst_1) %out_qubits_3 : !quantum.bit
+
+    and that the CNOT gate has been decomposed to its corresponding circuit
+    implementation using the RX, RY and MS gates:
+
+    .. code-block:: mlir
+
+        %cst_0 = arith.constant 1.5707963267948966 : f64
+        %cst_2 = arith.constant -1.5707963267948966 : f64
+        ...
+        %out_qubits_5 = quantum.custom "RY"(%cst_0) %out_qubits_4 : !quantum.bit
+        %out_qubits_6:2 = quantum.custom "MS"(%cst_0) %out_qubits_5, %2 : !quantum.bit, !quantum.bit
+        %out_qubits_7 = quantum.custom "RX"(%cst_2) %out_qubits_6#0 : !quantum.bit
+        %out_qubits_8 = quantum.custom "RY"(%cst_2) %out_qubits_6#1 : !quantum.bit
+        %out_qubits_9 = quantum.custom "RY"(%cst_2) %out_qubits_7 : !quantum.bit
+    """
 
     if not isinstance(qnode, qml.QNode):
         raise TypeError(f"A QNode is expected, got the classical function {qnode}")
