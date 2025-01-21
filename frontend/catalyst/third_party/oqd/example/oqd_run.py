57170924e9ef0ffab21af87c52236e7a69921537
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2025-01-21 17:01:37 -0500

    init: run a empty "hello oqd world!" capi stub

---
+++ frontend/catalyst/third_party/oqd/example/oqd_run.py
@@ -0,0 +1,21 @@
+import pennylane as qml
+import pytest
+
+import catalyst
+from catalyst import qjit
+from catalyst.third_party.oqd import OQDDevice
+from catalyst.debug import get_compilation_stage, replace_ir
+
+dev = OQDDevice(backend="default", shots=1000, wires=8)
+
+@qjit#(keep_intermediate=True)
+@qml.qnode(dev)
+def f():
+    qml.Hadamard(wires=0)
+    return qml.probs()
+
+with open("6_llvm_ir.ll", "r") as file:
+    ir = file.read()
+replace_ir(f, "llvm_ir", ir)
+
+f()
