bf40a514ce7fb45bbfb6ef2a6b509023f68284e7
Raul Torres <raul_torres80@hotmail.com>
2025-02-04 14:38:57 -0500

    Add test with abstract measurement

--- frontend/test/pytest/test_capture_integration.py
+++ frontend/test/pytest/test_capture_integration.py
@@ -261,3 +261,27 @@ class TestCapture:
         default_capture_result = qml.qjit(circuit)(0.1)
         experimental_capture_result = qml.qjit(circuit, experimental_capture=True)(0.1)
         assert default_capture_result == experimental_capture_result
+
+    def test_cond_workflow_with_abstract_measurement(self, backend):
+        """Test the integration for a circuit with a cond primitive containing an
+        abstract measurement."""
+
+        @qml.qnode(qml.device(backend, wires=1))
+        def circuit(x: float):
+
+            def ansatz_true():
+                qml.RX(x, wires=0)
+                qml.Hadamard(wires=0)
+                qml.state()  # Abstract measurement
+
+            def ansatz_false():
+                qml.RY(x, wires=0)
+                qml.state()  # Abstract measurement
+
+            qml.cond(x > 1.4, ansatz_true, ansatz_false)()
+
+            return qml.expval(qml.Z(0))
+
+        default_capture_result = qml.qjit(circuit)(0.1)
+        experimental_capture_result = qml.qjit(circuit, experimental_capture=True)(0.1)
+        assert default_capture_result == experimental_capture_result
