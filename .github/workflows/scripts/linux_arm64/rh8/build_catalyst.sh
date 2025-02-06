173704f6b399d44d0a1a64e0748e26738e1e2fb7
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-02-06 12:04:15 -0500

    wheels

--- .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
+++ .github/workflows/scripts/linux_arm64/rh8/build_catalyst.sh
@@ -42,7 +42,8 @@ cmake -S runtime -B runtime-build -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=runtime-build/lib \
     -DPython_EXECUTABLE=${PYTHON} \
-    -DENABLE_OPENQASM=ON
+    -DENABLE_OPENQASM=ON \
+    -DMLIR_DIR="/catalyst/llvm-build"
 cmake --build runtime-build --target rt_capi rtd_openqasm rtd_null_qubit
 
 # Build OQC
