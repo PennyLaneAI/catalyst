67c530b36de8d164f5251ec4c572525027e953a0
David Ittah <dime10@users.noreply.github.com>
2025-02-06 11:34:04 -0500

    Add Pauli Product dialect (QEC) (#1486)
    @sengthai We can use this as the target of our Pauli product passes.
    (feel free to adjust if the need arises)
    
    [sc-82892]
    
    ---------
    
    Co-authored-by: Sengthai Heng <sengthai37@gmail.com>

--- mlir/tools/quantum-lsp-server/quantum-lsp-server.cpp
+++ mlir/tools/quantum-lsp-server/quantum-lsp-server.cpp
@@ -20,6 +20,7 @@
 #include "Gradient/IR/GradientDialect.h"
 #include "Ion/IR/IonDialect.h"
 #include "Mitigation/IR/MitigationDialect.h"
+#include "QEC/IR/QECDialect.h"
 #include "Quantum/IR/QuantumDialect.h"
 
 #include "mhlo/IR/register.h"
@@ -31,6 +32,7 @@ int main(int argc, char **argv)
     mlir::registerAllDialects(registry);
     registry.insert<catalyst::CatalystDialect>();
     registry.insert<catalyst::quantum::QuantumDialect>();
+    registry.insert<catalyst::qec::QECDialect>();
     registry.insert<catalyst::gradient::GradientDialect>();
     registry.insert<catalyst::mitigation::MitigationDialect>();
     registry.insert<catalyst::ion::IonDialect>();
