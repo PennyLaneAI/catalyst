c1bc821b6d20241dfba42ee9e664162e2cee9dcd
paul0403 <paulhaochen.wang@gmail.com>
2025-02-12 12:09:38 -0500

    update oqd device.so path in test

--- runtime/tests/Test_OQDDevice.cpp
+++ runtime/tests/Test_OQDDevice.cpp
@@ -651,7 +651,7 @@ TEST_CASE("Test OpenAPL Program generation", "[OQD]")
     std::string this_file = __FILE__;
     std::string this_directory = this_file.substr(0, this_file.find("/Test_OQDDevice.cpp"));
     const auto [rtd_lib, rtd_name, rtd_kwargs] =
-        std::array<std::string, 3>{this_directory+"/../../frontend/catalyst/third_party/oqd/src/build/librtd_oqd.so", "oqd", R"({'shots': 0, 'mcmc': False}ION:
+        std::array<std::string, 3>{this_directory+"/../build/lib/librtd_oqd_device.so", "oqd", R"({'shots': 0, 'mcmc': False}ION:
       {
         "class_": "Ion",
         "mass": 171.0,
