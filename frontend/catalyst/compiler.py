e720cd8b65a26f9e0abd9f32759937b7dbd804aa
paul0403 <79805239+paul0403@users.noreply.github.com>
2025-02-18 10:28:18 -0500

    Update frontend/catalyst/compiler.py
    Co-authored-by: Mehrdad Malek <39844030+mehrdad2m@users.noreply.github.com>

--- frontend/catalyst/compiler.py
+++ frontend/catalyst/compiler.py
@@ -172,6 +172,7 @@ class LinkerDriver:
         ]
 
         # If OQD runtime capi is built, link to it as well
+        # TODO: This is not ideal and should be replaced when the compiler is device aware
         if os.path.isfile(os.path.join(rt_lib_path, "librt_OQD_capi" + file_extension)):
             default_flags.append("-lrt_OQD_capi")
 
