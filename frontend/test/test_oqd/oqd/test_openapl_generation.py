57446068928dbbc097456a761da9b8831fb902e5
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2025-02-20 10:43:34 -0500

    Fix paths

--- frontend/test/test_oqd/oqd/test_openapl_generation.py
+++ frontend/test/test_oqd/oqd/test_openapl_generation.py
@@ -43,11 +43,11 @@ class TestOpenAPL:
                 "func.func(ions-decomposition)",
                 "func.func(quantum-to-ion{"
                 + "device-toml-loc="
-                + "/Users/mehrdad.malek/catalyst/frontend/test/test_oqd/oqd/calibration_data/device.toml "
+                + "frontend/test/test_oqd/oqd/calibration_data/device.toml "
                 + "qubit-toml-loc="
-                + "/Users/mehrdad.malek/catalyst/frontend/test/test_oqd/oqd/calibration_data/qubit.toml "
+                + "frontend/test/test_oqd/oqd/calibration_data/qubit.toml "
                 + "gate-to-pulse-toml-loc="
-                + "/Users/mehrdad.malek/catalyst/frontend/test/test_oqd/oqd/calibration_data/gate.toml"
+                + "frontend/test/test_oqd/oqd/calibration_data/gate.toml"
                 + "})",
                 "convert-ion-to-llvm",
             ],
@@ -60,7 +60,7 @@ class TestOpenAPL:
         ),
     ]
 
-    output_f = "/Users/mehrdad.malek/catalyst/__openapl__output.json"
+    output_f = "__openapl__output.json"
 
     def test_RX_gate(self):
         """Test OpenAPL generation for a circuit with a single RX Gate."""
@@ -74,7 +74,7 @@ class TestOpenAPL:
 
         circuit(1.5708)
 
-        expected_f = "/Users/mehrdad.malek/catalyst/frontend/test/test_oqd/oqd/test_single_RX.json"
+        expected_f = "frontend/test/test_oqd/oqd/test_single_RX.json"
         with open(self.output_f, "r", encoding="utf-8") as f:
             catalyst_json = json.load(f)
 
@@ -104,13 +104,13 @@ class TestOpenAPL:
             catalyst_json = json.load(f)
 
         expected_f = (
-            "/Users/mehrdad.malek/catalyst/frontend/test/test_oqd/oqd/test_single_CNOT.json"
+            "frontend/test/test_oqd/oqd/test_single_CNOT.json"
         )
         with open(expected_f, "r", encoding="utf-8") as f:
             expected_json = json.load(f)
 
         assert sorted(catalyst_json.items()) == sorted(expected_json.items())
-        os.remove("/Users/mehrdad.malek/catalyst/__openapl__output.json")
+        os.remove("__openapl__output.json")
 
 
 if __name__ == "__main__":
