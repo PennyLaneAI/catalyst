f7e22d43a5170ba10049867660e3e682a2d5fa7e
paul0403 <paulhaochen.wang@gmail.com>
2025-02-27 09:36:28 -0500

    add for loop to probs(wires) pytest

--- frontend/test/pytest/test_measurement_dynamic_shapes.py
+++ frontend/test/pytest/test_measurement_dynamic_shapes.py
@@ -140,6 +140,11 @@ def test_dynamic_wires_probs_with_wires(backend, capfd):
 
         @qml.qnode(dev)
         def circ():
+            @catalyst.for_loop(0, num_qubits, 1)
+            def loop_0(i):
+                qml.RY(2.2, wires=i)
+
+            loop_0()
             qml.RX(1.23, wires=num_qubits-1)
             qml.RZ(3.45, wires=0)
             qml.CNOT(wires=[num_qubits-2, 1])
