PennyLaneAI/catalyst

commit 0b107d7646e8a1b5a4e6a44753d9dc5151880f7c

[Frontend] Refactor registering passes and caching primitive mechanisms (#1349)
**Context:** Registering a pass to run in MLIR was a bit messy. This
code simplifies that process.

**Description of the Change:**

* Removes the `apply_registered_pass_p` primitive and instead denotes
the pass as an object. This object is used as a JAX static parameter and
its properties are used to lower the proper transform schedule.
* Simplify the process of adding a pass to the pipeline by just
appending it to a kwargs sequence.
* Simplify the process of mlir_quantum_decomposition for the ion pass
(@joeycarter). Ideally we should have a field in the device that
corresponds to the default passes to be applied. This is instead of
detecting the device during the lowering and adding a pass specific for
that device.
* Refactors the caching mechanism for primitives.

**Benefits:** Less complexity, more maintainable code. We can still
improve upon this. This change makes it easier to implement loading pass
plugins from python.

**Possible Drawbacks:** Changes some of the original formulation of the
pipeline (instead of having a dictionary, users can now just pass a list
of Pass objects)

**Related GitHub Issues:**

frontend/test/lit/test_peephole_optimizations.py
@@ -69,12 +69,7 @@ def test_pipeline_lowering():
         qml.Hadamard(wires=[1])
         return qml.expval(qml.PauliY(wires=0))
 
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
+    # CHECK: pipeline=(remove-chained-self-inverse, merge-rotations)
     print_jaxpr(test_pipeline_lowering_workflow, 1.2)
 
     # CHECK: transform.named_sequence @__transform_main
@@ -112,19 +107,18 @@ def test_pipeline_lowering_keep_original():
         qml.Hadamard(wires=[1])
         return qml.expval(qml.PauliY(wires=0))
 
-    f_pipeline = pipeline(pass_pipeline=my_pipeline)(f)
+    f_pipeline = pipeline(my_pipeline)(f)
 
     @qjit(keep_intermediate=True)
     def test_pipeline_lowering_keep_original_workflow(x):
         return f(x), f_pipeline(x)
 
-    # CHECK: call_jaxpr=
-    # CHECK: call_jaxpr=
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
+    # COM: this if for f(x)
+    # CHECK: quantum_kernel
+    # COM: this if for f_pipeline(x)
+    # COM: Unfortunately, we don't have a nice repr for qnode
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(remove-chained-self-inverse, merge-rotations)
     print_jaxpr(test_pipeline_lowering_keep_original_workflow, 1.2)
 
     # COM: This is the one that is unchanged
@@ -182,18 +176,10 @@ def test_pipeline_lowering_global():
 
         return g(1.2), h(1.2)
 
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(remove-chained-self-inverse, merge-rotations)
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(remove-chained-self-inverse, merge-rotations)
     print_jaxpr(global_wf)
 
     # CHECK: transform.named_sequence @__transform_main
@@ -206,13 +192,13 @@ def test_pipeline_lowering_global():
     print_mlir(global_wf)
 
     # CHECK: func.func public @jit_global_wf()
-    # CHECK {{%.+}} = call @g_transformed(
-    # CHECK {{%.+}} = call @h_transformed(
-    # CHECK: func.func public @g_transformed
+    # CHECK {{%.+}} = call @g_0(
+    # CHECK {{%.+}} = call @h_0(
+    # CHECK: func.func public @g_0(
     # CHECK: {{%.+}} = quantum.custom "RX"({{%.+}}) {{%.+}} : !quantum.bit
     # CHECK-NOT: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
     # CHECK-NOT: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
-    # CHECK: func.func public @h_transformed
+    # CHECK: func.func public @h_0(
     # CHECK: {{%.+}} = quantum.custom "RX"({{%.+}}) {{%.+}} : !quantum.bit
     # CHECK-NOT: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
     # CHECK-NOT: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
@@ -246,7 +232,7 @@ def test_pipeline_lowering_globloc_override():
             qml.Hadamard(wires=[1])
             return qml.expval(qml.PauliY(wires=0))
 
-        @pipeline(pass_pipeline=local_pipeline)
+        @pipeline(local_pipeline)
         @qml.qnode(qml.device("lightning.qubit", wires=2))
         def h(x):
             qml.RX(x, wires=[0])
@@ -256,16 +242,10 @@ def test_pipeline_lowering_globloc_override():
 
         return g(1.2), h(1.2)
 
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK-NOT:   pass_name=remove-chained-self-inverse
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(remove-chained-self-inverse, merge-rotations)
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(merge-rotations,)
     print_jaxpr(global_wf)
 
     # CHECK: transform.named_sequence @__transform_main
@@ -278,13 +258,13 @@ def test_pipeline_lowering_globloc_override():
     print_mlir(global_wf)
 
     # CHECK: func.func public @jit_global_wf()
-    # CHECK {{%.+}} = call @g_transformed(
+    # CHECK {{%.+}} = call @g(
     # CHECK {{%.+}} = call @h_transformed(
-    # CHECK: func.func public @g_transformed
+    # CHECK: func.func public @g_0
     # CHECK: {{%.+}} = quantum.custom "RX"({{%.+}}) {{%.+}} : !quantum.bit
     # CHECK-NOT: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
     # CHECK-NOT: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
-    # CHECK: func.func public @h_transformed
+    # CHECK: func.func public @h_transformed_0
     # CHECK: {{%.+}} = quantum.custom "RX"({{%.+}}) {{%.+}} : !quantum.bit
     # CHECK: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
     # CHECK: {{%.+}} = quantum.custom "Hadamard"() {{%.+}} : !quantum.bit
@@ -336,14 +316,10 @@ def test_cancel_inverses_tracing_and_lowering():
         _h = h(xx)
         return _f, _g, _h
 
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: ]
-    # CHECK-NOT: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK-NOT:   pass_name=remove-chained-self-inverse
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(remove-chained-self-inverse,)
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(remove-chained-self-inverse,)
     print_jaxpr(test_cancel_inverses_tracing_and_lowering_workflow, 1.1)
 
     # CHECK: module @test_cancel_inverses_tracing_and_lowering_workflow
@@ -378,9 +354,8 @@ def test_cancel_inverses_tracing_and_lowering_outside_qjit():
         _f = f(xx)
         return _f
 
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=remove-chained-self-inverse
-    # CHECK: ]
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(remove-chained-self-inverse,)
     print_jaxpr(test_cancel_inverses_tracing_and_lowering_outside_qjit_workflow, 1.1)
 
     # CHECK: module @test_cancel_inverses_tracing_and_lowering_outside_qjit_workflow
@@ -561,14 +536,11 @@ def test_merge_rotations_tracing_and_lowering():
         _h = h(xx)
         return _f, _g, _h
 
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
-    # CHECK: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK:   pass_name=merge-rotations
-    # CHECK: ]
-    # CHECK-NOT: _:AbstractTransformMod() = apply_registered_pass[
-    # CHECK-NOT:   pass_name=merge-rotations
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(merge-rotations,)
+    # CHECK: quantum_kernel
+    # CHECK: pipeline=(merge-rotations,)
+    # CHECK: quantum_kernel
     print_jaxpr(test_merge_rotations_tracing_and_lowering_workflow, 1.1)
 
     # CHECK: module @test_merge_rotations_tracing_and_lowering_workflow

frontend/test/pytest/test_peephole_optimizations.py
@@ -189,7 +189,7 @@ def test_cancel_inverses_bad_usages():
             TypeError,
             match="A QNode is expected, got the classical function",
         ):
-            pipeline()(classical_func)
+            pipeline({})(classical_func)
 
         with pytest.raises(
             TypeError,

frontend/catalyst/jax_primitives_utils.py
@@ -1,0 +1,283 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module contains some helper functions for translating JAX
+primitives to MLIR"""
+
+import copy
+import functools
+
+import pennylane as qml
+from jax._src import core, util
+from jax._src.lib.mlir import ir
+from jax.interpreters import mlir
+from jaxlib.mlir.dialects.builtin import ModuleOp
+from jaxlib.mlir.dialects.func import CallOp
+from mlir_quantum.dialects._transform_ops_gen import (
+    ApplyRegisteredPassOp,
+    NamedSequenceOp,
+    YieldOp,
+)
+from mlir_quantum.dialects.catalyst import LaunchKernelOp
+
+
+def get_call_jaxpr(jaxpr):
+    """Extracts the `call_jaxpr` from a JAXPR if it exists.""" ""
+    for eqn in jaxpr.eqns:
+        if eqn.params.get("call_jaxpr"):
+            return eqn.params["call_jaxpr"]
+    raise AssertionError("No call_jaxpr found in the JAXPR.")
+
+
+def get_call_equation(jaxpr):
+    """Extracts the equation which has a call_jaxpr."""
+    for eqn in jaxpr.eqns:
+        if eqn.params.get("call_jaxpr"):
+            return eqn
+    raise AssertionError("No call_jaxpr found in the JAXPR.")
+
+
+def lower_jaxpr(ctx, jaxpr):
+    """Lowers a call primitive jaxpr, may be either func_p or quantum_kernel_p"""
+    equation = get_call_equation(jaxpr)
+    call_jaxpr = equation.params["call_jaxpr"]
+    callable_ = equation.params.get("fn")
+    if callable_ is None:
+        callable_ = equation.params.get("qnode")
+    pipeline = equation.params.get("pipeline")
+    return lower_callable(ctx, callable_, call_jaxpr, pipeline=pipeline)
+
+
+def lower_callable(ctx, callable_, call_jaxpr, pipeline=None):
+    """Lowers _callable to MLIR.
+
+    If callable_ is a qnode, then we will first create a module, then
+    create a FuncOp corresponding to call_jaxpr. Otherwise, a FuncOp
+    will be created in the current module. This function might
+    add more than one FuncOps. This depends on the contents of call_jaxpr.
+
+    Args:
+      ctx: LoweringRuleContext
+      callable_: python function
+      call_jaxpr: jaxpr representing callable_
+    Returns:
+      FuncOp
+    """
+    if pipeline is None:
+        pipeline = tuple()
+
+    if not isinstance(callable_, qml.QNode):
+        return get_or_create_funcop(ctx, callable_, call_jaxpr, pipeline)
+
+    return get_or_create_qnode_funcop(ctx, callable_, call_jaxpr, pipeline)
+
+
+def get_or_create_funcop(ctx, callable_, call_jaxpr, pipeline):
+    """Get funcOp from cache, or create it from scratch"""
+    key = (callable_, *pipeline)
+    if func_op := get_cached(ctx, key):
+        return func_op
+    func_op = lower_callable_to_funcop(ctx, callable_, call_jaxpr)
+    cache(ctx, key, func_op)
+    return func_op
+
+
+def lower_callable_to_funcop(ctx, callable_, call_jaxpr):
+    """Lower callable to either a FuncOp"""
+    if isinstance(call_jaxpr, core.Jaxpr):
+        call_jaxpr = core.ClosedJaxpr(call_jaxpr, ())
+
+    kwargs = {}
+    kwargs["ctx"] = ctx.module_context
+    if not isinstance(callable_, functools.partial):
+        name = callable_.__name__
+    else:
+        name = callable_.func.__name__ + ".partial"
+    kwargs["name"] = name
+    kwargs["jaxpr"] = call_jaxpr
+    kwargs["effects"] = []
+    kwargs["name_stack"] = ctx.name_stack
+    func_op = mlir.lower_jaxpr_to_fun(**kwargs)
+
+    if isinstance(callable_, qml.QNode):
+        func_op.attributes["qnode"] = ir.UnitAttr.get()
+        # "best", the default option in PennyLane, chooses backprop on the device
+        # if supported and parameter-shift otherwise. Emulating the same behaviour
+        # would require generating code to query the device.
+        # For simplicity, Catalyst instead defaults to parameter-shift.
+        diff_method = (
+            "parameter-shift" if callable_.diff_method == "best" else str(callable_.diff_method)
+        )
+        func_op.attributes["diff_method"] = ir.StringAttr.get(diff_method)
+
+    return func_op
+
+
+def get_or_create_qnode_funcop(ctx, callable_, call_jaxpr, pipeline):
+    """A wrapper around lower_qnode_to_funcop that will cache the FuncOp.
+
+    Args:
+      ctx: LoweringRuleContext
+      callable_: qml.Qnode
+      call_jaxpr: jaxpr representing callable_
+    Returns:
+      FuncOp
+    """
+    key = (callable_, *pipeline)
+    if func_op := get_cached(ctx, key):
+        return func_op
+    func_op = lower_qnode_to_funcop(ctx, callable_, call_jaxpr, pipeline)
+    cache(ctx, key, func_op)
+    return func_op
+
+
+def lower_qnode_to_funcop(ctx, callable_, call_jaxpr, pipeline):
+    """Lowers callable_ to MLIR.
+
+    Will create ModuleOp and then lower the callable_ to a
+    FuncOp inside the ModuleOp. The ModuleOp may have more
+    than one FuncOp. This depends on the contents of call_jaxpr.
+
+    Args:
+      ctx: LoweringRuleContext
+      callable_: qml.Qnode
+      call_jaxpr: jaxpr representing callable_
+    Returns:
+      FuncOp
+    """
+    assert isinstance(callable_, qml.QNode), "This function expects qnodes"
+
+    name = "module_" + callable_.__name__
+    # pylint: disable-next=no-member
+    with NestedModule(ctx, name) as module, ir.InsertionPoint(module.regions[0].blocks[0]) as ip:
+        transform_named_sequence_lowering(ctx, pipeline)
+        ctx.module_context.ip = ip
+        func_op = get_or_create_funcop(ctx, callable_, call_jaxpr, pipeline)
+        func_op.sym_visibility = ir.StringAttr.get("public")
+
+    return func_op
+
+
+def get_cached(ctx, key):
+    """Looks for key in the cache"""
+    return ctx.module_context.cached_primitive_lowerings.get(key)
+
+
+def cache(ctx, key, val):
+    """Caches value in cache with key"""
+    ctx.module_context.cached_primitive_lowerings[key] = val
+
+
+def get_symbolref(ctx, func_op):
+    """Get symbolref by deciding whether to constructo a symbolref or flatsymbolref"""
+    is_call_same_module = ctx.module_context.module.operation == func_op.parent
+    if is_call_same_module:
+        return ir.FlatSymbolRefAttr.get(func_op.name.value)
+    parent = func_op.parent
+    parent_name = parent.operation.attributes["sym_name"].value
+    child_name = func_op.name.value
+    return ir.SymbolRefAttr.get([parent_name, child_name])
+
+
+def create_call_op(ctx, func_op, *args):
+    """Create a func::CallOp from JAXPR."""
+    output_types = list(map(mlir.aval_to_ir_types, ctx.avals_out))
+    flat_output_types = util.flatten(output_types)
+    mlir_args = mlir.flatten_lowering_ir_args(args)
+    symbol_ref = get_symbolref(ctx, func_op)
+    is_call_same_module = ctx.module_context.module.operation == func_op.parent
+    constructor = CallOp if is_call_same_module else LaunchKernelOp
+    return constructor(flat_output_types, symbol_ref, mlir_args)
+
+
+def create_module_op(ctx, name):
+    """Create a module with name name"""
+
+    symbol_table = ctx.module_context.symbol_table
+    parent = ctx.module_context.module
+    with ir.InsertionPoint(parent.body):
+        module = ModuleOp()
+        symbol_attr = ir._symbolNameAttr(name, ctx.module_context.context)
+        module.operation.attributes["sym_name"] = symbol_attr
+        symbol_table.insert(module)
+
+    return module
+
+
+class NestedModule:
+    """Context manager for the nested module"""
+
+    def __init__(self, ctx, name):
+        self.ctx = ctx
+        self.moduleOp = create_module_op(ctx, name)
+        self.old_module_context = ctx.module_context
+
+    def __enter__(self):
+        self.ctx.module_context = copy.copy(self.ctx.module_context)
+        self.ctx.module_context.module = self.moduleOp
+        self.ctx.module_context.cached_primitive_lowerings = {}
+        return self.moduleOp
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.ctx.module_context = self.old_module_context
+
+
+def transform_named_sequence_lowering(jax_ctx: mlir.LoweringRuleContext, pipeline):
+    """Generate a transform module embedded in the current module and schedule
+    the transformations in pipeline"""
+
+    transform_mod_type = ir.OpaqueType.get("transform", 'op<"builtin.module">')
+    module = jax_ctx.module_context.module
+
+    # We wish to generate the transformer module, and place it in the top-level module
+    # The transformer module must be marked with the "transform.with_named_sequence" attribute
+    # The transformer module has a single block, and the block contains the
+    # "transform.named_sequence @__transform_main" operation
+
+    with ir.InsertionPoint(module.body):
+        transformer_module = ModuleOp()
+        with_named_sequence_attr = ir.UnitAttr.get(jax_ctx.module_context.context)
+        transformer_module.operation.attributes["transform.with_named_sequence"] = (
+            with_named_sequence_attr
+        )
+        bb_transformer = transformer_module.body
+
+    functype = ir.FunctionType.get(inputs=[transform_mod_type], results=[])
+    functype_attr = ir.TypeAttr.get(functype)
+
+    # Insert the transform.named_sequence op into the transformer module
+    # Note that InsertionPoint(Block) inserts after the last operation but still inside the block.
+    with ir.InsertionPoint(bb_transformer):
+        named_sequence_op = NamedSequenceOp(
+            sym_name="__transform_main",
+            function_type=functype_attr,
+        )
+
+        # transform.named_sequence op is the "main function" of the transform dialect
+        # and thus needs an entry block (which also should be its only block)
+        # The argument of the block is the payload module
+        bb_named_sequence = ir.Block.create_at_start(
+            named_sequence_op.body, arg_types=[transform_mod_type]
+        )
+
+        # The transform.named_sequence needs a terminator called "transform.yield"
+        with ir.InsertionPoint(bb_named_sequence):
+            target = bb_named_sequence.arguments[0]
+            for _pass in pipeline:
+                apply_registered_pass_op = ApplyRegisteredPassOp(
+                    result=transform_mod_type, target=target, pass_name=_pass.name
+                )
+                target = apply_registered_pass_op.result
+            transform_yield_op = YieldOp(operands_=[])  # pylint: disable=unused-variable
+
+    return named_sequence_op.results

frontend/catalyst/jax_tracer.py
@@ -97,7 +97,6 @@ from catalyst.jax_primitives import (
     var_p,
 )
 from catalyst.logging import debug_logger, debug_logger_init
-from catalyst.passes import _add_mlir_quantum_decomposition
 from catalyst.tracing.contexts import (
     EvaluationContext,
     EvaluationMode,
@@ -1145,9 +1144,6 @@ def trace_quantum_function(
         out_type: JAXPR output type (list of abstract values with explicitness flags).
         out_tree: PyTree shapen of the result
     """
-
-    # Add the decomposition passes with the transform dialect
-    _add_mlir_quantum_decomposition(device)
 
     with EvaluationContext(EvaluationMode.QUANTUM_COMPILATION) as ctx:
         # (1) - Classical tracing

frontend/catalyst/jit.py
@@ -637,9 +637,11 @@ class QJIT(CatalystCallable):
             params = {}
             params["static_argnums"] = kwargs.pop("static_argnums", static_argnums)
             params["_out_tree_expected"] = []
+            default_pass_pipeline = self.compile_options.circuit_transform_pipeline
+            pass_pipeline = params.get("pass_pipeline", default_pass_pipeline)
+            params["pass_pipeline"] = pass_pipeline
             return QFunc.__call__(
                 qnode,
-                pass_pipeline=self.compile_options.circuit_transform_pipeline,
                 *args,
                 **dict(params, **kwargs),
             )

commit ed7dc5a2442499742646c6e441e38e82d2e249ef

Fixed a bug when autograph is used with a pass like merge-rotations (#1541)
**Context:**
issue #1501, found a bug where using autograph in conjunction with
catalyst passes causes a crash. This issue is introduced by PR #1349,
where the `apply_registered_pass_p` is removed and passes are appended
to the qnode `kwargs`. In case of using autograph, this `kwarg` calld
`pass-pipeline` is passed down to the quantum function which does not
support such `kwarg` hence, causing the crash.

**Description of the Change:**
This PR resolves the issue by poping the `pass-pipeline` element from
the `kwargs` before passing it to the quantum function, and instead,
pass it to the call function of the newly created `qnode`

**Benefits:**
autograph can work when having catalyst pass decorators.

**Possible Drawbacks:**
None

**Related GitHub Issues:**
[sc-84121]

frontend/catalyst/autograph/ag_primitives.py
@@ -577,6 +577,8 @@ def converted_call(fn, args, kwargs, caller_fn_scope=None, options=None):
         # For QNode calls, since the class is not part of the Catalyst package, we manually add a
         # wrapper to correctly forward the quantum function call to autograph.
         if isinstance(fn, qml.QNode):
+            pass_pipeline = kwargs.pop("pass_pipeline", []) if kwargs is not None else []
+            new_kwargs = {"pass_pipeline": pass_pipeline} if pass_pipeline else {}
 
             @functools.wraps(fn.func)
             def qnode_call_wrapper():
@@ -585,7 +587,7 @@ def converted_call(fn, args, kwargs, caller_fn_scope=None, options=None):
             # Copy the original qnode but replace its function.
             new_qnode = copy.copy(fn)
             new_qnode.func = qnode_call_wrapper
-            return new_qnode()
+            return new_qnode(**new_kwargs)
 
         return ag_converted_call(fn, args, kwargs, caller_fn_scope, options)
 

frontend/test/lit/test_peephole_optimizations.py
@@ -272,6 +272,32 @@ def test_pipeline_lowering_globloc_override():
 
 test_pipeline_lowering_globloc_override()
 
+#
+# autograph
+#
+
+
+def test_single_pass_with_autograph():
+    """
+    Test tha peephole optimization work with autograph
+    """
+
+    @qjit(autograph=True, target="mlir")
+    @merge_rotations
+    @qml.qnode(qml.device("lightning.qubit", wires=1))
+    def f(x: float):
+        qml.RX(x, wires=0)
+        qml.RX(x, wires=0)
+        qml.Hadamard(wires=0)
+        return qml.expval(qml.PauliZ(0))
+
+    # CHECK: transform.named_sequence @__transform_main(
+    # CHECK-NEXT: transform.apply_registered_pass "merge-rotations" to {{%.+}}
+    # CHECK-NEXT: transform.yield
+    print(f.mlir)
+
+
+test_single_pass_with_autograph()
 
 #
 # cancel_inverses

frontend/test/pytest/test_autograph.py
@@ -27,6 +27,7 @@ from numpy.testing import assert_allclose
 
 from catalyst import *
 from catalyst.autograph.transformer import TRANSFORMER
+from catalyst.passes import merge_rotations
 from catalyst.utils.dummy import dummy_func
 from catalyst.utils.exceptions import CompileError
 