a71b14660c95bae6934a42c537c55450d1a5bf9e
paul0403 <paulhaochen.wang@gmail.com>
2025-03-04 14:23:29 -0500

    add conversion patterns for compbasis with qreg

--- mlir/lib/Quantum/Transforms/ConversionPatterns.cpp
+++ mlir/lib/Quantum/Transforms/ConversionPatterns.cpp
@@ -527,9 +527,47 @@ struct ComputationalBasisOpPattern : public OpConversionPattern<ComputationalBas
     {
         MLIRContext *ctx = getContext();
         const TypeConverter *conv = getTypeConverter();
+        // TODO: upstream mode attr into compbasis op itself?
+        StringRef mode;
 
-        rewriter.replaceOpWithNewOp<UnrealizedConversionCastOp>(
-            op, conv->convertType(ObservableType::get(ctx)), adaptor.getQubits());
+        SmallVector<Value> args;
+        if (adaptor.getQubits().size() != 0){
+            for (const Value &qubit : adaptor.getQubits()){
+                args.push_back(qubit);
+            }
+           mode = "qubits";
+        }
+        else if (adaptor.getQreg()){
+           // For qreg case, we also let the unrealized cast op carry
+           // the num_qubits SSA value for convenience
+           args.insert(args.end(),
+                       adaptor.getQreg());
+           mode = "qreg";
+
+            Value reg_value = op.getQreg();
+            Operation *reg_def = reg_value.getDefiningOp();
+            // Walk back to the original alloc of the register
+            // Only Alloc, Adjoint and Insert could have qreg as outputs
+            while (!isa<AllocOp>(reg_def)){
+                // TODO: switch case?
+                if (isa<AdjointOp>(reg_def)){
+                    reg_value = cast<AdjointOp>(reg_def).getQreg();
+                }
+                else if (isa<InsertOp>(reg_def)){
+                    reg_value = cast<InsertOp>(reg_def).getInQreg();
+                }
+                reg_def = reg_value.getDefiningOp();
+            }
+
+            auto allocOp = cast<AllocOp>(reg_def);
+            args.insert(args.end(),
+                       allocOp.getNqubits());
+        }
+
+        auto ucc = rewriter.create<UnrealizedConversionCastOp>(
+            op.getLoc(), conv->convertType(ObservableType::get(ctx)), args);
+        ucc->setAttr("mode", rewriter.getStringAttr(mode));
+        rewriter.replaceOp(op, ucc);
 
         return success();
     }
