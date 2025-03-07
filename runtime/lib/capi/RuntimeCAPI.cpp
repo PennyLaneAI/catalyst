3a2a9bce336d556bbdc15f8225331fe69d9ffe41
paul0403 <paulhaochen.wang@gmail.com>
2025-03-07 14:25:11 -0500

    update runtime release qubit array

--- runtime/lib/capi/RuntimeCAPI.cpp
+++ runtime/lib/capi/RuntimeCAPI.cpp
@@ -361,9 +361,12 @@ void __catalyst__rt__qubit_release(QUBIT *qubit)
 
 static int __catalyst__rt__qubit_release_array__impl(QirArray *qubit_array)
 {
-    getQuantumDevicePtr()->ReleaseAllQubits();
+    //getQuantumDevicePtr()->ReleaseAllQubits();
     std::vector<QubitIdType> *qubit_array_ptr =
         reinterpret_cast<std::vector<QubitIdType> *>(qubit_array);
+    for (auto qubit : *qubit_array_ptr) {
+        getQuantumDevicePtr()->ReleaseQubit(qubit);
+    }
     delete qubit_array_ptr;
     return 0;
 }
