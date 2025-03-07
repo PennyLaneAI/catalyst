badbe2ea32eea51f37a93fa1e6e5a7f2a354bb07
Raul Torres <raul_torres80@hotmail.com>
2025-03-07 02:21:13 -0500

    Remove unnecessary import

--- frontend/catalyst/from_plxpr.py
+++ frontend/catalyst/from_plxpr.py
@@ -15,7 +15,6 @@
 This submodule defines a utility for converting plxpr into Catalyst jaxpr.
 """
 # pylint: disable=protected-access
-from copy import copy
 from functools import partial
 from typing import Callable, Sequence
 
