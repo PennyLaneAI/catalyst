180bafff34ed8c410040bd1260d036d1ab81843b
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-03-13 18:15:35 +0000

    make interface private

---
+++ frontend/test/lit/test_opt.py
@@ -0,0 +1,54 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for opt"""
+
+# RUN: %PYTHON %s | FileCheck %s
+
+from textwrap import dedent
+
+from catalyst.compiler import _canonicalize, _opt
+
+
+def test_opt_happy_path():
+    """Test functionality of opt-tool"""
+    mlir = """
+    module {
+        // CHECK: llvm.func @foo
+        func.func @foo() {
+            return
+        }
+    }
+    """
+    output = _opt(input=mlir)
+    print(output)
+
+
+test_opt_happy_path()
+
+
+def test_opt_canonicalize():
+    mlir = """
+    module {
+        // CHECK: func.func @foo
+        func.func @foo() {
+            return
+        }
+    }
+    """
+    output = _canonicalize(input=mlir)
+    print(output)
+
+
+test_opt_canonicalize()
