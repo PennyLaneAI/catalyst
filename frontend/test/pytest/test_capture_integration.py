88be08063a1a6691962974bbba5c8cb2788ffa8e
Raul Torres <raul_torres80@hotmail.com>
2025-03-20 18:28:40 -0500

    Fix bug happenning when shots are used

--- frontend/test/pytest/test_capture_integration.py
+++ frontend/test/pytest/test_capture_integration.py
@@ -825,3 +825,21 @@ class TestCapture:
         no_capture_result = qml.qjit(func)()
         experimental_capture_result = captured_func()
         assert no_capture_result == experimental_capture_result
+
+    def test_shots_usage(self, backend):
+        """Test the integration for a circuit using shots explicitly."""
+
+        @qml.qnode(qml.device(backend, wires=2, shots=10))
+        def circuit(x: float):
+            @qml.for_loop(0, 2, 1)
+            def loop_0(i):
+                qml.Hadamard(wires=i)
+
+            loop_0()
+
+            qml.RX(x, wires=0)
+            return qml.sample()
+
+        captured_func = qml.qjit(circuit, experimental_capture=False, target="mlir")
+
+        assert "'shots': 10" in captured_func.mlir
