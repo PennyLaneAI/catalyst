e13325741a3fc422eee0a9404d07b34f193e6b56
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-04-01 13:21:53 +0000

    Remove target specific information

--- frontend/test/pytest/test_debug.py
+++ frontend/test/pytest/test_debug.py
@@ -527,22 +527,13 @@ class TestOptionsToCliFlags:
         # pylint: disable=line-too-long
         expected = textwrap.dedent(
             """
-        ; ModuleID = 'LLVMDialectModule'
-        source_filename = "LLVMDialectModule"
-        target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
-        target triple = "x86_64-unknown-linux-gnu"
-
         define void @foo() {
           ret void
         }
-
-        !llvm.module.flags = !{!0}
-
-        !0 = !{i32 2, !"Debug Info Version", i32 3}
         """
-        ).lstrip()
+        ).strip()
         # pylint: enable=line-too-long
-        assert expected == observed
+        assert expected in observed
 
 
 if __name__ == "__main__":
