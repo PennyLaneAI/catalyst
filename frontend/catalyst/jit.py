e2203a1f64dff7e915e84fd8812984ed3ceb796f
paul0403 <paulhaochen.wang@gmail.com>
2025-04-09 11:36:55 -0400

    joey's comments

--- frontend/catalyst/jit.py
+++ frontend/catalyst/jit.py
@@ -443,15 +443,15 @@ def qjit(
     .. details::
         :title: Qubit-invariant compilation
 
-        A powerful feature of Catalyst is that the same compiled qnode can be evoked with
+        A powerful feature of Catalyst is that the same compiled QNode can be invoked with
         different number of qubits. This is especially helpful for workflows where you would
         like to, for example, iterate through the wires without knowing how many of them
         there are in advance. For instance, many workflows (such as `Grover's algorithm
         <https://pennylane.ai/qml/demos/tutorial_grovers_algorithm>`_) have
         an entangling layer at the beginning, where a Hadamard gate is applied to every wire.
 
-        To use this feature the PennyLane device needs to be instantiated within the qjitted
-        function, or another function within its call graph. The `wires` parameter can then be
+        To use this feature, the PennyLane device needs to be instantiated within the qjitted
+        function, or another function within its call graph. The ``wires`` parameter can then be
         any (integer) program value, for example one of the function arguments.
 
         .. code-block:: python
