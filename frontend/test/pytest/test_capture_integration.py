f929fccbf479d8b9a23175e6729572493701c5c0
paul0403 <paulhaochen.wang@gmail.com>
2025-04-17 15:02:46 -0400

    xfail plxpr tests that need to wait for core PL's jax update: jax's broadcast_in_dim primitive got a new kwarg `out_sharding`

--- frontend/test/pytest/test_capture_integration.py
+++ frontend/test/pytest/test_capture_integration.py
@@ -681,6 +681,13 @@ class TestCapture:
             == rotations_inverses_result
         )
 
+    @pytest.mark.xfail(
+        reason="""
+        `broadcast_in_dim` primitive got a new kwarg `out_sharding`.
+        Corresponding updates are being made in core PL plxpr.
+        Re-enable this test after core PL's jax update is finished.
+        """
+    )
     def test_transform_unitary_to_rot_workflow(self, backend):
         """Test the integration for a circuit with a 'unitary_to_rot' transform."""
 
@@ -702,6 +709,13 @@ class TestCapture:
         experimental_capture_result = captured_func(U.matrix())
         assert no_capture_result == experimental_capture_result
 
+    @pytest.mark.xfail(
+        reason="""
+        `broadcast_in_dim` primitive got a new kwarg `out_sharding`.
+        Corresponding updates are being made in core PL plxpr.
+        Re-enable this test after core PL's jax update is finished.
+        """
+    )
     def test_mixed_transforms_workflow(self, backend):
         """Test the integration for a circuit with a combination of 'unitary_to_rot'
         and 'cancel_inverses' transforms."""
