fb68d6873ef26997d8ef58b2d5e80b32a8f7d8ac
Joey Carter <joseph.carter@xanadu.ai>
2025-04-23 13:16:44 -0400

    Initial commit

---
+++ mlir/lib/MBQC/Transforms/ConversionPatterns.cpp
@@ -0,0 +1,111 @@
+// Copyright 2025 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstdint>
+
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Transforms/DialectConversion.h"
+
+#include "MBQC/IR/MBQCOps.h"
+#include "MBQC/Transforms/Patterns.h"
+
+using namespace mlir;
+
+namespace {
+
+using namespace catalyst::mbqc;
+using namespace catalyst::quantum;
+
+constexpr int32_t NO_POSTSELECT = -1;
+
+// NOTE: This function is duplicated in a number of places in the Catalyst codebase
+LLVM::LLVMFuncOp ensureFunctionDeclaration(PatternRewriter &rewriter, Operation *op,
+                                           StringRef fnSymbol, Type fnType)
+{
+    Operation *fnDecl = SymbolTable::lookupNearestSymbolFrom(op, rewriter.getStringAttr(fnSymbol));
+
+    if (!fnDecl) {
+        PatternRewriter::InsertionGuard insertGuard(rewriter);
+        ModuleOp mod = op->getParentOfType<ModuleOp>();
+        rewriter.setInsertionPointToStart(mod.getBody());
+
+        fnDecl = rewriter.create<LLVM::LLVMFuncOp>(op->getLoc(), fnSymbol, fnType);
+    }
+    else {
+        assert(isa<LLVM::LLVMFuncOp>(fnDecl) && "QIR function declaration is not a LLVMFuncOp");
+    }
+
+    return cast<LLVM::LLVMFuncOp>(fnDecl);
+}
+
+struct MeasureInBasisOpPattern : public OpConversionPattern<MeasureInBasisOp> {
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(MeasureInBasisOp op, MeasureInBasisOpAdaptor adaptor,
+                                  ConversionPatternRewriter &rewriter) const override
+    {
+        Location loc = op.getLoc();
+        MLIRContext *ctx = getContext();
+        const TypeConverter *conv = getTypeConverter();
+
+        // Add types to the function signature
+        Type qubitTy = conv->convertType(QubitType::get(ctx));
+        Type planeTy = IntegerType::get(ctx, 32);
+        Type angleTy = Float64Type::get(ctx);
+        Type postselectTy = IntegerType::get(ctx, 32);
+        SmallVector<Type> argSignatures = {qubitTy, planeTy, angleTy, postselectTy};
+
+        // Define function signature
+        StringRef fnName = "__catalyst__mbqc__measure_in_basis";
+        Type fnSignature =
+            LLVM::LLVMFunctionType::get(conv->convertType(ResultType::get(ctx)), argSignatures);
+
+        LLVM::LLVMFuncOp fnDecl = ensureFunctionDeclaration(rewriter, op, fnName, fnSignature);
+
+        // Extract the integer value for the plane attribute from its enum
+        auto planeValueInt = static_cast<uint32_t>(op.getPlane());
+        Value planeValue =
+            rewriter.create<LLVM::ConstantOp>(loc, rewriter.getI32IntegerAttr(planeValueInt));
+
+        // Create the postselect value. If not given, it defaults to NO_POSTSELECT
+        LLVM::ConstantOp postselect = rewriter.create<LLVM::ConstantOp>(
+            loc, op.getPostselect() ? op.getPostselectAttr()
+                                    : rewriter.getI32IntegerAttr(NO_POSTSELECT));
+
+        // Add values as arguments of the CallOp
+        SmallVector<Value> args = {adaptor.getInQubit(), planeValue, op.getAngle(), postselect};
+
+        Value resultPtr = rewriter.create<LLVM::CallOp>(loc, fnDecl, args).getResult();
+        Value mres = rewriter.create<LLVM::LoadOp>(loc, IntegerType::get(ctx, 1), resultPtr);
+        rewriter.replaceOp(op, {mres, adaptor.getInQubit()});
+
+        return success();
+    }
+};
+
+} // namespace
+
+namespace catalyst {
+namespace mbqc {
+
+void populateConversionPatterns(LLVMTypeConverter &typeConverter, RewritePatternSet &patterns)
+{
+    patterns.add<MeasureInBasisOpPattern>(typeConverter, patterns.getContext());
+}
+
+} // namespace mbqc
+} // namespace catalyst
---
+++ mlir/test/MBQC/ConversionTest.mlir
@@ -0,0 +1,67 @@
+// Copyright 2025 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// RUN: quantum-opt %s --convert-mbqc-to-llvm --split-input-file -verify-diagnostics | FileCheck %s
+
+// CHECK-DAG: llvm.func @__catalyst__mbqc__measure_in_basis(!llvm.ptr, i32, f64, i32)
+
+// CHECK-LABEL: testXY
+func.func @testXY(%q1 : !quantum.bit) {
+    // CHECK: [[inqubit:%.+]] = builtin.unrealized_conversion_cast {{.+}} : !quantum.bit to !llvm.ptr
+
+    // CHECK: [[angle:%.+]] = arith.constant 3.1415926535897931 : f64
+    %angle = arith.constant 3.141592653589793 : f64
+
+    // CHECK: [[plane:%.+]] = llvm.mlir.constant(0 : i32) : i32
+    // CHECK: [[postselect:%.+]] = llvm.mlir.constant(-1 : i32) : i32
+    // CHECK: llvm.call @__catalyst__mbqc__measure_in_basis([[inqubit]], [[plane]], [[angle]], [[postselect]])
+    %res, %new_q = mbqc.measure_in_basis [XY, %angle] %q1 : i1, !quantum.bit
+    func.return
+}
+
+// -----
+
+// CHECK-DAG: llvm.func @__catalyst__mbqc__measure_in_basis(!llvm.ptr, i32, f64, i32)
+
+// CHECK-LABEL: testZX
+func.func @testZX(%q1 : !quantum.bit) {
+    // CHECK: [[inqubit:%.+]] = builtin.unrealized_conversion_cast {{.+}} : !quantum.bit to !llvm.ptr
+
+    // CHECK: [[angle:%.+]] = arith.constant 3.1415926535897931 : f64
+    %angle = arith.constant 3.141592653589793 : f64
+
+    // CHECK: [[plane:%.+]] = llvm.mlir.constant(1 : i32) : i32
+    // CHECK: [[postselect:%.+]] = llvm.mlir.constant(-1 : i32) : i32
+    // CHECK: llvm.call @__catalyst__mbqc__measure_in_basis([[inqubit]], [[plane]], [[angle]], [[postselect]])
+    %res, %new_q = mbqc.measure_in_basis [ZX, %angle] %q1 : i1, !quantum.bit
+    func.return
+}
+
+// -----
+
+// CHECK-DAG: llvm.func @__catalyst__mbqc__measure_in_basis(!llvm.ptr, i32, f64, i32)
+
+// CHECK-LABEL: testYZ
+func.func @testYZ(%q1 : !quantum.bit) {
+    // CHECK: [[inqubit:%.+]] = builtin.unrealized_conversion_cast {{.+}} : !quantum.bit to !llvm.ptr
+
+    // CHECK: [[angle:%.+]] = arith.constant 3.1415926535897931 : f64
+    %angle = arith.constant 3.141592653589793 : f64
+
+    // CHECK: [[plane:%.+]] = llvm.mlir.constant(2 : i32) : i32
+    // CHECK: [[postselect:%.+]] = llvm.mlir.constant(-1 : i32) : i32
+    // CHECK: llvm.call @__catalyst__mbqc__measure_in_basis([[inqubit]], [[plane]], [[angle]], [[postselect]])
+    %res, %new_q = mbqc.measure_in_basis [YZ, %angle] %q1 : i1, !quantum.bit
+    func.return
+}
