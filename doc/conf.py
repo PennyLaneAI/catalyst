263c4cf90e538d20e0f0dafdda9ad5f03cfed085
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2025-05-09 15:10:27 -0400

    bump sphinx related packages versions

--- doc/conf.py
+++ doc/conf.py
@@ -110,7 +110,7 @@ for mod_name in MOCK_MODULES:
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "3.3"
+needs_sphinx = "8.1"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
