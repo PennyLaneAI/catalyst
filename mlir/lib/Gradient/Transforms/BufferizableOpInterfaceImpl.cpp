fb9c6db250010f5573744ee2ea7e4e1df564f795
paul0403 <paulhaochen.wang@gmail.com>
2025-05-12 13:33:46 -0400

    boilerplate

---
+++ mlir/lib/Gradient/Transforms/BufferizableOpInterfaceImpl.cpp
@@ -0,0 +1,54 @@
+// Copyright 2024-2025 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "iostream"
+#include "llvm/Support/raw_ostream.h"
+
+#include "mlir/Dialect/Bufferization/IR/BufferizableOpInterface.h"
+#include "mlir/Dialect/Bufferization/IR/Bufferization.h"
+#include "mlir/Dialect/Index/IR/IndexOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/Location.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Interfaces/FunctionInterfaces.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/DialectConversion.h"
+
+#include "Gradient/IR/GradientOps.h"
+#include "Gradient/Transforms/Passes.h"
+#include "Gradient/Utils/GradientShape.h"
+
+using namespace mlir;
+using namespace mlir::bufferization;
+using namespace catalyst::gradient;
+
+/**
+ * Implementation of the BufferizableOpInterface for use with one-shot bufferization.
+ * For more information on the interface, refer to the documentation below:
+ *  https://mlir.llvm.org/docs/Bufferization/#extending-one-shot-bufferize
+ *  https://github.com/llvm/llvm-project/blob/main/mlir/include/mlir/Dialect/Bufferization/IR/BufferizableOpInterface.td#L14
+ */
+
+namespace {
+
+} // namespace
+
+void catalyst::gradient::registerBufferizableOpInterfaceExternalModels(DialectRegistry &registry)
+{
+    registry.addExtension(+[](MLIRContext *ctx, CatalystDialect *dialect) {
+       // CustomCallOp::attachInterface<CustomCallOpInterface>(*ctx);
+    });
+}
