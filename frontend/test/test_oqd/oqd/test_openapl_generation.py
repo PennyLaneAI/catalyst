1d5188f1b0d03517599a766d7f2a09193e2e876e
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2025-05-20 15:23:35 -0400

    Add more tests

--- frontend/test/test_oqd/oqd/test_openapl_generation.py
+++ frontend/test/test_oqd/oqd/test_openapl_generation.py
@@ -23,24 +23,49 @@ import pytest
 
 from catalyst import qjit
 from catalyst.third_party.oqd import OQDDevice, OQDDevicePipeline
+from pennylane_ionq import ops
 
 
-class TestOpenAPL:
-    """Test that the OQD device correctly generates an OpenAPL program."""
 
+@pytest.fixture(scope="module")
+def oqd_pipelines():
+    """Get the OQD device pipelines."""
     test_path = os.path.dirname(__file__)
     toml_path = os.path.join(test_path, "calibration_data/")
-    oqd_pipelines = OQDDevicePipeline(
+    return OQDDevicePipeline(
         toml_path + "device.toml", toml_path + "qubit.toml", toml_path + "gate.toml"
     )
 
-    output_f = "__openapl__output.json"
 
-    def test_RX_gate(self):
+@pytest.fixture(scope="function")
+def result_openapl_file():
+    """Create a temporary OpenAPL file with the given content."""
+    openapl_file = "__openapl__output.json"
+    yield openapl_file
+    # os.remove(openapl_file)
+
+
+def verify_json(correct_file_name, expected_file_name):
+    """Verify the two JSON files are identical."""
+    with open(correct_file_name, "r", encoding="utf-8") as f:
+        correct_json = json.load(f)
+
+    with open(expected_file_name, "r", encoding="utf-8") as f:
+        expected_json = json.load(f)
+
+    return sorted(correct_json.items()) == sorted(expected_json.items())
+
+
+class TestTargetGates:
+    """Test that the OQD device correctly generates an OpenAPL program for target gates of the OQD device ({'RX', 'RY', 'MS'})."""
+
+    test_path = os.path.dirname(__file__)
+
+    def test_RX_gate(self, oqd_pipelines, result_openapl_file):
         """Test OpenAPL generation for a circuit with a single RX Gate."""
         oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
 
-        @qjit(pipelines=self.oqd_pipelines)
+        @qjit(pipelines=oqd_pipelines)
         @qml.qnode(oqd_dev)
         def circuit(x):
             qml.RX(x, wires=0)
@@ -49,24 +74,55 @@ class TestOpenAPL:
         circuit(np.pi / 2)
 
         expected_f = os.path.join(self.test_path, "test_single_RX.json")
-        with open(self.output_f, "r", encoding="utf-8") as f:
-            catalyst_json = json.load(f)
+        assert verify_json(expected_f, result_openapl_file)
 
-        with open(
-            expected_f,
-            "r",
-            encoding="utf-8",
-        ) as f:
-            expected_json = json.load(f)
+    def test_RY_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single RY Gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
 
-        assert sorted(catalyst_json.items()) == sorted(expected_json.items())
-        os.remove(self.output_f)
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit(x):
+            qml.RY(x, wires=0)
+            return qml.counts(wires=0)
 
-    def test_CNOT_gate(self):
+        circuit(np.pi / 2)
+
+        expected_f = os.path.join(self.test_path, "test_single_RY.json")
+        assert verify_json(expected_f, result_openapl_file)
+
+
+class TestChainedGates:
+    """Test that the OQD device correctly generates an OpenAPL program for chained gates."""
+    test_path = os.path.dirname(__file__)
+    
+    def test_RX_RY_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single RX and RY Gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit():
+            qml.RX(np.pi / 2, wires=0)
+            qml.RY(np.pi / 2, wires=0)
+            return qml.counts(wires=0)
+
+        circuit()
+
+        expected_f = os.path.join(self.test_path, "test_RX_RY.json")
+        assert verify_json(expected_f, result_openapl_file)
+    
+
+class TestDecomposableGates:
+    """Test that the OQD device correctly generates an OpenAPL program for gates that can be decomposed into target gates of the OQD device."""
+
+    test_path = os.path.dirname(__file__)
+
+    def test_CNOT_gate(self, oqd_pipelines, result_openapl_file):
         """Test OpenAPL generation for a circuit with a single CNOT circuit."""
         oqd_dev = OQDDevice(backend="default", shots=4, wires=2)
 
-        @qjit(pipelines=self.oqd_pipelines)
+        @qjit(pipelines=oqd_pipelines)
         @qml.qnode(oqd_dev)
         def circuit():
             qml.CNOT(wires=[0, 1])
@@ -74,15 +130,120 @@ class TestOpenAPL:
 
         circuit()
 
-        with open(self.output_f, "r", encoding="utf-8") as f:
-            catalyst_json = json.load(f)
-
         expected_f = os.path.join(self.test_path, "test_single_CNOT.json")
-        with open(expected_f, "r", encoding="utf-8") as f:
-            expected_json = json.load(f)
+        assert verify_json(expected_f, result_openapl_file)
 
-        assert sorted(catalyst_json.items()) == sorted(expected_json.items())
-        os.remove("__openapl__output.json")
+    def test_Hadamard_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single Hadamard gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit():
+            qml.Hadamard(wires=0)
+            return qml.counts(wires=0)
+
+        circuit()
+
+        expected_f = os.path.join(self.test_path, "test_single_Hadamard.json")
+        assert verify_json(expected_f, result_openapl_file)
+
+    def test_PauliZ_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single PauliZ gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit():
+            qml.PauliZ(wires=0)
+            return qml.counts(wires=0)
+
+        circuit()
+
+        expected_f = os.path.join(self.test_path, "test_single_PauliZ.json")
+        assert verify_json(expected_f, result_openapl_file)
+
+    def test_PhaseShift_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single PhaseShift gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit():
+            qml.PhaseShift(np.pi/4, wires=0)
+            return qml.counts(wires=0)
+
+        circuit()
+
+        expected_f = os.path.join(self.test_path, "test_single_PhaseShift.json")
+        assert verify_json(expected_f, result_openapl_file)
+
+    def test_RZ_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single RZ gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit():
+            qml.RZ(np.pi/4, wires=0)
+            return qml.counts(wires=0)
+
+        circuit()
+
+        expected_f = os.path.join(self.test_path, "test_single_RZ.json")
+        assert verify_json(expected_f, result_openapl_file)
+
+    def test_T_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single T gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit():
+            qml.T(wires=0)
+            return qml.counts(wires=0)
+
+        circuit()
+
+        expected_f = os.path.join(self.test_path, "test_single_T.json")
+        assert verify_json(expected_f, result_openapl_file)
+
+    def test_S_gate(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a circuit with a single S gate."""
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=1)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit():
+            qml.S(wires=0)
+            return qml.counts(wires=0)
+
+        circuit()
+
+        expected_f = os.path.join(self.test_path, "test_single_S.json")
+        assert verify_json(expected_f, result_openapl_file)
+
+class TestComplexCircuits:
+    """Test that the OQD device correctly generates an OpenAPL program for more complex quantum circuits."""
+
+    test_path = os.path.dirname(__file__)
+
+    def test_2qubit_QFT(self, oqd_pipelines, result_openapl_file):
+        """Test OpenAPL generation for a 2-qubit Quantum Fourier Transform circuit."""
+        wires = 2
+        oqd_dev = OQDDevice(backend="default", shots=4, wires=wires)
+
+        @qjit(pipelines=oqd_pipelines)
+        @qml.qnode(oqd_dev)
+        def circuit(basis_state):
+            qml.BasisState(basis_state, wires=range(wires))
+            qml.QFT(wires=range(wires))
+            return qml.counts(wires=0)
+
+        circuit(np.array([0, 1]))
+
+        expected_f = os.path.join(self.test_path, "test_2qubit_QFT.json")
+        assert verify_json(expected_f, result_openapl_file)
 
 
 if __name__ == "__main__":
