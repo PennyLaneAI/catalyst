5d991cf49f7a26b5164532ad3c97aa8b4a2b8a81
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-05-21 13:55:00 +0000

    code factor

--- frontend/test/pytest/test_utils_runtime_environment.py
+++ frontend/test/pytest/test_utils_runtime_environment.py
@@ -12,7 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Tests reasonable output for get_include_path"""
+
 from pathlib import Path
+
+import pytest
+
 from catalyst.utils.runtime_environment import get_include_path
 
 
