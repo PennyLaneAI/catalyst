f338929bad36e81067ab43ed4b58c0a5e1389ea0
ritu-thombre99 <renitath99@gmail.com>
2025-05-26 19:16:38 -0400

    formatting changes

--- frontend/test/pytest/test_snapshot.py
+++ frontend/test/pytest/test_snapshot.py
@@ -42,8 +42,8 @@ class TestSnapshot:
         """Make sure only qml.state is allowed to be in qml.Snapshot"""
         with pytest.raises(
             NotImplementedError,
-            match = r"qml.Snapshot\(\) only supports qml.state\(\) when used from within Catalyst," +
-            f" but encountered {type(operation_passed_to_snapshot)}"
+            match=r"qml.Snapshot\(\) only supports qml.state\(\) when used from within Catalyst,"
+            + f" but encountered {type(operation_passed_to_snapshot)}",
         ):
             dev = qml.device("lightning.qubit", wires=1, shots=5)
 
