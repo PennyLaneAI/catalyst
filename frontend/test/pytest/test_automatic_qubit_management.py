8a12270b060041c624605124255e3b0b9af2d23d
paul0403 <paulhaochen.wang@gmail.com>
2025-06-05 16:25:46 -0400

    add sample and counts test

--- frontend/test/pytest/test_automatic_qubit_management.py
+++ frontend/test/pytest/test_automatic_qubit_management.py
@@ -11,10 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 This file contains tests for automatic qubit management.
 Automatic qubit management refers to when the user does not specify the total number of wires
 during device initialization.
+
+Note that, catalyst and pennylane handles device labels differently. For example, when a new label
+    qml.gate_or_measurement(wires=1000)
+is encountered, core pennylane considers "1000" as a pure wire label, and interprets that as
+*one* new wire, with the label "1000". However in catalyst we do not associate wires with
+arbitrary labels and require wires to be continuous integers from zero, and we would interpret this
+as "allocate new wires until we have 1001 wires, and act on wire[1000]".
+
+In other words, the reference runs of automatic qubit management should be qjit runs with wires
+specified during device initialization, instead of non qjit runs.
 """
 
 import numpy as np
@@ -25,12 +36,37 @@ import catalyst
 from catalyst import qjit
 
 
-def test_probs(backend):
+def test_partial_sample(backend):
+
+    def circuit():
+        qml.RX(0.0, wires=0)
+        return qml.sample(wires=[0, 2])
+
+    wires = [4, None]
+    devices = [qml.device(backend, shots=10, wires=wire) for wire in wires]
+    ref, observed = (qjit(qml.qnode(dev)(circuit))() for dev in devices)
+    assert np.allclose(ref, observed)
+
+
+def test_partial_counts(backend):
+
+    def circuit():
+        qml.RX(0.0, wires=0)
+        return qml.counts(wires=[0, 2])
+
+    wires = [4, None]
+    devices = [qml.device(backend, shots=10, wires=wire) for wire in wires]
+    ref, observed = (qjit(qml.qnode(dev)(circuit))() for dev in devices)
+    assert np.allclose(ref, observed)
+
+
+def test_partial_probs(backend):
 
     def circuit():
         qml.PauliX(wires=0)
-        return qml.probs(wires=[2, 3])
+        return qml.probs(wires=[0, 2])
 
-    ref = qjit(qml.qnode(qml.device(backend, wires=4))(circuit))()
-    observed = qjit(qml.qnode(qml.device(backend))(circuit))()
+    wires = [4, None]
+    devices = [qml.device(backend, wires=wire) for wire in wires]
+    ref, observed = (qjit(qml.qnode(dev)(circuit))() for dev in devices)
     assert np.allclose(ref, observed)
