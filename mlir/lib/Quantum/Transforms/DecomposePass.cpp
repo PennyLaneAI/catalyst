1c333683282788ea2bd43e770afa1b398b4dab2d
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2025-06-13 16:49:09 -0400

    Trying simple sleep command

--- mlir/lib/Quantum/Transforms/DecomposePass.cpp
+++ mlir/lib/Quantum/Transforms/DecomposePass.cpp
@@ -1,8 +1,16 @@
 #define DEBUG_TYPE "decompose"
+#include <iostream>
 
+#include "Catalyst/IR/CatalystDialect.h"
 #include "Quantum/IR/QuantumDialect.h"
+#include "Quantum/IR/QuantumOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Pass/Pass.h"
 #include "llvm/Support/Debug.h"
+
+#include "mlir/Rewrite/PatternApplicator.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 using namespace llvm;
 using namespace mlir;
@@ -13,10 +21,32 @@ namespace catalyst {
 #define GEN_PASS_DECL_DECOMPOSEPASS
 #include "Quantum/Transforms/Passes.h.inc"
 
+struct SleepOnceOnCustomOp : public OpRewritePattern<quantum::CustomOp> {
+    using OpRewritePattern::OpRewritePattern;
+
+    LogicalResult matchAndRewrite(quantum::CustomOp op, PatternRewriter &rewriter) const override
+    {
+
+        llvm::errs() << "CustomOp matched. Sleeping for 10 seconds...\n";
+        sleep(10);
+        return failure();
+    }
+};
+
 struct DecomposePass : public impl::DecomposePassBase<DecomposePass> {
     using impl::DecomposePassBase<DecomposePass>::DecomposePassBase;
 
-    void runOnOperation() override { llvm::errs() << "Decompose Pass called successfully\n"; }
+    void runOnOperation() final
+    {
+        LLVM_DEBUG(llvm::dbgs() << "Decompose Pass running\n");
+
+        RewritePatternSet patterns(&getContext());
+        patterns.add<SleepOnceOnCustomOp>(&getContext());
+
+        if (failed(applyPatternsGreedily(getOperation(), std::move(patterns)))) {
+            return signalPassFailure();
+        }
+    }
 };
 
 std::unique_ptr<Pass> createDecomposePass() { return std::make_unique<DecomposePass>(); }
