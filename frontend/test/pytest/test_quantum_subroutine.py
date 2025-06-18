6ccea59c446a54f9212c95aef9540f558ea49578
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-06-18 19:16:45 +0000

    code factor

--- frontend/test/pytest/test_quantum_subroutine.py
+++ frontend/test/pytest/test_quantum_subroutine.py
@@ -1,3 +1,19 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Integration tests for quantum subroutine"""
+
 import jax
 import numpy as np
 import pennylane as qml
@@ -20,6 +36,7 @@ def test_classical_subroutine():
 
 
 def test_quantum_subroutine():
+    """Test quantum subroutine"""
 
     @subroutine
     def Hadamard0(wire):
@@ -38,6 +55,7 @@ def test_quantum_subroutine():
 
 
 def test_quantum_subroutine_self_inverses():
+    """Test quantum subroutine multiple calls"""
 
     @subroutine
     def Hadamard0(wire):
@@ -60,11 +78,12 @@ def test_quantum_subroutine_self_inverses():
 
 
 def test_quantum_subroutine_conditional():
+    """Test quantum subroutine control flow"""
 
     @subroutine
     def Hadamard0(wire):
         def true_path():
-            qml.Hadamard(wires=[wire])
+            qml.Hadamard(wires=[0])
 
         def false_path(): ...
 
@@ -79,5 +98,5 @@ def test_quantum_subroutine_conditional():
         return qml.state()
 
     assert np.allclose(subroutine_test(0), jax.numpy.array([1.0, 0.0], dtype=complex))
-    assert np, allclose(subroutine_test(0), jax.numpy.array([0.70710678 + 0.0j, 0.70710678 + 0.0j]))
+    assert np.allclose(subroutine_test(1), jax.numpy.array([0.70710678 + 0.0j, 0.70710678 + 0.0j]))
     qml.capture.disable()
