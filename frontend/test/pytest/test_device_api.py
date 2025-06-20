a5b2032b9ac14ad1d8c3ac57da04fcadf555bf5f
paul0403 <paulhaochen.wang@gmail.com>
2025-06-20 17:39:42 -0400

    lit test

--- frontend/test/pytest/test_device_api.py
+++ frontend/test/pytest/test_device_api.py
@@ -127,22 +127,6 @@ def test_simple_circuit():
     assert circuit.mlir
 
 
-def test_simple_circuit_set_shots():
-    """Test that a circuit with the new device API is compiling to MLIR."""
-    dev = NullQubit(wires=2)
-
-    @qjit(target="mlir")
-    @partial(qml.set_shots, shots=2048)
-    @qml.qnode(device=dev)
-    def circuit():
-        qml.Hadamard(wires=0)
-        qml.CNOT(wires=[0, 1])
-        return qml.expval(qml.PauliZ(wires=0))
-
-    assert circuit._shots==qml.measurements.Shots(2048)
-    assert circuit.mlir
-
-
 def test_track_resources():
     """Test that resource tracking settings get passed to the device."""
     dev = NullQubit(wires=2)
