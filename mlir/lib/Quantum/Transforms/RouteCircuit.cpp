4d59bdd2345d164737fb3844f0ada2716e7073ff
ritu-thombre99 <renitath99@gmail.com>
2025-07-02 21:42:09 -0400

    try z3 with cmake

---
+++ mlir/lib/Quantum/Transforms/RouteCircuit.cpp
@@ -0,0 +1,66 @@
+// Copyright 2025 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This algorithm is taken from https://arxiv.org/pdf/2012.07711, table 6 (Equivalences for
+// basis-states in SWAP gate)
+
+#define DEBUG_TYPE "routecircuit"
+
+// #include <z3++.h>
+
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Pass/Pass.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/Support/Debug.h"
+
+#include "Catalyst/IR/CatalystDialect.h"
+#include "Quantum/IR/QuantumOps.h"
+
+
+using namespace mlir;
+using namespace catalyst;
+
+namespace catalyst {
+#define GEN_PASS_DEF_ROUTINGPASS
+#define GEN_PASS_DECL_ROUTINGPASS
+#include "Quantum/Transforms/Passes.h.inc"
+
+struct RoutingPass : public impl::RoutingPassBase<RoutingPass> {
+    using impl::RoutingPassBase<RoutingPass>::RoutingPassBase;
+
+    void runOnOperation() override {
+        llvm::outs() << "Hello\n"; 
+        // z3::context ctx;
+
+        // Declare a Boolean constant (variable)
+        // z3::expr p = ctx.bool_const("p");
+        // z3::expr q = ctx.bool_const("q");
+
+        // Create a solver instance
+        // z3::solver s(ctx);
+
+        // Add some assertions (constraints) to the solver
+        // Example: p AND q is true
+        // s.add(p && q);
+    }
+    // void runOnOperation() final
+};
+
+std::unique_ptr<Pass> createRoutingPass()
+{
+    return std::make_unique<RoutingPass>();
+}
+
+} // namespace catalyst
