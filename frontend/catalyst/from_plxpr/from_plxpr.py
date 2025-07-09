4bcba153512e9ead5c32d3274c53715fd7ebfe4d
paul0403 <paulhaochen.wang@gmail.com>
2025-07-09 14:34:42 -0400

    cleanup; doc the method; move the method from interpreter to qreg manager

--- frontend/catalyst/from_plxpr/from_plxpr.py
+++ frontend/catalyst/from_plxpr/from_plxpr.py
@@ -302,29 +302,10 @@ class PLxPRToQuantumJaxprInterpreter(PlxprInterpreter):
         self.subroutine_cache = cache
         super().__init__()
 
-    def _insert_dynamic_wires(self, op):
-        """Update with insertion and extraction in the
-        presence of dynamic wires"""
-        keep_cache = False
-
-        same_number_of_wires = len(op.wires) == len(self.qreg_manager)
-        all_static_requested = all(isinstance(wire, int) for wire in op.wires)
-        all_static_cached = all(isinstance(wire[0], int) for wire in self.qreg_manager)
-        same_wires = False
-        all_dynamic = False
-        all_static = all_static_requested and all_static_cached
-        if same_number_of_wires and not all_static:
-            wires_in_cache = all(wire in self.qreg_manager.wire_map.keys() for wire in op.wires)
-            all_dynamic = all(not isinstance(wire, int) for wire in op.wires)
-            keep_cache = wires_in_cache and all_dynamic
-
-        if not (all_static or keep_cache):
-            self.qreg_manager.insert_all_dangling_qubits()
-
     def interpret_operation(self, op):
         """Re-bind a pennylane operation as a catalyst instruction."""
 
-        self._insert_dynamic_wires(op)
+        self.qreg_manager.insert_dynamic_qubits(op.wires)
 
         in_qubits = [self.qreg_manager[w] for w in op.wires]
         out_qubits = qinst_p.bind(
