12c2d0f1ac6c80b2b143b2e0cb92f7e03d8913de
albi3ro <chrissie.c.l@gmail.com>
2025-07-10 18:04:42 -0400

    and fix

--- frontend/catalyst/from_plxpr/from_plxpr.py
+++ frontend/catalyst/from_plxpr/from_plxpr.py
@@ -17,6 +17,7 @@ This submodule defines a utility for converting plxpr into Catalyst jaxpr.
 # pylint: disable=protected-access
 from functools import partial
 from typing import Callable
+from uuid import uuid4
 
 import jax
 import jax.core
@@ -25,8 +26,9 @@ import pennylane as qml
 from jax._src.sharding_impls import UNSPECIFIED
 from jax.extend.core import ClosedJaxpr, Jaxpr
 from jax.extend.linear_util import wrap_init
-from pennylane.capture import PlxprInterpreter, qnode_prim
+from pennylane.capture import PlxprInterpreter, pause, qnode_prim
 from pennylane.capture.expand_transforms import ExpandTransformsInterpreter
+from pennylane.capture.primitives import grad_prim as plxpr_grad_prim
 from pennylane.capture.primitives import measure_prim as plxpr_measure_prim
 from pennylane.ftqc.primitives import measure_in_basis_prim as plxpr_measure_in_basis_prim
 from pennylane.ops.functions.map_wires import _map_wires_transform as pl_map_wires
@@ -40,8 +42,9 @@ from pennylane.transforms import unitary_to_rot as pl_unitary_to_rot
 
 from catalyst.device import extract_backend_info, get_device_capabilities
 from catalyst.from_plxpr.qreg_manager import QregManager
-from catalyst.jax_extras import jaxpr_pad_consts, make_jaxpr2, transient_jax_config
+from catalyst.jax_extras import jaxpr_pad_consts, make_jaxpr2, transient_jax_config, wrap_init
 from catalyst.jax_primitives import (
+    GradParams,
     MeasurementPlane,
     compbasis_p,
     cond_p,
@@ -49,7 +52,9 @@ from catalyst.jax_primitives import (
     device_init_p,
     device_release_p,
     expval_p,
+    func_p,
     gphase_p,
+    grad_p,
     hamiltonian_p,
     measure_in_basis_p,
     measure_p,
@@ -213,6 +218,29 @@ def handle_qnode(
     )
 
 
+@WorkflowInterpreter.register_primitive(plxpr_grad_prim)
+def _(self, *args, argnum, jaxpr, h, n_consts, method):
+    consts = args[:n_consts]
+    args = args[n_consts:]
+    grad_params = GradParams(
+        method=method or "auto",
+        scalar_out=True,
+        argnums=argnum,
+        expanded_argnums=argnum,
+        h=h,
+    )
+
+    jaxpr = ClosedJaxpr(jaxpr, consts)
+
+    def _eval_jaxpr(*inner_args):
+        return jax.core.eval_jaxpr(jaxpr.jaxpr, jaxpr.consts, *inner_args)
+    
+    def add_in_func_call(*inner_args):
+        return func_p.bind(wrap_init(_eval_jaxpr), *inner_args, call_jaxpr=jaxpr, fn=None)#type(str(uuid4()), (), {}))
+    
+    new_jaxpr = jax.make_jaxpr(add_in_func_call)(*args)
+
+    return grad_p.bind(*args, fn=None, jaxpr=new_jaxpr, grad_params=grad_params)
 # The map below describes the parity between PL transforms and Catalyst passes.
 # PL transforms having a Catalyst pass counterpart will have a name as value,
 # otherwise their value will be None. The second value indicates if the transform
@@ -321,8 +349,6 @@ class PLxPRToQuantumJaxprInterpreter(PlxprInterpreter):
                 control_wires=control_wires + tuple(op.control_wires),
             )
 
-        self.qreg_manager.insert_dynamic_qubits(op.wires)
-
         in_qubits = [self.qreg_manager[w] for w in op.wires]
         control_qubits = [self.qreg_manager[w] for w in control_wires]
         out_qubits = qinst_p.bind(
@@ -337,13 +363,14 @@ class PLxPRToQuantumJaxprInterpreter(PlxprInterpreter):
             self.qreg_manager[wire_values] = new_wire
 
         return out_qubits
-
+    
     def _obs(self, obs):
         """Interpret the observable equation corresponding to a measurement equation's input."""
         if isinstance(obs, qml.ops.Prod):
             return tensorobs_p.bind(*(self._obs(t) for t in obs))
         if obs.arithmetic_depth > 0:
-            coeffs, terms = obs.terms()
+            with pause():
+                coeffs, terms = obs.terms()
             terms = [self._obs(t) for t in terms]
             return hamiltonian_p.bind(jnp.stack(coeffs), *terms)
         wires = [self.qreg_manager[w] for w in obs.wires]
