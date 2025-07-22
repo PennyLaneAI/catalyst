3da8bb1a7a282f82de3f2b446751dda095a820b2
Ali Asadi <10773383+maliasadi@users.noreply.github.com>
2025-07-22 18:19:31 -0400

    Update code

--- test.py
+++ test.py
@@ -6,20 +6,21 @@ from catalyst.jax_primitives import AbstractQbit, qdef
 
 qml.capture.enable()
 
-
 @qdef
 def decomp(x, y, w):
     jax.debug.print("hello add {} and {}", x, y)
     qml.GlobalPhase(1.14)
-    qml.RX(x, wires=1)
-    qml.RY(y, wires=2)
-    
+    qml.RX(x, wires=w)
+    qml.RY(y, wires=w)
+    qml.CNOT(wires=[w, w+1])
+    qml.RZ(x+y, wires=w) # param wire
 
 @qjit
 @qml.qnode(qml.device("lightning.qubit", wires=1))
-def circuit(x: float):
+def circuit(x: float, w: int):
     qml.RX(x, wires=0)
-    decomp(float, float, AbstractQbit())
+    # decomp(float, float, AbstractQbit()) # not working with AbstractQbit for param wires 
+    decomp(float, float, int)
     return qml.expval(qml.PauliZ(0))
 
 
