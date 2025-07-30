e99f635a250f28bb38e5a63f526f69cf89abd60c
JerryChen97 <chenys13@outlook.com>
2025-07-30 10:40:40 -0400

    shut up codefactor

--- frontend/catalyst/api_extensions/control_flow.py
+++ frontend/catalyst/api_extensions/control_flow.py
@@ -416,8 +416,8 @@ def for_loop(lower_bound, upper_bound, step, allow_array_resizing=False):
 
     .. note::
 
-       ``catalyst.for_loop`` is not supported in program capture mode. If ``qml.capture`` is enabled,
-                please use ``qml.for_loop`` instead.
+       ``catalyst.for_loop`` is not supported in program capture mode.
+       If ``qml.capture`` is enabled, please use ``qml.for_loop`` instead.
 
         .. code-block:: python
 
@@ -555,8 +555,8 @@ def while_loop(cond_fn, allow_array_resizing: bool = False):
 
     .. note::
 
-       ``catalyst.while_loop`` is not supported in program capture mode. If ``qml.capture`` is enabled,
-       please use ``qml.while_loop`` instead.
+       ``catalyst.while_loop`` is not supported in program capture mode.
+       If ``qml.capture`` is enabled, please use ``qml.while_loop`` instead.
 
         .. code-block:: python
 
