07d17d5163e737eedfcb692b0d39b6c6f3a8515a
David Ittah <dime10@users.noreply.github.com>
2025-08-05 17:24:19 -0400

    Implement most todos in the jitting decorator
    In particular, the MLIR is automatically obtained from the user
    function, as are the input types and values and result types.
    
    The user function is currently traced with the static_argnum feature
    to effect the desired concretization, however this comes with two:
    - static_argnums are only allowed for hashable values, which arrays are
      not, so scalar arguments are currently assumed
    - while this achieves a removal of the function arguments, and in
      principle makes the program entirely static, computation based on
      those values will not necessarily be folded away
      the latter would require something like jax.ensure_compile_time_eval,
      or additional constant folding / partial eval in the compiler

--- ritu.py
+++ ritu.py
@@ -1,14 +1,13 @@
-import ctypes
 import functools
-import pathlib
-import subprocess
+import os
 from pathlib import Path
 
-import jax
+import numpy as np
 import pennylane as qml
 from jax.interpreters import mlir
 
-from catalyst import pure_callback
+from catalyst import QJIT, CompileOptions, pure_callback
+from catalyst.compiled_functions import CompiledFunction
 from catalyst.compiler import LinkerDriver, _catalyst
 
 
@@ -18,41 +17,53 @@ def jitting(qnode):
     @functools.wraps(qnode)
     def tracing_function(*args, **kwargs):
 
-        @pure_callback
-        def runtime_function(wire: int) -> jax.core.ShapedArray([4], float):
+        # trace the code once to get the i/o signature
+        # to avoid overheads, we create the QJIT object manually and invoke specific stages
+        class Basic_QJIT:
+            def __init__(self, qnode):
+                self.__name__ = "jit_" + qnode.__name__
+                self.user_function = qnode
+                self.compile_options = CompileOptions()
+
+        jit_obj = Basic_QJIT(qnode)
+        jaxpr, _, treedef, _ = QJIT.capture(jit_obj, args, **kwargs)
+
+        @pure_callback(result_type=treedef.unflatten(jaxpr.out_avals))
+        def runtime_function(*concrete_args):
             # INSIDE HERE WE ARE RUNNING...
 
             # TODO:
-            # * Add arguments to callback
-            # * Make this arguments literals into the program below
-            # * Obtain the program from str(module) but change the function to have no parameters
-            # * automatically update the return values
             # * Better names
-            # * automatically make callback arguments same as the ones from the original function.
-            program = f"""
-                func.func public @foobar() -> tensor<4xf64> attributes {{diff_method = "parameter-shift", llvm.emit_c_interface, qnode}} {{
-                    // SPECIALIZING THE WIRE AT RUNTIME
-                    %dyn_wire = arith.constant {wire} : i64
-                    %c0_i64 = arith.constant 0 : i64
-                    quantum.device shots(%c0_i64) ["{qnode.device.get_c_interface()[1]}", "{qnode.device.get_c_interface()[0]}", ""]
-                    %0 = quantum.alloc( 2) : !quantum.reg
-                    %1 = quantum.extract %0[ %dyn_wire] : !quantum.reg -> !quantum.bit
-                    %out_qubits = quantum.custom "Hadamard"() %1 : !quantum.bit
-                    %2 = quantum.insert %0[ %dyn_wire], %out_qubits : !quantum.reg, !quantum.bit
-                    %3 = quantum.compbasis qreg %2 : !quantum.obs
-                    %4 = quantum.probs %3 : tensor<4xf64>
-                    quantum.dealloc %2 : !quantum.reg
-                    quantum.device_release
-                    return %4 : tensor<4xf64>
-            }}
-            """
-            import os
+            # * static args handling currently only works for scalars
+
+            # ensure we are using static arguments for tracing this time
+            # this is actually tricky because arrays cannot be used as static args
+            # we might need a different way of removing arguments from the function
+            jit_obj.compile_options.static_argnums = tuple(range(len(concrete_args)))
+
+            # get the specialized jaxpr+mlir this time
+            #   (we could probably simplify further and just use `catalyst.qjit` directly)
+            jit_obj.jaxpr, jit_obj.out_type, jit_obj.treedef, jit_obj.dynamic_sig = QJIT.capture(
+                jit_obj, tuple(arg.item() for arg in concrete_args)
+            )
+            mlir_module = QJIT.generate_ir(jit_obj)
+
+            # eliminate setup/teardown functions which will mess with the runtime
+            for _ in range(2):
+                mlir_module.body.operations[-1].erase()
+
+            # get some properties of the entry point function
+            entry_point = mlir_module.body.operations[0]
+            entry_symbol = str(entry_point.name).replace('"', "")
+            entry_result_types = entry_point.type.results
 
             dir_path = os.path.dirname(os.path.realpath(__file__)) + "/ritu_dir"
             Path(dir_path).mkdir(parents=True, exist_ok=True)
+
             inp = dir_path + "/input.mlir"
-            with open(inp, "w") as file:
-                file.write(program)
+            with open(inp, "w+") as file:
+                file.write(str(mlir_module))
+
             out1 = _catalyst(
                 ("--tool=all"),
                 ("--workspace", str(dir_path)),
@@ -61,25 +72,20 @@ def jitting(qnode):
                 ("-o", dir_path + "/jit.so"),
                 (inp,),
             )
-            shared_object = LinkerDriver.run(
-                pathlib.Path(dir_path + "/catalyst_module.o").absolute()
-            )
-            output_object_name = str(pathlib.Path(shared_object).absolute())
+
+            shared_object = LinkerDriver.run(Path(dir_path + "/catalyst_module.o").absolute())
+            output_object_name = str(Path(shared_object).absolute())
+
             with mlir.ir.Context(), mlir.ir.Location.unknown():
                 f64 = mlir.ir.F64Type.get()
-                from catalyst.compiled_functions import CompiledFunction
 
                 compiled_function = CompiledFunction(
-                    output_object_name,
-                    "foobar",
-                    [mlir.ir.RankedTensorType.get((4,), f64)],
-                    None,
-                    None,
+                    output_object_name, entry_symbol, entry_result_types, None, None
                 )
 
             return compiled_function()
 
-        return runtime_function(args[0])
+        return runtime_function(*args)
 
     return tracing_function
 
@@ -87,8 +93,10 @@ def jitting(qnode):
 @qml.qjit
 @jitting
 @qml.qnode(qml.device("lightning.qubit", wires=2))
-def foo(wire: int):
+def foo(w1, w2):
+    qml.Hadamard(w1)
+    qml.CNOT([w1, w2])
     return qml.probs()
 
 
-print(foo(0))
+print(foo(0, 1))
