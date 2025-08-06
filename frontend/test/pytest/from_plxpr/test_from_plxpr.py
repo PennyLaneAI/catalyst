425a8e0db5a55a2b8a70da25d9443b199f79d5e9
JerryChen97 <chenys13@outlook.com>
2025-08-06 13:28:21 -0400

    very very last one

--- frontend/test/pytest/from_plxpr/test_from_plxpr.py
+++ frontend/test/pytest/from_plxpr/test_from_plxpr.py
@@ -378,27 +378,27 @@ class TestCatalystCompareJaxpr:
                 qml.X(0)
                 return qml.sample()
 
-        qml.capture.enable()
-        plxpr = jax.make_jaxpr(circuit)()
+            qml.capture.enable()
+            plxpr = jax.make_jaxpr(circuit)()
 
-        converted = from_plxpr(plxpr)()
-        qml.capture.disable()
+            converted = from_plxpr(plxpr)()
+            qml.capture.disable()
 
-        assert converted.eqns[0].primitive == catalyst.jax_primitives.quantum_kernel_p
-        assert converted.eqns[0].params["qnode"] is circuit
+            assert converted.eqns[0].primitive == catalyst.jax_primitives.quantum_kernel_p
+            assert converted.eqns[0].params["qnode"] is circuit
 
-        catalyst_res = catalyst_execute_jaxpr(converted)()
-        assert len(catalyst_res) == 1
-        expected = np.transpose(np.vstack([np.ones(50), np.zeros(50)]))
-        assert qml.math.allclose(catalyst_res[0], expected)
+            catalyst_res = catalyst_execute_jaxpr(converted)()
+            assert len(catalyst_res) == 1
+            expected = np.transpose(np.vstack([np.ones(50), np.zeros(50)]))
+            assert qml.math.allclose(catalyst_res[0], expected)
 
-        qjit_obj = qjit(circuit)
-        qjit_obj()
-        catalxpr = qjit_obj.jaxpr
-        call_jaxpr_pl = get_call_jaxpr(converted)
-        call_jaxpr_c = get_call_jaxpr(catalxpr)
+            qjit_obj = qjit(circuit)
+            qjit_obj()
+            catalxpr = qjit_obj.jaxpr
+            call_jaxpr_pl = get_call_jaxpr(converted)
+            call_jaxpr_c = get_call_jaxpr(catalxpr)
 
-        compare_call_jaxprs(call_jaxpr_pl, call_jaxpr_c)
+            compare_call_jaxprs(call_jaxpr_pl, call_jaxpr_c)
 
     @pytest.mark.xfail(reason="CountsMP returns a dictionary, which is not compatible with capture")
     def test_counts(self):
