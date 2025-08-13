9258b2ba102f21de6b7da1f90fcbad82d59c51fb
HSZheng <mathan0203@gmail.com>
2025-08-13 18:29:10 -0400

    Update frontend/test/pytest/test_conditionals.py
    Co-authored-by: Joey Carter <joseph.carter@xanadu.ai>

--- frontend/test/pytest/test_conditionals.py
+++ frontend/test/pytest/test_conditionals.py
@@ -329,7 +329,7 @@ class TestCond:
                 return False
 
             r = cond_fn()
-            assert r.dtype is jnp.dtype("float64")
+            assert r.dtype is jnp.dtype("float64") if jax.config.values["jax_enable_x64"] else r.dtype is jnp.dtype("float32") 
             return r
 
         assert 0.5 == circuit(False, True)
