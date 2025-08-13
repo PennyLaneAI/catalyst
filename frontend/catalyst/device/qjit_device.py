9313a7f1dafed8a58abcebba7c2282778b4b2cba
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-08-13 18:38:36 -0400

    Update frontend/catalyst/device/qjit_device.py
    Co-authored-by: David Ittah <dime10@users.noreply.github.com>

--- frontend/catalyst/device/qjit_device.py
+++ frontend/catalyst/device/qjit_device.py
@@ -354,6 +354,7 @@ class QJITDevice(qml.devices.Device):
         if shots is None:
             capabilities = self.capabilities
         else:
+            # recompute device capabilities if shots were provided dynamically (through set_shots)
             device_caps = get_device_capabilities(self.original_device, shots)
             capabilities = get_qjit_device_capabilities(device_caps)
 
