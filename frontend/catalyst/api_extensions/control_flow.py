28cc53d707133d765a2d966b18bb7c36364302bd
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-08-15 11:25:52 -0400

    Fix typo

--- frontend/catalyst/api_extensions/control_flow.py
+++ frontend/catalyst/api_extensions/control_flow.py
@@ -218,7 +218,7 @@ def cond(pred: DynamicJaxprTracer):
         ...         return x ** 2
         ...     return cond_fn()
         TypeError: Conditional requires a consistent return structure across all branches! Got
-        PyTreeDef(None) and PyTreeDef(None).
+        PyTreeDef(None) and PyTreeDef(*).
 
         >>> @qjit
         ... def f(x: float):
