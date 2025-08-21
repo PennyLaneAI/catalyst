a2bac31508296846a80c1ea8791ec6b843c50fca
paul0403 <paulhaochen.wang@gmail.com>
2025-08-21 16:58:52 -0400

    The runtime `qubit_release_array` CAPI should release only the qubits in the array, not all qubits on the device

--- runtime/lib/capi/RuntimeCAPI.cpp
+++ runtime/lib/capi/RuntimeCAPI.cpp
@@ -389,9 +389,11 @@ void __catalyst__rt__qubit_release(QUBIT *qubit)
 
 static int __catalyst__rt__qubit_release_array__impl(QirArray *qubit_array)
 {
-    getQuantumDevicePtr()->ReleaseAllQubits();
     std::vector<QubitIdType> *qubit_array_ptr =
         reinterpret_cast<std::vector<QubitIdType> *>(qubit_array);
+    for (QubitIdType q : *qubit_array_ptr) {
+        getQuantumDevicePtr()->ReleaseQubit(q);
+    }
     delete qubit_array_ptr;
     return 0;
 }
