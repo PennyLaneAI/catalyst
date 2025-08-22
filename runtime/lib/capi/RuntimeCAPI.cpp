98688830395f9c0a46d8a41fa0dfdd2179126ed0
paul0403 <paulhaochen.wang@gmail.com>
2025-08-22 10:57:48 -0400

    restore to complete dealloc

--- runtime/lib/capi/RuntimeCAPI.cpp
+++ runtime/lib/capi/RuntimeCAPI.cpp
@@ -389,12 +389,9 @@ void __catalyst__rt__qubit_release(QUBIT *qubit)
 
 static int __catalyst__rt__qubit_release_array__impl(QirArray *qubit_array)
 {
-    //getQuantumDevicePtr()->ReleaseAllQubits();
+    getQuantumDevicePtr()->ReleaseAllQubits();
     std::vector<QubitIdType> *qubit_array_ptr =
         reinterpret_cast<std::vector<QubitIdType> *>(qubit_array);
-    for (QubitIdType q : *qubit_array_ptr){
-        getQuantumDevicePtr()->ReleaseQubit(q);
-    }
     delete qubit_array_ptr;
     return 0;
 }
