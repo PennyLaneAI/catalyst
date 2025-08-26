119b096e653e857b44a17c0beb94658f96e55f92
paul0403 <paulhaochen.wang@gmail.com>
2025-08-26 15:19:58 -0400

    add qdealloc_p

---
+++ frontend/test/lit/test_dynamic_qubit_allocation.py
@@ -0,0 +1,46 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Unit tests for the dynamic qubit allocation.
+Note that this feature is only available under the plxpr pipeline.
+"""
+
+# RUN: %PYTHON %s | FileCheck %s
+
+import pennylane as qml
+
+from catalyst import qjit
+from catalyst.jax_primitives import qalloc_p, qdealloc_qb_p, qextract_p
+
+
+@qjit
+def test_single_qubit_dealloc():
+    """
+    Unit test for the single qubit dealloc primitive's lowerings.
+    """
+
+    # CHECK: [[qubit:.]]:AbstractQbit() = qextract {{.+}} 3
+    # CHECK: qdealloc_qb [[qubit]]
+
+    # CHECK: [[qubit:%.+]] = quantum.extract {{.+}} 3
+    # CHECK: quantum.dealloc_qb [[qubit]] : !quantum.bit
+
+    qreg = qalloc_p.bind(10)
+    qubit = qextract_p.bind(qreg, 3)
+    qdealloc_qb_p.bind(qubit)
+
+
+print(test_single_qubit_dealloc.jaxpr)
+print(test_single_qubit_dealloc.mlir)
