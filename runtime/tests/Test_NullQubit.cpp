d8f0a75990a0ce70e2fdaa08343dcbe1a0fb9176
Joey Carter <joseph.carter@xanadu.ai>
2025-08-29 17:32:29 -0400

    Remove git merge annotations!

--- runtime/tests/Test_NullQubit.cpp
+++ runtime/tests/Test_NullQubit.cpp
@@ -16,11 +16,8 @@
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
-<<<<<<< HEAD
 #include <cstdint>
 #include <cstdio>
-=======
->>>>>>> main
 
 #include "ExecutionContext.hpp"
 #include "QuantumDevice.hpp"
