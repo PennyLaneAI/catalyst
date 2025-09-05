1758d83513eaa255b814a6ef6ef426206c9e85e3
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-09-05 10:59:19 -0400

    Test

--- frontend/catalyst/third_party/oqc/src/oqc_python_module.cpp
+++ frontend/catalyst/third_party/oqc/src/oqc_python_module.cpp
@@ -22,33 +22,10 @@
 #include "Exception.hpp"
 
 std::string program = R"(
+# check if Python execution works
 import os
-
-try:
-    # Try to import OQC dependencies
-    from qcaas_client.client import OQCClient, QPUTask, CompilerConfig
-    from qcaas_client.config import QuantumResultsFormat, Tket, TketOptimizations
-
-    optimisations = Tket()
-    optimisations.tket_optimizations = TketOptimizations.DefaultMappingPass
-    RES_FORMAT = QuantumResultsFormat().binary_count()
-
-    # Try to execute OQC client
-    email = os.environ.get("OQC_EMAIL")
-    password = os.environ.get("OQC_PASSWORD")
-    url = os.environ.get("OQC_URL")
-    client = OQCClient(url=url, email=email, password=password)
-    client.authenticate()
-    oqc_config = CompilerConfig(repeats=shots, results_format=RES_FORMAT, optimizations=optimisations)
-    oqc_task = QPUTask(circuit, oqc_config)
-    res = client.execute_tasks(oqc_task)
-    counts = res[0].result["cbits"]
-
-except Exception as e:
-    print(f"circuit: {circuit}")
-    msg = str(e)
-    # Set fallback mock counts when OQC service fails or imports fail
-    counts = {i: 0 for i in range(2 ** num_qubits)}
+msg = ""
+print("Python code executed successfully")
 )";
 
 extern "C" {
