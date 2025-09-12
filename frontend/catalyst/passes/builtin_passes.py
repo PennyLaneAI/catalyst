877553056abc7d548d18630acb7d303c62249e63
Jeffrey Kam <jeffrey.kam@xanadu.ai>
2025-09-12 10:26:12 -0400

    Fixing formatting issue

--- frontend/catalyst/passes/builtin_passes.py
+++ frontend/catalyst/passes/builtin_passes.py
@@ -805,7 +805,7 @@ def ppr_to_ppm(qnode=None, *, decompose_method="pauli-corrected", avoid_y_measur
 
         avoid_y_measure (bool): Rather than performing a Pauli-Y measurement for Clifford rotations
             (sometimes more costly), a :math:`Y` state (:math:`Y\vert 0 \rangle`) is used instead
-            (requires :math:`Y` state preparation). 
+            (requires :math:`Y` state preparation).
             This is currently only supported when using the ``"auto-corrected"`` decomposition method.
             Defaults to ``False``.
 
