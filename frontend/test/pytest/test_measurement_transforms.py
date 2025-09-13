cfc10bf2ababeaba3bb71b381524fdd95805ec32
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-09-12 22:44:20 -0400

    formatting

--- frontend/test/pytest/test_measurement_transforms.py
+++ frontend/test/pytest/test_measurement_transforms.py
@@ -945,9 +945,7 @@ class TestTransform:
         "transform_measurement", (measurements_from_samples, measurements_from_counts)
     )
     @pytest.mark.parametrize("mcm_method", ("one-shot", "single-branch-statistics"))
-    def test_measurements_transform(
-        self, mcm_method, transform_measurement
-    ):
+    def test_measurements_transform(self, mcm_method, transform_measurement):
         """Test raise an error when measurements_from_samples is used with one-shot."""
         device = qml.device("lightning.qubit", wires=2)
 
