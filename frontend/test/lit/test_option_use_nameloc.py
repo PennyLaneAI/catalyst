17b1cfd528ee7678e7a6c01b68dfa5b175ffbf33
rturrado <rturrado@gmail.com>
2025-09-18 23:37:06 +0200

    Fix test_option_use_nameloc.py.

--- frontend/test/lit/test_option_use_nameloc.py
+++ frontend/test/lit/test_option_use_nameloc.py
@@ -23,13 +23,10 @@ from utils import print_mlir
 # CHECK-LABEL: @jit_f
 @qjit(use_nameloc=True)
 def f(x: float, y: float):
-    # CHECK: loc("x")
-    # CHECK: loc("y")
+    # CHECK: %x: tensor<f64>, %y: tensor<f64>
     return x * y
-
 
 assert str(f.mlir_module.body.operations[0].arguments[0].location) == 'loc("x")'
 assert str(f.mlir_module.body.operations[0].arguments[1].location) == 'loc("y")'
-assert "%x: tensor<f64>" in f.mlir
-assert "%y: tensor<f64>" in f.mlir
+
 print_mlir(f, 0.3, 0.4)
