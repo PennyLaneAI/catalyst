1297bd1aa29928b681a729c828d99e1516ed8b50
Jeffrey Kam <jeffrey.kam@xanadu.ai>
2025-10-03 16:17:13 -0400

    Fixing format

--- frontend/catalyst/qfunc.py
+++ frontend/catalyst/qfunc.py
@@ -432,7 +432,7 @@ def _process_terminal_measurements(mcm_method, cpy_tape, out, snapshots, shot_ve
         result = jnp.squeeze(out[idx])
         max_ndim = min(len(out[idx].shape), 2)
         if out[idx].shape[0] == 1:
-            # Adding the first axis back when the first axis in the original 
+            # Adding the first axis back when the first axis in the original
             # array is 1, since it corresponds to the shot's dimension.
             result = jnp.expand_dims(result, axis=0)
         if result.ndim == 1 and max_ndim == 2:
