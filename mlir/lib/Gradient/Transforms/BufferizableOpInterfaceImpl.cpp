b1082d0a0ffa38624aedff39297c9fcf9cd80058
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-10-16 13:54:57 -0400

    Fix formatting

--- mlir/lib/Gradient/Transforms/BufferizableOpInterfaceImpl.cpp
+++ mlir/lib/Gradient/Transforms/BufferizableOpInterfaceImpl.cpp
@@ -402,10 +402,10 @@ struct ForwardOpInterface
         return {};
     }
 
-    FailureOr<bufferization::BufferLikeType> getBufferType(Operation *op, Value value,
-                                            const bufferization::BufferizationOptions &options,
-                                            const bufferization::BufferizationState &state,
-                                            SmallVector<Value> &invocationStack) const
+    FailureOr<bufferization::BufferLikeType>
+    getBufferType(Operation *op, Value value, const bufferization::BufferizationOptions &options,
+                  const bufferization::BufferizationState &state,
+                  SmallVector<Value> &invocationStack) const
     {
         // The getBufferType() method is called on either BlockArguments or OpResults.
         // https://github.com/llvm/llvm-project/blob/main/mlir/include/mlir/Dialect/Bufferization/IR/BufferizableOpInterface.td#L506
