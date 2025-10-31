6102eaf9dcaffec58e6ca1daeda9b86dbeb5e63f
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-10-31 10:35:41 -0400

    update

--- mlir/lib/Quantum/IR/QuantumOps.cpp
+++ mlir/lib/Quantum/IR/QuantumOps.cpp
@@ -155,11 +155,9 @@ LogicalResult ExtractOp::canonicalize(ExtractOp extract, mlir::PatternRewriter &
         bool bothDynamic = !extract.getIdxAttr().has_value() && !insert.getIdxAttr().has_value();
         bool staticallyEqual = bothStatic && extract.getIdxAttrAttr() == insert.getIdxAttrAttr();
         bool dynamicallyEqual = bothDynamic && extract.getIdx() == insert.getIdx();
+        // if other users of insert are also `insert` or `extract`, we are good to go
         bool valid = llvm::all_of(insert.getResult().getUsers(), [&](Operation *op) {
-            if (isa<InsertOp>(op) || isa<ExtractOp>(op)) {
-                return true;
-            }
-            return false;
+            return isa<InsertOp>(op) || isa<ExtractOp>(op);
         });
         if ((staticallyEqual || dynamicallyEqual) && valid) {
             rewriter.replaceOp(extract, insert.getQubit());
