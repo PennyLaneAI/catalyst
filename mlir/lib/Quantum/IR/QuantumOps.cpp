347d25cd63bc72ff11d59d31446f5b0c4ce5bb6b
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-11-03 16:11:33 -0500

    fix

--- mlir/lib/Quantum/IR/QuantumOps.cpp
+++ mlir/lib/Quantum/IR/QuantumOps.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <llvm/ADT/STLExtras.h>
 #include <optional>
 #include <type_traits>
 
