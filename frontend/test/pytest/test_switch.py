8d4ed077e1763afb54eb0c32e785ca7926093634
River McCubbin <river.mccubbin@xanadu.ai>
2025-11-04 10:45:38 -0500

    added tests for interpreted switch

---
+++ frontend/test/pytest/test_switch.py
@@ -0,0 +1,48 @@
+import jax
+import pennylane as qml
+import pytest
+
+from catalyst import api_extensions, qjit, switch
+from catalyst.api_extensions.control_flow import SwitchCallable
+
+
+class TestInterpreted:
+    """Test that Catalyst switches can be used with the python interpreter."""
+
+    @pytest.mark.parametrize("i", [-1, -4, 0, 1, 2, 19])
+    def test_clamped_index(self, i):
+        branches = [lambda: 0, lambda: 1, lambda: 2]
+
+        assert SwitchCallable(i, branches)() == jax.lax.clamp(0, i, 2)
+
+    @pytest.mark.parametrize("x", [12, 9.3, complex(4, 3)])
+    def test_1_branch(self, x):
+        branches = [lambda x: 2 * x]
+
+        assert SwitchCallable(0, branches)(x) == 2 * x
+
+    @pytest.mark.parametrize(
+        "i,x", [(0, 1), (0, 1.3), (1, 1), (1, complex(1, 2)), (2, 1), (2, 19 / 2)]
+    )
+    def test_3_branch(self, i, x):
+        branches = [lambda x: -x, lambda x: 0, lambda x: x]
+
+        assert SwitchCallable(i, branches)(x) == branches[i](x)
+
+    def test_decorator(self):
+        def foo(i, x):
+            @switch(i)
+            def new_switch(x):
+                return 0
+
+            @new_switch.branch()
+            def branch_1(x):
+                return x
+
+            @new_switch.branch()
+            def branch_2(x):
+                return 2 * x
+
+            return new_switch(x)
+
+        assert foo(1, 1) == 1
