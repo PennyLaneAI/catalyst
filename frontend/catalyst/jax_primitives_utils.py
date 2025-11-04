b10edac093a99e95e3f3ff0d53acf646686c4f75
Mehrdad Malek <39844030+mehrdad2m@users.noreply.github.com>
2025-11-04 09:48:15 -0500

    Update frontend/catalyst/jax_primitives_utils.py
    Co-authored-by: Mudit Pandey <18223836+mudit2812@users.noreply.github.com>

--- frontend/catalyst/jax_primitives_utils.py
+++ frontend/catalyst/jax_primitives_utils.py
@@ -365,7 +365,8 @@ def transform_named_sequence_lowering(jax_ctx: mlir.LoweringRuleContext, pipelin
                 target = apply_registered_pass_op.result
 
                 try:
-                    from pennylane.compiler.python_compiler.pass_api import (  # pylint: disable=import-outside-toplevel
+                    # pylint: disable=import-outside-toplevel
+                    from pennylane.compiler.python_compiler.pass_api import (
                         is_xdsl_pass,
                     )
 
