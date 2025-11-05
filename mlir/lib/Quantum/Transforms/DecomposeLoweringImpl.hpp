828b9aee748c3b9aee964ad0c545bed63406d53b
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-11-04 21:15:18 -0500

    formatting

--- mlir/lib/Quantum/Transforms/DecomposeLoweringImpl.hpp
+++ mlir/lib/Quantum/Transforms/DecomposeLoweringImpl.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <variant>
 #include <assert.h>
+#include <variant>
 
 #include "llvm/ADT/StringMap.h"
 #include "llvm/ADT/StringSet.h"
