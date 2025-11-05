3cbe0c065b829ac87332fc8854382fb4666fc3f4
albi3ro <chrissie.c.l@gmail.com>
2025-11-05 13:43:32 -0500

    trying to organize the tests

--- frontend/test/pytest/conftest.py
+++ frontend/test/pytest/conftest.py
@@ -63,6 +63,8 @@ def use_capture():
 def use_both_frontend(request):
     """Runs the test once with capture enabled and once with it disabled."""
     if request.param == "capture":
+        if 'capture_todo' in request.keywords:
+            pytest.xfail("capture todo's do not yet work with program capture.")
         qml.capture.enable()
         try:
             yield
