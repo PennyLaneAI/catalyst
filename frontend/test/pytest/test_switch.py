4aed6fed5ad9b7f72b78ffc4ac55f43fe1e7fae2
River McCubbin <river.mccubbin@xanadu.ai>
2025-11-06 13:26:00 -0500

    added test for classical context

--- frontend/test/pytest/test_switch.py
+++ frontend/test/pytest/test_switch.py
@@ -32,7 +32,8 @@ class TestInterpreted:
 
         assert SwitchCallable(i, range(3), branches)(x) == branches[i](x)
 
-    def test_decorator(self):
+    @pytest.mark.parametrize("i,x", [(0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 5), (5, 6)])
+    def test_decorator(self, i, x):
         def foo(i, x):
             @switch(i)
             def new_switch(x):
@@ -52,4 +53,74 @@ class TestInterpreted:
 
             return new_switch(x)
 
-        assert foo(1, 1) == 1
+        assert foo(i, x) == (i * x if i in [0, 1, 2] else -1)
+
+
+class TestClassicalCompiled:
+    """Test compiled Catalyst switches."""
+
+    @pytest.mark.parametrize("i", [-1, -4, 0, 1, 2, 19])
+    def test_default(self, i):
+        @qjit
+        def foo(j):
+            @switch(j)
+            def my_switch():
+                return -1
+
+            @my_switch.branch(1)
+            def branch_1():
+                return 1
+
+            @my_switch.default()
+            def branch_2():
+                return 3
+
+            return my_switch()
+
+        assert foo(i) == (i * 2 - 1 if i in [0, 1] else 3)
+
+
+    @pytest.mark.parametrize("x", [1, 3, 5, 6, 8, 9, 11, 12])
+    def test_chosen_index(self, x):
+        @qjit
+        def foo(j, y):
+            @switch(j)
+            def my_switch(y, case=1):
+                return 3 * y + 1
+
+            @my_switch.default()
+            def branch_1(y):
+                return y // 2
+
+            return my_switch(y)
+
+        def collatz(x):
+            if x % 2:
+                return x // 2
+            return 3 * x + 1
+
+        assert foo(x % 2, x) == collatz(x)
+
+    @pytest.mark.parametrize("i", [-3, -2, -1, 0, 1, 2, 3, 4, 5, 8, 10, 12])
+    def test_non_sequential_indices(self, i):
+        @qjit
+        def foo(j):
+            @switch(j)
+            def my_switch():
+                return 0
+
+            @my_switch.branch(3)
+            def branch_3():
+                return 3
+
+            @my_switch.branch(-2)
+            def branch_m2():
+                return -2
+
+            @my_switch.default()
+            def default():
+                return 10
+
+            return my_switch()
+
+        assert foo(i) == (i if i in [0, 3, -2] else 10)
