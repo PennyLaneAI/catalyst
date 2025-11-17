a1abd24d02db5885997b58425876c6dd339c8f4a
albi3ro <chrissie.c.l@gmail.com>
2025-11-17 18:44:27 -0500

    [skip ci] starting to test

--- frontend/catalyst/qfunc.py
+++ frontend/catalyst/qfunc.py
@@ -43,7 +43,7 @@ from catalyst.jax_extras.tracing import uses_transform
 from catalyst.jax_primitives import quantum_kernel_p
 from catalyst.jax_tracer import Function, trace_quantum_function
 from catalyst.logging import debug_logger
-from catalyst.passes.pass_api import Pass, dictionary_to_list_of_passes
+from catalyst.passes.pass_api import dictionary_to_list_of_passes
 from catalyst.tracing.contexts import EvaluationContext
 from catalyst.tracing.type_signatures import filter_static_args
 from catalyst.utils.exceptions import CompileError
