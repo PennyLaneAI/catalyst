9a324f9c362c0cfbb206e0cd95c1913c3dca4037
Jake Zaia <jake.zaia@xanadu.ai>
2025-11-20 22:53:36 +0000

    Format

--- runtime/tests/Test_NullQubit.cpp
+++ runtime/tests/Test_NullQubit.cpp
@@ -944,7 +944,7 @@ TEST_CASE("Test NullQubit device resource tracking integration", "[NullQubit]")
     std::ifstream resource_file_r(RESOURCES_FILENAME);
     if (resource_file_r.is_open()) {
         FAIL("Resource file should not exist before releasing the device"); // LCOV_EXCL_LINE
-        std::remove(RESOURCES_FILENAME.c_str());                        // LCOV_EXCL_LINE
+        std::remove(RESOURCES_FILENAME.c_str());                            // LCOV_EXCL_LINE
     }
 
     sim.reset(); // Destroy the device to trigger writing the resource data
