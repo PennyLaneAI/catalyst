e17c1112364a6056d0cc2a739b998ab69b48f2dc
andrijapau <andrija.paurevic@xanadu.ai>
2025-11-21 11:07:29 -0500

    add test skeleton

---
+++ frontend/test/pytest/python_interface/visualization/test_construct_circuit_dag.py
@@ -0,0 +1,24 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit tests for the ConstructCircuitDAG utility."""
+
+import pytest
+
+pytestmark = pytest.mark.usefixtures("requires_xdsl")
+
+# pylint: disable=wrong-import-position
+# This import needs to be after pytest in order to prevent ImportErrors
+from catalyst.python_interface.visualization.construct_circuit_dag import ConstructCircuitDAG 
+from catalyst.python_interface.visualization.dag_builder import DAGBuilder 
+
