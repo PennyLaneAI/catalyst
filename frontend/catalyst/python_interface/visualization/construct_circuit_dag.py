cddb234a7b3c20d7d6657e0e4aebfdfb5468b80a
andrijapau <andrija.paurevic@xanadu.ai>
2025-11-25 09:24:28 -0500

    clean-up

--- frontend/catalyst/python_interface/visualization/construct_circuit_dag.py
+++ frontend/catalyst/python_interface/visualization/construct_circuit_dag.py
@@ -24,11 +24,11 @@ from catalyst.python_interface.visualization.dag_builder import DAGBuilder
 
 
 class ConstructCircuitDAG:
-    """A tool that analyzes an xDSL module and constructs a Directed Acyclic Graph (DAG)
-    using an injected DAGBuilder instance. This tool does not mutate the xDSL module."""
+    """A tool that traverses an xDSL module and constructs a Directed Acyclic Graph (DAG)
+    of it's quantum program using an injected DAGBuilder instance. This tool does not mutate the xDSL module."""
 
     def __init__(self, dag_builder: DAGBuilder) -> None:
-        """Initialize the analysis pass by injecting the DAG builder dependency.
+        """Initialize the utility by injecting the DAG builder dependency.
 
         Args:
             dag_builder (DAGBuilder): The concrete builder instance used for graph construction.
@@ -45,7 +45,12 @@ class ConstructCircuitDAG:
         to the specialized handler registered for its type."""
 
     def construct(self, module: builtin.ModuleOp) -> None:
-        """Constructs the DAG from the module."""
+        """Constructs the DAG from the module.
+
+        Args:
+            module (xdsl.builtin.ModuleOp): The module containing the quantum program to visualize.
+
+        """
         for op in module.ops:
             self._visit(op)
 
