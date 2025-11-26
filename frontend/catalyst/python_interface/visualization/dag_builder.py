194f14a55d86cb713584a7ac722105cb2f12bd48
Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
2025-11-26 13:48:16 -0500

    Update frontend/catalyst/python_interface/visualization/dag_builder.py
    Co-authored-by: Mehrdad Malek <39844030+mehrdad2m@users.noreply.github.com>

--- frontend/catalyst/python_interface/visualization/dag_builder.py
+++ frontend/catalyst/python_interface/visualization/dag_builder.py
@@ -110,7 +110,7 @@ class DAGBuilder(ABC):
         """Retrieve the current set of clusters in the graph.
 
         Returns:
-            clusters (dict[str, dict[str, Any]]): A dictionary that maps the cluster's ID to it's cluster information.
+            clusters (dict[str, dict[str, Any]]): A dictionary that maps the cluster's ID to its cluster information.
         """
         raise NotImplementedError
 
