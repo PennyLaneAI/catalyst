ae0ff325ad8063bf837073179d36f1687087afb1
andrijapau <andrija.paurevic@xanadu.ai>
2025-11-26 16:22:59 -0500

    format

--- frontend/test/pytest/python_interface/visualization/test_construct_circuit_dag.py
+++ frontend/test/pytest/python_interface/visualization/test_construct_circuit_dag.py
@@ -22,14 +22,15 @@ pytestmark = pytest.mark.usefixtures("requires_xdsl")
 # pylint: disable=wrong-import-position
 # This import needs to be after pytest in order to prevent ImportErrors
 import pennylane as qml
+from xdsl.dialects import test
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.ir.core import Block, Region
+
 from catalyst.python_interface.conversion import xdsl_from_qjit
 from catalyst.python_interface.visualization.construct_circuit_dag import (
     ConstructCircuitDAG,
 )
 from catalyst.python_interface.visualization.dag_builder import DAGBuilder
-from xdsl.dialects import test
-from xdsl.dialects.builtin import ModuleOp
-from xdsl.ir.core import Block, Region
 
 
 class FakeDAGBuilder(DAGBuilder):
@@ -155,9 +156,7 @@ class TestFuncOpVisualization:
             "setup",
             "teardown",
         ]
-        generated_cluster_labels = {
-            info["cluster_label"] for info in graph_clusters.values()
-        }
+        generated_cluster_labels = {info["cluster_label"] for info in graph_clusters.values()}
         for cluster_label in expected_cluster_labels:
             assert cluster_label in generated_cluster_labels
 
@@ -269,9 +268,7 @@ class TestDeviceNode:
             for child_cluster in graph_clusters.values()
             if child_cluster.get("cluster_id") is not None
         )
-        node_label_to_parent_label: dict[str, str] = dict(
-            zip(tuple(node_labels), parent_labels)
-        )
+        node_label_to_parent_label: dict[str, str] = dict(zip(tuple(node_labels), parent_labels))
         assert node_label_to_parent_label["NullQubit"] == "my_workflow"
 
     def test_nested_qnodes(self):
@@ -330,8 +327,6 @@ class TestDeviceNode:
             if child_node.get("cluster_id") is not None
         )
         node_labels = {info["label"] for info in graph_nodes.values()}
-        node_label_to_parent_label: dict[str, str] = dict(
-            zip(tuple(node_labels), parent_labels)
-        )
+        node_label_to_parent_label: dict[str, str] = dict(zip(tuple(node_labels), parent_labels))
         assert node_label_to_parent_label["NullQubit"] == "my_qnode1"
         assert node_label_to_parent_label["LightningSimulator"] == "my_qnode2"
