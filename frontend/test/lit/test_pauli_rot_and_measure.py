d26e506dd9d4d532be33dbecc1fb074b787c14ce
Jeffrey Kam <jeffrey.kam@xanadu.ai>
2025-11-27 18:08:29 -0500

    Fixing a missing line

--- frontend/test/lit/test_pauli_rot_and_measure.py
+++ frontend/test/lit/test_pauli_rot_and_measure.py
@@ -160,6 +160,7 @@ def test_dynamic_angle_multi_qubit_pauli_rotations():
 
     # CHECK: [[extracted:%.+]] = tensor.extract
     # CHECK: [[q0:%.+]]:2 = qec.ppr.arbitrary ["X", "Z"]([[extracted]])
+    # CHECK: [[q1:%.+]]:2 = qec.ppr.arbitrary ["Y", "X"]([[extracted]]) [[q0]]#0, [[q0]]#1
     print(circuit.mlir_opt)
     qml.capture.disable()
 
