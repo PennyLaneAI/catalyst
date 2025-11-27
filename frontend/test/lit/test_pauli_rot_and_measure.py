f6f11280c2065ea41f9ac65c28e8443991e6f180
Jeffrey Kam <jeffrey.kam@xanadu.ai>
2025-11-27 18:05:49 -0500

    Adding multi-qubit dynamic and arbitrary angles PauliRot lit test

--- frontend/test/lit/test_pauli_rot_and_measure.py
+++ frontend/test/lit/test_pauli_rot_and_measure.py
@@ -121,6 +121,52 @@ def test_multi_qubit_pauli_rotations():
 test_multi_qubit_pauli_rotations()
 
 
+def test_arbitrary_angle_multi_qubit_pauli_rotations():
+    """Test arbitrary angle multi-qubit PauliRot"""
+    qml.capture.enable()
+    dev = qml.device("null.qubit", wires=3)
+
+    pipeline = [("pipe", ["enforce-runtime-invariants-pipeline"])]
+
+    @qjit(pipelines=pipeline, target="mlir")
+    @qml.qnode(device=dev)
+    def circuit():
+        qml.PauliRot(0.42, "XZ", wires=[0, 1])
+        qml.PauliRot(0.84, "YX", wires=[0, 1])
+
+    # CHECK: [[cst:%.+]] = arith.constant 8.400000e-01 : f64
+    # CHECK: [[cst_1:%.+]] = arith.constant 4.200000e-01 : f64
+    # CHECK: [[q0:%.+]]:2 = qec.ppr.arbitrary ["X", "Z"]([[cst_1]])
+    # CHECK: [[q1:%.+]]:2 = qec.ppr.arbitrary ["Y", "X"]([[cst]]) [[q0]]#0, [[q0]]#1
+    print(circuit.mlir_opt)
+    qml.capture.disable()
+
+
+test_arbitrary_angle_multi_qubit_pauli_rotations()
+
+
+def test_dynamic_angle_multi_qubit_pauli_rotations():
+    """Test dynamic angle multi-qubit PauliRot"""
+    qml.capture.enable()
+    dev = qml.device("null.qubit", wires=3)
+
+    pipeline = [("pipe", ["enforce-runtime-invariants-pipeline"])]
+    
+    @qjit(pipelines=pipeline, target="mlir")
+    @qml.qnode(device=dev)
+    def circuit(x: float):
+        qml.PauliRot(x, "XZ", wires=[0, 1])
+        qml.PauliRot(x, "YX", wires=[0, 1])
+
+    # CHECK: [[extracted:%.+]] = tensor.extract
+    # CHECK: [[q0:%.+]]:2 = qec.ppr.arbitrary ["X", "Z"]([[extracted]])
+    print(circuit.mlir_opt)
+    qml.capture.disable()
+
+
+test_dynamic_angle_multi_qubit_pauli_rotations()
+
+
 def test_single_qubit_pauli_measurements():
     """Test single qubit PauliMeasure"""
     qml.capture.enable()
