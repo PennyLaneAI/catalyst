4f851ef3c2554602c1acc61c8dc4932b1834e255
Hong-Sheng Zheng <mathan0203@gmail.com>
2025-12-02 16:18:46 -0500

    add missing include

--- mlir/lib/Ion/Transforms/ion-to-rtio.cpp
+++ mlir/lib/Ion/Transforms/ion-to-rtio.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <queue>
+
 #include "llvm/Support/JSON.h"
 #include "llvm/Support/MemoryBuffer.h"
 
