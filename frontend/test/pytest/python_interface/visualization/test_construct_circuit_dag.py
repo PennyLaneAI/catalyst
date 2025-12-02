7eb31b61dcb07b8401526a94ea6d07081d757a58
andrijapau <andrija.paurevic@xanadu.ai>
2025-12-02 14:08:15 -0500

    format

--- frontend/test/pytest/python_interface/visualization/test_construct_circuit_dag.py
+++ frontend/test/pytest/python_interface/visualization/test_construct_circuit_dag.py
@@ -23,14 +23,15 @@ pytestmark = pytest.mark.usefixtures("requires_xdsl")
 # pylint: disable=wrong-import-position
 # This import needs to be after pytest in order to prevent ImportErrors
 import pennylane as qml
+from xdsl.dialects import test
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.ir.core import Block, Region
+
 from catalyst.python_interface.conversion import xdsl_from_qjit
 from catalyst.python_interface.visualization.construct_circuit_dag import (
     ConstructCircuitDAG,
 )
 from catalyst.python_interface.visualization.dag_builder import DAGBuilder
-from xdsl.dialects import test
-from xdsl.dialects.builtin import ModuleOp
-from xdsl.ir.core import Block, Region
 
 
 class FakeDAGBuilder(DAGBuilder):
@@ -96,9 +97,7 @@ class FakeDAGBuilder(DAGBuilder):
         cluster_labels = []
         for cluster_data in self._clusters.values():
             if cluster_data["parent_cluster_uid"] == parent_cluster_uid:
-                cluster_label = (
-                    cluster_data["cluster_label"] or cluster_data["node_label"]
-                )
+                cluster_label = cluster_data["cluster_label"] or cluster_data["node_label"]
                 cluster_labels.append(cluster_label)
         return cluster_labels
 
@@ -155,9 +154,7 @@ class TestFakeDAGBuilder:
         builder = FakeDAGBuilder()
 
         # Cluster set-up
-        builder.add_cluster(
-            "c0", label="Company", cluster_uid=None
-        )  # Add to base graph
+        builder.add_cluster("c0", label="Company", cluster_uid=None)  # Add to base graph
         builder.add_cluster("c1", label="Marketing", cluster_uid="c0")
         builder.add_cluster("c2", label="Finance", cluster_uid="c0")
 
