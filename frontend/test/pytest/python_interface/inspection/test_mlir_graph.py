d62ba55f28cc5fcecda214603dc19b0c7276f686
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-12-09 16:41:16 -0500

    Update how we check for graphviz installation

--- frontend/test/pytest/python_interface/inspection/test_mlir_graph.py
+++ frontend/test/pytest/python_interface/inspection/test_mlir_graph.py
@@ -14,7 +14,7 @@
 """Unit test module for the MLIR graph generation in the Unified Compiler visualization module."""
 
 from pathlib import Path
-from subprocess import run
+from shutil import which
 
 import pennylane as qml
 import pytest
@@ -28,7 +28,7 @@ from catalyst.python_interface.transforms import (
 pytestmark = pytest.mark.xdsl
 graphviz = pytest.importorskip("graphviz")
 
-if run(["/usr/bin/which", "dot"], check=False).returncode != 0:
+if which("dot") is None:
     pytest.skip(reason="Graphviz isn't installed.")
 
 
