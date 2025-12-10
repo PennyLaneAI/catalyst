80798f92a4905f7f48b0413614822cd8f2859cb9
andrijapau <andrija.paurevic@xanadu.ai>
2025-12-10 15:02:00 -0500

    fix cluster

--- frontend/catalyst/python_interface/visualization/construct_circuit_dag.py
+++ frontend/catalyst/python_interface/visualization/construct_circuit_dag.py
@@ -153,6 +153,7 @@ class ConstructCircuitDAG:
                 uid,
                 label=cluster_label,
                 labeljust="l",
+                style="dashed",
                 cluster_uid=self._cluster_uid_stack[-1],
             )
             self._cluster_uid_stack.append(uid)
