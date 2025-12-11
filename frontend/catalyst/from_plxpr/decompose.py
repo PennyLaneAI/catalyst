0bab96b198fe02085cdb3a00da1d2145870bca65
Jeffrey Kam <jeffrey.kam@xanadu.ai>
2025-12-11 18:18:13 -0500

    Minor fixes

--- frontend/catalyst/from_plxpr/decompose.py
+++ frontend/catalyst/from_plxpr/decompose.py
@@ -232,15 +232,14 @@ class DecompRuleInterpreter(qml.capture.PlxprInterpreter):
                     # in the circuit, but is used inside a decomposition rule.
                     # In this case, we fall back to using the COMPILER_OPS_FOR_DECOMPOSITION
                     # dictionary to get the number of wires.
+                    num_wires, num_params = COMPILER_OPS_FOR_DECOMPOSITION[op.op.name]
                     pauli_word = op.op.params.get("pauli_word", None)
+
                     if op.op.name == "PauliRot":
                         num_wires = len(pauli_word)
-                        num_params = 1  # theta
                     elif op.op.name == "MultiRZ":
                         num_wires = op_num_wires
-                        num_params = 1  # theta
-                    else:
-                        num_wires, num_params = COMPILER_OPS_FOR_DECOMPOSITION[op.op.name]
+
                     _create_decomposition_rule(
                         rule,
                         op_name=op.op.name,
@@ -251,7 +250,14 @@ class DecompRuleInterpreter(qml.capture.PlxprInterpreter):
                         pauli_word=pauli_word,
                     )
                 elif not any(
-                    keyword in getattr(op.op, "name", "") for keyword in ("Adjoint", "Controlled")
+                    keyword in getattr(op.op, "name", "")
+                    for keyword in (
+                        "Adjoint",
+                        "Controlled",
+                        "TemporaryAND",
+                        "ChangeBasisOp",
+                        "Prod",
+                    )
                 ):  # pragma: no cover
                     # Note that the graph-decomposition returns abstracted rules
                     # for Adjoint and Controlled operations, so we skip them here.
@@ -272,7 +278,7 @@ def _create_decomposition_rule(
     num_params: int,
     requires_copy: bool = False,
     ag_enabled: bool = False,
-    pauli_word=None,
+    pauli_word: str | None = None,
 ):
     """Create a decomposition rule from a callable.
 
