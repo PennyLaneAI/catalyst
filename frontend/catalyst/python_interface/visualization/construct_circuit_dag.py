ccd9c65c382342a48eb3517b83f3bbb66b0ec44d
andrijapau <andrija.paurevic@xanadu.ai>
2025-12-11 10:45:00 -0500

    Add back comment

--- frontend/catalyst/python_interface/visualization/construct_circuit_dag.py
+++ frontend/catalyst/python_interface/visualization/construct_circuit_dag.py
@@ -224,6 +224,7 @@ class ConstructCircuitDAG:
             cluster_uid=self._cluster_uid_stack[-1],
             fillcolor="lightpink",
             color="lightpink3",
+            # NOTE: "record" allows us to use ports (https://graphviz.org/doc/info/shapes.html#record)
             shape="record",
         )
         self._node_uid_counter += 1
