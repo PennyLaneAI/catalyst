461674ebbca1ae8c91b8c37167ab2af8529feb4f
Jeffrey Kam <jeffrey.kam@xanadu.ai>
2025-12-12 16:38:50 -0500

    Fix format

--- frontend/test/pytest/test_peephole_optimizations.py
+++ frontend/test/pytest/test_peephole_optimizations.py
@@ -17,6 +17,7 @@
 import numpy as np
 import pennylane as qml
 import pytest
+import pennylane.ftqc.catalyst_pass_aliases as catalyst_passes
 
 from catalyst import measure, pipeline, qjit
 from catalyst.passes import (
@@ -506,13 +507,10 @@ def test_merge_rotation_ppr():
 
     my_pipeline = [("pipe", ["quantum-compilation-stage"])]
 
-    # Need to import this version of to_ppr to be capture-compatible
-    from pennylane.ftqc.catalyst_pass_aliases import to_ppr
-
     @qml.qjit(pipelines=my_pipeline, target="mlir")
     def test_merge_rotation_ppr_workflow():
         @qml.transforms.merge_rotations  # have to use qml to be capture-compatible
-        @to_ppr
+        @catalyst_passes.to_ppr
         @qml.qnode(qml.device("lightning.qubit", wires=3))
         def circuit():
             qml.PauliRot(np.pi / 2, pauli_word="XYZ", wires=[0, 1, 2])
