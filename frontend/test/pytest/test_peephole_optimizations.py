Fixing merge rotation pytest

frontend/test/pytest/test_peephole_optimizations.py
@@ -506,9 +506,13 @@ def test_merge_rotation_ppr():
 
     my_pipeline = [("pipe", ["quantum-compilation-stage"])]
 
+    # Need to import this version of to_ppr to be capture-compatible
+    from pennylane.ftqc.catalyst_pass_aliases import to_ppr
+
     @qml.qjit(pipelines=my_pipeline, target="mlir")
     def test_merge_rotation_ppr_workflow():
         @qml.transforms.merge_rotations  # have to use qml to be capture-compatible
+        @to_ppr
         @qml.qnode(qml.device("lightning.qubit", wires=3))
         def circuit():
             qml.PauliRot(np.pi / 2, pauli_word="XYZ", wires=[0, 1, 2])
@@ -520,7 +524,6 @@ def test_merge_rotation_ppr():
     ir_opt = test_merge_rotation_ppr_workflow.mlir_opt
 
     assert 'transform.apply_registered_pass "merge-rotations"' in ir
-    assert "qec.ppr" in ir
     assert 'qec.ppr ["X", "Y", "Z"](4)' not in ir_opt
     assert 'qec.ppr ["X", "Y", "Z"](2)' in ir_opt
 