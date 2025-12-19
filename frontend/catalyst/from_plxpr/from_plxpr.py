ff69cfb08c9255903cdd2a920841feac197c7fcc
albi3ro <chrissie.c.l@gmail.com>
2025-12-19 12:58:52 -0500

    translate and lower jvp

--- frontend/catalyst/from_plxpr/from_plxpr.py
+++ frontend/catalyst/from_plxpr/from_plxpr.py
@@ -30,6 +30,7 @@ from pennylane.capture.expand_transforms import ExpandTransformsInterpreter
 from pennylane.capture.primitives import jacobian_prim as pl_jac_prim
 from pennylane.capture.primitives import transform_prim
 from pennylane.capture.primitives import vjp_prim as pl_vjp_prim
+from pennylane.capture.primitives import jvp_prim as pl_jvp_prim
 from pennylane.transforms import commute_controlled as pl_commute_controlled
 from pennylane.transforms import decompose as pl_decompose
 from pennylane.transforms import gridsynth as pl_gridsynth
@@ -250,6 +251,19 @@ def handle_vjp(self, *args, jaxpr, **kwargs):
     j = new_jaxpr.jaxpr
     new_j = j.replace(constvars=(), invars=j.constvars + j.invars)
     return pl_vjp_prim.bind(*new_args, jaxpr=new_j, **kwargs)
+
+
+@WorkflowInterpreter.register_primitive(pl_jvp_prim)
+def handle_jvp(self, *args, jaxpr, **kwargs):
+    """Translate a grad equation."""
+    f = partial(copy(self).eval, jaxpr, [])
+    new_jaxpr = jax.make_jaxpr(f)(*args[: -len(jaxpr.outvars)])
+
+    new_args = (*new_jaxpr.consts, *args)
+    j = new_jaxpr.jaxpr
+    new_j = j.replace(constvars=(), invars=j.constvars + j.invars)
+    return pl_jvp_prim.bind(*new_args, jaxpr=new_j, **kwargs)
+
 
 
 # pylint: disable=unused-argument, too-many-arguments
