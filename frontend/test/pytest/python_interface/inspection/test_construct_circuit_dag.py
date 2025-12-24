2a33e022fe8ce4d6d54cc3c87637e55cdc43ccff
andrijapau <andrija.paurevic@xanadu.ai>
2025-12-24 13:51:48 -0500

    disable some pylint stuff

--- frontend/test/pytest/python_interface/inspection/test_construct_circuit_dag.py
+++ frontend/test/pytest/python_interface/inspection/test_construct_circuit_dag.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Unit tests for the ConstructCircuitDAG utility."""
+# pylint: disable=unused-argument, unused-variable
 
 import re
 from unittest.mock import Mock
