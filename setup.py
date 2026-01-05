1f2ff43654e97e3b0cab34aa1f4f2b54e27540b0
paul0403 <paulhaochen.wang@gmail.com>
2026-01-05 16:24:06 -0500

    docs req

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ else:
     lightning_dep = f"pennylane-lightning>={lq_min_release}"
     kokkos_dep = ""
 
-# On if targeting RC branch, we must install latest RC packages from TestPyPI
+# If targeting RC branch, we must install latest RC packages from TestPyPI
 # Revert when merging RC branch back to main
 pennylane_dep = f"pennylane>=0.44.0rc0,<=0.44.0"
 lightning_dep = f"pennylane-lightning>=0.44.0rc0,<=0.44.0"
