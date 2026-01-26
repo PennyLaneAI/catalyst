99b9d2b39771a1e19f90e90bd8e373d501518e63
Hong-Sheng Zheng <mathan0203@gmail.com>
2026-01-26 10:28:55 -0500

    formatting

--- frontend/test/test_oqd/oqd/test_oqd_artiq_llvmir.py
+++ frontend/test/test_oqd/oqd/test_oqd_artiq_llvmir.py
@@ -20,7 +20,7 @@ import numpy as np
 import pennylane as qml
 import pytest
 
-from catalyst import qjit, CompileError
+from catalyst import CompileError, qjit
 from catalyst.third_party.oqd import OQDDevice, OQDDevicePipeline
 
 MODULE_TEST_PATH = os.path.dirname(__file__)
