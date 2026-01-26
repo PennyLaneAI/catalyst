deac3f23f3de4f5cfb6efef6d3629824420b2fc1
Hong-Sheng Zheng <mathan0203@gmail.com>
2026-01-26 10:27:33 -0500

    fix

--- frontend/test/test_oqd/oqd/conftest.py
+++ frontend/test/test_oqd/oqd/conftest.py
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Pytest configuration file for OQD test suite.
+"""
+import os
+from tempfile import TemporaryDirectory
+
 import pennylane as qml
 import pytest
 
