5b4a90f5b1b3be22137037b6a8a9d8fa23c6778f
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-01-27 14:24:49 -0500

    Codefactor

--- frontend/catalyst/python_interface/dialects/quantum/operations/__init__.py
+++ frontend/catalyst/python_interface/dialects/quantum/operations/__init__.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Operations for the xDSL Quantum dialect, which mirrors Catalyst's MLIR
+Quantum dialect."""
 
 from .gates import (
     CustomOp,
