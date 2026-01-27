e9fb7e40102e2544c9884543949302cfe6a8535b
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-01-27 14:16:24 -0500

    Restructure files for quantum dialect

---
+++ frontend/catalyst/python_interface/dialects/quantum/operations/__init__.py
@@ -0,0 +1,100 @@
+# Copyright 2026 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .gates import (
+    CustomOp,
+    GateOp,
+    GlobalPhaseOp,
+    MultiRZOp,
+    PauliRotOp,
+    PCPhaseOp,
+    QubitUnitaryOp,
+    SetBasisStateOp,
+    SetStateOp,
+)
+from .measurements import (
+    CountsOp,
+    ExpvalOp,
+    MeasureOp,
+    ProbsOp,
+    SampleOp,
+    StateOp,
+    TerminalMeasurementOp,
+    VarianceOp,
+)
+from .miscellaneous import (
+    AdjointOp,
+    DeviceInitOp,
+    DeviceReleaseOp,
+    FinalizeOp,
+    InitializeOp,
+    NumQubitsOp,
+    YieldOp,
+)
+from .observables import (
+    ComputationalBasisOp,
+    HamiltonianOp,
+    HermitianOp,
+    NamedObsOp,
+    ObservableOp,
+    TensorOp,
+)
+from .qubit_ops import AllocOp, AllocQubitOp, DeallocOp, DeallocQubitOp, ExtractOp, InsertOp
+
+__all__ = [
+    # Operation bases
+    "GateOp",
+    "ObservableOp",
+    "TerminalMeasurementOp",
+    # Gates
+    "CustomOp",
+    "GlobalPhaseOp",
+    "MultiRZOp",
+    "PauliRotOp",
+    "PCPhaseOp",
+    "QubitUnitaryOp",
+    # State preparation
+    "SetBasisStateOp",
+    "SetStateOp",
+    # Observables
+    "ComputationalBasisOp",
+    "HamiltonianOp",
+    "HermitianOp",
+    "NamedObsOp",
+    "TensorOp",
+    # Mid-circuit measurements
+    "MeasureOp",
+    # Terminal measurements
+    "CountsOp",
+    "ExpvalOp",
+    "ProbsOp",
+    "SampleOp",
+    "StateOp",
+    "VarianceOp",
+    # Qubit operations
+    "AllocOp",
+    "AllocQubitOp",
+    "DeallocOp",
+    "DeallocQubitOp",
+    "ExtractOp",
+    "InsertOp",
+    # Miscellaneous
+    "AdjointOp",
+    "DeviceInitOp",
+    "DeviceReleaseOp",
+    "FinalizeOp",
+    "InitializeOp",
+    "NumQubitsOp",
+    "YieldOp",
+]
