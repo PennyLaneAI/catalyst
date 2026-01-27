e53da336915da94480f95971ca0b700a90652054
Christina Lee <chrissie.c.l@gmail.com>
2026-01-27 16:22:04 -0500

    Add fixtures for pytest organization (#2438)
    **Context:**
    
    Adds markers for `old_frontend` and `capture_todo` so we can better
    organize our pytests.
    
    Pulled from #2178 because too much was going wrong there.
    
    **Description of the Change:**
    
    **Benefits:**
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**

--- frontend/test/pytest/conftest.py
+++ frontend/test/pytest/conftest.py
@@ -73,6 +73,8 @@ def use_capture_dgraph():
 def use_both_frontend(request):
     """Runs the test once with capture enabled and once with it disabled."""
     if request.param == "capture":
+        if "capture_todo" in request.keywords:
+            pytest.xfail("capture todo's do not yet work with program capture.")
         qml.capture.enable()
         try:
             yield
