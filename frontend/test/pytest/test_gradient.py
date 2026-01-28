442cd5ff84ce88e55c0a9d9ab38eafc5fe4001f3
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2026-01-28 17:34:04 -0500

    Revert "test with xfail before execution"
    This reverts commit d90a8bc702389478063c5c049b3703a40d88d349.

--- frontend/test/pytest/test_gradient.py
+++ frontend/test/pytest/test_gradient.py
@@ -1704,7 +1704,6 @@ def test_ellipsis_differentiation(backend, diff_method):
 
 def test_vmap_worflow_derivation(backend):
     """Check the gradient of a vmap workflow"""
-    pytest.xfail("Avoid segfault in CI: vmap differentiation not stable yet (see #332).")
     n_wires = 5
     data = jnp.sin(jnp.mgrid[-2:2:0.2].reshape(n_wires, -1)) ** 3
 
@@ -1760,7 +1759,6 @@ def test_vmap_worflow_derivation(backend):
 
 def test_forloop_vmap_worflow_derivation(backend):
     """Test a forloop vmap."""
-    pytest.xfail("Avoid segfault in CI: vmap differentiation not stable yet (see #332).")
     n_wires = 5
     data = jnp.sin(jnp.mgrid[-2:2:0.2].reshape(n_wires, -1)) ** 3
     weights = jnp.ones([n_wires, 3])
