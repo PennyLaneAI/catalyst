941e3d91a54f92a56e3bb33ddeb9074d8e357a43
Hong-Sheng Zheng <mathan0203@gmail.com>
2026-02-02 20:46:33 -0500

    remove unused import

--- frontend/test/test_oqd/oqd/test_oqd_artiq_llvmir.py
+++ frontend/test/test_oqd/oqd/test_oqd_artiq_llvmir.py
@@ -20,7 +20,7 @@ import numpy as np
 import pennylane as qml
 import pytest
 
-from catalyst import QJIT, qjit
+from catalyst import QJIT
 from catalyst.third_party.oqd import OQDDevice, OQDDevicePipeline
 from catalyst.pipelines import CompileOptions
 
