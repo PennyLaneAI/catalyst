399777fc728e5ec286fa1bd8564be31b61bf09d9
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-02-26 16:49:18 -0500

    Add device preprocessing to from_plxpr

--- frontend/catalyst/from_plxpr/from_plxpr.py
+++ frontend/catalyst/from_plxpr/from_plxpr.py
@@ -37,6 +37,7 @@ from pennylane.transforms import gridsynth as pl_gridsynth
 from pennylane.transforms import merge_amplitude_embedding as pl_merge_amplitude_embedding
 from pennylane.transforms import single_qubit_fusion as pl_single_qubit_fusion
 from pennylane.transforms import unitary_to_rot as pl_unitary_to_rot
+from pennylane.transforms.core import BoundTransform
 
 from catalyst.device import extract_backend_info
 from catalyst.from_plxpr.decompose import COMPILER_OPS_FOR_DECOMPOSITION, DecompRuleInterpreter
@@ -51,6 +52,7 @@ from catalyst.jax_primitives import (
 )
 from catalyst.utils.patching import Patcher
 
+from .device_utils import create_device_preprocessing_pipeline
 from .qfunc_interpreter import PLxPRToQuantumJaxprInterpreter
 from .qubit_handler import (
     QubitHandler,
@@ -359,11 +361,14 @@ def handle_qnode(
         gateset = [_get_operator_name(op) for op in self.decompose_tkwargs.get("gate_set", [])]
         setattr(qnode, "decompose_gatesets", [gateset])
 
+    device_pipeline = create_device_preprocessing_pipeline(
+        qnode.device, execution_config, shots, warn=True
+    )
     return quantum_kernel_p.bind(
         wrap_init(calling_convention, debug_info=qfunc_jaxpr.debug_info),
         *non_const_args,
         qnode=qnode,
-        pipeline=self._pass_pipeline,
+        pipeline=tuple(self._pass_pipeline + device_pipeline),
     )
 
 
@@ -417,7 +422,7 @@ def _handle_decompose_transform(self, inner_jaxpr, consts, non_const_args, tkwar
     # Add the decompose-lowering pass to the start of the pipeline
     if use_graph:
         t = qml.transform(pass_name="decompose-lowering")
-        pass_container = qml.transforms.core.TransformContainer(t)
+        pass_container = BoundTransform(t)
         next_eval._pass_pipeline.insert(0, pass_container)
 
     # We still need to construct and solve the graph based on
@@ -487,7 +492,7 @@ def handle_transform(
     # Apply the corresponding Catalyst pass counterpart
     next_eval = copy(self)
     t = qml.transform(pass_name=catalyst_pass_name)
-    bound_pass = qml.transforms.core.TransformContainer(t, args=targs, kwargs=tkwargs)
+    bound_pass = BoundTransform(t, args=targs, kwargs=tkwargs)
     next_eval._pass_pipeline.insert(0, bound_pass)
     return next_eval.eval(inner_jaxpr, consts, *non_const_args)
 
