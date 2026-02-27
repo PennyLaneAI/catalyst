6803bdfc6495abe184fb0f0b29760e7f639a5bc6
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-02-27 11:07:23 -0500

    Revert MLIR lowering changes

--- mlir/lib/Catalyst/Transforms/ApplyTransformSequencePass.cpp
+++ mlir/lib/Catalyst/Transforms/ApplyTransformSequencePass.cpp
@@ -177,22 +177,26 @@ struct ApplyTransformSequencePass
         // a valid transform with the transform dialect
         // We need to extract the transform.named_sequence in the
         // transformer module.
+        transform::NamedSequenceOp transformer_main_sequence;
         transformer->walk([&](transform::NamedSequenceOp op) {
-
-            if (PassInstrumentor *passInstrumentor = getAnalysisManager().getPassInstrumentor()) {
-                // Manually execute the transform sequence with individual subpass tracking
-                if (failed(applyTransformsWithSubpassTracking(payload, op,
-                                                            passInstrumentor))) {
-                    return signalPassFailure();
-                }
-            }
-            else {
-                if (failed(transform::applyTransforms(payload, op, {},
-                                                    transform::TransformOptions(), false))) {
-                    return signalPassFailure();
-                }
-            }
+            assert(!transformer_main_sequence &&
+                   "expected only one transform sequence in the transform module");
+            transformer_main_sequence = op;
         });
+
+        if (PassInstrumentor *passInstrumentor = getAnalysisManager().getPassInstrumentor()) {
+            // Manually execute the transform sequence with individual subpass tracking
+            if (failed(applyTransformsWithSubpassTracking(payload, transformer_main_sequence,
+                                                          passInstrumentor))) {
+                return signalPassFailure();
+            }
+        }
+        else {
+            if (failed(transform::applyTransforms(payload, transformer_main_sequence, {},
+                                                  transform::TransformOptions(), false))) {
+                return signalPassFailure();
+            }
+        }
 
         transformer.erase();
     }
