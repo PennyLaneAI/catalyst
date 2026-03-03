640e3d3fb8f363dd69257a88a1432648c05c3e63
lillian542 <Lillian.frederiksen@xanadu.ai>
2026-03-03 11:56:32 -0500

    add to TestAssemblyFormat::test_observables

--- frontend/test/pytest/python_interface/dialects/test_quantum_dialect.py
+++ frontend/test/pytest/python_interface/dialects/test_quantum_dialect.py
@@ -644,6 +644,19 @@ class TestAssemblyFormat:
         // CHECK: {{%.+}} = quantum.compbasis qreg [[QREG]] : !quantum.obs
         %cb_01 = quantum.compbasis qubits %q0, %q1 : !quantum.obs
         %cb_all = quantum.compbasis qreg %qreg : !quantum.obs
+
+        //////////// **MCMObsOp** ////////////
+        // create mcms
+        // CHECK: [[MCM1:%.+]] = "test.op"() : () -> i1
+        // CHECK: [[MCM2:%.+]] = "test.op"() : () -> i1
+        %mcm1 = "test.op"() : () -> i1
+        %mcm2 = "test.op"() : () -> i1
+
+        // CHECK: {{%.+}} = quantum.mcmobs [[MCM1]] : !quantum.obs
+        %mcm_obs1 = quantum.mcmobs %mcm1 : !quantum.obs
+
+        // CHECK: {{%.+}} = quantum.mcmobs [[MCM1]], [[MCM2]] : !quantum.obs
+        %mcm_obs2 = quantum.mcmobs %mcm1, %mcm2 : !quantum.obs
         """
 
         run_filecheck(program, roundtrip=True, verify=True, pretty_print=pretty_print)
