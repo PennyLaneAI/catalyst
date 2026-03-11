51b4cf17490d820950a04979ea774b821da9e555
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-03-11 16:58:53 -0400

    Module docstring

--- frontend/test/lit/test_empty.py
+++ frontend/test/lit/test_empty.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Unit tests for the MLIR 'empty' pass.
+"""
 
 # RUN: %PYTHON %s | FileCheck %s
 
