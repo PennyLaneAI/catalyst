7123f6236b95eeb6b36dd211c0d93a7fa9c01b9f
Joey Carter <joseph.carter@xanadu.ai>
2026-03-11 14:01:51 -0400

    Add nocover on mapping_type_vars methods

--- frontend/catalyst/python_interface/dialects/qecp.py
+++ frontend/catalyst/python_interface/dialects/qecp.py
@@ -258,7 +258,7 @@ class PhysicalHyperRegisterTypeConstraint(AttrConstraint):
         """A helper function to make type vars used in attribute definitions concrete when creating
         constraints for new attributes or operations.
         """
-        return self
+        return self  # pragma: nocover
 
 
 class PhysicalCodeblockTypeConstraint(AttrConstraint):
@@ -292,7 +292,7 @@ class PhysicalCodeblockTypeConstraint(AttrConstraint):
         """A helper function to make type vars used in attribute definitions concrete when creating
         constraints for new attributes or operations.
         """
-        return self
+        return self  # pragma: nocover
 
 
 @irdl_op_definition
