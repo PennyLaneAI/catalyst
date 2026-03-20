4ba29431b53842f9039e2b34ef8fcf2864d120c1
lillian542 <Lillian.frederiksen@xanadu.ai>
2026-03-20 15:35:46 -0400

    shorten error message

--- frontend/catalyst/python_interface/transforms/quantum/measurements_from_samples.py
+++ frontend/catalyst/python_interface/transforms/quantum/measurements_from_samples.py
@@ -574,9 +574,8 @@ def _get_static_shots_value_from_first_device_op(module: builtin.ModuleOp) -> in
         shots_constant_op = shots_extract_op.operands[0].owner
         if not hasattr(shots_constant_op, "properties"):
             raise CompileError(
-                "Cannot get concrete number of shots. Note that using a dynamic number of " \
-                "shots is not supported with measurements-from-samples; ensure shots are set " \
-                "to a concrete value."            )
+                "Cannot get number of shots. Note that using a dynamic number of shots is not " \
+                "supported with measurements-from-samples."          )
         shots_value_attribute: builtin.DenseIntOrFPElementsAttr = shots_constant_op.properties.get(
             "value"
         )
@@ -592,9 +591,8 @@ def _get_static_shots_value_from_first_device_op(module: builtin.ModuleOp) -> in
     if isinstance(shots_extract_op, arith.ConstantOp):
         if not hasattr(shots_extract_op, "properties"):
             raise CompileError(
-                "Cannot get concrete number of shots. Note that using a dynamic number of " \
-                "shots is not supported with measurements-from-samples; ensure shots are set " \
-                "to a concrete value."
+                "Cannot get number of shots. Note that using a dynamic number of shots is not " \
+                "supported with measurements-from-samples."
             )
         shots_value_attribute: builtin.IntAttr = shots_extract_op.properties.get("value")
         return shots_value_attribute.value.data
