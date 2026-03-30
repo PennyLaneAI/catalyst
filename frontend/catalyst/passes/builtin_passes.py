01bef4ef1cd2c7884e68df9e72e5217daac6ab9a
Shuli Shu <08cnbj@gmail.com>
2026-03-30 11:11:02 -0400

    fix typo

--- frontend/catalyst/passes/builtin_passes.py
+++ frontend/catalyst/passes/builtin_passes.py
@@ -148,7 +148,7 @@ def diagonalize_measurements(
     for diagonalize_measurements (peephole) will be applied.
 
     Args:
-        qnode (QNode): the QNode to apply the gridsynth compiler pass to.
+        qnode (QNode): the QNode to apply the diagonalize_final_measurement compiler pass to.
         supported_base_obs (tuple[str]): A list of supported base observable names.
         Allowed observables are ``PauliX``, ``PauliY``, ``PauliZ``, ``Hadamard`` and ``Identity``.
         ``PauliZ`` and ``Identity`` are always treated as supported, regardless of input. Defaults to
