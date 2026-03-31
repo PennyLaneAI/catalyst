da94a0e67c29493e518074b19c3706c1819a67de
David Ittah <dime10@users.noreply.github.com>
2026-03-31 10:27:22 -0400

    Fix rt test

--- runtime/tests/Test_NullQubit.cpp
+++ runtime/tests/Test_NullQubit.cpp
@@ -280,8 +280,9 @@ TEST_CASE_METHOD(NullQubitRuntimeFixture,
     auto reg_vec = *reinterpret_cast<std::vector<QubitIdType> *>(reg);
 
     // PauliMeasure is unsupported by device
-    CHECK_THROWS_WITH(__catalyst__qis__PauliMeasure("XYZ", 3, reg_vec[0], reg_vec[1], reg_vec[2]),
-                      ContainsSubstring("PauliMeasure is unsupported by device"));
+    CHECK_THROWS_WITH(
+        __catalyst__qis__PauliMeasure("XYZ", nullptr, false, 3, reg_vec[0], reg_vec[1], reg_vec[2]),
+        ContainsSubstring("PauliMeasure is unsupported by device"));
 
     NullQubit device;
     CHECK_THROWS_WITH(device.PauliMeasure("X", {0}),
