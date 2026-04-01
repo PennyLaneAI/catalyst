ae64d1110942a5cd35797ac253bf7635316ad53c
River McCubbin <river.mccubbin@xanadu.ai>
2026-04-01 15:48:43 -0400

    update test rules

--- mlir/test/lit.cfg.py
+++ mlir/test/lit.cfg.py
@@ -36,6 +36,7 @@ if "Address" in getattr(config, "llvm_use_sanitizer", ""):
 
 config.substitutions.append(("%PYTHON", python_executable))
 config.substitutions.append(("%BYTECODE_PATH", config.bytecode_path))
+config.excludes.append("test_rules.mlir")
 
 # Define PATH to include the various tools needed for our tests.
 try:
