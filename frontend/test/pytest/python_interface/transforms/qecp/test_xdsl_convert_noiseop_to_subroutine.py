f92b97793a3e58acbf0c69e2257a45aac972acdc
Shuli Shu <08cnbj@gmail.com>
2026-04-14 11:53:46 -0400

    update unit tests with rigorous check

--- frontend/test/pytest/python_interface/transforms/qecp/test_xdsl_convert_noiseop_to_subroutine.py
+++ frontend/test/pytest/python_interface/transforms/qecp/test_xdsl_convert_noiseop_to_subroutine.py
@@ -31,12 +31,33 @@ class TestConvertNoiseOpToSubroutinePass:
         program = """
             builtin.module @module_circuit {
                 func.func @test_func() attributes {quantum.node} {
+                    // CHECK: [[codeblock:%.*]] = "test.op"() : () -> !qecp.codeblock<1 x 7>
                     %0 = "test.op"() : () -> !qecp.codeblock<1 x 7>
 
-                    // CHECK: func.call @noise_subroutine_code1x7
+                    // CHECK-NEXT: [[qubit_indices:%.*]] = arith.constant dense<{{.*}}> : tensor<1xi64>
+                    // CHECK-NEXT: [[rotation_params:%.*]] = arith.constant dense<[{{.*}}]> : tensor<1x3xf64>
+                    // CHECK-NEXT: func.call @noise_subroutine_code1x7([[codeblock]], [[qubit_indices]], [[rotation_params]])
                     %1 = qecp.noise %0 : !qecp.codeblock<1 x 7>
                     return
                 }
+                // CHECK-LABEL: func.func private @noise_subroutine_code1x7([[codeblock:%.*]]: !qecp.codeblock<1 x 7>, [[qubit_indices:%.*]]: tensor<1xi64>, [[rotation_params:%.*]]: tensor<1x3xf64>)
+                // CHECK-SAME: attributes {noise_subroutine_code1x7 = none}
+                // CHECK-NEXT: [[num_errors:%.*]] = arith.constant 1 : i64
+                // CHECK-NEXT: [[zero:%.*]] = arith.constant 0 : i64
+                // CHECK-NEXT: [[one:%.*]] = arith.constant 1 : i64
+                // CHECK-NEXT: [[two:%.*]] = arith.constant 2 : i64
+                // CHECK-NEXT: [[noisy_codeblock:%.*]] = scf.for [[index:%.*]] = [[zero]] to [[num_errors]] step [[one]] iter_args([[current_codeblock:%.*]] = [[codeblock]]) -> (!qecp.codeblock<1 x 7>)
+                // CHECK-NEXT: [[index_int:%.*]] = arith.index_cast [[index]] : index to i64
+                // CHECK-NEXT: [[qubit_index:%.*]] = tensor.extract [[qubit_indices]][[[index_int]]] : tensor<1xi64>
+                // CHECK-NEXT: [[phi:%.*]] = tensor.extract [[rotation_params]][[[index_int]], [[zero]]] : tensor<1x3xf64>
+                // CHECK-NEXT: [[theta:%.*]] = tensor.extract [[rotation_params]][[[index_int]], [[one]]] : tensor<1x3xf64>
+                // CHECK-NEXT: [[omega:%.*]] = tensor.extract [[rotation_params]][[[index_int]], [[two]]] : tensor<1x3xf64>
+                // CHECK-NEXT: [[qubit:%.*]] = qecp.extract [[current_codeblock]][[[qubit_index]]] : !qecp.codeblock<1 x 7> -> !qecp.qubit<data>
+                // CHECK-NEXT: [[updated_qubit:%.*]] = qecp.rot([[phi]], [[theta]], [[omega]]) [[qubit]] : !qecp.qubit<data>
+                // CHECK-NEXT: [[updated_codeblock:%.*]] = qecp.insert [[current_codeblock]][[[qubit_index]]], [[updated_qubit]] : !qecp.codeblock<1 x 7>, !qecp.qubit<data>
+                // CHECK-NEXT: scf.yield [[updated_codeblock]] : !qecp.codeblock<1 x 7>
+                // CHECK-NEXT: }
+                // CHECK: func.return [[noisy_codeblock]] : !qecp.codeblock<1 x 7>
             }
             """
 
