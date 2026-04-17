15cf5ebedba57a0a4c783665423db65759c39f01
Shuli Shu <08cnbj@gmail.com>
2026-04-17 14:59:12 -0400

    fix CF

--- frontend/test/pytest/python_interface/transforms/qecl/test_xdsl_inject_noise_to_qecl.py
+++ frontend/test/pytest/python_interface/transforms/qecl/test_xdsl_inject_noise_to_qecl.py
@@ -22,7 +22,6 @@ from catalyst.python_interface.transforms.qecl import (
     convert_quantum_to_qecl_pass,
     inject_noise_to_qecl_pass,
 )
-from catalyst.utils.exceptions import CompileError
 
 pytestmark = pytest.mark.xdsl
 
