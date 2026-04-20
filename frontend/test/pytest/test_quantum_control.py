c8793de69e7ed30b0d73b29c7fe13c5f68f87b94
Hong-Sheng Zheng <mathan0203@gmail.com>
2026-04-20 10:05:14 -0400

    Update frontend/test/pytest/test_quantum_control.py
    Co-authored-by: David Wierichs <david.wierichs@xanadu.ai>

--- frontend/test/pytest/test_quantum_control.py
+++ frontend/test/pytest/test_quantum_control.py
@@ -503,9 +503,11 @@ class TestCatalystOnlyControlled:
     @pytest.mark.parametrize("work_wire_type", ["zeroed", "borrowed"])
     def test_qctrl_work_wire_type_operator(self, work_wire_type):
         """Test that work_wire_type is preserved on a Controlled op inside qjit"""
-        x_wires = [0, 1, 2, 3]
-        output = [4, 5, 6, 7]
-        work_wires = [-1, 8, 9, 10, 11, 12]
+        c_wire = 0
+        x_wires = [1, 2, 3]
+        output = [4, 5, 6]
+        work_wires_add = [7, 8]
+        work_wires_ctrl = [9]
 
         @qjit
         def func():
@@ -513,18 +515,18 @@ class TestCatalystOnlyControlled:
                 qml.SemiAdder(
                     x_wires=x_wires,
                     y_wires=output,
-                    work_wires=work_wires[1 : len(output)],
+                    work_wires=work_wires_add,
                 ),
-                control=work_wires[:1],
-                work_wires=work_wires[len(output) :],
+                control=c_wire,
+                work_wires=work_wires_ctrl,
                 work_wire_type=work_wire_type,
             )
 
         op = func()
         assert op.hyperparameters["work_wire_type"] == work_wire_type
         assert op.work_wire_type == work_wire_type
-        assert op.control_wires == Wires([-1])
-        assert op.work_wires == Wires([11, 12])
+        assert op.control_wires == Wires([0])
+        assert op.work_wires == Wires([9])
 
         @qjit
         def func_native():
@@ -532,10 +534,10 @@ class TestCatalystOnlyControlled:
                 qml.SemiAdder(
                     x_wires=x_wires,
                     y_wires=output,
-                    work_wires=work_wires[1 : len(output)],
+                    work_wires=work_wires_add,
                 ),
-                control=work_wires[:1],
-                work_wires=work_wires[len(output) :],
+                control=c_wire,
+                work_wires=work_wires_ctrl,
                 work_wire_type=work_wire_type,
             )
 
