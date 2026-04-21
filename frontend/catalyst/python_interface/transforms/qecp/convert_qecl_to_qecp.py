17ba183bce10b40b076a06d127246575447219f5
lillian542 <Lillian.frederiksen@xanadu.ai>
2026-04-21 18:33:56 -0400

    add lowering pattern for encode

--- frontend/catalyst/python_interface/transforms/qecp/convert_qecl_to_qecp.py
+++ frontend/catalyst/python_interface/transforms/qecp/convert_qecl_to_qecp.py
@@ -17,6 +17,7 @@
 This module contains the implementation of the xDSL convert-qecl-to-qecp dialect-conversion pass.
 To apply this pass, the QEC code must be know.
 """
+
 import numpy as np
 
 from dataclasses import dataclass, field
@@ -42,13 +43,6 @@ from catalyst.python_interface.pass_api.compiler_transform import compiler_trans
 from catalyst.utils.exceptions import CompileError
 
 from .qec_code_lib import QecCode
-
-Steane713 = (
-    7, 
-    1, 
-    np.array([[1, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 1, 0], [0, 0, 1, 1, 0, 1, 1]]), 
-    np.array([[1, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 1, 0], [0, 0, 1, 1, 0, 1, 1]]),
-    )
 
 # MARK: Type Conversion Pattern
 
@@ -77,6 +71,38 @@ class HyperRegisterTypeConversion(TypeConversionPattern):
         return qecp.PhysicalHyperRegisterType(typ.width, typ.k, self.qec_code.n)
 
 
+# MARK: Encode Op Pattern
+
+
+class EncodeOpConversion(RewritePattern):
+    """Converts qecl.encode [zero] to the equivalent subroutine of qecp gates"""
+
+    def __init__(self, qec_code: QecCode, encode_subroutine: func.FuncOp):
+        self.qec_code = qec_code
+        self.encode_subroutine = encode_subroutine
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: qecl.EncodeOp, rewriter: PatternRewriter):
+        """Rewrite pattern for `qecl.encode [zero]` op"""
+
+        if not op.init_state.data == "zero":
+            raise NotImplementedError(
+                "Lowering qecl.EncodeOp to the qecp dialect is only implemented for init_state 'zero'"
+            )
+
+        if (k := op.in_codeblock.type.k.value.data) != self.qec_code.k:
+            raise CompileError(
+                f"Circuit expressed in the qecl dialect with k={k} is not compatible with lowering to a code with k={self.qec_code.k}"
+            )
+
+        callee = builtin.SymbolRefAttr(self.encode_subroutine.sym_name)
+        arguments = (op.in_codeblock,)
+        return_types = self.encode_subroutine.function_type.outputs.data
+        callOp = func.CallOp(callee, arguments, return_types)
+
+        rewriter.replace_op(op, callOp)
+
+
 # MARK: Conversion Pass
 
 
@@ -94,91 +120,118 @@ class ConvertQecLogicalToQecPhysicalPass(ModulePass):
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
         """Apply the convert-qecl-to-qecp pass."""
 
+        assert op.regions[0].blocks.first is not None
+
+        encode_funcop = self.create_encode_cycle()
+        op.regions[0].blocks.first.add_op(encode_funcop)
+
         PatternRewriteWalker(
             GreedyRewritePatternApplier(
                 [
                     CodeblockTypeConversion(qec_code=self.qec_code),
                     HyperRegisterTypeConversion(qec_code=self.qec_code),
-                    EncodeOpConversion(self.qec_code),
+                    EncodeOpConversion(qec_code=self.qec_code, encode_subroutine=encode_funcop),
                 ]
             )
         ).rewrite_module(op)
 
+    def create_encode_cycle(self):
+        # ToDo: docstring
 
-# MARK: Encode Op Pattern
+        codeblock_type = qecp.PhysicalCodeblockType(self.qec_code.k, self.qec_code.n)
+        input_types = (codeblock_type,)
+        output_types = (codeblock_type,)
 
+        block = Block(arg_types=input_types)
 
-class EncodeOpConversion(RewritePattern):
-    """Converts qecl.encode [zero] to the equivalent subroutine of qecp gates"""
+        with builder.ImplicitBuilder(block):
 
-    def __init__(self, qec_code: str):
-        self.code_name = qec_code
+            (codeblock,) = block.args
 
-        code = QEC_codes[qec_code]
-        self._n = code.n
-        self._k = code.k
-        self._x_tanner = code.x_tanner
-        self._z_tanner = code.z_tanner
+            aux_allocate_ops = (qecp.AllocAuxQubitOp() for row in self.qec_code.z_tanner)
+            aux_qubits = [op.results[0] for op in aux_allocate_ops]
 
-    # def _create_encode_subroutine(self):
-    #     codeblock_type = qecp.PhysicalCodeblockType(self._k, self._n)
-    #     input_types = (codeblock_type,)
-    #     output_types = (codeblock_type,)
+            measure_ops, codeblock = self._z_check_pattern(aux_qubits, codeblock)
 
-    #     block = Block(arg_types=input_types)
+            dealloc_ops = [qecp.DeallocAuxQubitOp(meas_op.results[1]) for meas_op in measure_ops]
 
-    #     with builder.ImplicitBuilder(block):
-    #         (in_codeblock,) = block.args
-    #         func.ReturnOp(in_codeblock)
+            func.ReturnOp(codeblock)
 
-    #     region = Region([block])
-    #     symbol_name = f"encode_zero_{self.code_name}"
-    #     funcOp = func.FuncOp(
-    #         symbol_name,
-    #         (input_types, output_types),
-    #         visibility="private",
-    #         region=region,
-    #     )
+        region = Region([block])
 
-    #     return funcOp
-
-    @op_type_rewrite_pattern
-    def match_and_rewrite(self, op: qecl.EncodeOp, rewriter: PatternRewriter):
-        """Rewrite pattern for `qecl.encode [zero]` op"""
-    
-        if not op.init_state.data == "zero":
-            raise NotImplementedError("Lowering qecl.EncodeOp to the qecp dialect is only implemented for init_state 'zero'")
-
-        k = op.in_codeblock.type.k.value.data
-        if not k == self._k:
-            raise CompileError(f"Circuit expressed in the qecl dialect with k={k} is not compatible with lowering to a code with k={self._k}")
-
-        # # we will only need to do this the first time!!!
-        # # Insert encode subroutine into the module.
-        # encode_subroutine = self._create_encode_subroutine()
-        # assert op.regions[0].blocks.first is not None
-        # op.regions[0].blocks.first.add_op(encode_subroutine)
-    
-        in_block_cast = builtin.UnrealizedConversionCastOp.get(
-            (op.in_codeblock,), (qecp.PhysicalCodeblockType(self._k, self._n),)
+        symbol_name = f"encode_zero_{self.qec_code.name}"
+        funcOp = func.FuncOp(
+            symbol_name,
+            (input_types, output_types),
+            visibility="private",
+            region=region,
         )
 
-        # ops_to_insert = [in_block_cast,]
-        # # mock-up of code, not correct
-        for row in self._x_tanner:
-            alloc_op = qecp.AllocAuxQubitOp()
-        #     hadamard_op = qecp.HadamardOp(alloc_op.results[0])
-        #     for val, idx in enumerate(row):
-        #         if val:
-        #             #probably need to insert the idx
-        #             extract_op = qecp.ExtractQubitOp(in_block_cast.results[0], idx)
-        #             cnot = qecp.CnotOp(hadamard_op.results[0], extract_op.results[0])
+        return funcOp
 
-        rewriter.insert_op(in_block_cast, InsertPoint.before(op))
-        # rewriter.replace_op(op, *ops_to_insert)
+    def _z_check_pattern(self, aux_qbs, codeblock):
+        # ToDo: add docstring
 
-        rewriter.erase_op(op)
+        tanner_graph = self.qec_code.z_tanner
 
+        hadamard_ops = [qecp.HadamardOp(qb) for qb in aux_qbs]  # skip for X check
+        aux_qbs = [h_op.results[0] for h_op in hadamard_ops]
+
+        extract_ops = [qecp.ExtractQubitOp(codeblock, i) for i in range(self.qec_code.n)]
+        data_qbs = [ext_op.results[0] for ext_op in extract_ops]
+
+        aux_qbs, data_qbs = self._cnot_pattern(aux_qbs, data_qbs, check_type="Z")
+
+        for i in range(self.qec_code.n):
+            insert_op = qecp.InsertQubitOp(codeblock, i, data_qbs[i])
+            codeblock = insert_op.results[0]
+
+        hadamard_ops2 = [qecp.HadamardOp(aux) for aux in aux_qbs]  # skip for X check
+        measure_ops = [qecp.MeasureOp(h_op.results[0]) for h_op in hadamard_ops2]
+
+        return measure_ops, codeblock
+
+    def _x_check_pattern(self, aux_qbs, codeblock):
+        # ToDo: add docstring
+
+        tanner_graph = self.qec_code.z_tanner
+
+        extract_ops = [qecp.ExtractQubitOp(codeblock, i) for i in range(self.qec_code.n)]
+        data_qbs = [ext_op.results[0] for ext_op in extract_ops]
+
+        aux_qbs, data_qbs = self._cnot_pattern(aux_qbs, data_qbs, check_type="Z")
+
+        measure_ops = [qecp.MeasureOp(qb) for qb in aux_qbs]
+
+        for i in range(self.qec_code.n):
+            insert_op = qecp.InsertQubitOp(codeblock, i, data_qbs[i])
+            codeblock = insert_op.results[0]
+
+        return measure_ops, codeblock
+
+    def _cnot_pattern(self, aux_qubits, data_qubits, check_type):
+        # ToDo: docstring
+
+        aux_qbs_out = []
+        if check_type == "Z":
+            tanner_graph = self.qec_code.z_tanner
+        elif check_type == "X":
+            tanner_graph = (
+                self.qec_code.x_tanner
+            )  # ToDo: reconsider all of this once the QecCode PR is merged
+
+        for aux_qb, row in zip(aux_qubits, tanner_graph):
+            indices = [idx for idx, val in enumerate(row) if val]
+            for idx in indices:
+                if check_type == "Z":
+                    cnot_op = qecp.CnotOp(aux_qb, data_qubits[idx])
+                elif check_type == "X":
+                    cnot_op = qecp.CnotOp(data_qubits[idx], aux_qb)
+                aux_qb, data_qb = cnot_op.results
+                data_qubits[idx] = data_qb
+            aux_qbs_out.append(aux_qb)
+
+        return aux_qbs_out, data_qubits
 
 
 convert_qecl_to_qecp_pass = compiler_transform(ConvertQecLogicalToQecPhysicalPass)
