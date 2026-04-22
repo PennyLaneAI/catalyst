8ef5f3f5baf194470587bf933ec929e31760bfbd
Shuli Shu <08cnbj@gmail.com>
2026-04-22 11:50:00 -0400

    make format

--- runtime/lib/capi/RuntimeCAPI.cpp
+++ runtime/lib/capi/RuntimeCAPI.cpp
@@ -1277,17 +1277,23 @@ RESULT *__catalyst__mbqc__measure_in_basis(QUBIT *wire, uint32_t plane, double a
 // -------------------------------------------------------------------------- //
 // QECP Runtime CAPI
 // -------------------------------------------------------------------------- //
-// NOTE: Syndrome results are currently represented as int8_t, following the design choice of SetBasisState.
-int64_t *__catalyst__qecp__decode_steane_lut(MemRefT_int64_1d *row_idx, MemRefT_int64_1d *col_ptr, MemRefT_int8_1d *syndrome_results)
+// The implementation follows the blog post here:
+// https://pennylane.ai/qml/demos/tutorial_bp_catalyst . NOTE: Syndrome results are currently
+// represented as int8_t, following the design choice of SetBasisState. NOTE: we decided to add two
+// additional args: d, for the code distance; check_type, to indicate where to pull the precompute
+// look up table.
+int64_t *__catalyst__qecp__decode_steane_lut(MemRefT_int64_1d *row_idx, MemRefT_int64_1d *col_ptr,
+                                             MemRefT_int8_1d *syndrome_results)
 {
-    // 1. Recover the parity check matrix from the CSC sparse representation. 
+    // 1. Recover the parity check matrix from the CSC sparse representation.
     // Get shapes of the dense parity check matrix (H) from the input sparse representation.
     const size_t nnz = row_idx->sizes[0]; // number of non-zero elements
     std::vector<size_t> row_idx_vec(row_idx->data_aligned, row_idx->data_aligned + nnz);
 
     const size_t n = col_ptr->sizes[0] - 1; // number of columns
     std::vector<size_t> col_ptr_vec(col_ptr->data_aligned, col_ptr->data_aligned + n + 1);
-    const size_t m = *std::max_element(row_idx_vec.begin(), row_idx_vec.end()) + 1; // number of rows
+    const size_t m =
+        *std::max_element(row_idx_vec.begin(), row_idx_vec.end()) + 1; // number of rows
 
     // Construct the parity check matrix (H) from the sparse representation.
     std::vector<std::vector<uint8_t>> H(m, std::vector<uint8_t>(n, 0));
@@ -1297,14 +1303,29 @@ int64_t *__catalyst__qecp__decode_steane_lut(MemRefT_int64_1d *row_idx, MemRefT_
             H[row][col] = 1;
         }
     }
-    // 2. Create a look up table 
-    // std::vector<std::vector<int8_t>> lut(1<<m, n);
+    // 2. Create a look up table using a singleton pattern, which would only be initialized once
+    // during the first call to the function, and would be reused for subsequent calls.
 
-    // 3. Initalize the look up table
+    // NOTE: We need to use std::next_permutation to iterate over all possible weight-w errors,
+    // which is not straightforward to implement in a constexpr function. Hence we use a static
+    // variable to store the look up table, which is initialized at runtime during the first call to
+    // the function.
 
-    // 4. Process the given syndrome results to get the index of the look up table entry corresponding to the given syndrome results.
+    // NOTE: The look up table is an unordered_map<str, std::vector<int64_t>>, whose key is the str
+    // representation of sydrome results, and value is the corresponding the indices of qubits to
+    // correct.
 
-    // 5. Return the look up table entry corresponding to the given syndrome results, which contains the error correction information (e.g., which qubits to apply X or Z gates to correct the error).
+    // NOTE: We need two separate look up tables for X and Z errors, since they are decoded using
+    // different parity check matrices (H_X and H_Z). This implies that we need to implement
+    // separate singleton functions to initialize the look up tables for X and Z errors? I believe
+    // so.
+
+    std::vector<std::vector<uint8_t>> lut(1 << m, std::vector<uint8_t>(n, 0));
+
+    // 3. Convert the given syndrome results (represented as int8_t) into a string representation,
+    // which can be used as the key to look up the look up table.
+
+    // 4. Return the look up table entry corresponding to the given syndrome results
 
     return nullptr;
 }
