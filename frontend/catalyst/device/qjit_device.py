7ec44c88cb2ae6a98cd88413968cc82a39affead
River McCubbin <river.mccubbin@xanadu.ai>
2026-04-23 11:57:27 -0400

    general replace

--- frontend/catalyst/device/qjit_device.py
+++ frontend/catalyst/device/qjit_device.py
@@ -27,7 +27,7 @@ from copy import deepcopy
 from dataclasses import dataclass, replace
 from typing import Any, Dict, Optional
 
-import pennylane as qml
+import pennylane as qp
 from jax.interpreters.partial_eval import DynamicJaxprTracer
 from pennylane import CompilePipeline
 from pennylane.devices.capabilities import (
@@ -148,12 +148,12 @@ class BackendInfo:
 
 # pylint: disable=too-many-branches
 @debug_logger
-def extract_backend_info(device: qml.devices.QubitDevice) -> BackendInfo:
+def extract_backend_info(device: qp.devices.QubitDevice) -> BackendInfo:
     """Extract the backend info from a quantum device. The device is expected to carry a reference
     to a valid TOML config file."""
 
     dname = device.name
-    if isinstance(device, qml.devices.LegacyDeviceFacade):
+    if isinstance(device, qp.devices.LegacyDeviceFacade):
         dname = device.target_device.short_name  # pragma: no cover
 
     device_name = ""
@@ -292,7 +292,7 @@ def get_qjit_device_capabilities(target_capabilities: DeviceCapabilities) -> Dev
     return qjit_capabilities
 
 
-class QJITDevice(qml.devices.Device):
+class QJITDevice(qp.devices.Device):
     """QJIT device for the new device API.
     A device that interfaces the compilation pipeline of Pennylane programs.
     Args:
@@ -336,7 +336,7 @@ class QJITDevice(qml.devices.Device):
     @debug_logger
     def preprocess(
         self,
-        execution_config: Optional[qml.devices.ExecutionConfig] = None,
+        execution_config: Optional[qp.devices.ExecutionConfig] = None,
     ):
         """This function defines the device transform program to be applied and an updated device
         configuration. The transform program will be created and applied to the tape before
@@ -369,7 +369,7 @@ class QJITDevice(qml.devices.Device):
         # pylint: disable=unused-argument
 
         if execution_config is None:
-            execution_config = qml.devices.ExecutionConfig()
+            execution_config = qp.devices.ExecutionConfig()
 
         # Extract catalyst-specific options from device_options
         shots = execution_config.device_options.get("catalyst_shots")
@@ -387,7 +387,7 @@ class QJITDevice(qml.devices.Device):
         # Note that this new set of capabilities are only temporarily needed for the computation
         # of the preprocessing transform program.
         shots_not_provided = (shots is None) or (
-            isinstance(shots, qml.measurements.shots.Shots) and shots.total_shots is None
+            isinstance(shots, qp.measurements.shots.Shots) and shots.total_shots is None
         )
         if shots_not_provided and _requires_shots(self.capabilities):
             raise CompileError(textwrap.dedent(f"""
@@ -477,10 +477,10 @@ class QJITDevice(qml.devices.Device):
                 # Pauli + Hadamard observables, so here it is needed
                 measurement_pipeline.add_transform(split_non_commuting)
             _obs_dict = {
-                "PauliX": qml.X,
-                "PauliY": qml.Y,
-                "PauliZ": qml.Z,
-                "Hadamard": qml.Hadamard,
+                "PauliX": qp.X,
+                "PauliY": qp.Y,
+                "PauliZ": qp.Z,
+                "Hadamard": qp.Hadamard,
             }
             # checking which base observables are unsupported and need to be diagonalized
             supported_observables = {"PauliX", "PauliY", "PauliZ", "Hadamard"}.intersection(
@@ -536,7 +536,7 @@ def _load_device_capabilities(device) -> DeviceCapabilities:
     else:
         # TODO: Remove this section when devices are guaranteed to have their own config file
         device_lpath = pathlib.Path(get_lib_path("runtime", "RUNTIME_LIB_DIR"))
-        name = device.short_name if isinstance(device, qml.devices.LegacyDevice) else device.name
+        name = device.short_name if isinstance(device, qp.devices.LegacyDevice) else device.name
         # The toml files name convention we follow is to replace
         # the dots with underscores in the device short name.
         toml_file_name = name.replace(".", "_") + ".toml"
@@ -598,7 +598,7 @@ def get_device_capabilities(device, shots=False) -> DeviceCapabilities:
     )
 
 
-def is_dynamic_wires(wires: qml.wires.Wires):
+def is_dynamic_wires(wires: qp.wires.Wires):
     """
     Checks if a pennylane Wires object corresponds to a concrete number
     of wires or a dynamic number of wires.
@@ -623,7 +623,7 @@ def check_device_wires(wires):
     if len(wires) >= 2 or (not is_dynamic_wires(wires)):
         # A dynamic number of wires correspond to a single tracer for the number
         # Thus if more than one entry, must be static wires
-        assert isinstance(wires, qml.wires.Wires)
+        assert isinstance(wires, qp.wires.Wires)
 
         if not all(isinstance(wire, int) for wire in wires.labels):
             raise AttributeError("Catalyst requires continuous integer wire labels starting at 0.")
