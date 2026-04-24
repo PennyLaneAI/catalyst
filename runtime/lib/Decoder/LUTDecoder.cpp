ee02e7a16e28854b9d61a9c7e063e46221bb3b54
Shuli Shu <08cnbj@gmail.com>
2026-04-24 16:01:41 -0400

    refactor the code

--- runtime/lib/Decoder/LUTDecoder.cpp
+++ runtime/lib/Decoder/LUTDecoder.cpp
@@ -15,6 +15,7 @@
 #include "LUTDecoder.hpp"
 
 #include <algorithm>
+#include <numeric>
 #include <ranges>
 #include <vector>
 
@@ -34,6 +35,15 @@ void __catalyst__qecp__lut_decoder(MemRefT_int64_1d *row_idx_tanner,
                                    MemRefT_int64_1d *col_ptr_tanner,
                                    MemRefT_int8_1d *current_syndromes, MemRefT_int64_1d *err_idx)
 {
+    // TODOs: We should expect the following const value from args.
+    // The default values here only work for the [[7, 1, 3]] Steane code.
+    const size_t code_size = 7;
+    const size_t code_distance = 3;
+    const size_t aux_col_offset = 7;
+
+    std::vector<size_t> aux_cols((code_size - 1) / 2);
+    std::iota(aux_cols.begin(), aux_cols.end(), aux_col_offset);
+
     // 1. Recover the parity check matrix from a tanner graph
     const size_t nnz = row_idx_tanner->sizes[0];
     std::vector<size_t> row_idx_tanner_vec(row_idx_tanner->data_aligned,
@@ -46,19 +56,10 @@ void __catalyst__qecp__lut_decoder(MemRefT_int64_1d *row_idx_tanner,
     std::vector<int8_t> current_syndromes_res(current_syndromes->data_aligned,
                                               current_syndromes->data_aligned + num_aux);
 
-    // TODOs: Hardcoded for the [[7,1,3]] Steane code and current design choice of Tanner graph for
-    // self-dual CSS code.
-    const std::vector<size_t> aux_cols{7, 8, 9};
-
     auto csc_parity_matrix =
         get_parity_check_matrix(row_idx_tanner_vec, col_ptr_tanner_vec, aux_cols);
     auto row_idx_parity = csc_parity_matrix.first;
     auto col_ptr_parity = csc_parity_matrix.second;
-
-    // Generate LUT from parity check matrix
-    // TODOs: We should expect the follow code_size and code_distance info from the compilation.
-    const size_t code_size = 7;
-    const size_t code_distance = 3;
 
     auto lut = generate_lookup_table(row_idx_parity, col_ptr_parity, code_size, code_distance);
 
