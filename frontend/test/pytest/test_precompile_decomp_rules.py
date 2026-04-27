2addd29c2cecce9884faedd4be3fa4decaf3702f
River McCubbin <river.mccubbin@xanadu.ai>
2026-04-27 10:31:58 -0400

    add skip_flaky mark and apply for precompile warnings

--- frontend/test/pytest/test_precompile_decomp_rules.py
+++ frontend/test/pytest/test_precompile_decomp_rules.py
@@ -81,49 +81,47 @@ class TestCompileOpDecompRules:
         with pytest.warns():
             compile_op_decomp_rules(qp.Identity)
 
+    @pytest.mark.skip_flaky
     def test_compile_error(self):
         """Test that compile_op_decomp_rules warns when compilation of a rule fails."""
 
-        class FakeOp(qp.operation.Operator):
-            """Test class with incompatible decomp rule."""
-
-            num_wires = 3
-            num_params = 1
-            ndim_params = (0,)
-
-        @qp.register_resources({})
-        def fake_op_decomp(param, wires):
-            _quantum_opt(stdin="module {1}")
-            return param, wires
-
-        qp.add_decomps(FakeOp, fake_op_decomp)
-
         with pytest.warns(match="Failed to compile"):
+
+            class FakeOp(qp.operation.Operator):
+                """Test class with incompatible decomp rule."""
+
+                num_wires = 3
+                num_params = 1
+                ndim_params = (0,)
+
+            @qp.register_resources({})
+            def fake_op_decomp(param, wires):
+                _quantum_opt(stdin="module {")
+                return param, wires
+
+            qp.add_decomps(FakeOp, fake_op_decomp)
 
             compile_op_decomp_rules(FakeOp)
 
-        qp.decomposition.decomposition_rule._decompositions_private.pop("FakeOp")
-
+    @pytest.mark.skip_flaky
     def test_unexpected_error(self):
         """Test that compile_op_decomp_rules warns when an unexpected exception is thrown."""
 
-        class NewFakeOp(qp.operation.Operator):
-            """Test class without ndim_params."""
-
-            num_wires = 1
-            num_params = 1
-
-        @qp.register_resources({})
-        def fake_op_decomp(string):
-            qp.PauliRot(2, string)
-
-        qp.add_decomps(NewFakeOp, fake_op_decomp)
-
         with pytest.warns(match="Unexpected error"):
 
-            compile_op_decomp_rules(NewFakeOp)
+            class NewFakeOp(qp.operation.Operator):
+                """Test class without ndim_params."""
 
-        qp.decomposition.decomposition_rule._decompositions_private.pop("NewFakeOp")
+                num_wires = 1
+                num_params = 1
+
+            @qp.register_resources({})
+            def fake_op_decomp(string):
+                qp.PauliRot(2, string)
+
+            qp.add_decomps(NewFakeOp, fake_op_decomp)
+
+            compile_op_decomp_rules(NewFakeOp)
 
 
 def test_bytecode_file():
