3ced84c39ad3babdc5c7eae8683eee11c9af1644
River McCubbin <river.mccubbin@xanadu.ai>
2026-05-07 15:56:24 -0400

    Update frontend/catalyst/passes/builtin_passes.py
    Co-authored-by: Ali Asadi <10773383+maliasadi@users.noreply.github.com>

--- frontend/catalyst/passes/builtin_passes.py
+++ frontend/catalyst/passes/builtin_passes.py
@@ -1821,10 +1821,10 @@ def graph_decomposition_setup_inputs(
         fn (QNode): the QNode to apply the graph decomposition compiler pass to.
         gate_set (Iterable[type | str] | dict[type | str, float]): the set of gates that are
             permissable after decomposition.
-        fixed_decomps (dict | None): map ops to specific decomps that will be applied if the
-                                     operator needs to be decomposed (i.e. when they're not
+        fixed_decomps (dict | None): map operators to specific decomposition rules that will be applied if the
+                                     operators need to be decomposed (i.e. when they're not
                                      in the target gate set).
-        alt_decomps (dict | None): map ops to lists of decomps that the graph system will consider.
+       alt_decomps (dict | None): map operators to lists of decomposition rules that the graph system will consider as alternative rules for operators.
 
     Returns:
         ~.QNode:
