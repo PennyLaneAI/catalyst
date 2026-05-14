90541922439a0e20def230b3ddfee696875d1c74
JerryChen97 <chenys13@outlook.com>
2026-05-14 12:19:03 -0400

    fix decorator_kwargs

--- frontend/catalyst/autograph/ag_primitives.py
+++ frontend/catalyst/autograph/ag_primitives.py
@@ -615,9 +615,7 @@ def converted_call(fn, args, kwargs, caller_fn_scope=None, options=None):
             # the same as the decorator arguments
             closure_vars = inspect.getclosurevars(fn)
             decorator_kwargs = {
-                k: v
-                for k, v in closure_vars.nonlocals.items()
-                if k in ("id", "lazy")
+                "lazy": closure_vars.nonlocals["lazy"],
             }
 
             # Convert the original function with autograph
