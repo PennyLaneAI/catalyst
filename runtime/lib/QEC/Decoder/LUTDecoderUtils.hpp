727d2cc268b09eeb7267e424557778e29b741b0c
Shuli Shu <08cnbj@gmail.com>
2026-05-17 16:17:58 -0400

    use while loop

--- runtime/lib/QEC/Decoder/LUTDecoderUtils.hpp
+++ runtime/lib/QEC/Decoder/LUTDecoderUtils.hpp
@@ -194,11 +194,13 @@ generate_lookup_table(const std::vector<TANNER_GRAPH_INT> &parity_mat_row_idx,
     RT_ASSERT(num_errors <= num_data_qubits);
 
     // Traverse all possible quantum error combinations
+    size_t i = 0;
     // Add i <= num_data_qubits here to pass static analysis check
-    for (size_t i = 0; i <= num_errors && i <= num_data_qubits; i++) {
+    while (i <= num_errors && i <= num_data_qubits) {
         // create a base error vector
         std::vector<int8_t> err_vector(num_data_qubits, 0);
-        std::fill(err_vector.end() - i, err_vector.end(), 1);
+        std::fill(err_vector.begin(), err_vector.begin() + i, 1);
+        std::reverse(err_vector.begin(), err_vector.end());
 
         do {
             std::string syndrome_str = get_syndrome_from_errors<TANNER_GRAPH_INT>(
@@ -209,6 +211,7 @@ generate_lookup_table(const std::vector<TANNER_GRAPH_INT> &parity_mat_row_idx,
             // We assume that 1:1 mapping for the syndrome and err_vector
             lut.try_emplace(syndrome_str, std::move(error_indices));
         } while (std::next_permutation(err_vector.begin(), err_vector.end()));
+        i++;
     }
 
     return lut;
