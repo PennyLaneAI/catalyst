a758ca81980c9424ebd95c291db6f033fadece4c
Shuli Shu <08cnbj@gmail.com>
2026-05-17 16:05:20 -0400

    refactor

--- runtime/lib/QEC/Decoder/LUTDecoderUtils.hpp
+++ runtime/lib/QEC/Decoder/LUTDecoderUtils.hpp
@@ -191,13 +191,14 @@ generate_lookup_table(const std::vector<TANNER_GRAPH_INT> &parity_mat_row_idx,
     // Get number of errors can be detected from code distance
     const size_t num_errors = (code_distance - 1) / 2;
 
+    RT_ASSERT(num_errors <= num_data_qubits);
+
     // Traverse all possible quantum error combinations
-    for (size_t i = 0; i <= num_errors; i++) {
+    // Add i <= num_data_qubits here to pass static analysis check
+    for (size_t i = 0; i <= num_errors && i <= num_data_qubits; i++) {
         // create a base error vector
-        const size_t err_size = std::max(num_data_qubits, num_errors);
-        std::vector<int8_t> err_vector(err_size, 0);
-        std::fill(err_vector.begin(), err_vector.begin() + i, 1);
-        std::reverse(err_vector.begin(), err_vector.end());
+        std::vector<int8_t> err_vector(num_data_qubits, 0);
+        std::fill(err_vector.end() - i, err_vector.end(), 1);
 
         do {
             std::string syndrome_str = get_syndrome_from_errors<TANNER_GRAPH_INT>(
