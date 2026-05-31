e5e099d113994fbcbb9c4eee4f6f57805ae8983c
paul0403 <paulhaochen.wang@gmail.com>
2026-05-31 15:07:03 -0400

    codecov

--- frontend/catalyst/python_interface/transforms/qecp/convert_qecp_to_quantum.py
+++ frontend/catalyst/python_interface/transforms/qecp/convert_qecp_to_quantum.py
@@ -333,13 +333,12 @@ class ConvertQecPhysicalToQuantumPass(ModulePass):
                         case func.CallOp() if "encode_zero_" in quantum_op.callee.string_value():
                             regs.append(quantum_op.results[0])
                         case qecp.ExtractCodeblockOp():
+                            assert (quantum_op.idx is not None) ^ (quantum_op.idx_attr is not None)
                             qecp_ops_to_remove.append(quantum_op)
                             if quantum_op.idx is not None:
                                 idx = resolve_constant_params(quantum_op.idx)
-                            elif quantum_op.idx_attr is not None:
-                                idx = quantum_op.idx_attr.value.data
                             else:
-                                raise CompileError("Expected an index on qecp.extract_codeblock op")
+                                idx = quantum_op.idx_attr.value.data
                             quantum_op.codeblock.replace_all_uses_with(regs[idx])
                         case qecp.InsertCodeblockOp():
                             qecp_ops_to_remove.append(quantum_op)
