6bdbb376b637d0fc34fd4e850ff9cb339733a2fd
River McCubbin <river.mccubbin@xanadu.ai>
2026-06-17 13:20:38 -0400

    remove test_rules from lit cfg

--- frontend/test/lit/lit.cfg.py
+++ frontend/test/lit/lit.cfg.py
@@ -23,7 +23,7 @@ config.test_format = lit.formats.ShTest(True)
 
 # Define the file extensions to treat as test files (with the exception of this file).
 config.suffixes = [".py", ".mlir"]
-config.excludes = ["lit.cfg.py", "utils.py", "catalyst.autograph.exclusion.py", "test_rules.mlir"]
+config.excludes = ["lit.cfg.py", "utils.py", "catalyst.autograph.exclusion.py"]
 
 # Define the root path of where to look for tests.
 config.test_source_root = os.path.dirname(__file__)
