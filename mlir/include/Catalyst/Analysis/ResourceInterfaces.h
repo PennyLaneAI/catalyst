45ef7298765f93d8dcc4a27b296b7bf26ec97b7c
sengthai <sengthai37@gmail.com>
2026-07-15 11:32:22 -0400

    Fixed format

--- mlir/include/Catalyst/Analysis/ResourceInterfaces.h
+++ mlir/include/Catalyst/Analysis/ResourceInterfaces.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #pragma once
 #include "mlir/IR/OpDefinition.h"
+
 #include "Catalyst/Analysis/ResourceInterfaces.h.inc"
