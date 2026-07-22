febec6c548b08e130b517b5073fce7efda4f13ea
River McCubbin <river.mccubbin@xanadu.ai>
2026-07-22 12:34:37 -0400

    migrate pass & solver to ID

--- mlir/lib/Quantum/Transforms/GraphDecomposition/graph_decomposition.cpp
+++ mlir/lib/Quantum/Transforms/GraphDecomposition/graph_decomposition.cpp
@@ -506,10 +506,6 @@ struct GraphDecompositionPass : public impl::GraphDecompositionPassBase<GraphDec
                 node.numParams = 0;
             }
 
-            if (auto decompGate = dyn_cast<DecomposableGate>(op.getOperation())) {
-                node.id = decompGate.getGraphOpId();
-            }
-
             operators.push_back(node);
         });
     }
