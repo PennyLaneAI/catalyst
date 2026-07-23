a39091e025220b1c2be33fef324fe6c3e9e1f2d6
River McCubbin <river.mccubbin@xanadu.ai>
2026-07-23 11:49:48 -0400

    update module in QPD path

--- mlir/lib/Quantum/Transforms/QuantumPythonDecompositions/PythonFunction.cpp
+++ mlir/lib/Quantum/Transforms/QuantumPythonDecompositions/PythonFunction.cpp
@@ -73,7 +73,7 @@ static nb::object getPyvalFromMlirAttribute(mlir::Attribute attr) {
 std::string pythonRuleLowering(catalyst::quantum::DecomposableGate op) {
     QuantumPythonDecompositions::PyInterpreterGuard guard;
     std::string mlirText = guard.withGil([&] -> std::string {
-        const char *moduleName = "catalyst.device.python_decompositions";
+        const char *moduleName = "catalyst.decomposition.python_decompositions";
         const char *functionName = "python_decomposition_wrapper";
 
         try {
