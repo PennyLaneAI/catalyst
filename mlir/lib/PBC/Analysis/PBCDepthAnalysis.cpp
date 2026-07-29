2f5d38b6cf221be99aa85b310e6a177d2201a733
sengthai <sengthai37@gmail.com>
2026-07-29 10:54:36 -0400

    Improve PBCDepthExtension::toJson()

--- mlir/lib/PBC/Analysis/PBCDepthAnalysis.cpp
+++ mlir/lib/PBC/Analysis/PBCDepthAnalysis.cpp
@@ -32,9 +32,7 @@ llvm::json::Value PBCDepthExtension::toJson() const {
         depthObj["qubit_disjoint_depth"] = depth->second;
         return depthObj;
     }
-    else {
-        return nullptr;
-    }
+    return nullptr;
 }
 
 void PBCDepthExtension::mergeWith(const ResourceResultExtension &other, MergeMethod mergeMethod) {
