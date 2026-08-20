432f7478ef6678a9018bfb53ca0399394f3d0274
paul0403 <paulhaochen.wang@gmail.com>
2026-08-20 17:20:36 -0400

    add comment about macro

--- mlir/lib/QRef/Transforms/value_semantics_conversion.cpp
+++ mlir/lib/QRef/Transforms/value_semantics_conversion.cpp
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 #define DEBUG_TYPE "value-semantics-conversion"
+
+// The macro REFERENCE_SEMANTICS_GATE_OPS defines a list of operations that can take in !qref.bit
+// values as operands, and do not return any observables as results.
 #define REFERENCE_SEMANTICS_GATE_OPS                                                               \
     qref::QuantumOperation, qref::MeasureOp, qref::CtrlOp, mbqc::RefMeasureInBasisOp,              \
         pbc::RefPPMeasurementOp
