3665e5be44d0cac8c2a66285db84e3769e273835
Nikhil Sreekumar <nikhil.sreekumar@xanadu.ai>
2026-09-14 14:54:59 -0400

    getDynamicShape/printDynamicShape tested scenario with multiple indexes for 1 param

--- mlir/unittests/QRef/TestDecomposableGateInterface.cpp
+++ mlir/unittests/QRef/TestDecomposableGateInterface.cpp
@@ -550,3 +550,42 @@ func.func @testfunc(%first : tensor<1xi64>, %secondthird : tensor<2xi64>) {
     ASSERT_EQ(op.getGraphOpId(),
               "testOperatorUID{angle:[f64],flag:[i1],index:[i64]}{reg:3}{}[248]");
 }
+
+
+TEST(DecomposableGateInterfaceTests, OperatorOpMultiIndexedParams) {
+    std::string moduleStr = R"mlir(
+        module {
+          %arg1 = arith.constant 1.0 : f64
+          %arg2 = "test.op0"() : () -> tensor<2xi64>
+          %arg3 = arith.constant 3.0 : f64
+          %q0 = qref.alloc_qb : !qref.bit
+          qref.operator "testInterfaceOp"(%arg1: f64, %arg2: tensor<2xi64>, %arg3: f64) qubits(%q0) param_map = {multi_index_param=[0, 1, 2]} qubit_map = {wire1 = [0]}
+        }
+            )mlir";
+
+    // Parsing boilerplate
+    DialectRegistry registry;
+    registry.insert<mlir::arith::ArithDialect, QRefDialect>();
+    test::registerTestDialect(registry);
+    MLIRContext context(registry);
+    ParserConfig config(&context, /*verifyAfterParse=*/false);
+    OwningOpRef<ModuleOp> module = parseSourceString<ModuleOp>(moduleStr, config);
+
+    auto operators = module->getOps<OperatorOp>();
+    DecomposableGate op = *operators.begin();
+
+    ASSERT_EQ(op.getOperatorName(), "testInterfaceOp");
+
+    llvm::StringMap<llvm::SmallVector<mlir::Type>> expectedDynamicShape = {
+        {"multi_index_param", {mlir::Float64Type::get(&context), mlir::RankedTensorType::get({2}, mlir::IntegerType::get(&context, 64)), mlir::Float64Type::get(&context)}}};
+
+    ASSERT_EQ(op.getDynamicShape(), expectedDynamicShape);
+
+    llvm::StringMap<size_t> expectedWires = {{"wire1", 1}};
+    ASSERT_EQ(op.getWireLens(), expectedWires);
+
+    ASSERT_EQ(op.getStaticData(), mlir::DictionaryAttr::get(&context, {}));
+
+    ASSERT_EQ(op.getGraphOpId(),
+              "testInterfaceOp{multi_index_param:[f64,tensor<2xi64>,f64]}{wire1:1}{}");
+}
