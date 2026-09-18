cd5aef03ad6b0dff03126a53db54d5eee3f8e1cb
paul0403 <paulhaochen.wang@gmail.com>
2026-09-17 21:28:13 -0400

    Fix extract insert canonicalization (for sos)

--- mlir/lib/Quantum/IR/QuantumOps.cpp
+++ mlir/lib/Quantum/IR/QuantumOps.cpp
@@ -190,8 +190,10 @@ struct RedirectExtractThroughDistinctInsert : public mlir::OpRewritePattern<Extr
             return failure();
         }
         // Read the pre-insert register.
-        rewriter.modifyOpInPlace(extract,
-                                 [&] { extract.getQregMutable().assign(insert.getInQreg()); });
+        rewriter.modifyOpInPlace(extract, [&] {
+            extract.getQregMutable().assign(insert.getInQreg());
+            extract->moveBefore(insert);
+        });
 
         // Sink the bypassed insert to just above the earliest remaining user of its result.
         Operation *earliestUser = nullptr;
