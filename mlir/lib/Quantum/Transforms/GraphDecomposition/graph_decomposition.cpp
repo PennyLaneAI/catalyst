c61fa7fd0fb36f651063e87d99926eb69b2fd775
Ali Asadi <10773383+maliasadi@users.noreply.github.com>
2026-09-18 15:12:17 -0400

    Update

--- mlir/lib/Quantum/Transforms/GraphDecomposition/graph_decomposition.cpp
+++ mlir/lib/Quantum/Transforms/GraphDecomposition/graph_decomposition.cpp
@@ -168,7 +168,6 @@ struct GraphDecompositionPass : public impl::GraphDecompositionPassBase<GraphDec
             AltDecomps altDecomps = buildAltDecomps(opToAltDecompNames, rulesByName);
             DecompositionGraph graph(setOfOps, targetGateSet, setOfRules, std::move(fixedDecomps),
                                      std::move(altDecomps));
-            graph.showGraph();
             DecompositionSolver solver(graph);
             solution = solver.solve();
         }
