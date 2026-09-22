9e7cd789b3843e3a7a0bfbf1f33fdcfd3a2d4b26
Ali Asadi <10773383+maliasadi@users.noreply.github.com>
2026-09-22 15:10:45 -0400

    Update

--- mlir/include/Driver/DefaultPipelines/DefaultPipelines.h
+++ mlir/include/Driver/DefaultPipelines/DefaultPipelines.h
@@ -41,6 +41,10 @@ const PipelineList pipelineList{
       // tapes will generate multiple qnodes. One for each tape.
       // Split multiple tapes enforces that invariant.
       "split-multiple-tapes",
+      // Reduce quantum.ctrl/quantum.adjoint regions in the input to
+      // op-level modifiers before the transform sequence runs.
+      "ctrl-lowering",
+      "adjoint-lowering",
       // Run the transform sequence defined in the MLIR module
       "builtin.module(apply-transform-sequence)",
       // Nested modules are something that will be used in the future
