05137941cc620cc9dd269c41356c7c0cfaf34568
River McCubbin <river.mccubbin@xanadu.ai>
2026-09-23 11:04:11 -0400

    fix casting

--- mlir/lib/PBC/Transforms/ToPPR.cpp
+++ mlir/lib/PBC/Transforms/ToPPR.cpp
@@ -12,15 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cassert>
+#include <cmath>
 #include <cstdint>
+#include <cstdlib>
+#include <optional>
 
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/STLForwardCompat.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/Location.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/PatternMatch.h"
 #include "mlir/IR/TypeRange.h"
+#include "mlir/IR/Types.h"
+#include "mlir/IR/Value.h"
+#include "mlir/IR/ValueRange.h"
+#include "mlir/Support/LLVM.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/DialectConversion.h"
 
 #include "Catalyst/Utils/ConstantResolve.h"
 #include "PBC/IR/PBCOps.h"
 #include "PBC/Transforms/Patterns.h"
+#include "Quantum/IR/QuantumInterfaces.h"
 #include "Quantum/IR/QuantumOps.h"
+#include "Quantum/IR/QuantumTypes.h"
 
 using namespace mlir;
 using namespace catalyst;
@@ -425,7 +444,8 @@ LogicalResult convertPPROperator(OperatorOp op, ConversionPatternRewriter &rewri
     ArrayAttr pauliProduct = rewriter.getArrayAttr(pauliCharacters);
 
     auto denominatorAttr = staticData.getAs<IntegerAttr>("angle_denominator");
-    int8_t rotationKind = static_cast<int8_t>(denominatorAttr.getInt());
+    // don't use getInt, since it asserts signless (cannot handle signed data)
+    int8_t rotationKind = static_cast<int8_t>(denominatorAttr.getValue().getSExtValue());
     if (op.getAdjoint()) {
         rotationKind = -rotationKind;
     }
